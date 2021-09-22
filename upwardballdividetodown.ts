33124cf24def6bc7c1a51b39e855e6f7c9ea4939
Anwesh43 <anweshthecool0@gmail.com>
2021-09-22 12:18:32 +0530

    adding ScaleUtil class

--- upwardballdividetodown.ts
+++ upwardballdividetodown.ts
@@ -1,3 +1,5 @@
+import { min } from "lodash"
+
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const parts : number = 3 
@@ -11,4 +13,15 @@ const colors : Array<string> = [
     "#FFC107",
     "#795548",
     "#3F51B5"
-]
+]   
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
