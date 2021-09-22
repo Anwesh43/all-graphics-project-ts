7043e9afc27392ea4cd1a50c535d78d53341b9b8
Anwesh43 <anweshthecool0@gmail.com>
2021-09-22 13:14:02 +0530

    adding UBDDNode class

--- upwardballdividetodown.ts
+++ upwardballdividetodown.ts
@@ -142,3 +142,46 @@ class Animator {
         }
     }
 }
+
+class UBDDNode {
+
+
+    prev : UBDDNode
+    next : UBDDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new UBDDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawUBDDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : UBDDNode {
+        var curr : UBDDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
