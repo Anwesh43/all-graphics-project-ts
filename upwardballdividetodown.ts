34388278ee5634f6f0ede0f9e0c537fab324eed1
Anwesh43 <anweshthecool0@gmail.com>
2021-09-22 12:15:18 +0530

    adding constants

---
+++ upwardballdividetodown.ts
@@ -0,0 +1,14 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 3 
+const scGap : number = 0.03 / parts 
+const sizeFactor : number = 15.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#F44336",
+    "#4CAF50",
+    "#FFC107",
+    "#795548",
+    "#3F51B5"
+]
