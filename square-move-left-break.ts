3e118a22ae99727b7ce8578d20c958b13da1a89b
Anwesh43 <anweshthecool0@gmail.com>
2021-10-01 14:11:08 +0530

    adding constatns

---
+++ square-move-left-break.ts
@@ -0,0 +1,14 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const sizeFactor : number = 12.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#F44336",
+    "#03A9F4",
+    "#FF9800",
+    "#8BC34A",
+    "#795548"
+]
