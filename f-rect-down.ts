45ec94e6fba846601c1c8b23c6dbd0c0d006e290
Anwesh43 <anweshthecool0@gmail.com>
2021-10-04 13:41:14 +0530

    adding FRDNode class

--- f-rect-down.ts
+++ f-rect-down.ts
@@ -138,3 +138,47 @@ class Animator {
         }
     }
 }
+
+class FRDNode {
+
+    next : FRDNode 
+    prev : FRDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new FRDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawFRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : FRDNode {
+        var curr : FRDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+
+
+}
