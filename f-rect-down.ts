2174c2cef67034f3265f4039ae43e5dab721d1ae
Anwesh43 <anweshthecool0@gmail.com>
2021-10-04 13:47:43 +0530

    addong FRectDown class

--- f-rect-down.ts
+++ f-rect-down.ts
@@ -179,6 +179,27 @@ class FRDNode {
         cb()
         return this 
     }
+}
 
+class FRectDown {
 
+    curr : FRDNode = new FRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
 }
