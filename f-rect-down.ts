823ff82864cdb16b7a3ec9d5bd440fbe39c7c934
Anwesh43 <anweshthecool0@gmail.com>
2021-10-04 13:04:53 +0530

    adding constants

---
+++ f-rect-down.ts
@@ -0,0 +1,15 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const delay : number = 20 
+const sizeFactor : number = 11.9 
+const backColor : string = "#BDBDBD"
+const deg : number = Math.PI
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]  
