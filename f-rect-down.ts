594970467ea81373ff7dae4e06f8c3c3511e34c9
Anwesh43 <anweshthecool0@gmail.com>
2021-10-04 13:20:13 +0530

    adding DrawingUtil class

--- f-rect-down.ts
+++ f-rect-down.ts
@@ -13,6 +13,7 @@ const colors : Array<string> = [
     "#795548",
     "#8BC34A"
 ]  
+const strokeFactor : number = 90 
 
 class ScaleUtil {
 
@@ -22,5 +23,43 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawFRect(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2) * sc4)
+        context.rotate(deg * sc4)
+        DrawingUtil.drawLine(context, 0, 0, 0, -size * sc1)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.translate(0, -size + size * 0.5 * j)
+            DrawingUtil.drawLine(context, 0, 0, size * sc2, 0)
+            context.restore()
+        }
+        context.fillRect(0, -size, size * 0.5 * sc3, size * 0.5)
+        context.restore()
+    }
+
+    static drawFRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawFRect(context, scale)
     }
 }
