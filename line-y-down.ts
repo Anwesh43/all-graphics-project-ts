d48552224fa2ab96502e3129d4412f47ca4c3258
Anwesh43 <anweshthecool0@gmail.com>
2021-10-06 12:18:59 +0530

    adding LineYDown class

--- line-y-down.ts
+++ line-y-down.ts
@@ -185,3 +185,26 @@ class LYDNode {
         return this 
     }
 }
+
+class LineYDown {
+
+    curr : LYDNode = new LYDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb :Function) {
+        this.curr.startUpdating(cb)
+    }
+}
