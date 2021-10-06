d125e991577a2ec2fc3903f543e7b8c26f6601b8
Anwesh43 <anweshthecool0@gmail.com>
2021-10-06 10:04:41 +0530

    line-y-down: adding constants

---
+++ line-y-down.ts
@@ -0,0 +1,17 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 12.9
+const delay : number = 20 
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const backColor : string = "#bdbdbd"
+const rot : number = Math.PI 
+const deg : number = Math.PI / 4
