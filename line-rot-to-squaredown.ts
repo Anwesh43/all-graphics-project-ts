e4f382430428c9ec249c21652a484a36cc299121
Anwesh43 <anweshthecool0@gmail.com>
2021-10-07 19:49:46 +0530

    line-rot-to-squaredown: adding constants

---
+++ line-rot-to-squaredown.ts
@@ -0,0 +1,17 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const parts : number = 4 
+const strokeFactor : number = 90 
+const scGap : number = 0.04 / parts 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const deg : number = Math.PI / 2
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+
