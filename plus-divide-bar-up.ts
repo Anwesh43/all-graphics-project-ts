400d1eb39046933393c8abbc5a20fe67af3882eb
Anwesh43 <anweshthecool0@gmail.com>
2021-10-09 13:41:40 +0530

    plus-divide-bar-up: adding constants

---
+++ plus-divide-bar-up.ts
@@ -0,0 +1,17 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const deg : number = Math.PI / 2
+
