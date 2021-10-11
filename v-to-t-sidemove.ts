5d5a7dc6186950ad1cc0277cd882f2590231f0bc
Anwesh43 <anweshthecool0@gmail.com>
2021-10-11 13:21:37 +0530

    v-to-t-sidemove: adding VTSMNode class

--- v-to-t-sidemove.ts
+++ v-to-t-sidemove.ts
@@ -140,5 +140,46 @@ class Animator {
             clearInterval(this.interval)
         }
     }
+}
 
+class VTSMNode {
+
+    next : VTSMNode 
+    prev : VTSMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length) {
+            this.next = new VTSMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawVTSMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : VTSMNode {
+        var curr : VTSMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
 }
