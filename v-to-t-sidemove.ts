e17b4a3561fc4e8b4cc6015ca7eac074b74dde47
Anwesh43 <anweshthecool0@gmail.com>
2021-10-11 12:57:43 +0530

    v-to-t-sidemove: adding DrawingUtil class

--- v-to-t-sidemove.ts
+++ v-to-t-sidemove.ts
@@ -14,6 +14,7 @@ const colors : Array<string> = [
     "#795548",
     "#8BC34A"
 ]
+const deg : number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -23,5 +24,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawVToSideMove(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc5, h / 2)
+        context.rotate(deg * sc3)
+        context.save()
+        context.translate(-size / 2 + (w / 2 + size / 2) * sc2, 0)
+        DrawingUtil.drawLine(context, -size / 2, 0, size / 2, 0)
+        context.restore()
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * (1 - 2 * j) * (1 - sc4))
+            DrawingUtil.drawLine(context, 0, 0, 0, size * sc1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawVTSMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawVToSideMove(context, scale)
     }
 }
