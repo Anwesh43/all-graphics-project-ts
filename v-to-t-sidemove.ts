be631054f6eeefa0f0b4d485312a6a5893c45680
Anwesh43 <anweshthecool0@gmail.com>
2021-10-11 12:04:17 +0530

    v-to-t-sidemove: adding constants

---
+++ v-to-t-sidemove.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 5
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const rot : number = Math.PI / 6 
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
