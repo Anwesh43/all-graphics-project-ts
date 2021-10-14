b8e707b944d9598f04a222b5825c2389fc58adf3
Anwesh43 <anweshthecool0@gmail.com>
2021-10-14 16:43:37 +0530

    split-line-bar-down: adding ScaleUtil class

--- split-line-bar-down.ts
+++ split-line-bar-down.ts
@@ -12,3 +12,14 @@ const sizeFactor : number = 12.9
 const rot : number = Math.PI / 4 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
