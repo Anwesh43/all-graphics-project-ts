c6ad01acbcb28efdb78e4c4ee5daef60fbf45a02
Anwesh43 <anweshthecool0@gmail.com>
2021-10-14 18:19:40 +0530

    split-line-bar-down: adding SplitLineBarDown class

--- split-line-bar-down.ts
+++ split-line-bar-down.ts
@@ -179,3 +179,26 @@ class SLBDNode {
         return this 
     }
 }
+
+class SplitLineBarDown {
+
+    curr : SLBDNode = new SLBDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
