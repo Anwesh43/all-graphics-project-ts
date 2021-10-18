a311b482e4b8d41de6d81e5f9bf05d6d90f1ed39
Anwesh43 <anweshthecool0@gmail.com>
2021-10-18 16:35:56 +0530

    three-bars-grow-shrink: adding cosntants and colors

---
+++ three-bars-grow-shrink.ts
@@ -0,0 +1,13 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const bars : number = 3 
+const parts : number = 2
+const scGap : number = 0.04 / (parts * bars)
+const hFactor : number = 11
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
