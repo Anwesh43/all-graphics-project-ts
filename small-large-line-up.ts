9668beccdb24980b631806eb0bc0e38c53ac2162
Anwesh43 <anweshthecool0@gmail.com>
2021-10-19 16:22:01 +0530

    small-large-line-up: adding SLLUNode class

--- small-large-line-up.ts
+++ small-large-line-up.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class SLLUNode {
+
+    prev : SLLUNode 
+    next : SLLUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+           this.next = new SLLUNode(this.i + 1)
+           this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLLUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SLLUNode {
+        var curr : SLLUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
