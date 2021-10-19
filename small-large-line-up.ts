442ad82722a964ec28c71f90f714c2a885bc780e
Anwesh43 <anweshthecool0@gmail.com>
2021-10-19 14:36:46 +0530

    small-large-line-up: adding constants

---
+++ small-large-line-up.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4 
+const scGap : number = 0.03 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.2
+const backColor : string = "#bdbdbd"
+const delay : number = 20 
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
