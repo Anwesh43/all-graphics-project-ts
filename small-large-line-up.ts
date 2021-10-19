8640f336c9789dd1a26ed203096dd503f3d40702
Anwesh43 <anweshthecool0@gmail.com>
2021-10-19 14:47:37 +0530

    small-large-line-up: adding DrawingUtil class

--- small-large-line-up.ts
+++ small-large-line-up.ts
@@ -24,3 +24,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSmallLargeLineUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2) * sc4)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.translate(-size / 2 + size * j, 0)
+            DrawingUtil.drawLine(context, 0, 0, 0, -(size / 2 + size * 0.5 * j) * ScaleUtil.divideScale(scale, j, parts))
+            context.restore()
+        }
+        context.save()
+        context.translate(0, h * 0.5 * (1 - sc3))
+        DrawingUtil.drawLine(context, -size / 2, 0, size / 2, 0)
+        context.restore()
+        context.restore()
+    }
+
+    static drawSLLUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSmallLargeLineUp(context, scale)
+    }
+}
