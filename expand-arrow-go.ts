ccefd5642c1a3fe233113c3cd7b5fb0363be0987
Anwesh43 <anweshthecool0@gmail.com>
2021-10-21 14:53:21 +0530

    expand-arrow-go: adding constants

---
+++ expand-arrow-go.ts
@@ -0,0 +1,15 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 3
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 12.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
