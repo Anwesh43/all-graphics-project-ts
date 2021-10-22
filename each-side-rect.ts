f38666904fbd18ecf2c33419f8c539b33c9f10ab
Anwesh43 <anweshthecool0@gmail.com>
2021-10-22 20:14:37 +0530

    each-side-rect: adding constants

---
+++ each-side-rect.ts
@@ -0,0 +1,15 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 0.03 
+const sizeFactor : number = 8.9 
+const rectFactor : number = 15.9
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+ 
