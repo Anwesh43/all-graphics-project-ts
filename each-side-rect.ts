8fa11c6f13c54addda8d9c23aeba22880b00ac8c
Anwesh43 <anweshthecool0@gmail.com>
2021-10-22 20:31:03 +0530

    each-side-rect: adding ESRNode class

--- each-side-rect.ts
+++ each-side-rect.ts
@@ -128,3 +128,45 @@ class Animator {
         }
     }
 }
+
+class ESRNode {
+
+    prev : ESRNode 
+    next : ESRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ESRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawESRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : ESRNode {
+        var curr : ESRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
