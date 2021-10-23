0f8a1078be4c12bf0e30e5c58b5e08bdd6b6cfad
Anwesh43 <anweshthecool0@gmail.com>
2021-10-23 20:52:24 +0530

    middle-line-ball: adding constants

---
+++ middle-line-ball.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4
+const scGap : number = 0.03 / parts 
+const rFactor : number = 11.9 
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const strokeFactor : number = 90 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
