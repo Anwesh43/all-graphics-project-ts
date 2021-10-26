c949de4034f8b14f0606904b0e9a102a66de2114
Anwesh43 <anweshthecool0@gmail.com>
2021-10-26 20:00:12 +0530

    middle-line-ball: adding ScaleUtil class

--- middle-line-ball.ts
+++ middle-line-ball.ts
@@ -13,3 +13,15 @@ const h : number = window.innerHeight
 const strokeFactor : number = 90 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
+
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
