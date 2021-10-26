e3938ca84788060ed01c618bb48c86125d4edb94
Anwesh43 <anweshthecool0@gmail.com>
2021-10-26 20:58:58 +0530

    middle-line-ball: adding MiddleLineBall class

--- middle-line-ball.ts
+++ middle-line-ball.ts
@@ -189,3 +189,26 @@ class MLBNode {
         return this 
     }
 }
+
+class MiddleLineBall {
+
+    curr : MLBNode = new MLBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
