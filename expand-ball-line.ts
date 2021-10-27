8e4b7b2f88378963102265c2ca4ac517a433d3ad
Anwesh43 <anweshthecool0@gmail.com>
2021-10-28 01:56:29 +0530

    expand-ball-line: adding constants and libraries

---
+++ expand-ball-line.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4
+const scGap : number = 0.03 / parts 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const strokeFactor : number = 90 
+const lSizeFactor : number = 11.9
+const rFactor : number = 25.6
