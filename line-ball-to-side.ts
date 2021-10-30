d8d6d9872e9d803ca0c155deec417f86de33e77f
Anwesh43 <anweshthecool0@gmail.com>
2021-10-30 12:25:50 +0530

    line-ball-to-side: adding DrawingUtil class

--- line-ball-to-side.ts
+++ line-ball-to-side.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawLineBallToSide(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const r : number = Math.min(w, h) / rFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        context.save()
+        context.translate(w / 2 + (w * 0.5 * sc3), h / 2)
+        DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+        DrawingUtil.drawCircle(context, size, (-r + (h + r) * sc2), r)
+        context.restore()
+    }
+
+    static drawLBTSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineBallToSide(context, scale)
+    }
+}
