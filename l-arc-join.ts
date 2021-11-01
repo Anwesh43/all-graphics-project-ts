66db578aa2b12be6836d8d64a18cbc12ffc87f93
Anwesh43 <anweshthecool0@gmail.com>
2021-11-01 19:39:17 +0530

    l-arc-join: adding constants

---
+++ l-arc-join.ts
@@ -0,0 +1,15 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 3 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const deg : number = Math.PI / 2
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
