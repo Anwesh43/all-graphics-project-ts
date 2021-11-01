923e51063ab7f8ea4d1a7b49a5dbf79238bf9347
Anwesh43 <anweshthecool0@gmail.com>
2021-11-01 19:58:13 +0530

    l-arc-join: adding DrawingUtil class

--- l-arc-join.ts
+++ l-arc-join.ts
@@ -5,7 +5,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 11.9 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const deg : number = Math.PI / 2
+const deg : number = 90
 const colors : Array<string> = [
     "#4A148C",
     "#004D40",
@@ -22,5 +22,58 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArc(context : CanvasRenderingContext2D, x : number, y : number, r : number, sc : number) {
+        context.save()
+        context.translate(x, y)
+        context.beginPath()
+        for (var j = 0; j <= deg * sc; j++) {
+            const a : number = r * Math.cos(j * Math.PI / 180)
+            const b : number = r * Math.sin(j * Math.PI / 180)
+            if (j == 0) {
+                context.moveTo(a, b)
+            } else {
+                context.lineTo(a, b)
+            }
+        }
+        context.stroke()
+        context.restore()
+    }
+
+    static drawLArcJoin(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc3)
+        context.rotate(deg * sc3)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(j * deg * Math.PI / 180)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * sc1)
+            context.restore()
+        }
+        DrawingUtil.drawArc(context, 0, 0, size, sc2)
+        context.restore()
+    } 
+
+    static drawLAJNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLArcJoin(context, scale)
     }
 }
