28143d5776aff6217e91bdd35b4e08ad54c6c28c
Anwesh43 <anweshthecool0@gmail.com>
2021-11-01 19:40:34 +0530

    l-arc-join: adding ScaleUtil class

--- l-arc-join.ts
+++ l-arc-join.ts
@@ -13,3 +13,14 @@ const colors : Array<string> = [
     "#D50000",
     "#43A047"
 ]
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
