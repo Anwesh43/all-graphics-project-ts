f8b910fbcaf53d0e780ff9450f68984634e92486
Anwesh43 <anweshthecool0@gmail.com>
2021-11-04 19:11:21 +0530

    multi-l-up-down: adding MULDNode class

--- multi-l-up-down.ts
+++ multi-l-up-down.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class MULDNode {
+
+    prev : MULDNode 
+    next : MULDNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new MULDNode(this.i + 1)
+            this.prev.next = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawMLUDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : MULDNode {
+        var curr : MULDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
