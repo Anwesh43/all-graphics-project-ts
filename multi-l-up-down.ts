e1ebfde15f5958a80e51997cdebf2d85110b5e8c
Anwesh43 <anweshthecool0@gmail.com>
2021-11-04 18:49:48 +0530

    multi-l-up-down: adding constants

---
+++ multi-l-up-down.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 3
+const scGap : number = 0.03
+const lines : number = 5 
+const sizeFactor :  number = 11.9 
+const delay : number = 20 
+const deg : number = Math.PI / 2 
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#B71C1C",
+    "#33691E",
+    "#0091EA",
+    "#00BFA5",
+    "#FF6D00"
+] 
