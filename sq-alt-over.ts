8a0405c5f9587131a402ff9115854b83317121b4
Anwesh43 <anweshthecool0@gmail.com>
2021-11-06 17:44:45 +0530

    sq-alt-over: adding constants

---
+++ sq-alt-over.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#33691E",
+    "#0091EA",
+    "#00BFA5",
+    "#FF6D00"
+]
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 4
+const scGap : number = 0.03 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 5.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
