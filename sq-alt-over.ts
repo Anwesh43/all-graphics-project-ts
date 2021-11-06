40d36ce92c4faec43ab2f86f013dc9e681eeb06f
Anwesh43 <anweshthecool0@gmail.com>
2021-11-06 18:00:29 +0530

    sq-alt-over: adding DrawingUtil class

--- sq-alt-over.ts
+++ sq-alt-over.ts
@@ -24,3 +24,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSqAltOver(context : CanvasRenderingContext2D, scale : number) {
+        const constantSize : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = constantSize * (1 - sc4)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            const upSize : number = size * 0.25 * (1 - sc3)
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.translate(upSize, upSize)
+            DrawingUtil.drawLine(context, size / 4, -size / 4, size / 4, -size / 4 + size * 0.5 * sc1)
+            DrawingUtil.drawLine(context, size / 4, size / 4, size / 4 - size * 0.5 * sc2, size / 4)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSAONode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSqAltOver(context, scale)
+    }
+}
