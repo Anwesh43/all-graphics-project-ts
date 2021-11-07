61d06bf132727a8c527af581db479ac075b22495
Anwesh43 <anweshthecool0@gmail.com>
2021-11-07 11:24:12 +0530

    down-h-create-shrink: adding DrawingUtil class

--- down-h-create-shrink.ts
+++ down-h-create-shrink.ts
@@ -23,3 +23,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDownHCreateShrink(context : CanvasRenderingContext2D, scale : number) {
+        const cSize : number = Math.min(w, h) / sizeFactor 
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = cSize * (1 - sc4)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size / 2, 0)
+            for (var k = 0; k < 2; k++) {
+                context.save()
+                context.translate(0, -h / 2 + (h - size * 0.5 * k) * ScaleUtil.divideScale(scale, j * 2, parts))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size / 2)
+                context.restore()
+            }
+            context.restore()
+        }
+        DrawingUtil.drawLine(
+            context,
+            -size / 2,
+            h / 2 - size / 2,
+            -size / 2 + size * sc2,
+            h / 2 - size / 2
+        )
+        context.restore()
+    }
+
+    static drawDHCSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDownHCreateShrink(context, scale)
+    }
+}
