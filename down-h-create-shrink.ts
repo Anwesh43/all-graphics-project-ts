98fa78520e99284d58d1472b791fae73896fee9e
Anwesh43 <anweshthecool0@gmail.com>
2021-11-07 11:26:01 +0530

    down-h-create-shrink: adding Stage class

--- down-h-create-shrink.ts
+++ down-h-create-shrink.ts
@@ -12,6 +12,7 @@ const colors : Array<string> = [
     "#00BFA5",
     "#FF6D00"
 ]
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -67,5 +68,36 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         DrawingUtil.drawDownHCreateShrink(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
