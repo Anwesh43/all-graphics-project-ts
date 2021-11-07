299034902a73853e91b3ba0cda3c993c9a5dc52d
Anwesh43 <anweshthecool0@gmail.com>
2021-11-07 11:10:41 +0530

    down-h-create-shrink: adding constants

---
+++ down-h-create-shrink.ts
@@ -0,0 +1,14 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.03 / parts 
+const delay : number = 20 
+const strokeFactor : number = 90 
+const sizeFactor : number = 12.9 
+const colors : Array<string> = [
+    "#B71C1C",
+    "#33691E",
+    "#0091EA",
+    "#00BFA5",
+    "#FF6D00"
+]
