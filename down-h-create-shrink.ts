70ac83509533f038cb9606eab117eb09d126daba
Anwesh43 <anweshthecool0@gmail.com>
2021-11-07 11:12:06 +0530

    down-h-create-shrink: adding ScaleUtil class

--- down-h-create-shrink.ts
+++ down-h-create-shrink.ts
@@ -12,3 +12,14 @@ const colors : Array<string> = [
     "#00BFA5",
     "#FF6D00"
 ]
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
+    }
+}
