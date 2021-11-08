22820da53e47bc64d9777811b8638337cb10d9a9
Anwesh43 <anweshthecool0@gmail.com>
2021-11-08 15:19:45 +0530

    bi-line-closure-up: adding BLCUNode class

--- bi-line-closure-up.ts
+++ bi-line-closure-up.ts
@@ -135,3 +135,46 @@ class Animator {
         }
     }
 }
+
+class BLCUNode {
+
+    next : BLCUNode 
+    prev : BLCUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLCUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLCUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BLCUNode {
+        var curr : BLCUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
+
