2fa842aeace109aac747baad8b19ca901aeedade
Anwesh43 <anweshthecool0@gmail.com>
2021-11-08 11:13:39 +0530

    bi-line-closure-up: adding constants

---
+++ bi-line-closure-up.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#33691E",
+    "#0091EA",
+    "#00BFA5",
+    "#FF6D00"
+]
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
+const strokeFactor : number = 90
+const sizeFactor : number = 11.2  
+const parts : number = 3
+const scGap : number = 0.03 / parts 
