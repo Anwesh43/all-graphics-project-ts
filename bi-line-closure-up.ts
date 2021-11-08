b2b1846220853d29027101205a83102c48a0f5c1
Anwesh43 <anweshthecool0@gmail.com>
2021-11-08 11:14:56 +0530

    bi-line-closure-up: adding ScaleUtil class

--- bi-line-closure-up.ts
+++ bi-line-closure-up.ts
@@ -13,3 +13,14 @@ const strokeFactor : number = 90
 const sizeFactor : number = 11.2  
 const parts : number = 3
 const scGap : number = 0.03 / parts 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
