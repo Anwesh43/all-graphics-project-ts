0a094dfdf1d9a7c4aa0010c5f6c1d80f04b95304
Anwesh43 <anweshthecool0@gmail.com>
2021-11-08 13:27:11 +0530

    bi-line-closure-up: adding DrawingUtil class

--- bi-line-closure-up.ts
+++ bi-line-closure-up.ts
@@ -24,3 +24,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiLineClosureUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(0, -h / 2 + h  * sc1)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, -size)
+            context.restore()
+        
+        }
+        DrawingUtil.drawLine(context, -size / 2, h / 2 - size, -size / 2 + size * sc2, h / 2 - size)
+        context.restore()
+    }
+
+    static drawBLCUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBiLineClosureUp(context, scale)
+    }
+}
