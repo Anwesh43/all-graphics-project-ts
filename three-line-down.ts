d4120a9f9b56bc1f19262b132d33162e1a49f970
Anwesh43 <anweshthecool0@gmail.com>
2021-11-12 15:00:40 +0530

    three-line-down: adding constants

---
+++ three-line-down.ts
@@ -0,0 +1,14 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#33691E",
+    "#0091EA",
+    "#00BFA5",
+    "#FF6D00"
+]
+
+const parts : number = 3
+const scGap : number = 0.03 / parts 
+const delay : number = 20 
+const lines : number = 3 
+const strokeFactor : number = 90 
+
