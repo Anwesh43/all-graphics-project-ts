65c0b932a3f3e52391cf8ff2295c189f489042d1
Anwesh43 <anweshthecool0@gmail.com>
2021-11-13 03:58:32 +0530

    three-line-down: adding Renderer class

--- three-line-down.ts
+++ three-line-down.ts
@@ -207,3 +207,25 @@ class ThreeLineDown {
         this.curr.startUpdating(cb)
     }
 }
+
+class Renderer {
+
+    curr : ThreeLineDown = new ThreeLineDown()
+    animator : Animator = new Animator()
+
+    render(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    handleTap(cb : Function) {
+        this.curr.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.curr.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
+    }
+}
