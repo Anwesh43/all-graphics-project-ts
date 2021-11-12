84f3d087902c3fe211e9f580d1928075a9c8e3e7
Anwesh43 <anweshthecool0@gmail.com>
2021-11-12 20:02:55 +0530

    three-line-down: adding Stage class

--- three-line-down.ts
+++ three-line-down.ts
@@ -13,6 +13,7 @@ const strokeFactor : number = 90
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const sizeFactor : number = 12.9 
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -64,5 +65,36 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         DrawingUtil.drawThreeLineDown(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
