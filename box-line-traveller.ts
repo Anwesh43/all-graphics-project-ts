a3fb7f1dc067544327f70e5614b8135f332d9184
Anwesh43 <anweshthecool0@gmail.com>
2021-11-13 15:07:50 +0530

    box-line-traveller: adding BLTNode class

--- box-line-traveller.ts
+++ box-line-traveller.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class BLTNode {
+
+    prev : BLTNode 
+    next : BLTNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLTNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLTNode(context, this.i, this.state.scale)
+    } 
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BLTNode {
+        var curr : BLTNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
