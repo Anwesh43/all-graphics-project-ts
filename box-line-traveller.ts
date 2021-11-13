c29528c8e0ec683d1e4c19a3e68edd306d7a280d
Anwesh43 <anweshthecool0@gmail.com>
2021-11-13 14:53:12 +0530

    box-line-traveller: adding DtawingUtil class

--- box-line-traveller.ts
+++ box-line-traveller.ts
@@ -6,6 +6,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 12.9 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
+const boxSizeFactor : number = 11.9 
 const colors : Array<string> = [
     "#B71C1C",
     "#00C853",
@@ -22,5 +23,52 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawAtXY(context : CanvasRenderingContext2D, x : number, y : number, cb : Function) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBoxLineTraveller(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const boxSize : number = Math.min(w, h) / boxSizeFactor
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawAtXY(context, -size / 2, -h / 2 + h * sc1, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+            DrawingUtil.drawAtXY(context, -size / 2, -h / 2 + h * sc2, () => {
+                context.fillRect(-boxSize / 2, -boxSize / 2, boxSize, boxSize)
+            })
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBLTNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBoxLineTraveller(context, scale)
     }
 }
