42ff89ccd056f16357ff4735f1ac836bc186c03e
Anwesh43 <anweshthecool0@gmail.com>
2021-11-14 12:39:13 +0530

    cross-line-rotator: adding constants

---
+++ cross-line-rotator.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#00C853",
+    "#0091EA",
+    "#AA00FF",
+    "#FFAB00"
+]
+const parts : number = 4
+const scGap : number = 0.03 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 8.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const deg : number = Math.PI / 2 
