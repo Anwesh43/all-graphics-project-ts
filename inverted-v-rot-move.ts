a244ab59aac22b415db1ce1bfebb579c3ac13851
Anwesh43 <anweshthecool0@gmail.com>
2021-11-20 15:53:45 +0530

    inverted-v-rot-move: adding constants

---
+++ inverted-v-rot-move.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 12.9 
+const colors : Array<string> = [
+    "#B71C1C",
+    "#00C853",
+    "#0091EA",
+    "#AA00FF",
+    "#FFAB00"
+]
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const deg : number = Math.PI / 2
