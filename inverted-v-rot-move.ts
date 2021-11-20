07bc2a3b24f24b79a0e10d5fbfda59ea4979983f
Anwesh43 <anweshthecool0@gmail.com>
2021-11-20 16:44:43 +0530

    inverted-v-rot-move: adding DrawingUtil class

--- inverted-v-rot-move.ts
+++ inverted-v-rot-move.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawInvertedVRotMove(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.save()
+        context.translate(0, (h / 2 - size / 2) * (1 - sc2))
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, size / 2, 0, size / 2 - size * 0.5 * sc1, -size * 0.5 * sc1)
+            context.restore()
+        }
+        context.restore()
+        context.restore()
+    }
+
+    static drawIVRMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawInvertedVRotMove(context, scale)
+    }
+}
