3a38da3874d9b423ca557c4a02492878da03803f
Anwesh43 <anweshthecool0@gmail.com>
2021-11-23 13:55:50 +0530

    break-stick-line: adding constants

---
+++ break-stick-line.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 3 
+const scGap : number = 0.03 / parts 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 8.9 
+const barFactor : number = 12.9 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
