b04bc9e2bdad56e20ddc897fcb0a91de34f53558
Anwesh43 <anweshthecool0@gmail.com>
2021-11-23 15:17:59 +0530

    break-stick-line: adding DrawingUtil class

--- break-stick-line.ts
+++ break-stick-line.ts
@@ -13,6 +13,7 @@ const sizeFactor : number = 8.9
 const barFactor : number = 12.9 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const strokeFactor : number = 90 
 
 class ScaleUtil {
 
@@ -22,6 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBreakStickLine(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / barFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-barSize / 2 - w * 0.5 * sc3, 0)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * sc1)
+            context.save()
+            context.translate(0, -h / 2 + (h / 2 - size) * sc2)
+            context.fillRect(-barSize / 2, -barSize, barSize, barSize)
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBSLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBreakStickLine(context, scale)
     }
 }
 
