b7345c780169dae30cfe725e60be6f0291d9462b
Anwesh43 <anweshthecool0@gmail.com>
2021-11-28 18:50:21 +0530

    right-angle-triangle-fill: adding constants

---
+++ right-angle-triangle-fill.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 12.9
+const delay : number = 90 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const backColor : string = "#BDBDBD"
+
