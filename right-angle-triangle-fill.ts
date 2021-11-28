317345440865fb246df56a222be6f5f07fea00e1
Anwesh43 <anweshthecool0@gmail.com>
2021-11-28 19:13:51 +0530

    right-angle-triangle-fill: adding RATFNode class

--- right-angle-triangle-fill.ts
+++ right-angle-triangle-fill.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class RATFNode {
+
+    prev : RATFNode 
+    next : RATFNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RATFNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRATFNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RATFNode {
+        var curr : RATFNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
