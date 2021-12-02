3ff7b7e29f95be05eda83842f4ce4512218ab46e
Anwesh43 <anweshthecool0@gmail.com>
2021-12-02 16:11:19 +0530

    alt-line-sqbar: adding renderer in stage

--- alt-line-sqbar.ts
+++ alt-line-sqbar.ts
@@ -70,7 +70,8 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D 
-
+    renderer : Renderer = new Renderer()
+    
     initCanvas() {
         this.canvas.width = w 
         this.canvas.height = h 
@@ -81,11 +82,14 @@ class Stage {
     render() {
         this.context.fillStyle = backColor 
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
