14c2da5c77e447fe2e420cffa06a65ec7ae020e2
Anwesh43 <anweshthecool0@gmail.com>
2021-12-04 23:30:21 +0530

    three-line-join-down: adding DrawingUtil class

--- three-line-join-down.ts
+++ three-line-join-down.ts
@@ -15,6 +15,7 @@ const backColor : string = "#BDBDBD"
 const deg : number = Math.PI 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const rot : number = deg / 2
 
 class ScaleUtil {
 
@@ -24,5 +25,42 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawThreeLineJoinDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+        context.rotate(deg * sc3)
+        for (let j = 0; j < lines; j++) {
+            const scj : number = ScaleUtil.divideScale(sc1, j, lines)
+            context.save()
+            context.rotate(rot * j)
+            DrawingUtil.drawLine(context, -size / 2, size / 2, -size / 2, size / 2 - size * scj)
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, -size * 0.5 * sc2, 0, size * 0.5 * sc2, 0)
+        context.restore()
+    }
+
+    static drawTLJDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawThreeLineJoinDown(context, scale)
     }
 }
