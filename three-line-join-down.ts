713cb178d8089f0e418875fbb7f6eaf9842ee20b
Anwesh43 <anweshthecool0@gmail.com>
2021-12-05 01:57:25 +0530

    three-line-join-down: adding TLJDNode class

--- three-line-join-down.ts
+++ three-line-join-down.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class TLJDNode {
+
+    prev : TLJDNode 
+    next : TLJDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TLJDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTLJDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TLJDNode {
+        var curr : TLJDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
