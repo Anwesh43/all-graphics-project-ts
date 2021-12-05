fc430bb6cf16dec8ddfb5de649d1cf98427b83ec
Anwesh43 <anweshthecool0@gmail.com>
2021-12-05 17:08:48 +0530

    square-alt-side: adding constants

---
+++ square-alt-side.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const deg : number = Math.PI / 2
