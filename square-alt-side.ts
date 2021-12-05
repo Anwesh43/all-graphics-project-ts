22bb09ff97d391f49baaf625c003040d8de31138
Anwesh43 <anweshthecool0@gmail.com>
2021-12-05 17:40:04 +0530

    square-alt-side: adding SASNode class

--- square-alt-side.ts
+++ square-alt-side.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class SASNode {
+
+    prev : SASNode 
+    next : SASNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SASNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSASNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SASNode {
+        var curr : SASNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
