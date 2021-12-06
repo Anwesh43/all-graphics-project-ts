0656c5a9da0922a466fbe1445a1f00c88947a56a
Anwesh43 <anweshthecool0@gmail.com>
2021-12-06 09:59:03 +0530

    line-square-holder: adding LSHNode class

--- line-square-holder.ts
+++ line-square-holder.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class LSHNode {
+
+    prev : LSHNode 
+    next : LSHNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSHNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LSHNode {
+        var curr : LSHNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
