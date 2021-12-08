b41356669d44584fcf0e96510b654532406bbf97
Anwesh43 <anweshthecool0@gmail.com>
2021-12-08 08:34:32 +0530

    stroke-square-cross: adding ScaleUtil class

--- stroke-square-cross.ts
+++ stroke-square-cross.ts
@@ -14,3 +14,14 @@ const sizeFactor : number = 11.9
 const lineFactor : number = 4.9 
 const delay : number = 20 
 const deg : number = Math.PI / 4 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
