75289b86541151aac8895d903171363fec7f1066
Anwesh43 <anweshthecool0@gmail.com>
2021-12-08 08:33:31 +0530

    stroke-square-cross: adding constants

---
+++ stroke-square-cross.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const colors : Array<string> = [
+    "#B71C1C",
+    "#00C853",
+    "#0091EA",
+    "#AA00FF",
+    "#FFAB00"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const lineFactor : number = 4.9 
+const delay : number = 20 
+const deg : number = Math.PI / 4 
