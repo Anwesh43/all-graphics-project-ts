a675647e5831c249e687efb086289434e590c33d
Anwesh43 <anweshthecool0@gmail.com>
2021-12-10 17:06:54 +0530

    line-side-beaker: adding constants

---
+++ line-side-beaker.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const lineFactor : number = 15.9 
+const sizeFactor : number = 8.9
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const strokeFactor : number = 90 
+ 
