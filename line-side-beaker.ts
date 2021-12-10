3cb47525ad3956367ca77d44ae14bd42552a0535
Anwesh43 <anweshthecool0@gmail.com>
2021-12-10 20:01:35 +0530

    line-side-beaker: adding LineSideBeaker class

--- line-side-beaker.ts
+++ line-side-beaker.ts
@@ -179,3 +179,26 @@ class LSBNode {
         return this 
     }
 }
+
+class LineSideBeaker {
+    
+    curr : LSBNode = new LSBNode(0)
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
