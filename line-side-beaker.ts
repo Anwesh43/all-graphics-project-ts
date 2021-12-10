8d131d32418c9893c43caa2820523c427bf4e759
Anwesh43 <anweshthecool0@gmail.com>
2021-12-10 20:03:02 +0530

    line-side-beaker: adding Renderer class

--- line-side-beaker.ts
+++ line-side-beaker.ts
@@ -202,3 +202,25 @@ class LineSideBeaker {
         this.curr.startUpdating(cb)
     }
 }
+
+class Renderer {
+
+    curr : LineSideBeaker = new LineSideBeaker()
+    animator : Animator = new Animator()
+
+    render(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    } 
+
+    handleTap(cb : Function) {
+        this.curr.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.curr.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
+    }
+}
