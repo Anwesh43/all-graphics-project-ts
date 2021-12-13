543e573af8550f48f3a27a58190ec4e14ded16ae
Anwesh43 <anweshthecool0@gmail.com>
2021-12-13 11:38:58 +0530

    line-rect-holder: adding constants

---
+++ line-rect-holder.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#00C853",
+    "#0091EA",
+    "#AA00FF",
+    "#FFAB00"
+]
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 6.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
