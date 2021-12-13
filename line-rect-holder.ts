8093b1db52046864d577ff7ec137786b1d1f61e8
Anwesh43 <anweshthecool0@gmail.com>
2021-12-13 12:16:12 +0530

    line-rect-holder: adding DrawingUtil class

--- line-rect-holder.ts
+++ line-rect-holder.ts
@@ -24,3 +24,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRectHolder(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(Math.PI * 0.5 * sc3)
+        context.save()
+        context.translate(0, -h / 2 + (h / 2) * sc1)
+        context.fillRect(0, -size, size, size)
+        context.restore()
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, size * sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLRHNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineRectHolder(context, i)
+    }
+}
