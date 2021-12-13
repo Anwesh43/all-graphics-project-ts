32c92a82dc018d02dfb3bb4803c62ee893ba38e7
Anwesh43 <anweshthecool0@gmail.com>
2021-12-13 12:42:57 +0530

    line-rect-holder: adding fixes

--- line-rect-holder.ts
+++ line-rect-holder.ts
@@ -61,7 +61,7 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         context.fillStyle = colors[i]
-        DrawingUtil.drawLineRectHolder(context, i)
+        DrawingUtil.drawLineRectHolder(context, scale)
     }
 }
 
