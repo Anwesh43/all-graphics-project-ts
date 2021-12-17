ceebedacdcedc9824aa9c89d08399f659a017d61
Anwesh43 <anweshthecool0@gmail.com>
2021-12-18 03:36:57 +0530

    line-rot-square-up: adding ScaleUtil class

--- line-rot-square-up.ts
+++ line-rot-square-up.ts
@@ -14,3 +14,14 @@ const colors : Array<string> = [
 ]
 const backColor : string = "#BDBDBD"
 const deg : number = Math.PI / 2
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n))
+    }
+}
