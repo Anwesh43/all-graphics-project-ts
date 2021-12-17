a9ce15616554ee4bdafa0fcadc711193a3cd3c58
Anwesh43 <anweshthecool0@gmail.com>
2021-12-18 03:59:56 +0530

    line-rot-square-up: adding DrawingUtil class

--- line-rot-square-up.ts
+++ line-rot-square-up.ts
@@ -25,3 +25,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n))
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRotSquareUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h  / 2 + size) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.rotate(deg * sc2)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, -size * 0.5 * sc1)
+            context.restore()
+        }
+        context.fillRect(-size / 2, -size * sc3, size, size * sc3)
+        context.restore()
+    }
+
+    static drawLRSUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 2
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineRotSquareUp(context, scale)
+    }
+}
+
