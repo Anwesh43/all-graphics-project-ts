079da4228c5fb1dba2ada250fa776084048c594e
Anwesh43 <anweshthecool0@gmail.com>
2021-12-23 03:55:46 +0530

    right-angle-bar-catcher: adding RABCNode class

--- right-angle-bar-catcher.ts
+++ right-angle-bar-catcher.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class RABCNode {
+
+    next : RABCNode
+    prev : RABCNode
+    state : State = new State() 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RABCNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRABCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RABCNode {
+        var curr : RABCNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
