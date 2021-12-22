bffc39487f78d04656982139f01351e8f33ff060
Anwesh43 <anweshthecool0@gmail.com>
2021-12-23 02:45:04 +0530

    right-angle-bar-catcher: adding constants

---
+++ right-angle-bar-catcher.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#bdbdbd"
+const deg : number = Math.PI / 2 
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
