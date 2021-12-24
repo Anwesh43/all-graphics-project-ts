385e7d9a9e7962b9bc8f884d0b59e8edf8975b70
Anwesh43 <anweshthecool0@gmail.com>
2021-12-24 15:26:43 +0530

    cross-line-alt-square: adding fillstyle

--- cross-line-alt-square.ts
+++ cross-line-alt-square.ts
@@ -63,6 +63,7 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
         DrawingUtil.drawCrossLineAltSquare(context, scale)
     }
 }
