8b1839d250fe093f689e3cfce077c5e4c9c7f237
Anwesh43 <anweshthecool0@gmail.com>
2021-12-24 14:23:21 +0530

    cross-line-alt-square: adding constants

---
+++ cross-line-alt-square.ts
@@ -0,0 +1,14 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 3 
+const scGap : number = 0.03 / parts  
+const strokeFactor : number = 90 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 11.9 
+const rot : number = Math.PI / 2
