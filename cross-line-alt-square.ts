47e204ae2193b320d4d171ab14be504f4597b753
Anwesh43 <anweshthecool0@gmail.com>
2021-12-24 14:55:17 +0530

    cross-line-alt-square: adding DrawingUtil class

--- cross-line-alt-square.ts
+++ cross-line-alt-square.ts
@@ -12,6 +12,8 @@ const delay : number = 20
 const backColor : string = "#BDBDBD"
 const sizeFactor : number = 11.9 
 const rot : number = Math.PI / 2
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
 
 class ScaleUtil {
 
@@ -23,3 +25,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCrossLineAltSquare(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size / 2) * sc3)
+        context.rotate(rot * sc3)
+        for (var j = 0; j < 2; j++) {
+            const y : number = (w / 2 + size / 2) * (1 - j) + (h / 2 + size / 2) * j 
+            context.save()
+            context.rotate(rot * j)
+            context.translate(y * (1 - sc1), 0)
+            DrawingUtil.drawLine(context, -size / 2, 0, size / 2, 0)
+            context.restore()
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.fillRect(0, -size / 2, size * 0.5 * sc2, size / 2)
+            context.restore()
+        }
+    
+        context.restore()
+    }
+
+    static drawCLASNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawCrossLineAltSquare(context, scale)
+    }
+}
+
