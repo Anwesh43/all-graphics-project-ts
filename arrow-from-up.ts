66487c0a10cb037b4dabd8bf2b62c8502ecc7a2e
Anwesh43 <anweshthecool0@gmail.com>
2021-12-25 07:10:56 +0530

    arrow-from-up: adding ScaleUtil class

--- arrow-from-up.ts
+++ arrow-from-up.ts
@@ -15,3 +15,14 @@ const parts : number = 4
 const scGap : number = 0.04 / parts 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
