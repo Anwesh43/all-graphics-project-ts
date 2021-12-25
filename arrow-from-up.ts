6be013439a9a32dcf7476cd6963545b1ebf3ecf4
Anwesh43 <anweshthecool0@gmail.com>
2021-12-25 07:29:45 +0530

    arrow-from-up: adding DrawingUtil class

--- arrow-from-up.ts
+++ arrow-from-up.ts
@@ -26,3 +26,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArrowFromUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const arrowLineSize : number = Math.min(w, h) / arrowLineSizeFactor
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            context.rotate(rot * sc2)
+            DrawingUtil.drawLine(context, 0, arrowLineSize, 0, arrowLineSize * (1 - sc1))
+            context.restore()
+        }
+        context.save()
+        context.translate(-w * 0.5 * (1 - sc3), 0)
+        DrawingUtil.drawLine(context, 0, 0, -size, 0)
+        context.restore()
+        context.restore()
+    }
+
+    static drawAFUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawArrowFromUp(context, scale)
+    }
+}
