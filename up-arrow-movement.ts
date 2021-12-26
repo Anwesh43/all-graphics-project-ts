c75409fe3ea032c876460794f426ba8cc6c0fec6
Anwesh43 <anweshthecool0@gmail.com>
2021-12-27 00:03:05 +0530

    up-arrow-movement: adding fixes 2

--- up-arrow-movement.ts
+++ up-arrow-movement.ts
@@ -110,7 +110,7 @@ class State {
 
     update(cb : Function) {
         this.scale += scGap * this.dir 
-        if (Math.abs(this.scale - this.prevScale) >> 1) {
+        if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir 
             this.dir = 0 
             this.prevScale = this.scale 
