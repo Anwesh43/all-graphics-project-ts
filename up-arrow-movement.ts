9638bcb17182866d3146d034126d856d86d88fbf
Anwesh43 <anweshthecool0@gmail.com>
2021-12-26 19:57:45 +0530

    up-arrow-movement: adding constants

---
+++ up-arrow-movement.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4
+const scGap : number = 0.03 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const arrowSizeFactor : number = 24.9 
+const delay : number = 20
+const deg : number = Math.PI / 4 
