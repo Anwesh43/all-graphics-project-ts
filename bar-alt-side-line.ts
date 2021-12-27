6d94ea754267a1b220d249eeb3ceb59df28c088e
Anwesh43 <anweshthecool0@gmail.com>
2021-12-27 12:49:31 +0530

    bar-alt-side-line: adding constants

---
+++ bar-alt-side-line.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 9.2 
+const strokeFactor : number = 90 
