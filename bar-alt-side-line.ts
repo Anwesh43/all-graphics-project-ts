1a687a0ee25364d85d1906c0fa6ea0d738ca6cb4
Anwesh43 <anweshthecool0@gmail.com>
2021-12-27 12:55:59 +0530

    bar-alt-side-line: adding ScaleUtil class

--- bar-alt-side-line.ts
+++ bar-alt-side-line.ts
@@ -13,3 +13,13 @@ const delay : number = 20
 const backColor : string = "#BDBDBD"
 const sizeFactor : number = 9.2 
 const strokeFactor : number = 90 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
