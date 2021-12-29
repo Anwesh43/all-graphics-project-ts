1d19d8f0d0679ca8d53d16c1fad2df7786aaf10f
Anwesh43 <anweshthecool0@gmail.com>
2021-12-29 13:18:43 +0530

    circle-line-sweep: adding CircleLineSweep class

--- circle-line-sweep.ts
+++ circle-line-sweep.ts
@@ -182,3 +182,26 @@ class CLSNode {
         return this 
     }
 }
+
+class CircleLineSweep {
+
+    curr : CLSNode = new CLSNode(0)
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
