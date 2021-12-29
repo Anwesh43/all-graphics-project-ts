eba5292c366d14e3f9c2f840204bf3bf60d76bd3
Anwesh43 <anweshthecool0@gmail.com>
2021-12-29 13:12:40 +0530

    circle-line-sweep: adding CLSNode class

--- circle-line-sweep.ts
+++ circle-line-sweep.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class CLSNode {
+
+    state : State = new State()
+    prev : CLSNode 
+    next : CLSNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new CLSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }   
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawCLSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CLSNode {
+        var curr : CLSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
