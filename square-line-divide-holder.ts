222741b3192dd8b7a550bbcacce8066f504b0781
Anwesh43 <anweshthecool0@gmail.com>
2022-01-01 11:15:35 +0530

    square-line-divide-holder: adding ScaleUtil class

--- square-line-divide-holder.ts
+++ square-line-divide-holder.ts
@@ -14,3 +14,14 @@ const backColor : string = "#BDBDBD"
 const strokeFactor : number = 90 
 const sizeFactor : number = 11.9 
 const deg : number = Math.PI 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
