b853c50bb68e682dc75597c749c0cb4deb9b177d
Anwesh43 <anweshthecool0@gmail.com>
2022-01-02 14:41:41 +0530

    line-dive-bar-mover: adding LDBMNode class

--- line-dive-bar-mover.ts
+++ line-dive-bar-mover.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class LDBMNode {
+
+    prev : LDBMNode 
+    next : LDBMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDBMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLDBMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LDBMNode {
+        var curr : LDBMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
