a9b877898358763e895d07201dfc06c800658fe8
Anwesh43 <anweshthecool0@gmail.com>
2022-01-03 22:26:09 +0530

    bar-screen-alt-rot: adding DrawingUtil class

--- bar-screen-alt-rot.ts
+++ bar-screen-alt-rot.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarScreenAltRot(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h * 0.5 * sc4))
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.rotate(deg * sc3)
+            DrawingUtil.drawLine(context, size / 2, 0, size / 2, -size * sc1)
+            context.fillRect(size / 2 - size * 0.5 * sc2, -size, size * 0.5 * sc2, size)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBSARNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarScreenAltRot(context, scale)
+    }
+}
