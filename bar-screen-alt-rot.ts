a1db023f371a5cb61fd524a1c9c49d8f94360624
Anwesh43 <anweshthecool0@gmail.com>
2022-01-03 22:38:19 +0530

    bar-screen-alt-rot: adding BarScreenAltRot class

--- bar-screen-alt-rot.ts
+++ bar-screen-alt-rot.ts
@@ -179,3 +179,26 @@ class BSARNode {
         return this 
     }
 }
+
+class BarScreenAltRot {
+
+    curr : BSARNode = new BSARNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: Function) {
+        this.curr.startUpdating(cb)
+    }
+}
