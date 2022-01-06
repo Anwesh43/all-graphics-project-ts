b2653ec753600cda7da9cf49d58c12eda6af16a4
Anwesh43 <anweshthecool0@gmail.com>
2022-01-06 21:43:02 +0530

    three-line-shooter: adding TLSNode class

--- three-line-shooter.ts
+++ three-line-shooter.ts
@@ -136,3 +136,45 @@ class Animator {
         }
     }
 }
+
+class TLSNode {
+
+    prev : TLSNode 
+    next : TLSNode  
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TLSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTLSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TLSNode {
+        var curr : TLSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
