dada26a7d58ff90909f84b70e2dbe51a90096a89
Anwesh43 <anweshthecool0@gmail.com>
2022-01-06 21:33:48 +0530

    three-line-shooter: adding DrawingUtil class

--- three-line-shooter.ts
+++ three-line-shooter.ts
@@ -5,8 +5,8 @@ const colors : Array<string> = [
     "#795548",
     "#8BC34A"
 ]
-const parts : number = 3 
-const scGap : number = 0.02 / parts 
+const parts : number = 4
+const scGap : number = 0.03 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 11.2 
 const delay : number = 20 
@@ -23,5 +23,41 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawThreeLineShooter(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2) * sc4, h / 2)
+        context.rotate(deg * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.25 * sc1)
+            DrawingUtil.drawLine(context, size / 2, 0, size / 2, -size * sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawTLSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawThreeLineShooter(context, scale)
     }
 }
