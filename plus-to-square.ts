e5cffed417b42b23826fcdb06f2ba3cca9f5e486
Anwesh43 <anweshthecool0@gmail.com>
2022-01-07 06:00:34 +0530

    plus-to-square: adding DrawingUtil class

--- plus-to-square.ts
+++ plus-to-square.ts
@@ -25,3 +25,38 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+    
+    static drawPlusToSquare(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2  + size) * sc4)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(deg * sc3)
+            context.translate(0, -size * 0.5 * sc2)
+            DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawPTSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPlusToSquare(context, scale)
+    }
+}
