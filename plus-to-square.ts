5f2212a9e516361e6d4f67e5b680d65051e3f2cc
Anwesh43 <anweshthecool0@gmail.com>
2022-01-07 06:28:36 +0530

    plus-to-square: adding rotation fixes

--- plus-to-square.ts
+++ plus-to-square.ts
@@ -43,7 +43,7 @@ class DrawingUtil {
         const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
         context.save()
         context.translate(w / 2, h / 2 + (h / 2  + size) * sc4)
-        for (var j = 0; j < 4; j++) {
+        for (var j = 0; j < 2 + 2 * Math.floor(sc2); j++) {
             context.save()
             context.rotate(deg * sc2 * j)
             context.translate(0, -size * 0.5 * sc3)
