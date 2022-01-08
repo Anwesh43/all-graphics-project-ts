28b1b1b85023efb6b0912eff3ee2768376492939
Anwesh43 <anweshthecool0@gmail.com>
2022-01-08 13:08:21 +0530

    line-stack-over-up: adding constants

---
+++ line-stack-over-up.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const backColor : string  = "#BDBDBD"
+const delay : number = 20
+const lines : number = 3  
+const parts : number = lines + 2
+const scGap : number = 0.02 / parts 
+const sizeFactor : number = 6.3  
+const divideFactor : number = 4 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const strokeFactor : number = 90 
+const deg : number = Math.PI  
