746e9f8fddba235990877b16b3c61528891874ce
Anwesh43 <anweshthecool0@gmail.com>
2022-01-09 12:13:31 +0530

    bar-line-join: adding constants

---
+++ bar-line-join.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const parts : number = 5 
+const scGap : number = 0.05 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const deg : number = Math.PI 
+const barSizeFactor : number = 4
