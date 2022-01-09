8fba66c872e4fe649caef56a38fdc7ba1df6db5c
Anwesh43 <anweshthecool0@gmail.com>
2022-01-09 12:54:33 +0530

    bar-line-join: adding DrawingUtil class

--- bar-line-join.ts
+++ bar-line-join.ts
@@ -26,3 +26,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarLineJoin(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2) * sc5)
+        context.rotate(deg * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, -size / 4, 0, -size / 4 - size * 0.5 * sc1, 0)
+            DrawingUtil.drawLine(context, -size / 4, 0, -size / 4, -size * 0.25 * sc2)
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, -size / 4, -size / 4, -size / 4 + size * 0.5 * sc3, -size / 4)
+        context.restore()
+    }
+
+    static drawBLJNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBarLineJoin(context, scale)
+    }
+}
