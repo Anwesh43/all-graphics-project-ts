39817db545bfa75ca3392d215773b93bf1c3e6b3
Anwesh43 <anweshthecool0@gmail.com>
2022-01-09 12:16:08 +0530

    bar-line-join: adding ScaleUtil class

--- bar-line-join.ts
+++ bar-line-join.ts
@@ -15,3 +15,14 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight 
 const deg : number = Math.PI 
 const barSizeFactor : number = 4
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
