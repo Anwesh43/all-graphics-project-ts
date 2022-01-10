9b5210eff0e2799ef8f3536ed27237c0d28eed5d
Anwesh43 <anweshthecool0@gmail.com>
2022-01-10 14:00:25 +0530

    tree-node-bench: adding constants

---
+++ tree-node-bench.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const backColor : string = "#BDBDBD"
+const sizeFactor : number  = 5.9 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const strokeFactor : number = 90 
+const delay : number = 20
+const parts : number = 2 
+const scGap : number = 0.02 / parts 
