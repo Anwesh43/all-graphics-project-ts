f99c1c2b99b65216e036dcf2a09a56d60e4750ca
Anwesh43 <anweshthecool0@gmail.com>
2022-01-10 14:02:02 +0530

    tree-node-bench: adding ScaleUtil class

--- tree-node-bench.ts
+++ tree-node-bench.ts
@@ -13,3 +13,14 @@ const strokeFactor : number = 90
 const delay : number = 20
 const parts : number = 2 
 const scGap : number = 0.02 / parts 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+    
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
