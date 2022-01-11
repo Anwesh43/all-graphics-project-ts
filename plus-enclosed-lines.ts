80bc08222a6d2e08dd1e05506f5f40aa19b117ec
Anwesh43 <anweshthecool0@gmail.com>
2022-01-11 17:07:25 +0530

    plus-enclosed-lines: adding constants

---
+++ plus-enclosed-lines.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const strokeFactor : number = 90 
+const sizeFactor : number = 12.9 
+const deg : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const rot : number = Math.PI / 2 
