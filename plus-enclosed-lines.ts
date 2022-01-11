739f94ed3f98e92401a8f3dfd9aeba8d623a7488
Anwesh43 <anweshthecool0@gmail.com>
2022-01-11 17:08:27 +0530

    plus-enclosed-lines: adding ScaleUtil class

--- plus-enclosed-lines.ts
+++ plus-enclosed-lines.ts
@@ -15,3 +15,14 @@ const h : number = window.innerHeight
 const parts : number = 4
 const scGap : number = 0.04 / parts 
 const rot : number = Math.PI / 2 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
