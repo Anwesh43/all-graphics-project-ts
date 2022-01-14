0e1a62d613803555b5b988385400c3cc0d054f81
Anwesh43 <anweshthecool0@gmail.com>
2022-01-14 23:40:30 +0530

    bar-plus-rot-up: adding BPRUNode class

--- bar-plus-rot-up.ts
+++ bar-plus-rot-up.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class BPRUNode {
+
+    next : BPRUNode
+    prev : BPRUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BPRUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBPRUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) {
+        var curr : BPRUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
