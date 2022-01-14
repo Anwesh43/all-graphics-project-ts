f1c3ce733042e099baec791d0ecbfe3415be1d2b
Anwesh43 <anweshthecool0@gmail.com>
2022-01-14 22:53:29 +0530

    bar-plus-rot-up: adding DrawingUtil class

--- bar-plus-rot-up.ts
+++ bar-plus-rot-up.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarPlusRotUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2 + size) * sc4)
+        context.rotate(deg * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(j * Math.PI / 2)
+            DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc2, 0)
+            context.restore()
+        }
+        context.fillRect(-size / 2, 0, size, size * 0.5 * sc2)
+        context.restore()
+    }
+
+    static drawBPRUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarPlusRotUp(context, scale)
+    }
+}
