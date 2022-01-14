473790df489eaa0d4a3d4e9275f83065ea0ea574
Anwesh43 <anweshthecool0@gmail.com>
2022-01-14 20:51:14 +0530

    bar-plus-rot-up: adding constants

---
+++ bar-plus-rot-up.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const parts : number = 4
+const scGap : number = 0.03 / parts 
+const backColor : string = "#BDBDBD"
+const deg : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
