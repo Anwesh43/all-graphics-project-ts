6b9011c7f8f94ff78df63652c98096c4d1af60f5
Anwesh43 <anweshthecool0@gmail.com>
2022-01-19 21:27:03 +0530

    bar-stack-rotate-left: adding DrawingUtil class

--- bar-stack-rotate-left.ts
+++ bar-stack-rotate-left.ts
@@ -24,3 +24,36 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarStackRotateLeft(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(rot * sc3)
+        for (let j = 0; j < 2; j++) {
+            const barSize : number = (size) / (j + 1)
+            const scj : number = ScaleUtil.divideScale(scale, j, parts)
+            context.save()
+            context.translate(-barSize / 2, -h / 2 - barSize + (h / 2) * scj)
+            context.fillRect(0, 0, barSize, barSize)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBSLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarStackRotateLeft(context, scale)
+    }
+}
