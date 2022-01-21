904cf6ade3a35dda40e4af6fcd590fbdb56a6888
Anwesh43 <anweshthecool0@gmail.com>
2022-01-22 00:10:54 +0530

    criss-cross-lines: adding CCLNode class

--- criss-cross-lines.ts
+++ criss-cross-lines.ts
@@ -136,3 +136,45 @@ class Animator {
         }
     }
 }
+
+class CCLNode {
+
+    prev : CCLNode 
+    next : CCLNode
+    state : State = new State() 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new CCLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawCCLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CCLNode {
+        var curr : CCLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
