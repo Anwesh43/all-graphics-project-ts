35b7bd050e22a5a5727d2008e2cf616cfbc865df
Anwesh43 <anweshthecool0@gmail.com>
2022-01-22 00:25:40 +0530

    criss-cross-lines: adding fixes

--- criss-cross-lines.ts
+++ criss-cross-lines.ts
@@ -47,8 +47,9 @@ class DrawingUtil {
         for (let j = 0; j < 2; j++) {
             context.save()
             context.scale(1 - 2 * j, 1 - 2 * j)
+            context.rotate(deg * j)
             DrawingUtil.drawLine(context, size * 0.5 * sc2, -size * 0.5 * sc1, size * 0.5 * sc2, size * 0.5 * sc1)
-            DrawingUtil.drawLine(context, size * 0.5 * sc2, size * 0.5, -size * 0.5 * sc2, -size * 0.5)
+            DrawingUtil.drawLine(context, size * 0.5 * sc2, size * 0.5 * sc1, -size * 0.5 * sc2, -size * 0.5 * sc1)
             context.restore()
         }
         context.restore()
