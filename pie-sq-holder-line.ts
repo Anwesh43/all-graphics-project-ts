9edb554c2dc81969223942f81526468865ca6c21
Anwesh43 <anweshthecool0@gmail.com>
2022-01-26 15:41:59 +0530

    pie-sq-holder-line: adding DrawingUtil class

--- pie-sq-holder-line.ts
+++ pie-sq-holder-line.ts
@@ -25,3 +25,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawPieSqHolderLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc5, h / 2)
+        context.rotate(deg * sc4)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, 0, 0, -size * 0.5 * sc1, size * 0.5 * sc1)
+            DrawingUtil.drawLine(context, 0, 0, -size * 0.5 * sc2, 0)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, -size * 0.5 * sc3)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawPSHLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPieSqHolderLine(context, scale)
+    }
+}
