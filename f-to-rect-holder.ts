8d84829ef2b1a99aeb4c193a732bdd2fbc2f031e
Anwesh43 <anweshthecool0@gmail.com>
2022-01-29 14:14:32 +0530

    f-to-rect-holder: adding DrawingUtil class

--- f-to-rect-holder.ts
+++ f-to-rect-holder.ts
@@ -7,7 +7,7 @@ const colors : Array<string> = [
 ]
 const parts : number = 4
 const scGap : number = 0.04 / parts 
-const deg : number = Math.PI / 2 
+const deg : number = Math.PI 
 const strokeFactor : number = 90
 const sizeFactor : number = 11.2 
 const delay : number = 20 
@@ -23,5 +23,43 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawFToRectHolder(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size / 2, 0)
+            context.rotate(-deg * sc3)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * sc1)
+            DrawingUtil.drawLine(context, 0, -size, -size * 0.5 * sc2, -size)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawFTRHNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawFToRectHolder(context, scale)
     }
 }
