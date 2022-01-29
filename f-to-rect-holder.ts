7e4fdd3e888345a54562931673081f1cb11b82c7
Anwesh43 <anweshthecool0@gmail.com>
2022-01-29 14:20:43 +0530

    f-to-rect-holder: adding FTRHNode class

--- f-to-rect-holder.ts
+++ f-to-rect-holder.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class FTRHNode {
+
+    prev : FTRHNode 
+    next : FTRHNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new FTRHNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawFTRHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : FTRHNode {
+        var curr : FTRHNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
