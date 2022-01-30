128f446a3b29196e490437e7c738bf98b659dd92
Anwesh43 <anweshthecool0@gmail.com>
2022-01-30 13:18:24 +0530

    trident-rot-right: adding constants

---
+++ trident-rot-right.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const sizeFactor : number = 4.7
+const triSizeFactro : number = 15.2 
+const parts : number = 5
+const scGap : number = 0.04 / 5 
+const deg : number = Math.PI / 2
+const lines : number = 3 
+const strokeFactor : number = 90 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
