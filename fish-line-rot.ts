85d0434b5d59d973927d7084f2f62c710c151c97
Anwesh43 <anweshthecool0@gmail.com>
2022-02-01 02:50:21 +0530

    fish-line-rot: adding DrawingUtil class

--- fish-line-rot.ts
+++ fish-line-rot.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawFishLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size / 2) * sc4, h / 2)
+        DrawingUtil.drawLine(context, size / 2, -size * 0.5 * sc1, size / 2, size * 0.5 * sc1)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            context.translate(size / 2, size / 2)
+            context.rotate(rot * sc3)
+            DrawingUtil.drawLine(context, 0, 0, -size * sc2, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawFLRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawFishLineRot(context, scale)
+    }
+}
