43f2c09eb21a9812976fd54384ece125bb90a458
Anwesh43 <anweshthecool0@gmail.com>
2022-02-03 23:29:31 +0530

    bar-push-line: adding DrawingUtil class

--- bar-push-line.ts
+++ bar-push-line.ts
@@ -24,3 +24,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarPushLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2) * sc4, h / 2)
+        DrawingUtil.drawLine(
+            context,
+            size / 2,
+            -size * 0.5 * sc1,
+            size / 2,
+            size * 0.5 * sc1
+        )
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            context.translate(0, (-size / 2 + size / 20) * (1 - sc3))
+            context.fillRect(0, -size / 20, size * 0.5 * sc2, size / 10)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBPLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarPushLine(context, scale)
+    }
+}   
