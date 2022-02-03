ea1c4ee6f4b5d3f440624c6741134769298d4504
Anwesh43 <anweshthecool0@gmail.com>
2022-02-03 23:37:56 +0530

    bar-push-line: adding BPLNode class

--- bar-push-line.ts
+++ bar-push-line.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class BPLNode {
+
+    next : BPLNode 
+    prev : BPLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BPLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBPLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BPLNode {
+        var curr : BPLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
