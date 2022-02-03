9bb9033fa35d3d5a9b0b711f84e878c610bfff1f
Anwesh43 <anweshthecool0@gmail.com>
2022-02-03 23:17:31 +0530

    bar-push-line: adding constants

---
+++ bar-push-line.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const strokeFactor : number = 90 
+const sizeFactor : number = 5.9
+const parts : number = 4  
+const scGap : number = 0.04 / parts 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
