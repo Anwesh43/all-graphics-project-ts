809caa99d1e178cdf22a46dbcf85352301cb9b97
Anwesh43 <anweshthecool0@gmail.com>
2022-02-05 14:13:02 +0530

    open-box-rect-fill: adding DrawingUtil class

--- open-box-rect-fill.ts
+++ open-box-rect-fill.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#795548",
     "#8BC34A"
 ]
-const parts : number = 4 
+const parts : number = 5 
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90  
 const sizeFactor : number = 11.9 
@@ -22,5 +22,44 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawOpenBoxRectFill(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2) * sc5)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size / 2, 0)
+            context.rotate(deg * sc3)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * sc2)
+            context.restore()
+        }
+        context.fillRect(-size / 2, -size * sc4, size, size * sc4)
+        context.restore()
+    }
+    
+    static drawOBRFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawOpenBoxRectFill(context, scale)
     }
 }
