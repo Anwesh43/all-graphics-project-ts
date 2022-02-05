08c0b6dcacda230ffe8f04424408c0ed9ec5272a
Anwesh43 <anweshthecool0@gmail.com>
2022-02-05 15:21:35 +0530

    open-box-rect-fill: adding renderer in stage

--- open-box-rect-fill.ts
+++ open-box-rect-fill.ts
@@ -10,7 +10,7 @@ const scGap : number = 0.04 / parts
 const strokeFactor : number = 90  
 const sizeFactor : number = 11.9 
 const backColor : string = "#BDBDBD"
-const deg : number = Math.PI / 2
+const deg : number = Math.PI
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const delay : number = 20 
@@ -61,6 +61,7 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
         DrawingUtil.drawOpenBoxRectFill(context, scale)
     }
 }
