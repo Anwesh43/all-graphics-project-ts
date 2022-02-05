a65aba3988c643d28ac6422feca41673e9959599
Anwesh43 <anweshthecool0@gmail.com>
2022-02-05 14:30:39 +0530

    open-box-rect-fill: adding OBRFNode class

--- open-box-rect-fill.ts
+++ open-box-rect-fill.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class OBRFNode {
+
+    prev : OBRFNode 
+    next : OBRFNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new OBRFNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawOBRFNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : OBRFNode {
+        var curr : OBRFNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
