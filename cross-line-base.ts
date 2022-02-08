52f2c5585d0ad809e4577863026772eea5ecd6a6
Anwesh43 <anweshthecool0@gmail.com>
2022-02-08 15:20:49 +0530

    cross-line-base: adding constants

---
+++ cross-line-base.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const backColor : string = "#BDBDBD"
+const delay : number = 90 
+const deg : number = 45 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
