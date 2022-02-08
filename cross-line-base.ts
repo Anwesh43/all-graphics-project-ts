7b66340be66109229e7d02510f554e073bd22b4f
Anwesh43 <anweshthecool0@gmail.com>
2022-02-08 15:30:00 +0530

    cross-line-base: adding DrawingUtil class

--- cross-line-base.ts
+++ cross-line-base.ts
@@ -25,3 +25,38 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCrossLineBase(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / strokeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(deg * (1 - 2 * j) * sc2)
+            DrawingUtil.drawLine(context, 0, -size * 0.5 * sc1, 0, size * 0.5 * sc1)
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, size / 2, size * 0.5 * sc1, size / 2)
+        context.restore()
+    }
+
+    static drawCLBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawCrossLineBase(context, scale)
+    }
+}
