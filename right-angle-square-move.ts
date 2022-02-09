f80f808da96d693e775894fd5dc1ccf1997f5842
Anwesh43 <anweshthecool0@gmail.com>
2022-02-09 13:47:48 +0530

    right-angle-square-move: adding fillStyle for rect

--- right-angle-square-move.ts
+++ right-angle-square-move.ts
@@ -59,6 +59,7 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
         DrawingUtil.drawRightAngleSquareMove(context, scale)
     } 
 }
