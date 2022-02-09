9ca64f24b7e2517e242685e135b863d54209e078
Anwesh43 <anweshthecool0@gmail.com>
2022-02-09 13:43:44 +0530

    right-angle-square-move: adding RASMNode class

--- right-angle-square-move.ts
+++ right-angle-square-move.ts
@@ -179,3 +179,26 @@ class RASMNode {
         return this 
     }
 }
+
+class RightAngleSquareMove {
+    
+    curr : RASMNode = new RASMNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUdpating(cb)
+    }
+}
