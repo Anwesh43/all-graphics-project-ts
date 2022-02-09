4535b4e2c4b628da2299c806096698b57d876218
Anwesh43 <anweshthecool0@gmail.com>
2022-02-10 04:14:36 +0530

    bar-shift-in-rect-holde: adding DrawingUtil class

--- bar-shift-in-rect-holde.ts
+++ bar-shift-in-rect-holde.ts
@@ -24,3 +24,48 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 } 
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarShiftInRectHolder(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2) * sc4)
+        context.save()
+        context.translate(0, -size * sc2)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        context.restore()
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1- 2 * j)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, -size * sc2)
+            context.restore()
+        }
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(-size / 2, -size + (size - size / 10) * j)
+            context.fillRect(0, 0, size * sc3, size / 10)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBSIRHNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawBarShiftInRectHolder(context, scale)
+    }
+}
