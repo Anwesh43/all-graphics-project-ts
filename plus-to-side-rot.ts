54cb9a7945d4f45e29aa2743585ade11879acf5f
Anwesh43 <anweshthecool0@gmail.com>
2022-02-22 22:25:52 +0530

    plus-to-side-rot: adding DrawingUtil class

--- plus-to-side-rot.ts
+++ plus-to-side-rot.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawPlusToSideRot(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(j * Math.PI / 2)
+            context.translate(0, -(size / 2) * (1 - j) * sc2)
+            context.rotate(rot * (1 - j) * sc3)
+            for (let k = 0; k < 2; k++) {
+                DrawingUtil.drawLine(
+                    context,
+                    0,
+                    0,
+                    size * 0.5 * sc1 * (1 - 2 * k),
+                    0
+                )
+            }           
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawPTSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPlusToSideRot(context, scale)
+    }
+}
