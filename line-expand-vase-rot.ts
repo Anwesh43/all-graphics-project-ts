d23d080dca9e527690d152d7db3851881e1f7a15
Anwesh43 <anweshthecool0@gmail.com>
2022-02-25 01:01:23 +0530

    line-expand-vase-rot: adding LEVRNode class

--- line-expand-vase-rot.ts
+++ line-expand-vase-rot.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LEVRNode {
+
+    prev : LEVRNode 
+    next : LEVRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LEVRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLEVRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LEVRNode {
+        var curr : LEVRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
