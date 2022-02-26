3826de1cd8ef40a7a5e210d313d66b9e597d3c11
Anwesh43 <anweshthecool0@gmail.com>
2022-02-26 21:40:43 +0530

    bar-to-thick-line: addding constants

---
+++ bar-to-thick-line.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const backColor : string = "#BDBDBD"
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const delay : number = 20 
+const rot : number = Math.PI / 2
+const sizeFactor : number = 11.3 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
