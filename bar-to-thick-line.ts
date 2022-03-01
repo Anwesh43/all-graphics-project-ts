fa75889e26e2d77f1c26228b02f908d3657a800e
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 09:48:29 +0530

    bar-to-thick-line: addding BTTLNode class

--- bar-to-thick-line.ts
+++ bar-to-thick-line.ts
@@ -133,3 +133,45 @@ class Animator {
         }
     }
 }
+
+class BTTLNode {
+
+    prev : BTTLNode 
+    next : BTTLNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BTTLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBTTLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BTTLNode {
+        var curr : BTTLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
