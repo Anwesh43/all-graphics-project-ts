2c9aca195fe97c60fde3c5d1263147a0c90014c5
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 10:00:36 +0530

    bar-to-thick-line: addding fixes

--- bar-to-thick-line.ts
+++ bar-to-thick-line.ts
@@ -37,7 +37,7 @@ class DrawingUtil {
         const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
 
         context.save()
-        context.translate(w / 2, h / 2)
+        context.translate(w / 2, h / 2  + (h  / 2 + size) * sc4)
         for (let j = 0; j < 2; j++) {
             context.save()
             context.rotate(deg * sc3 * (1 - 2 * j))
