12b81db7e3ee221117cef3b223091879c45a8b8b
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 09:52:55 +0530

    bar-to-thick-line: addding BarToThickLine class

--- bar-to-thick-line.ts
+++ bar-to-thick-line.ts
@@ -175,3 +175,26 @@ class BTTLNode {
         return this 
     }
 }
+
+class BarToThickLine {
+
+    curr : BTTLNode = new BTTLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
