ce92b9b3b01118dbb075579410aba021d1183057
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 12:59:39 +0530

    cross-lapse-rot-line: adding DrawingUtil class

--- cross-lapse-rot-line.ts
+++ cross-lapse-rot-line.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCrossLapseRotLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / strokeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2) * sc4, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * (1 - 2 * j) * sc2)
+            context.save()
+            context.translate(-size / 2 + size * 0.5 * sc3, 0)
+            DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawCLRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawCrossLapseRotLine(context, scale)
+    } 
+}
