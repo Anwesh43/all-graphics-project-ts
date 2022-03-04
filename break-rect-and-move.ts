f4eb26f4ffbad64343bfc8c89fa94d5cd26f2497
Anwesh43 <anweshthecool0@gmail.com>
2022-03-04 14:29:36 +0530

    break-rect-and-move: adding constants

---
+++ break-rect-and-move.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 11.2 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
