7cd16a52701110b3c69c97f561989d6c554c9df8
Anwesh43 <anweshthecool0@gmail.com>
2022-03-08 14:29:51 +0530

    ball-arrow-indicator-mover: adding size fix

--- ball-arrow-indicator-mover.ts
+++ ball-arrow-indicator-mover.ts
@@ -9,8 +9,8 @@ const backColor : string = "#BDBDBD"
 const parts : number = 4 
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
-const sizeFactor : number = 11.2 
-const arrowSizeFactor : number = 31.2 
+const sizeFactor : number = 7.2 
+const arrowSizeFactor : number = 21.2 
 const delay : number = 20 
 const deg : number = Math.PI / 4 
 const rot : number = Math.PI / 2
@@ -57,7 +57,7 @@ class DrawingUtil {
         for (let j = 0; j < 2; j++) {
             context.save()
             context.rotate(deg * (1 - 2 * j))
-            DrawingUtil.drawLine(context, 0, 0, -size * sc2, 0)
+            DrawingUtil.drawLine(context, 0, 0, -arrowSize * sc2, 0)
             context.restore()
         }
         context.restore()
