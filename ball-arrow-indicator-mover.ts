5e3968ab972d09f08cc6ad2d466b8f848594158e
Anwesh43 <anweshthecool0@gmail.com>
2022-03-08 13:42:41 +0530

    ball-arrow-indicator-mover: adding DrawingUtil class

--- ball-arrow-indicator-mover.ts
+++ ball-arrow-indicator-mover.ts
@@ -27,3 +27,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawBallArrowIndicatorMover(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h)  / strokeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const arrowSize : number = Math.min(w, h) / arrowSizeFactor
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawCircle(context, 0, 0, size)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(deg * (1 - 2 * j))
+            DrawingUtil.drawLine(context, 0, 0, -size * sc2, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBAIMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawBallArrowIndicatorMover(context, scale)
+    }
+}
