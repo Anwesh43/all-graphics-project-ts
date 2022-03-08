cf23305bad9f5ffa54c5051db01dff7f96838578
Anwesh43 <anweshthecool0@gmail.com>
2022-03-08 13:25:45 +0530

    ball-arrow-indicator-mover: adding constants and libraries

---
+++ ball-arrow-indicator-mover.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const backColor : string = "#BDBDBD"
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.2 
+const arrowSizeFactor : number = 31.2 
+const delay : number = 20 
+const deg : number = Math.PI / 4 
+const rot : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight   
