526eb5485a8c67d00b3ea01fe0fb8573e0183142
Anwesh43 <anweshthecool0@gmail.com>
2022-03-15 15:11:22 +0530

    main-block-face: adding renderer in stage

--- main-block-face.ts
+++ main-block-face.ts
@@ -59,6 +59,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
@@ -70,11 +71,14 @@ class Stage {
     render() {
         this.context.fillStyle = backColor 
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
