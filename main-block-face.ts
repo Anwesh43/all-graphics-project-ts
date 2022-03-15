9846c27747adb1a76ceaabfb7a377fb8aa81d29d
Anwesh43 <anweshthecool0@gmail.com>
2022-03-15 14:37:47 +0530

    main-block-face: adding DrawingUtil class

--- main-block-face.ts
+++ main-block-face.ts
@@ -25,3 +25,32 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawMainBlockFace(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const bSize : number = size / 8 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        context.rotate(-deg * sc3)
+        context.fillRect(-size / 2, -size / 2, size / 2, size * sc1)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(size / 2, 0)
+            context.scale(1, 1 - 2 * j)
+            context.fillRect(-bSize / 2, -1.5 * bSize, bSize * sc2, bSize)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawMBFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawMainBlockFace(context, scale)
+    }
+}
