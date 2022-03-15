57f05d750055a892e7909611a3649ef23676f02c
Anwesh43 <anweshthecool0@gmail.com>
2022-03-15 14:57:35 +0530

    main-block-face: adding MBFNode class

--- main-block-face.ts
+++ main-block-face.ts
@@ -129,3 +129,45 @@ class Animator {
         }
     }
 }
+
+class MBFNode {
+
+    prev : MBFNode 
+    next : MBFNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new MBFNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawMBFNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : MBFNode {
+        var curr : MBFNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
