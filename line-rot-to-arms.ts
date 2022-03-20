f1d94791aaf40b7d7e72ab490d507914b26598ef
Anwesh43 <anweshthecool0@gmail.com>
2022-03-20 14:25:49 +0530

    line-rot-to-arms: adding DrawingUtil class

--- line-rot-to-arms.ts
+++ line-rot-to-arms.ts
@@ -24,3 +24,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRotToArm(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + context.lineWidth) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(-deg * sc2 * j)
+            DrawingUtil.drawLine(context, 0, 0, -size * sc1, 0)
+            context.restore()
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, size, 0, size, size * sc3)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLRTMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineRotToArm(context, scale)
+    }
+}
