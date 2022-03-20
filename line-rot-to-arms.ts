94b2201d7762af11e8d753a69874f9c88815fc68
Anwesh43 <anweshthecool0@gmail.com>
2022-03-20 14:35:55 +0530

    line-rot-to-arms: adding LRTMNode class

--- line-rot-to-arms.ts
+++ line-rot-to-arms.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class LRTMNode {
+
+    prev : LRTMNode 
+    next : LRTMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRTMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRTMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : LRTMNode {
+        var curr : LRTMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
