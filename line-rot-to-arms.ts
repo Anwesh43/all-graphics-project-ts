892832a41048a65bb5e0a2e6d37821cac1dbaeda
Anwesh43 <anweshthecool0@gmail.com>
2022-03-20 14:29:45 +0530

    line-rot-to-arms: adding Stage class

--- line-rot-to-arms.ts
+++ line-rot-to-arms.ts
@@ -13,6 +13,7 @@ const delay : number = 20
 const deg : number = Math.PI
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -60,5 +61,23 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         DrawingUtil.drawLineRotToArm(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor  
+        this.context.fillRect(0, 0, w, h)
     }
 }
