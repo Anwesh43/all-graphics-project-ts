059016a35f1214d6e0f1d7ca992feb43bc912ada
Anwesh43 <anweshthecool0@gmail.com>
2022-03-22 02:36:23 +0530

    open-line-arm-down: adding OpenLineArmDown class

--- open-line-arm-down.ts
+++ open-line-arm-down.ts
@@ -193,3 +193,26 @@ class OLADNode {
         return this 
     }
 }
+
+class OpenLineArmDown {
+
+    curr : OLADNode = new OLADNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
