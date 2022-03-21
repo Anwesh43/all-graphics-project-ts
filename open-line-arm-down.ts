d12d1afc57de99e8513b3006f3c7833684301edd
Anwesh43 <anweshthecool0@gmail.com>
2022-03-22 02:35:00 +0530

    open-line-arm-down: adding OLADNode class

--- open-line-arm-down.ts
+++ open-line-arm-down.ts
@@ -151,3 +151,45 @@ class Animator {
         }
     }
 }
+
+class OLADNode {
+
+    prev : OLADNode 
+    next : OLADNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new OLADNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }   
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawOLADNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : OLADNode {
+        var curr : OLADNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
