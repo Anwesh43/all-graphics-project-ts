2f91bdc1c0d19cb37bcc41d05aa66cc8a17f55f2
Anwesh43 <anweshthecool0@gmail.com>
2022-03-22 02:28:44 +0530

    open-line-arm-down: adding DrawingUtil class

--- open-line-arm-down.ts
+++ open-line-arm-down.ts
@@ -24,3 +24,55 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }   
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawOpenLine(context : CanvasRenderingContext2D, size : number, sc1 : number, sc2 : number, sc3 : number) {
+        context.save()
+        context.translate(0, -size + size * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(deg * (1 - 2 * j) * sc2)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * sc1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawArms(context : CanvasRenderingContext2D, size : number, sc3 : number) {
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size, -size)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * sc3)
+            context.restore()
+        }
+    }
+
+    static drawOpenLineArmDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawOpenLine(context, size, sc1, sc2, sc3)
+        DrawingUtil.drawArms(context, size, sc3)
+        context.restore()     
+    }
+
+    static drawOLADNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawOpenLineArmDown(context, scale)
+    }
+}
