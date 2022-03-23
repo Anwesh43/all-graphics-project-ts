2d99ee6de07f43230f9a79fabdcaae1d259e7af7
Anwesh43 <anweshthecool0@gmail.com>
2022-03-23 21:06:03 +0530

    perfect-arrow-mover: adding PerfectArrowMover class

--- perfect-arrow-mover.ts
+++ perfect-arrow-mover.ts
@@ -198,3 +198,26 @@ class Animator {
         return this 
     }
  }
+
+ class PerfectArrowMover {
+
+    curr : PAMNode = new PAMNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+ }
