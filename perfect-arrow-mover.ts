5d15081c126d7bf7dbc299e1e74009cbcc86e9bf
Anwesh43 <anweshthecool0@gmail.com>
2022-03-23 13:48:36 +0530

    perfect-arrow-mover: adding constants

---
+++ perfect-arrow-mover.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const arrowTailFactor : number = 8.9 
+const arrowHeadFactor : number = 7.2 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 6.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const rot : number = Math.PI / 4 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
