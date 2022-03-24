e2fd7b5dba7fe1a828363dedc5b5af8b73002b94
Anwesh43 <anweshthecool0@gmail.com>
2022-03-24 22:15:15 +0530

    line-divider-down: adding LineDividerDown class

--- line-divider-down.ts
+++ line-divider-down.ts
@@ -179,3 +179,27 @@ class LDDNode {
         return this 
     }
 }
+
+class LineDividerDown {
+
+    curr : LDDNode = new LDDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+
+}
