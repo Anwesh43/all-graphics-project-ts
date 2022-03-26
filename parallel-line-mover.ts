d5ec0a4767671013152cd2a094b8856fae496096
Anwesh43 <anweshthecool0@gmail.com>
2022-03-26 11:47:19 +0530

    parallel-line-mover: adding constants

---
+++ parallel-line-mover.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const sizeFactor : number = 11.9 
+const strokeFactor : number = 90 
+const rot : number = Math.PI 
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
