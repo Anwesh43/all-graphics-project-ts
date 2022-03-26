7cfeb10948e6336278b90b7c4d623fa3f9b4f5dc
Anwesh43 <anweshthecool0@gmail.com>
2022-03-26 12:29:33 +0530

    parallel-line-mover: adding renderer in stage

--- parallel-line-mover.ts
+++ parallel-line-mover.ts
@@ -66,6 +66,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
@@ -77,6 +78,9 @@ class Stage {
     render() {
         this.context.fillStyle = backColor 
         this.context.fillRect(0, 0, w, h)
+        this.renderer.handleTap(() => {
+            this.render()
+        })
     }
 
     handleTap() {
