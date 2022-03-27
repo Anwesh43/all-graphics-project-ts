7fef35d617ec855d0c02be9f823ed35e5a402be4
Anwesh43 <anweshthecool0@gmail.com>
2022-03-27 15:10:38 +0530

    either-line-drop: adding ELDNode class

--- either-line-drop.ts
+++ either-line-drop.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class ELDNode {
+
+    prev : ELDNode 
+    next : ELDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ELDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawELDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : ELDNode {
+        var curr : ELDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
