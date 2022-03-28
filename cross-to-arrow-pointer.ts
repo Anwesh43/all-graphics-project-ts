a7931c7f19568afe2d80dc2f087bb06c7ddcfbd1
Anwesh43 <anweshthecool0@gmail.com>
2022-03-28 22:33:36 +0530

    cross-to-arrow-pointer: adding DrawingUtil class

--- cross-to-arrow-pointer.ts
+++ cross-to-arrow-pointer.ts
@@ -26,3 +26,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+    
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCrossToArrowPointer(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2))
+        context.rotate(rot * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.rotate(rot)
+            DrawingUtil.drawLine(context, size * 0.5 * (sc2 - sc1), 0, size * 0.5  * (sc2 + sc1), 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawCTAPNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawCrossToArrowPointer(context, scale)
+    }
+}
+
