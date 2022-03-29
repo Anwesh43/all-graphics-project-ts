b86146782197dc491dec1bef8a5d45ba1a05ddcd
Anwesh43 <anweshthecool0@gmail.com>
2022-03-29 11:20:11 +0530

    perp-line-to-parallel: adding PerpLineToParallel class

--- perp-line-to-parallel.ts
+++ perp-line-to-parallel.ts
@@ -179,3 +179,26 @@ class PLTPNode {
         return this 
     }
 }
+
+class PerpLineToParallel {
+
+    curr : PLTPNode = new PLTPNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
