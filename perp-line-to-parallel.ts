430e5c4b7ec061fe986e179a3dce14efed870eb4
Anwesh43 <anweshthecool0@gmail.com>
2022-03-29 10:17:06 +0530

    perp-line-to-parallel: adding DrawingUtil class

--- perp-line-to-parallel.ts
+++ perp-line-to-parallel.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawPerpLineToParallel(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / strokeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(size * 0.5 * sc3, -h / 2 + (h / 2) * sc2)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        context.restore()
+    }
+
+    static drawPLTPNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPerpLineToParallel(context, scale)
+    }
+}
