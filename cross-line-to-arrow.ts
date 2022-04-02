c92e1f4d0e7c2ad02bfd359e33a705d60c3679ed
Anwesh43 <anweshthecool0@gmail.com>
2022-04-02 13:42:34 +0530

    cross-line-to-arrow: adding constants

---
+++ cross-line-to-arrow.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4 
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const rot : number = Math.PI / 4 
+const deg : number = Math.PI  
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
