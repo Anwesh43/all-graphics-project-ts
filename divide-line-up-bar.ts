343086745f1e330d705e9cddd5d076060e487c0d
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 00:47:27 +0530

    divide-line-up-bar: adding Animator class

--- divide-line-up-bar.ts
+++ divide-line-up-bar.ts
@@ -120,3 +120,22 @@ class State {
     }
 }
 
+class Animated {
+
+    animated : boolean = false 
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    }
+}
