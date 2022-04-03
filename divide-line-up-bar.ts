110a5f4db8d5e71fe19f4eda7d90a66bd9decdf9
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 00:55:46 +0530

    divide-line-up-bar: adding DivideLineUpBar class

--- divide-line-up-bar.ts
+++ divide-line-up-bar.ts
@@ -179,3 +179,26 @@ class DLUBNode {
         return this 
     }
 }
+
+class DivideLineUpBar {
+
+    curr : DLUBNode = new DLUBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+    
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
