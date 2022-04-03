583c1add1a1f158495fac03a289582114db1b9cc
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 00:53:36 +0530

    divide-line-up-bar: adding DLUBNode class

--- divide-line-up-bar.ts
+++ divide-line-up-bar.ts
@@ -139,3 +139,43 @@ class Animated {
         }
     }
 }
+
+class DLUBNode {
+
+    prev : DLUBNode 
+    next : DLUBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        this.next = new DLUBNode(this.i + 1)
+        this.next.prev = this 
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDLUBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DLUBNode {
+        let curr : DLUBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr
+        } 
+        cb()
+        return this 
+    }
+}
