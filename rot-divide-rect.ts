a938085cd19e9916870c41d1e71eb04feaca3876
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 16:22:57 +0530

    rot-divide-rect: adding RotDivideRect class

--- rot-divide-rect.ts
+++ rot-divide-rect.ts
@@ -170,3 +170,26 @@ class RDRNode {
         return this 
     }
 }
+
+class RotDivideRect {
+
+    curr : RDRNode = new RDRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.udpate(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
