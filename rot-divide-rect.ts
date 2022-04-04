448ac69ba26ba6c5603283341e451d28f179aaac
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 16:05:19 +0530

    rot-divide-rect: adding ScaleUtil class

--- rot-divide-rect.ts
+++ rot-divide-rect.ts
@@ -14,3 +14,14 @@ const rot : number = Math.PI / 4
 const deg : number = Math.PI / 2 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+     }
+}
