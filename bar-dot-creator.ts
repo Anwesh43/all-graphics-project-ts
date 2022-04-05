c9769051ac4c303b31704250b0d2be5facaf71bd
Anwesh43 <anweshthecool0@gmail.com>
2022-04-05 18:02:19 +0530

    bar-dot-creator: adding BDCNode class

--- bar-dot-creator.ts
+++ bar-dot-creator.ts
@@ -131,3 +131,45 @@ class Animator {
         }
     }
 }
+
+class BDCNode {
+
+    prev : BDCNode 
+    next : BDCNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BDCNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBDCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BDCNode {
+        var curr : BDCNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
