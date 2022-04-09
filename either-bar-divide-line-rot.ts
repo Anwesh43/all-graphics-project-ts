b460ba43260230c2b1e3c3a34503a02ec84c1cfe
Anwesh43 <anweshthecool0@gmail.com>
2022-04-09 13:53:06 +0530

    either-bar-divide-line-rot: adding constants

---
+++ either-bar-divide-line-rot.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 5 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const barWFactor : number = 5.9 
+const delay : number = 20 
+const rot : number = Math.PI / 2
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
