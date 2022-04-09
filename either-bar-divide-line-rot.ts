30cd5e052512947334a39cbe64d6ad6811694c66
Anwesh43 <anweshthecool0@gmail.com>
2022-04-09 14:01:17 +0530

    either-bar-divide-line-rot: adding DrawingUtil class

--- either-bar-divide-line-rot.ts
+++ either-bar-divide-line-rot.ts
@@ -26,3 +26,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n  
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawEitherBarDivideLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barW : number = size / barWFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + context.lineWidth) * sc5)
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size / 2, -size / 2)
+            context.fillRect(0, 0, barW * sc2, size / 2)
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, 0, 0, 0, -size * sc3)
+        context.restore()
+    }
+
+    static drawEBDLRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawEitherBarDivideLineRot(context, scale)
+    }
+}
