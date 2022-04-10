c96862bb5e02b1ebcfb6f8884aac370cd578c570
Anwesh43 <anweshthecool0@gmail.com>
2022-04-10 13:04:12 +0530

    external-arc-right-angle: adding renderer in stage

--- external-arc-right-angle.ts
+++ external-arc-right-angle.ts
@@ -84,6 +84,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
@@ -95,11 +96,14 @@ class Stage {
     render() {
         this.context.fillStyle = backColor 
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
     
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
