264667f607669ed160de35797f5c0ce618ebb433
Anwesh43 <anweshthecool0@gmail.com>
2022-04-10 12:15:24 +0530

    external-arc-right-angle: adding constantss

---
+++ external-arc-right-angle.ts
@@ -0,0 +1,20 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const deg : number = Math.PI / 2 
+const start : number = Math.PI 
+const sweep : number = 1.5 * Math.PI 
+const sizeFactor : number = 11.9
+const rFactor : number = 5.2 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const strokeFactor : number = 90 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
