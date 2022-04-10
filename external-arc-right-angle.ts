bd8f40023e547405d39773c8cac2a546ef7eea03
Anwesh43 <anweshthecool0@gmail.com>
2022-04-10 12:52:27 +0530

    external-arc-right-angle: adding EARANode class

--- external-arc-right-angle.ts
+++ external-arc-right-angle.ts
@@ -72,7 +72,7 @@ class DrawingUtil {
         context.restore()
     }
 
-    static drawRARANode(context : CanvasRenderingContext2D, i : number, scale : number) {
+    static drawEARANode(context : CanvasRenderingContext2D, i : number, scale : number) {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
@@ -153,4 +153,46 @@ class Animator {
             clearInterval(this.interval)
         }
     } 
+}
+
+class EARANode {
+
+    prev : EARANode 
+    next : EARANode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new EARANode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawEARANode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : EARANode {
+        var curr : EARANode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
 }
