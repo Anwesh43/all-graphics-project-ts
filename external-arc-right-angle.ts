7d7dccf15846822db52d67744ceccebf81f38ec8
Anwesh43 <anweshthecool0@gmail.com>
2022-04-10 12:41:35 +0530

    external-arc-right-angle: adding Animator class

--- external-arc-right-angle.ts
+++ external-arc-right-angle.ts
@@ -134,3 +134,23 @@ class State {
         }
     }
 }
+
+class Animator {
+
+    animated : boolean 
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    } 
+}
