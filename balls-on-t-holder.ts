60cc57a6e533ad70b1bc5e2f8d38a075f9a73af3
Anwesh43 <anweshthecool0@gmail.com>
2022-04-15 12:47:24 +0530

    balls-on-t-holder: adding constants

---
+++ balls-on-t-holder.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+] 
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 11.9 
+const rFactor : number = 7.2 
+const strokeFactor : number = 90 
+const rot : number = Math.PI / 2
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
