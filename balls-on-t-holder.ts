54adc1d5326c9edb80ac59d72292df217f4dcbf9
Anwesh43 <anweshthecool0@gmail.com>
2022-04-15 13:10:13 +0530

    balls-on-t-holder: adding BOTHNode class

--- balls-on-t-holder.ts
+++ balls-on-t-holder.ts
@@ -149,3 +149,46 @@ class Animator {
         }
     }
 }
+
+class BOTHNode {
+
+    prev : BOTHNode 
+    next : BOTHNode 
+
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BOTHNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBOTHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir :  number, cb : Function) : BOTHNode {
+        var curr : BOTHNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
