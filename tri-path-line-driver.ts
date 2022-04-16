13ea43e139cd64e9efb69749b6ea99a184ebd4b7
Anwesh43 <anweshthecool0@gmail.com>
2022-04-16 14:30:37 +0530

    tri-path-line-driver: adding DrawingUtil class

--- tri-path-line-driver.ts
+++ tri-path-line-driver.ts
@@ -13,6 +13,9 @@ const delay : number = 20
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const deg : number = Math.PI / 2
+const angle : number = Math.PI / 4 
+const lSizeFactor : number = 3.9 
 
 class ScaleUtil {
 
@@ -22,5 +25,44 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawTriPathLineDriver(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const lSize : number = Math.min(w, h) / lSizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        context.rotate(deg * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(-size * 0.5 * (1 - 2 * j), 0)
+            context.save()
+            DrawingUtil.drawLine(context, 0, 0, lSize * 0.5 * sc1, -lSize * 0.5 * sc1)
+            context.restore()
+            DrawingUtil.drawLine(context, 0, 0, 0, size * sc2)
+            context.restore()
+        }   
+        context.restore()
+    }
+
+    static drawTPLDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawTriPathLineDriver(context, scale)
     }
 }
