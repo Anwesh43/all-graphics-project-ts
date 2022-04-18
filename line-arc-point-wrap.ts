bff34129b1f334d1f99eaea073d66263b547be2e
Anwesh43 <anweshthecool0@gmail.com>
2022-04-18 16:14:53 +0530

    line-arc-point-wrap: adding LineArcPointWrap class

--- line-arc-point-wrap.ts
+++ line-arc-point-wrap.ts
@@ -196,3 +196,26 @@ class LAPWNode {
         return this 
     }
 }
+
+class LineArcPointWrap {
+
+    curr : LAPWNode = new LAPWNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
