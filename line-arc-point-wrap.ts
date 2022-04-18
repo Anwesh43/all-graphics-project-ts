74ab1651d39e3f8c40272c7ecda88466f5447974
Anwesh43 <anweshthecool0@gmail.com>
2022-04-18 15:08:12 +0530

    line-arc-point-wrap: adding constants

---
+++ line-arc-point-wrap.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 5
+const scGap : number = 0.04 / parts 
+const deg : number = -90 
+const sweep : number = 270 
+const rot : number = Math.PI / 2 
+const delay : number = 20 
+const strokeFactor : number = 90 
+const sizeFactor : number = 5.9 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
