7db8ff325e967689204f18e9e79122fca288fd69
Anwesh43 <anweshthecool0@gmail.com>
2022-04-18 15:34:53 +0530

    line-arc-point-wrap: adding DrawingUtil class

--- line-arc-point-wrap.ts
+++ line-arc-point-wrap.ts
@@ -16,7 +16,7 @@ const sizeFactor : number = 5.9
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
-
+const pointDeg : number = Math.PI / 4
 class ScaleUtil {
 
     static maxScale(scale : number, i : number, n : number) : number {
@@ -25,5 +25,57 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSweepArc(context : CanvasRenderingContext2D, x : number, y : number, r : number, sc : number) {
+        context.beginPath()
+        for (let j = deg; j <= deg + sweep * sc; j++) {
+            const x1 : number = x + r * Math.cos(j * Math.PI / 180)
+            const y1 : number = y + r * Math.sin(j * Math.PI / 180)
+            if (j == deg) {
+                context.moveTo(x1, y1)
+            } else {
+                context.lineTo(x1, y1)
+            }
+        }
+        context.stroke()
+    }
+
+    static drawLineArcPointWrap(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        context.rotate(pointDeg * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * j)
+            if (sc1 > 0) {
+                DrawingUtil.drawLine(context, 0, 0, -size * sc1, 0)
+            }
+            context.restore()
+        }
+        DrawingUtil.drawSweepArc(context, 0, 0, size, sc2)       
+        context.restore()
+    }
+
+    static drawLAPWNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineArcPointWrap(context, scale)
     }
 }
