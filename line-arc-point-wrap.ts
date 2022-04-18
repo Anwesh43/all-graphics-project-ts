33843e66f3eaf93838f3e6e41688bcaa68f18d64
Anwesh43 <anweshthecool0@gmail.com>
2022-04-18 16:01:04 +0530

    line-arc-point-wrap: adding LAPWNode class

--- line-arc-point-wrap.ts
+++ line-arc-point-wrap.ts
@@ -154,3 +154,45 @@ class Animator {
         }
     }
 }
+
+class LAPWNode {
+
+    next : LAPWNode 
+    prev : LAPWNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LAPWNode(this.i + 1)
+            this.next.prev = this            
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLAPWNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LAPWNode  {
+        var curr : LAPWNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
