a4c433078a748fb5cb308890cf78c94a13badf1c
Anwesh43 <anweshthecool0@gmail.com>
2022-04-21 00:15:06 +0530

    side-wise-mid-line: adding DrawingUtil class

--- side-wise-mid-line.ts
+++ side-wise-mid-line.ts
@@ -25,3 +25,47 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSideWiseMidLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+        context.rotate(rot * sc4)
+        if (sc1 > 0) {
+            DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        }
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            if (sc2 > 0) {
+                DrawingUtil.drawLine(context, size / 2, 0, size / 2, size * 0.5 * sc2)
+            }
+            if (sc3 > 0) {
+                DrawingUtil.drawLine(context, size / 2, size / 2, size / 2 - size * sc3, size / 2)
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSWMLNode(context : CanvasRenderingContext2D, i : number,  scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSideWiseMidLine(context, scale)
+    }
+}
