3cf3c4c85102751963023b607011bf247e660526
Anwesh43 <anweshthecool0@gmail.com>
2022-04-26 12:16:19 +0530

    arc-line-direc-traverse: adding constants

---
+++ arc-line-direc-traverse.ts
@@ -0,0 +1,20 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4 
+const scGap : number = 0.03 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.2 
+const lineSizeFactor : number = 23.2 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const deg : number = -90 
+const rot : number = 180
+const rotateDeg : number = 2 * Math.PI 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
