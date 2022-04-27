ee2df12f77e3198264015ac9505ee690b311529f
Anwesh43 <anweshthecool0@gmail.com>
2022-04-28 02:57:37 +0530

    bi-t-with-lint: adding BTWLNode class

--- bi-t-with-lint.ts
+++ bi-t-with-lint.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class BTWLNode {
+
+    prev : BTWLNode 
+    next : BTWLNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BTWLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBTWLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BTWLNode {
+        var curr : BTWLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
