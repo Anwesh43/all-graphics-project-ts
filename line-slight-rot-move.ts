f14d66e4655a5b29fae20f8805e6fc12f11da446
Anwesh43 <anweshthecool0@gmail.com>
2022-05-02 21:07:44 +0530

    line-slight-rot-move: adding ScaleUtil class

--- line-slight-rot-move.ts
+++ line-slight-rot-move.ts
@@ -15,4 +15,13 @@ const h : number = window.innerHeight
 const strokeFactor : number = 90 
 const sizeFactor : number = 11.9 
 
+class ScaleUtil {
 
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i: number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
