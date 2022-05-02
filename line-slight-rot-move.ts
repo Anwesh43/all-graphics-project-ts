d9f3ec439e3c92f8764c8c9b3c4cc2934096ed75
Anwesh43 <anweshthecool0@gmail.com>
2022-05-02 21:31:51 +0530

    line-slight-rot-move: adding ScaleUtil class

--- line-slight-rot-move.ts
+++ line-slight-rot-move.ts
@@ -9,7 +9,7 @@ const parts : number = 4
 const scGap : number = 0.04 / parts 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2
+const rot : number = Math.PI / 4
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const strokeFactor : number = 90 
@@ -23,5 +23,37 @@ class ScaleUtil {
 
     static divideScale(scale : number, i: number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineeSlightRotMove(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * (1 - 2 * j) * ScaleUtil.divideScale(scale, j + 1, parts))
+            DrawingUtil.drawLine(context, 0, 0, 0, size * 0.5 * (sc1 - sc3) * (1 - 2 * j))   
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLSRMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawLineeSlightRotMove(context, scale)
     }
 }
