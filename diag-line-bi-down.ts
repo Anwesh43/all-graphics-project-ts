d2010930c46e53c170ba58d46eecbba5708a59b3
Anwesh43 <anweshthecool0@gmail.com>
2022-05-03 11:07:51 +0530

    diag-line-bi-down: adding DrawingUtil class

--- diag-line-bi-down.ts
+++ diag-line-bi-down.ts
@@ -14,6 +14,7 @@ const rot : number = Math.PI / 4
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const deg : number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -23,5 +24,39 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDiagLineBiDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 - (w /2 + size) * sc4, h / 2 + (h / 2 + size) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(
+                -deg * j - rot * (1 - 2 * j) * ScaleUtil.divideScale(scale, j + 1, parts)
+            )
+            DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawDLBDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDiagLineBiDown(context, scale)
     }
 }
