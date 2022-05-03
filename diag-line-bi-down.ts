dfdfd4ff09ef22081123aac8c28fe17096856594
Anwesh43 <anweshthecool0@gmail.com>
2022-05-03 11:15:43 +0530

    diag-line-bi-down: adding DLBDNode class

--- diag-line-bi-down.ts
+++ diag-line-bi-down.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class DLBDNode {
+
+    prev : DLBDNode 
+    next : DLBDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DLBDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDLBDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DLBDNode {
+        var curr : DLBDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
