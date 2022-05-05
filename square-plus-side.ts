23c28e5f6f9fff1a3179375e9d13b2491baf75aa
Anwesh43 <anweshthecool0@gmail.com>
2022-05-05 09:53:12 +0530

    square-plus-side: adding DrawingUtil class

--- square-plus-side.ts
+++ square-plus-side.ts
@@ -25,3 +25,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSquarePlusSide(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 , h / 2 + (size * 1.5) * sc4)
+        context.rotate(rot  * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * j)
+            DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+            context.restore()
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size, 0)
+            context.fillRect(-size * 0.5 * sc2, -size * 0.5 * sc2, size * sc2, size * sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSPSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSquarePlusSide(context, scale)
+    } 
+}
