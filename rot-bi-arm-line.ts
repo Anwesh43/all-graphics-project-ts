346d2ed841eccfc390000898c6d729aefe287c3e
Anwesh43 <anweshthecool0@gmail.com>
2022-05-06 07:38:27 +0530

    rot-bi-arm-line: adding RBALNode class

--- rot-bi-arm-line.ts
+++ rot-bi-arm-line.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class RBALNode {
+
+    prev : RBALNode 
+    next : RBALNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RBALNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRBALNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RBALNode {
+        var curr : RBALNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
