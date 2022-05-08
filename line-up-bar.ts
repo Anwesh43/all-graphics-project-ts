24b6bdf4e2347ced18e0e6e766d3577612e1dd41
Anwesh43 <anweshthecool0@gmail.com>
2022-05-08 11:29:10 +0530

    line-up-bar: adding DrawingUtil class

--- line-up-bar.ts
+++ line-up-bar.ts
@@ -25,3 +25,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineUpBar(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / barFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        for (let j = 0; j < 2; j++) {
+            const y : number = barSize * ScaleUtil.divideScale(scale, j + 1, parts)
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size / 2, 0)
+            context.fillRect(0, -y, barSize, y)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLUBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w,  h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineUpBar(context, scale)
+    }
+}
