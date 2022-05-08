814e773e0e41ab034776ec466711a949dc06b40d
Anwesh43 <anweshthecool0@gmail.com>
2022-05-08 11:06:28 +0530

    line-up-bar: adding constants

---
+++ line-up-bar.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 5 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 8.9 
+const barFactor : number = 21.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
