4b4c8156901672db468615431c744ba50d72b77d
Anwesh43 <anweshthecool0@gmail.com>
2022-05-10 10:00:55 +0530

    side-alt-bar-block: addding DrawingUtil class

--- side-alt-bar-block.ts
+++ side-alt-bar-block.ts
@@ -25,3 +25,30 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawSideAltBarBlock(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = size / blockSizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+        context.rotate(rot * sc4)
+        context.fillRect(-size * 0.5 * sc1, 0, size * sc1, size / 2)
+        for (let j = 0;  j < 2; j++) {
+            context.save()
+            context.translate(-size / 2 + (size - barSize) * j, -(size / 2 - barSize) * ScaleUtil.divideScale(scale, j + 1, parts))
+            context.fillRect(0, 0, barSize * Math.floor(sc1), barSize * Math.floor(sc1))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSABBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSideAltBarBlock(context, scale)
+    }
+}
