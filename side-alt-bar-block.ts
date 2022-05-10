3ff5c46cd73f74eacb602f89da0c0b0b19d37031
Anwesh43 <anweshthecool0@gmail.com>
2022-05-10 09:49:35 +0530

    side-alt-bar-block: addding ScaleUtil class

--- side-alt-bar-block.ts
+++ side-alt-bar-block.ts
@@ -14,3 +14,14 @@ const blockSizeFactor : number = 3.9
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const rot : number = Math.PI / 2 
+
+class ScaleUtil {
+
+    static maxScale(scale :  number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
