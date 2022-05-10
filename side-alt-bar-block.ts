17054b10badd55929f9095f1c5e2da77ceb231d7
Anwesh43 <anweshthecool0@gmail.com>
2022-05-10 10:19:06 +0530

    side-alt-bar-block: addding SideAltBarBlock class

--- side-alt-bar-block.ts
+++ side-alt-bar-block.ts
@@ -170,3 +170,26 @@ class SABBNode {
         return this 
     }
 }
+
+class SideAltBarBlock {
+
+    curr : SABBNode = new SABBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
