36b935033c782956ba5aa42ed748f6c8bac54e08
Anwesh43 <anweshthecool0@gmail.com>
2022-05-14 10:26:28 +0530

    divide-line-t-rot: adding DrawingUtil class

--- divide-line-t-rot.ts
+++ divide-line-t-rot.ts
@@ -25,3 +25,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDivideLineToRot(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc11 : number = ScaleUtil.divideScale(sc1, 0, parts)
+        const sc12 : number = ScaleUtil.divideScale(sc1, 1, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, -size, 0, -size + size * sc11, 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * (1 - 2 * j) * sc2)
+            DrawingUtil.drawLine(context, 0, 0, size * sc12, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawDLTRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDivideLineToRot(context, scale)
+    }
+}
