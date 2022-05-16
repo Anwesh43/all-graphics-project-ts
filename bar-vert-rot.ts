b615346e73c70f6b3f242250dc3938c8d66a2b99
Anwesh43 <anweshthecool0@gmail.com>
2022-05-17 04:35:04 +0530

    bar-vert-rot: adding ScaleUtil class

--- bar-vert-rot.ts
+++ bar-vert-rot.ts
@@ -13,3 +13,14 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight 
 const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 2
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
