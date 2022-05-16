e08348efe6d6d0176a9f8ba727258c63ee5d132d
Anwesh43 <anweshthecool0@gmail.com>
2022-05-17 04:42:54 +0530

    bar-vert-rot: adding DrawingUtil class

--- bar-vert-rot.ts
+++ bar-vert-rot.ts
@@ -24,3 +24,30 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawBarVertRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(rot * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.translate(0, size / 2 - size * 0.5 * sc2)
+            context.fillRect(-size / 2, -size * sc1, size / 2 , size * sc1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBVRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarVertRot(context, scale)
+    }
+}
