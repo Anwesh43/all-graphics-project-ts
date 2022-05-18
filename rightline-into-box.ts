e4c72ab2e5f2623972282ead8b215835b4e65626
Anwesh43 <anweshthecool0@gmail.com>
2022-05-18 18:36:20 +0530

    rightline-into-box: adding DrawingUtil class

--- rightline-into-box.ts
+++ rightline-into-box.ts
@@ -25,3 +25,36 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRightLineIntoBox(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size / 2) * sc4, h / 2)
+        context.save()
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+        context.restore()
+        context.fillRect(0, -size, size * sc1, size)
+        context.restore()
+    }
+
+    static drawRLIBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = backColor 
+        DrawingUtil.drawRightLineIntoBox(context, scale)
+    }
+}
