722d713ec81ba1a1f203605168fa1c48789b331f
Anwesh43 <anweshthecool0@gmail.com>
2022-05-18 18:15:03 +0530

    rightline-into-box: adding constants

---
+++ rightline-into-box.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const rot : number = Math.PI / 2
