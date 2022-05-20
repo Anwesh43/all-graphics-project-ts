1a44bb1613107edea688660acc6a4bec8946a8fa
Anwesh43 <anweshthecool0@gmail.com>
2022-05-21 01:35:59 +0530

    four-grid-down: adding DrawingUtil class

--- four-grid-down.ts
+++ four-grid-down.ts
@@ -26,3 +26,42 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawFourGridDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * sc2)
+            for (let i = 0; i < lines; i++) {
+                context.save()
+                context.translate(0, size * 0.5 * (1 - i) * sc3)
+                DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawFGDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawFourGridDown(context, scale)
+    }
+}
