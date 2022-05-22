1e407219cf957725ce14ab267178029e0deba9ed
Anwesh43 <anweshthecool0@gmail.com>
2022-05-22 20:24:13 +0530

    adding Stage and Renderer class

--- line-alt-block-join.ts
+++ line-alt-block-join.ts
@@ -10,8 +10,8 @@ const scGap : number = 0.04 / parts
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const strokeFactor : number = 90 
-const sizeFactor : number = 11.9 
-const barSizeFactor : number = 26.9 
+const sizeFactor : number = 6.9  
+const barSizeFactor : number = 16.9 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 
@@ -49,7 +49,7 @@ class DrawingUtil {
         for (let j = 0; j < 2; j++) {
             context.save()
             context.scale(1 - 2 * j, 1 - 2 * j)
-            context.translate((size / 2 - barSize) * (1 - sc3), 0)
+            context.translate((size / 2 - barSize) * (1 - sc3) - barSize * 0.5 * sc3, 0)
             context.fillRect(0, -barSize * sc2 * 0.5, barSize, barSize * 0.5 * sc2)
             context.restore()
         }
@@ -62,5 +62,171 @@ class DrawingUtil {
         context.fillStyle = colors[i]
         context.strokeStyle = colors[i]
         DrawingUtil.drawLineAltBlockJoin(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+    renderer : Renderer = new Renderer()
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            this.renderer.handleTap(() => {
+                this.render()
+            })
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0 
+    prevScale : number = 0 
+
+    update(cb : Function) {
+        this.scale += scGap * this.dir 
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir 
+            this.dir = 0
+            this.prevScale = this.scale 
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        } 
+    }
+}
+
+class Animator {
+
+    animated : boolean = false 
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (!this.animated) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    }
+}
+
+class LABJNode {
+
+    prev : LABJNode 
+    next : LABJNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LABJNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLABJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : LABJNode {
+        var curr : LABJNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
+
+class LineAltBlockJoin {
+
+    curr : LABJNode = new LABJNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
+
+class Renderer {
+
+    labj : LineAltBlockJoin = new LineAltBlockJoin()
+    animator : Animator = new Animator()
+
+    render(context : CanvasRenderingContext2D) {
+        this.labj.draw(context)
+    }
+
+    handleTap(cb : Function) {
+        this.labj.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.labj.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
     }
 }
