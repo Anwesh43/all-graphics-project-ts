787c55c9db48d65434794420405296fa8c3ac141
Anwesh43 <anweshthecool0@gmail.com>
2022-05-23 08:55:03 +0530

    perp-line-down-rot: adding PerpLineDownRot class

--- perp-line-down-rot.ts
+++ perp-line-down-rot.ts
@@ -173,3 +173,26 @@ class PLDRNode {
         return this 
     }
 }
+
+class PerpLineDownRot {
+
+    curr : PLDRNode = new PLDRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
