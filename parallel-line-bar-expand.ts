e609077de2624a295da30ef09364b2e32bbd011c
Anwesh43 <anweshthecool0@gmail.com>
2022-05-24 07:50:14 +0530

    parallel-line-bar-expand: adding PLBENode class

--- parallel-line-bar-expand.ts
+++ parallel-line-bar-expand.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class PLBENode {
+
+    prev : PLBENode 
+    next : PLBENode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PLBENode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPLBENode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : PLBENode  {
+        let curr : PLBENode = this.prev 
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
