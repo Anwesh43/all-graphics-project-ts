2dbc9e1b4f4eee684cf772c87ae0ec73c08975f4
Anwesh43 <anweshthecool0@gmail.com>
2022-05-24 07:34:05 +0530

    parallel-line-bar-expand: adding DrawingUtil class

--- parallel-line-bar-expand.ts
+++ parallel-line-bar-expand.ts
@@ -26,3 +26,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawParallelLineBarExpand(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const gap : number = size * 0.1 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-gap - (h / 2) * sc4, 0)
+            DrawingUtil.drawLine(context, 0, -size * 0.5 * sc1, 0, size * 0.5 * sc1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawPLBENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawParallelLineBarExpand(context, scale)
+    }
+}
