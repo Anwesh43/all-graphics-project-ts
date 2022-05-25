e9a8e6b26f497083ca2cbad35c409b18db07cd0f
Anwesh43 <anweshthecool0@gmail.com>
2022-05-25 09:18:44 +0530

    parallel-pipe-rot-down: adding fixes

--- parallel-pipe-rot-down.ts
+++ parallel-pipe-rot-down.ts
@@ -40,6 +40,7 @@ class DrawingUtil {
         const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
         const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
         const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        console.log("Sc1, sc2, sc3, sc4", sc1, sc2, sc3, sc4)
         const size : number = Math.min(w, h) / sizeFactor 
         context.save()
         context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
