18e0873fe79f3d738c87b8e3af546a732e25531e
Anwesh43 <anweshthecool0@gmail.com>
2022-05-25 09:04:18 +0530

    parallel-pipe-rot-down: adding PPRDNode class

--- parallel-pipe-rot-down.ts
+++ parallel-pipe-rot-down.ts
@@ -136,3 +136,45 @@ class Animator {
         }
     }
 }
+
+class PPRDNode {
+
+    prev : PPRDNode 
+    next : PPRDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PPRDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPPRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : PPRDNode {
+        var curr : PPRDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
