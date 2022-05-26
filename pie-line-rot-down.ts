65794a23f782376b4112f968240ee1d1dbd6a84e
Anwesh43 <anweshthecool0@gmail.com>
2022-05-27 01:36:57 +0530

    pie-line-rot-down: adding PLRDNode class

--- pie-line-rot-down.ts
+++ pie-line-rot-down.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class PLRDNode {
+
+    state : State = new State()
+    prev : PLRDNode 
+    next : PLRDNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PLRDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPLRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : PLRDNode {
+        var curr : PLRDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
