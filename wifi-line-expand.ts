4324cbd81281d2842934190903092612bac157b5
Anwesh43 <anweshthecool0@gmail.com>
2022-05-26 08:29:07 +0530

    wifi-line-expand: adding ScaleUtil class

--- wifi-line-expand.ts
+++ wifi-line-expand.ts
@@ -14,3 +14,14 @@ const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const rot : number = Math.PI / 2 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n , ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
