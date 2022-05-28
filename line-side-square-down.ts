be36e17218ab95a37e6023f554b678ab5ef1b31f
Anwesh43 <anweshthecool0@gmail.com>
2022-05-28 08:58:05 +0530

    line-side-square-down: adding LineSideSquareDown class

--- line-side-square-down.ts
+++ line-side-square-down.ts
@@ -176,3 +176,26 @@ class LSSDNode {
         return this 
     }
 }
+
+class LineSideSquareDown {
+
+    curr : LSSDNode = new LSSDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
