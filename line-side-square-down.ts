31c4575318b13c0f5477baf69c7ab14842357871
Anwesh43 <anweshthecool0@gmail.com>
2022-05-28 08:38:01 +0530

    line-side-square-down: adding DrawingUtil class

--- line-side-square-down.ts
+++ line-side-square-down.ts
@@ -25,3 +25,37 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n  
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSideSquareDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+        context.rotate(rot * sc4)
+        DrawingUtil.drawLine(context, -size / 2, 0, -size / 2 + size * sc1, 0)
+        DrawingUtil.drawLine(context, size / 2, 0, size / 2, - size * 0.25 * sc2)
+        context.fillRect(size / 2 - size * 0.25 * sc3, -size * 0.25, size * 0.25 * sc3, size * 0.25)
+        context.restore()
+    }
+
+    static drawLSSDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineSideSquareDown(context, scale)
+    }
+}
