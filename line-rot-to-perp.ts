89d392df920e090c0354e8822926ad0d8cc2c8f1
Anwesh43 <anweshthecool0@gmail.com>
2022-05-29 09:36:50 +0530

    line-rot-to-perp: adding Stage class

--- line-rot-to-perp.ts
+++ line-rot-to-perp.ts
@@ -14,7 +14,7 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight 
 const delay : number = 20 
 const deg : number = Math.PI / 2 
-
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -62,3 +62,35 @@ class DrawingUtil {
         DrawingUtil.drawLineRotToPerp(context, scale)
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
+
