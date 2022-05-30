cd59c94a4f8547c0b84af349cd1fdabca1ee2ee5
Anwesh43 <anweshthecool0@gmail.com>
2022-05-30 10:13:11 +0530

    line-divide-rot-down: adding renderer in stage

--- line-divide-rot-down.ts
+++ line-divide-rot-down.ts
@@ -65,7 +65,8 @@ class DrawingUtil {
 class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
-    context : CanvasRenderingContext2D 
+    context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer() 
 
     initCanvas() {
         this.canvas.width = w 
@@ -77,11 +78,14 @@ class Stage {
     render() {
         this.context.fillStyle = backColor 
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
-        this.canvas.onmousedown = () => {
-
+        this.canvas.onmousedown = () => {   
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
