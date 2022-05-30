db94ee9f1395784073a6ed888e109ab2c50a2b20
Anwesh43 <anweshthecool0@gmail.com>
2022-05-30 10:16:54 +0530

    line-divide-rot-down: adding fixes 3

--- line-divide-rot-down.ts
+++ line-divide-rot-down.ts
@@ -44,7 +44,7 @@ class DrawingUtil {
         const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
         context.save()
         context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
-        context.rotate((deg - rot / 2) * sc3)
+        context.rotate(-(deg - rot / 2) * sc3)
         for (let j = 0; j < 2; j++) {
             context.save()
             context.rotate(-rot * j * sc2)
