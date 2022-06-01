1964bf4b4f60e965810d592fab6ea84f53087775
Anwesh43 <anweshthecool0@gmail.com>
2022-06-01 09:28:50 +0530

    bi-line-shift-rot-right: adding ScaleUtill class

--- bi-line-shift-rot-right.ts
+++ bi-line-shift-rot-right.ts
@@ -14,3 +14,14 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight 
 const rot : number = Math.PI / 4 
 const backColor : string = "#BDBDBD"
+
+class ScaleUtil {
+    
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
