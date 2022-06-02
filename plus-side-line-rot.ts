2b163f71f8c74d2d1274cc23009a5070220c3e06
Anwesh43 <anweshthecool0@gmail.com>
2022-06-02 08:27:42 +0530

    plus-side-line-rot: adding constants

---
+++ plus-side-line-rot.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 8.9 
+const lineSizeFactor : number = 14.9 
+const delay : number = 20 
+const deg : number = Math.PI / 2
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
