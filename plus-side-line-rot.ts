189569e173ecbb52e1eea58a143c62f218b7f767
Anwesh43 <anweshthecool0@gmail.com>
2022-06-02 08:37:15 +0530

    plus-side-line-rot: adding ScaleUtil class

--- plus-side-line-rot.ts
+++ plus-side-line-rot.ts
@@ -26,3 +26,52 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawPlusLine(context : CanvasRenderingContext2D, size : number, i : number, sc : number) {
+        context.save()
+        context.rotate(deg * i)
+        DrawingUtil.drawLine(context, 0, -size * 0.5 * sc, 0, size * 0.5 * sc)
+        context.restore()
+    }
+
+    static drawSideLine(context : CanvasRenderingContext2D, size : number, i : number, sc : number) {
+        context.save()
+        context.scale(1 - 2 * i, 1)
+        context.translate(-size / 2, 0)
+        DrawingUtil.drawLine(context, 0, 0, 0, size * sc)
+        context.restore()
+    }
+
+    static drawPlusSideLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const lineSize : number = Math.min(w, h) / lineSizeFactor 
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(deg * sc3)
+        for (let j = 0; j < 2; j++) {
+            DrawingUtil.drawPlusLine(context, size, j, sc1)
+            DrawingUtil.drawSideLine(context, lineSize, j, sc1)
+        }
+        context.restore()
+    }
+
+    static drawPSLRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPlusSideLineRot(context, scale)
+    }
+}
