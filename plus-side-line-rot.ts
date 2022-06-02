a4b1749cba1e3a85dd55504db799d727fe83888f
Anwesh43 <anweshthecool0@gmail.com>
2022-06-02 08:48:12 +0530

    plus-side-line-rot: adding PSLRNode class

--- plus-side-line-rot.ts
+++ plus-side-line-rot.ts
@@ -150,3 +150,45 @@ class Animator {
         }
     }
 }
+
+class PSLRNode {
+
+    prev : PSLRNode 
+    next : PSLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PSLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPSLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : PSLRNode {
+        var curr : PSLRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
