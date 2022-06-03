f8b3fbc40df21d2fc81289e575d02fb280df2573
Anwesh43 <anweshthecool0@gmail.com>
2022-06-03 11:45:28 +0530

    line-join-sq-extend: adding DrawingUtil class

--- line-join-sq-extend.ts
+++ line-join-sq-extend.ts
@@ -25,3 +25,42 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i,n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineJoinSqExtend(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size / 2) * sc4, h / 2)
+        context.rotate(rot * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            DrawingUtil.drawLine(context, 0, 0, size * 0.5 * (1 - 2 * j) * sc2, -size * 0.5 * sc2)
+            DrawingUtil.drawLine(context, 0, -size / 2, -size * (1 - 2 * j) * 0.5 * sc1, -size / 2)
+            context.save()
+            context.translate(-size * 0.5 * (1 - 2 * j), -size / 2)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * 0.25 * sc2)
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLJSENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineJoinSqExtend(context, scale)       
+    }
+}
