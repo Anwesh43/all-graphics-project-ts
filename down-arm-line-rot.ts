3a534df775cfad353281950d68342c2b2278f258
Anwesh43 <anweshthecool0@gmail.com>
2022-06-05 16:33:56 +0530

    down-arm-line-rot: adding DownArmLineRot class

--- down-arm-line-rot.ts
+++ down-arm-line-rot.ts
@@ -179,3 +179,26 @@ class DALRNode {
         return this 
     }
 }
+
+class DownArmLineRot {
+
+    curr : DALRNode = new DALRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
