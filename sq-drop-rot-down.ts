3d648b56a96092aa736fe84a412aea3c9d9b310b
Anwesh43 <anweshthecool0@gmail.com>
2022-06-06 14:28:46 +0530

    sq-drop-rot-down: adding SDRDNode class

--- sq-drop-rot-down.ts
+++ sq-drop-rot-down.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class SDRDNode {
+
+    prev : SDRDNode 
+    next : SDRDNode 
+    state : State = new State()
+
+    constructor(private  i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SDRDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSDRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SDRDNode {
+        var curr : SDRDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
