b0c411cd486a1d44cf43d2df354c2d261a1940f7
Anwesh43 <anweshthecool0@gmail.com>
2022-06-06 13:50:31 +0530

    sq-drop-rot-down: adding DrawingUtil class

--- sq-drop-rot-down.ts
+++ sq-drop-rot-down.ts
@@ -26,3 +26,36 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSqDropRotDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / sqSizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sc2)
+        context.translate(-(w / 2 + size / 2) * (1 - sc1), (h / 2) * sc4)
+        DrawingUtil.drawLine(context, -size * 0.5 * (1 - sc3), 0, size * 0.5 * (1 - sc3), 0)
+        context.fillRect(-barSize / 2, -barSize, barSize, barSize)
+        context.restore()
+    }
+
+    static drawSDRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSqDropRotDown(context, scale)
+    }
+}
