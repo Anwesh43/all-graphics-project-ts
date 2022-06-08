c1968c76c94d49dcde0d1cd3a542c0ea97e46026
Anwesh43 <anweshthecool0@gmail.com>
2022-06-08 08:56:07 +0530

    block-gap-line-rot: adding DtawingUtil class

--- block-gap-line-rot.ts
+++ block-gap-line-rot.ts
@@ -26,3 +26,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBlockGapLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const gap : number = size / gapFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 :  number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.save()
+        context.translate((-w / 2 - size / 2) * (1 - sc1), 0)
+        context.fillRect(-size / 2, -size / 2, size, size)
+        context.restore()
+        context.save()
+        context.translate((-w / 2 - size / 2) * (1 - sc2), -size / 2 - gap)
+        DrawingUtil.drawLine(context, -size / 2, 0, size / 2, 0)
+        context.restore()
+        context.restore()
+    }
+
+    static drawBGLRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBlockGapLineRot(context, scale)
+    }
+}
