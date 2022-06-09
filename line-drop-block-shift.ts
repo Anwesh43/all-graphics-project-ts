a332cb14b17ab8280a1ee519a40967ccffcd9023
Anwesh43 <anweshthecool0@gmail.com>
2022-06-10 00:30:17 +0530

    line-drop-block-shift: adding DrawingUtil class

--- line-drop-block-shift.ts
+++ line-drop-block-shift.ts
@@ -25,3 +25,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+    
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineDropBlockShift(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sc3)
+        for (let i = 0; i < 2; i++) {
+            context.save()
+            context.scale(1 - 2 * i, 1)
+            context.translate((h / 2) * sc4, -h /2 + (h / 2) * sc1)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            context.fillRect(-size * sc2, -size, size, size * sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLDBSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineDropBlockShift(context, scale)
+    }
+}
