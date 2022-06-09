bca19b0ed27bd1985b38dde045d131e02cb06f9e
Anwesh43 <anweshthecool0@gmail.com>
2022-06-09 12:02:56 +0530

    ball-catch-then-rot-drop: adding constants

---
+++ ball-catch-then-rot-drop.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 6
+const scGap : number = 0.05 / parts  
+const deg : number = Math.PI 
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
