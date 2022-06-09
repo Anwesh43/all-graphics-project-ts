b76c4f12d42f4157e7da4992fa5088674b4643bc
Anwesh43 <anweshthecool0@gmail.com>
2022-06-09 13:10:02 +0530

    ball-catch-then-rot-drop: adding BCTRDNode class

--- ball-catch-then-rot-drop.ts
+++ ball-catch-then-rot-drop.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class BCTRDNode {
+
+    prev : BCTRDNode 
+    next : BCTRDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BCTRDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBCTRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BCTRDNode {
+        var curr : BCTRDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
