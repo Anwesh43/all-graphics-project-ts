98ea6ba01038f73514ba280580765f3a73b51a08
Anwesh43 <anweshthecool0@gmail.com>
2022-06-11 10:28:59 +0530

    line-divide-box-rot-down: fix for fillStyle

--- line-divide-box-rot-down.ts
+++ line-divide-box-rot-down.ts
@@ -61,6 +61,7 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
         DrawingUtil.drawLineDivideBoxRotDown(context, scale)
     }
 }
