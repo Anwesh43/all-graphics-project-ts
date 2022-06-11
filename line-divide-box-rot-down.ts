9035176c23eff5dc1764e83164f1c6a7e389edba
Anwesh43 <anweshthecool0@gmail.com>
2022-06-11 10:04:38 +0530

    line-divide-box-rot-down: adding DrawingUtil class

--- line-divide-box-rot-down.ts
+++ line-divide-box-rot-down.ts
@@ -26,3 +26,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineDivideBoxRotDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+        context.rotate(deg * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * sc2 * (1 - 2 * j))
+            DrawingUtil.drawLine(context, 0, 0, -size * sc1, 0)
+            context.restore()
+        }
+        context.fillRect(0, -size / 2, size * sc2, size)
+        context.restore()
+    }
+
+    static drawLDBRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineDivideBoxRotDown(context, scale)
+    }
+}
