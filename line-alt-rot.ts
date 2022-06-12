f2a6cfa25f5ccbde5c171c7d02180a2063cb11da
Anwesh43 <anweshthecool0@gmail.com>
2022-06-12 09:32:34 +0530

    line-alt-rot: adding DrawingUtil class

--- line-alt-rot.ts
+++ line-alt-rot.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.max(0, scale - i / n)
     } 
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineAltRot(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 - (w / 2) * sc4, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(Math.PI / 2 * (1 + j) * (1 - 2 * j) * sc2)
+            DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+            context.restore()
+        }
+        context.fillRect(0, -size * sc3, size * sc3, size)
+        context.restore()
+    }
+
+    static drawLARNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineAltRot(context, scale)
+    }
+}
