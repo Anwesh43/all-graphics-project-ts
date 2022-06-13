f532a62affb6f78221e0796fa82fb8821fc9623f
Anwesh43 <anweshthecool0@gmail.com>
2022-06-13 10:37:10 +0530

    block-cross-container: adding BlockCrossContainer class

--- block-cross-container.ts
+++ block-cross-container.ts
@@ -183,3 +183,26 @@ class BCCNode {
         return this 
     }
 }
+
+class BlockCrossContainer {
+
+    curr : BCCNode = new BCCNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
