952336167a51f03784d680df9d233129622d5224
Anwesh43 <anweshthecool0@gmail.com>
2022-06-14 08:44:42 +0530

    sq-line-alt-mover: adding DrawingUtil class

--- sq-line-alt-mover.ts
+++ sq-line-alt-mover.ts
@@ -26,3 +26,42 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSqLineAltMover(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sq : number = Math.min(w, h) / sqFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.translate(-size / 2 + (h / 2 + size) * sc4, size / 4)
+            DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+            context.fillRect(0, 0, sq, sq * sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSLAMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSqLineAltMover(context, scale)
+    }
+}
