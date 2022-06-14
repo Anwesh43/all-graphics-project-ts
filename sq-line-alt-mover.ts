1be4033db75a46df222b18706dfcb25ee807cd47
Anwesh43 <anweshthecool0@gmail.com>
2022-06-14 09:04:16 +0530

    sq-line-alt-mover: adding SLAMNode class

--- sq-line-alt-mover.ts
+++ sq-line-alt-mover.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class SLAMNode {
+
+    prev : SLAMNode 
+    next : SLAMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLAMNode(this.i + 1)
+            this.next.prev = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLAMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SLAMNode {
+        var curr : SLAMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
