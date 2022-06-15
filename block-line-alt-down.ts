0fe8988206d1b92519544e694242758ae661312e
Anwesh43 <anweshthecool0@gmail.com>
2022-06-15 09:53:26 +0530

    block-line-alt-down: adding BLADNode class

--- block-line-alt-down.ts
+++ block-line-alt-down.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class BLADNode {
+
+    next : BLADNode 
+    prev : BLADNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLADNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLADNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BLADNode {
+        var curr : BLADNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
