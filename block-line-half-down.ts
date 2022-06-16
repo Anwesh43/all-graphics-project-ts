dfcc83334dd822963d180757e356321b87bdf3fc
Anwesh43 <anweshthecool0@gmail.com>
2022-06-16 14:09:39 +0530

    block-line-half-down: adding DrawingUtil class

--- block-line-half-down.ts
+++ block-line-half-down.ts
@@ -26,3 +26,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBlockLineHalfDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const lSize : number = Math.min(w, h) / lSizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        context.rotate(deg * sc3)
+        DrawingUtil.drawLine(context, 0, -lSize * 0.5 * sc1, 0, lSize * 0.5 * sc1)
+        context.save()
+        context.translate(-size - w / 2 + (w / 2) * sc1, -size / 2)
+        context.fillRect(0, 0, size, size)
+        context.restore()
+        context.restore()
+    }
+
+    static drawBLHDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBlockLineHalfDown(context, scale)
+    }
+}
