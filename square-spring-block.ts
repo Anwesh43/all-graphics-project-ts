6aad3d6e06a741f6de67596cab0ebe1e26c5998d
Anwesh43 <anweshthecool0@gmail.com>
2022-06-18 14:31:47 +0530

    square-spring-block: adding SSBNode class

--- square-spring-block.ts
+++ square-spring-block.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class SSBNode {
+
+    prev : SSBNode 
+    next : SSBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SSBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSSBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SSBNode {
+        var curr : SSBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
