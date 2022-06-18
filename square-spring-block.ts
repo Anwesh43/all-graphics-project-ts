e4cc51d1ec5828ec74e805f7c8338672539f5e16
Anwesh43 <anweshthecool0@gmail.com>
2022-06-18 14:09:10 +0530

    square-spring-block: adding constants

---
+++ square-spring-block.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#03A9F4",
+    "#FF9800",
+    "#8BC34A",
+    "#795548"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 6.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
