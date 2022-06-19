c469ec21a37ca8067249098e3ab588cf35940bab
Anwesh43 <anweshthecool0@gmail.com>
2022-06-20 01:55:38 +0530

    cross-line-join-down: adding DrawingUtil class

--- cross-line-join-down.ts
+++ cross-line-join-down.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCrossLineJoinDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        context.rotate(deg * sc3)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            DrawingUtil.drawLine(context, -size * 0.5 * sc1, size * 0.5 * sc1, size * 0.5 * sc1, -size * 0.5 * sc1)
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, size / 2, -size * 0.5 * sc2, size / 2, size * 0.5 * sc2)
+        context.restore()
+    }
+
+    static drawCLJDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawCLJDNode(context, i, scale)
+    }
+}
