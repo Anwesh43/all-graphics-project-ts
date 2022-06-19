6296881c44fd5dd7eac62cc67b351ca918756268
Anwesh43 <anweshthecool0@gmail.com>
2022-06-20 02:30:30 +0530

    cross-line-join-down: adding fixes 4

--- cross-line-join-down.ts
+++ cross-line-join-down.ts
@@ -47,7 +47,7 @@ class DrawingUtil {
         context.rotate(deg * sc3)
         for (let j = 0; j < 2; j++) {
             context.save()
-            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.scale(1 - 2 * j, 1)
             DrawingUtil.drawLine(context, -size * 0.5 * sc1, size * 0.5 * sc1, size * 0.5 * sc1, -size * 0.5 * sc1)
             context.restore()
         }
