b5b5d8f93c3b7d94816189d73113af41a9cc8ac9
Anwesh43 <anweshthecool0@gmail.com>
2022-06-21 09:57:21 +0530

    block-break-line-down: adding BBLDNode class

--- block-break-line-down.ts
+++ block-break-line-down.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class BBLDNode {
+
+    prev : BBLDNode 
+    next : BBLDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BBLDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBBLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BBLDNode {
+        var curr : BBLDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
