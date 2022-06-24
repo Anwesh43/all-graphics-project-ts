c642cb16c824cecbe4e5f74345232ffaef7e939a
Anwesh43 <anweshthecool0@gmail.com>
2022-06-24 09:27:40 +0530

    line-break-square-down: adding constants

---
+++ line-break-square-down.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 5.9 
+const rot : number = Math.PI / 2
+const delay : number = 20 
+const BACKcoLOR : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
