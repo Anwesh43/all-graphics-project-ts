5d3eaa2eed0b533c79ddd13c5894f838fec7a9c9
Anwesh43 <anweshthecool0@gmail.com>
2022-06-24 09:41:26 +0530

    line-break-square-down: adding DrawingUtil class

--- line-break-square-down.ts
+++ line-break-square-down.ts
@@ -14,6 +14,7 @@ const delay : number = 20
 const BACKcoLOR : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const barSizeFactor : number = 9.2 
 
 class ScaleUtil {
 
@@ -23,5 +24,42 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBreakSquareDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor
+        const barSize : number = Math.min(w, h) / barSizeFactor  
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.rotate(rot * sc3)
+            DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+            context.fillRect((size - barSize), 0, barSize, barSize * sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLBSDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineBreakSquareDown(context, scale)
     }
 }
