48f2d8f6cb04a76db83dbf2cbbcf466f6b75d828
Anwesh43 <anweshthecool0@gmail.com>
2022-06-24 09:44:22 +0530

    line-break-square-down: adding Stage class

--- line-break-square-down.ts
+++ line-break-square-down.ts
@@ -11,7 +11,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 5.9 
 const rot : number = Math.PI / 2
 const delay : number = 20 
-const BACKcoLOR : string = "#BDBDBD"
+const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const barSizeFactor : number = 9.2 
@@ -61,5 +61,38 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         DrawingUtil.drawLineBreakSquareDown(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
