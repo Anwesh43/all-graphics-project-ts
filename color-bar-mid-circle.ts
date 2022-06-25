9a65143fe1fcbae535755ce004c6c0a6b7860355
Anwesh43 <anweshthecool0@gmail.com>
2022-06-26 02:00:51 +0530

    color-bar-mid-circle: adding CBMCNode class

--- color-bar-mid-circle.ts
+++ color-bar-mid-circle.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class CBMCNode {
+
+    prev : CBMCNode 
+    next : CBMCNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new CBMCNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawCBMCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CBMCNode {
+        var curr : CBMCNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
