99cb839464a2ccb1405225b3ea3a5e6bc59613c7
Anwesh43 <anweshthecool0@gmail.com>
2022-06-27 10:43:42 +0530

    bar-from-line-downrot: adding DrawingUtil class

--- bar-from-line-downrot.ts
+++ bar-from-line-downrot.ts
@@ -26,3 +26,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    } 
+
+    static drawBarFromLineDownRot(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / barSizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.save()
+        context.translate(-size / 2, 0)
+        context.fillRect(0, -barSize * sc2, barSize, barSize * sc2)
+        context.save()
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+        context.restore()
+        context.restore()
+        context.restore()
+    }
+
+    static drawBFLDRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / sizeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarFromLineDownRot(context, scale)
+    }
+}
