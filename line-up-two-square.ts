0eb4240a082e01fa51a884bf005c6553352aa192
Anwesh43 <anweshthecool0@gmail.com>
2022-06-28 02:02:47 +0530

    line-up-two-square: adding DrawingUtil class

--- line-up-two-square.ts
+++ line-up-two-square.ts
@@ -26,3 +26,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineUpTwoSquare(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / squareSizeFactor
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + barSize) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(-size / 2, 0)
+            context.fillRect(0, -barSize * sc2, barSize, barSize * sc2)
+            context.save()
+            context.rotate(deg * sc3)
+            DrawingUtil.drawLine(context, 0, 0, size * 0.5 * sc1, 0)
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLUTSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineUpTwoSquare(context, scale)
+    }
+}
