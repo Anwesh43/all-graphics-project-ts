fcf680a09568cd2664e2d91e27be736a1ebf2481
Anwesh43 <anweshthecool0@gmail.com>
2022-06-28 02:16:03 +0530

    line-up-two-square: adding LineUpTwoSquare class

--- line-up-two-square.ts
+++ line-up-two-square.ts
@@ -187,3 +187,26 @@ class LUTSNode {
         return this 
     }
 }
+
+class LineUpTwoSquare {
+
+    curr : LUTSNode = new LUTSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
