3b9b71bb87ad08c18a187c87ceaf6da8283fe13b
Anwesh43 <anweshthecool0@gmail.com>
2022-06-29 12:09:49 +0530

    drop-block-from-line: adding DrawingUtil class

--- drop-block-from-line.ts
+++ drop-block-from-line.ts
@@ -26,3 +26,36 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDropBlockFromLine(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / barSizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2) * sc4)
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        context.fillRect(-size / 2, -size * sc2, size, size * sc2)
+        context.restore()
+    }
+
+    static drawDBFLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawDropBlockFromLine(context, scale)
+    }
+}
