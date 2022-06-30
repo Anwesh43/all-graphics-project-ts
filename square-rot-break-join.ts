74a7c6d8e4ba95086cd2fe4b04f67e7ed9e3574d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-01 00:57:12 +0530

    square-rot-break-join: adding DrawingUtil class

--- square-rot-break-join.ts
+++ square-rot-break-join.ts
@@ -26,3 +26,48 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+    
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSquareRotBreakJoin(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / barFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h  / 2 + barSize + size) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            for (let k = 0; k < 2; k++) {
+                context.save()
+                context.scale(1 - 2 * k, 1)
+                context.rotate(rot * (1 - sc3))
+                context.save()
+                context.translate(0, -size * 0.5 * sc2)
+                context.fillRect(-barSize * 0.5 * sc1, -barSize * 0.5 * sc1, barSize * 0.5 * sc1, barSize * 0.5 * sc1)
+                context.restore()
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSRBJNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSquareRotBreakJoin(context, scale)
+    }
+}
