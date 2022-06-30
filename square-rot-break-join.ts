4980025e890b97242881bcb5b42840a9487bfab3
Anwesh43 <anweshthecool0@gmail.com>
2022-07-01 01:09:17 +0530

    square-rot-break-join: adding SRBJNode class

--- square-rot-break-join.ts
+++ square-rot-break-join.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class SRBJNode {
+
+    next : SRBJNode 
+    prev : SRBJNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SRBJNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSRBJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SRBJNode {
+        var curr : SRBJNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
