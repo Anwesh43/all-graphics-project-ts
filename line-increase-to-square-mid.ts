9c14353e779cc769e2b8b70e199972bfe16fa662
Anwesh43 <anweshthecool0@gmail.com>
2022-07-02 05:31:19 +0530

    line-increase-to-square-mid: adding ScaleUtil class

--- line-increase-to-square-mid.ts
+++ line-increase-to-square-mid.ts
@@ -14,3 +14,14 @@ const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const shiftFactor : number = 12.9 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
+    }
+}
