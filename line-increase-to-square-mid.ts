38bfc4ab4676808548437d72b91297fc968b98bf
Anwesh43 <anweshthecool0@gmail.com>
2022-07-02 05:36:05 +0530

    line-increase-to-square-mid: adding DrawingUtil class

--- line-increase-to-square-mid.ts
+++ line-increase-to-square-mid.ts
@@ -25,3 +25,26 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineIncreaseToSquareMid(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawLine(context, 0, -size * sc1, 0, size * sc1)
+        context.strokeRect(-size * 0.5 * sc2, -size * 0.5 * sc2, size * sc2, size * sc2)
+        context.restore()
+    }
+}
