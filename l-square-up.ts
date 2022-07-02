1d7ec21b449e1cccc8d482a2472900fce471a1e9
Anwesh43 <anweshthecool0@gmail.com>
2022-07-03 02:04:57 +0530

    l-square-up: adding DrawingUtil class

--- l-square-up.ts
+++ l-square-up.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLSquareUp(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * sc2 * j)
+            DrawingUtil.drawLine(context, 0, 0, 0, -(size - size * 0.5 * j) * sc1)
+            context.restore()
+        }
+        context.fillRect(-size * 0.5, -size * 0.5 * sc3, size * 0.5, size * 0.5 * sc3)
+        context.restore()
+    }
+
+    static drawLSUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLSquareUp(context, scale)
+    }
+}
