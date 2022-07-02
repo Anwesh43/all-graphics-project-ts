a706efdd7f4494a4e57d493f026bcd0287a4c57d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-02 05:46:00 +0530

    line-increase-to-square-mid: adding LITSMNode class

--- line-increase-to-square-mid.ts
+++ line-increase-to-square-mid.ts
@@ -133,3 +133,45 @@ class Animator {
         }
     }
 }
+
+class LITSMNode {
+
+    prev : LITSMNode 
+    next : LITSMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LITSMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLITSMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LITSMNode {
+        var curr : LITSMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
