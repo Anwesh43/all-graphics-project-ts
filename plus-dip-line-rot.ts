8a59f94ba71fb55dee5366071411423901ca5b14
Anwesh43 <anweshthecool0@gmail.com>
2022-07-05 01:06:33 +0530

    plus-dip-line-rot: adding PlusDipLineROT class

--- plus-dip-line-rot.ts
+++ plus-dip-line-rot.ts
@@ -175,3 +175,26 @@ class PDLRNode {
         return this 
     }
 }
+
+class PlusDipLineRot {
+
+    curr : PDLRNode = new PDLRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
