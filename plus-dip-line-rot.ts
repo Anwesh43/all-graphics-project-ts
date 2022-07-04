90617e022c3f88f92ddd1272d7e7ee37cb8a58f5
Anwesh43 <anweshthecool0@gmail.com>
2022-07-05 01:05:09 +0530

    plus-dip-line-rot: adding PDLRNode class

--- plus-dip-line-rot.ts
+++ plus-dip-line-rot.ts
@@ -133,3 +133,45 @@ class Animator {
         }
     }
 }
+
+class PDLRNode {
+
+    prev : PDLRNode 
+    next : PDLRNode  
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PDLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPDLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : PDLRNode {
+        var curr : PDLRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
