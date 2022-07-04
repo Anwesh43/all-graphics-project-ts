e8b90276e2ce6676a65115f44e3c471189ef5516
Anwesh43 <anweshthecool0@gmail.com>
2022-07-04 15:13:08 +0530

    z-bar-right: adding DrawingUtil class

--- z-bar-right.ts
+++ z-bar-right.ts
@@ -9,11 +9,12 @@ const parts : number = 4
 const scGap : number = 0.04 / parts
 const strokeFactor : number = 90 
 const sizeFactor : number = 7.9 
-const barWFator : number = 16.9 
+const barWFactor : number = 16.9 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const rot : number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -23,5 +24,43 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawZBarRight(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor
+        const barSize : number = Math.min(w, h) / barWFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.fillRect(-size / 2, -size * 0.5 * sc2, barSize, size * 0.5 * sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawZBRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawZBarRight(context, scale)
     }
 }
