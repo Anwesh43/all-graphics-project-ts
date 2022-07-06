b160e539d8a3c2ba6359275195ba55b4d9e4c5cd
Anwesh43 <anweshthecool0@gmail.com>
2022-07-06 09:45:01 +0530

    line-rot-block: adding constants

---
+++ line-rot-block.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 5.9 
+const barSizeFactor : number = 12.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const rot : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
