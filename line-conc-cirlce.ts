3169848162a03b763d5c8339b4ccf327e1955ea6
Anwesh43 <anweshthecool0@gmail.com>
2022-07-07 01:39:49 +0530

    line-conc-cirlce: adding constants

---
+++ line-conc-cirlce.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 5 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const r1Factor : number = 12.9 
+const r2Factor : number = 5.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const deg : number = Math.PI 
+
