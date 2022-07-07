58adf1e9fa7ed756849f3fb6b1841ff0d27f07bc
Anwesh43 <anweshthecool0@gmail.com>
2022-07-08 01:12:53 +0530

    half-square-line-up: adding DrawingUtil class

--- half-square-line-up.ts
+++ half-square-line-up.ts
@@ -25,3 +25,35 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawHalfSquareLineUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc2, -size / 2, size * 0.5 * sc2, -size / 2)
+        context.fillRect(-size / 2, size / 2 - size * 0.5 * sc1, size, size * 0.5 * sc1)
+        context.restore()
+    }
+
+    static drawHSLUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawHalfSquareLineUp(context, scale)
+    }
+}
