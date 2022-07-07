903daee9ee27d7e39a9a62e6f47686f99f4e87b1
Anwesh43 <anweshthecool0@gmail.com>
2022-07-08 01:42:09 +0530

    half-square-line-up: adding HalfSquareLineUp class

--- half-square-line-up.ts
+++ half-square-line-up.ts
@@ -176,3 +176,26 @@ class HSLUNode {
         return this 
     }
 }
+
+class HalfSquareLineUp {
+
+    curr : HSLUNode = new HSLUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
