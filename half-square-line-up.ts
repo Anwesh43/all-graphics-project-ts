955b238f61a19724598713ae2f4cfa7f04e26fd4
Anwesh43 <anweshthecool0@gmail.com>
2022-07-08 01:39:04 +0530

    half-square-line-up: adding HSLUNode class

--- half-square-line-up.ts
+++ half-square-line-up.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class HSLUNode {
+
+    prev : HSLUNode 
+    next : HSLUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new HSLUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawHSLUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUdpating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : HSLUNode {
+        var curr : HSLUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
