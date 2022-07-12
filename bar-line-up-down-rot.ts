5d6263dd46c34334d91a4149102a643c87012bc6
Anwesh43 <anweshthecool0@gmail.com>
2022-07-12 10:26:33 +0530

    bar-line-up-down-rot: adding fixes 2

--- bar-line-up-down-rot.ts
+++ bar-line-up-down-rot.ts
@@ -50,7 +50,7 @@ class DrawingUtil {
             context.save()
             context.scale(1 - 2 * j, 1)
             DrawingUtil.drawLine(context, size / 2, 0, size / 2, -size * sc1)
-            context.fillRect(0, -barSize * sc2, size, barSize * sc2)
+            context.fillRect(0, -barSize * sc2, size / 2, barSize * sc2)
             context.restore()
         }
         
