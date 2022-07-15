0139f738446ab182c154b780d8e09e7a03c9a0a6
Anwesh43 <anweshthecool0@gmail.com>
2022-07-15 15:06:00 +0530

    line-square-holder-rot: adding LSHRNode class

--- line-square-holder-rot.ts
+++ line-square-holder-rot.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class LSHRNode {
+
+    prev : LSHRNode 
+    next : LSHRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSHRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSHRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LSHRNode {
+        var curr : LSHRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
