9ed00c86fad4a1d95db0f24e07a5ba9e31432a3c
Anwesh43 <anweshthecool0@gmail.com>
2022-07-15 14:34:43 +0530

    line-square-holder-rot: adding constants

---
+++ line-square-holder-rot.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#00C853",
+    "#0091EA",
+    "#AA00FF",
+    "#FFAB00"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const barFactor : number = 11.9 
+const delay : number = 20 
+const deg : number = Math.PI 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
