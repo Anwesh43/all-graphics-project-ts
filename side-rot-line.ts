44dd867d9b92f294a9192b1898136d2dc136218d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-18 13:45:36 +0530

    side-rot-line: adding SRLNode class

--- side-rot-line.ts
+++ side-rot-line.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class SRLNode {
+
+    prev : SRLNode 
+    next : SRLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+    
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SRLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SRLNode {
+        var curr : SRLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
