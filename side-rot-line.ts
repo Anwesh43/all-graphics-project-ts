d54fe2efcf6504f68887eed64fa4b822bda173ab
Anwesh43 <anweshthecool0@gmail.com>
2022-07-18 13:21:55 +0530

    side-rot-line: adding DrawingUtil class

--- side-rot-line.ts
+++ side-rot-line.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#AA00FF",
     "#FFAB00"
 ]
-const parts : number = 4 
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 5.9 
@@ -14,7 +14,7 @@ const rot : number = Math.PI / 2
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth
 const h : number = window.innerHeight 
-
+const deg : number = Math.PI / 4 
 class ScaleUtil {
 
     static maxScale(scale : number, i : number, n : number) : number {
@@ -23,5 +23,46 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSideRotLine(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+        context.rotate(deg * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(w / 2, h / 2)
+            DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+            context.save()
+            context.translate(size / 2, 0)
+            context.rotate(-deg * sc3)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.5 * sc2)
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSideRotLine(context, scale)
     }
 }
