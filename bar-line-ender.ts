7ede2f812460f59e719786135a92f2f9f13ba138
Anwesh43 <anweshthecool0@gmail.com>
2022-07-20 02:56:28 +0530

    bar-line-ender: adding DrawingUtil class

--- bar-line-ender.ts
+++ bar-line-ender.ts
@@ -13,6 +13,7 @@ const delay : number = 20
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const lines : number = 2 
 
 class ScaleUtil {
 
@@ -22,5 +23,42 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarLineEnder(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const lSize : number = Math.min(w, h) / (lines + 2)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.fillRect(-size / 2, -size / 2, size * (sc1 - sc4), size)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(0, -size / 2 + lSize * (1 + j))
+            DrawingUtil.drawLine(context, 0, 0, 0, lSize * (sc2 - sc3))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBLENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = backColor
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarLineEnder(context, scale)
     }
 }
