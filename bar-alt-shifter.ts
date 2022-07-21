4969f81f62ba02da132c2c26e88ae42a46cb0a59
Anwesh43 <anweshthecool0@gmail.com>
2022-07-21 16:31:33 +0530

    bar-alt-shifter: adding BASNode class

--- bar-alt-shifter.ts
+++ bar-alt-shifter.ts
@@ -136,3 +136,45 @@ class Animator {
         }
     }
 }
+
+class BASNode {
+
+    prev : BASNode 
+    next : BASNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BASNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBASNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : (a : number) => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BASNode {
+        var curr : BASNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    } 
+}
