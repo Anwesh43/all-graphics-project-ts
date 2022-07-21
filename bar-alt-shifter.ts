cb91629dd0d4650bb1cc93167ab2e94f92168900
Anwesh43 <anweshthecool0@gmail.com>
2022-07-21 15:45:28 +0530

    bar-alt-shifter: adding constants

---
+++ bar-alt-shifter.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4
+const scGap : number = 0.02 / parts 
+const bars : number = 5
+const sizeFactor : number = 5.9 
+const barHFactor : number = 14.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const strokeFactor : number = 90 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
