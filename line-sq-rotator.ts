2b33f1a8f4fab1114aed1f2aa4f8e66cf4f12336
Anwesh43 <anweshthecool0@gmail.com>
2022-07-22 15:29:30 +0530

    line-sq-rotator: adding constants

---
+++ line-sq-rotator.ts
@@ -0,0 +1,16 @@
+const colors : Array<string>  = [
+    "#F44336",
+    "#03A9F4",
+    "#FF9800",
+    "#8BC34A",
+    "#795548"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const strokeFactor : number = 90 
+const sizeFactor : number = 11.9 
+const deg : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
