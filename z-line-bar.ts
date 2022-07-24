9d9165144e3e7f0a7795fafb3f44e14f988a2972
Anwesh43 <anweshthecool0@gmail.com>
2022-07-24 16:38:16 +0530

    z-line-bar: adding ZLBNode class

--- z-line-bar.ts
+++ z-line-bar.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class ZLBNode {
+
+    prev : ZLBNode 
+    next : ZLBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ZLBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawZLBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : ZLBNode  {
+        var curr : ZLBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
