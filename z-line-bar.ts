bec9292dedbc02a84c9c5fbbb186f71786798f0e
Anwesh43 <anweshthecool0@gmail.com>
2022-07-24 15:52:48 +0530

    z-line-bar: adding DrawingUtil class

--- z-line-bar.ts
+++ z-line-bar.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#8BC34A",
     "#795548"
 ]
-const parts : number = 4 
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 11.9
@@ -23,5 +23,48 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawZLineBar(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)       
+        const sc5 : number = ScaleUtil.divideScale(scale, 4, parts)
+        context.save()
+        context.translate(w / 2  + (w / 2 + size) * sc5, h / 2)
+        context.rotate(rot * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.5 * ScaleUtil.divideScale(sc1, 0, 2))
+            context.save()
+            context.translate(0, -size * 0.5)
+            context.rotate(rot * sc2)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.5 * ScaleUtil.divideScale(sc1, 1, 2))
+            context.restore()
+            context.fillRect(0, -size * 0.5, size * 0.5, size * 0.5 * sc3)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawZLBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor  
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawZLineBar(context, scale)
     }
 }
