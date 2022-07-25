e87bba787d4965d2602b768b6aa6a838a045873c
Anwesh43 <anweshthecool0@gmail.com>
2022-07-25 13:01:43 +0530

    block-right-angle-point: adding BRAPNode class

--- block-right-angle-point.ts
+++ block-right-angle-point.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
  }
+
+ class BRAPNode {
+
+    prev : BRAPNode 
+    next : BRAPNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BRAPNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBRAPNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BRAPNode {
+        var curr : BRAPNode | null  = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr != null) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+ }
