94ad632649fc392e361c63eec2541310b36894a0
Anwesh43 <anweshthecool0@gmail.com>
2022-07-27 14:43:55 +0530

    alt-balls-line-rot-mover: adding AltBallsLineRotMover class

--- alt-balls-line-rot-mover.ts
+++ alt-balls-line-rot-mover.ts
@@ -191,3 +191,26 @@ class ABLRMNode {
         return this 
     }
 }
+
+class AltBallsLineRotMover {
+
+    curr : ABLRMNode = new ABLRMNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+    
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    } 
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
