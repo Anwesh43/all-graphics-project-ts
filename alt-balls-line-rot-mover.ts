a7676a77cbda7a1fe46d7030eee7b4324fdc86f8
Anwesh43 <anweshthecool0@gmail.com>
2022-07-27 14:42:24 +0530

    alt-balls-line-rot-mover: adding ABLRMNode class

--- alt-balls-line-rot-mover.ts
+++ alt-balls-line-rot-mover.ts
@@ -149,3 +149,45 @@ class Animator {
         }
     }
 }
+
+class ABLRMNode {
+
+    prev : ABLRMNode 
+    next : ABLRMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ABLRMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawABLRMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : ABLRMNode {
+        var curr : ABLRMNode = this.next 
+        if (dir == -1) {
+            curr = this.prev 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
