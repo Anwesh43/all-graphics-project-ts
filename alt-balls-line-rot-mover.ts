f97a6863afd6191ca503fb9a1f72f7808fad9551
Anwesh43 <anweshthecool0@gmail.com>
2022-07-27 14:33:05 +0530

    alt-balls-line-rot-mover: adding DrawingUtil class

--- alt-balls-line-rot-mover.ts
+++ alt-balls-line-rot-mover.ts
@@ -26,3 +26,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+    
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0,2 * Math.PI)
+        context.fill()
+    }
+
+    static drawAltBallsLineRotMover(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const ballR : number = Math.min(w, h) / ballRFactor
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4 , h / 2)
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, -size * 0.5 * sc1, 0, size * 0.5 * sc1, 0)
+        for (let i = 0; i < 2; i++) {
+            context.save()
+            context.scale(1 - 2 * i, 1)
+            context.translate(size / 2 - ballR, ballR + size / 10)
+            DrawingUtil.drawCircle(context, 0, 0, ballR * sc2)
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, 0, -size / 10, 0, -size / 10 - size * 0.5 * sc2)
+        context.restore()
+    }
+
+    static drawABLRMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawAltBallsLineRotMover(context, scale)
+    }
+}
