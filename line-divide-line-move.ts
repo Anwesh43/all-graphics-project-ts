4e77c93bad13074fd65a3b65a7459f929e093b9b
Anwesh43 <anweshthecool0@gmail.com>
2022-07-29 19:32:00 +0530

    line-divide-line-move: adding LineDivideLineMove class

--- line-divide-line-move.ts
+++ line-divide-line-move.ts
@@ -182,3 +182,26 @@ class LDLMNode {
         return this 
     }
 }
+
+class LineDivideLineMove {
+
+    curr : LDLMNode = new LDLMNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
