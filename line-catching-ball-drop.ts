7712cb33a6da09f509957f6f0b68ef82f016f4b9
Anwesh43 <anweshthecool0@gmail.com>
2022-08-01 14:30:29 +0530

    line-catching-ball-drop: adding LCBDNode class

--- line-catching-ball-drop.ts
+++ line-catching-ball-drop.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LCBDNode {
+
+    prev : LCBDNode  
+    next : LCBDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LCBDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLCBDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LCBDNode {
+        var curr : LCBDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
