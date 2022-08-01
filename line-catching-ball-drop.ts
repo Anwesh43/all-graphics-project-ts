a0a6fa20d163e66c7e70472e73f04c9e90c498cb
Anwesh43 <anweshthecool0@gmail.com>
2022-08-01 14:31:58 +0530

    line-catching-ball-drop: adding LineCatchingBallDrop class

--- line-catching-ball-drop.ts
+++ line-catching-ball-drop.ts
@@ -188,3 +188,26 @@ class LCBDNode {
         return this 
     }
 }
+
+class LineCatchingBallDrop {
+
+    curr : LCBDNode = new LCBDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
