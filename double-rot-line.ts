4ba33fba40f5284b075a7873dbd067e93ed2509a
Anwesh43 <anweshthecool0@gmail.com>
2022-08-05 06:18:32 +0530

    double-rot-line: adding DrawingUtil class

--- double-rot-line.ts
+++ double-rot-line.ts
@@ -25,3 +25,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDoubleRotLine(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2 + size) * sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            context.rotate(deg * j * sc2)
+            for (let k = 0; k < 2; k++) {
+                const sc1k : number = ScaleUtil.divideScale(sc1, k, 2)
+                context.save()
+                context.translate(0, size * 0.5 * k)
+                context.rotate(-deg * sc3 * k * j)
+                DrawingUtil.drawLine(context, 0, 0, 0, size * 0.5 * sc1k)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+    
+    static drawDRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDoubleRotLine(context, scale)
+    }
+}
