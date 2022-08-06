88f62f612891b5b79ddefb619d726b26f4efc177
Anwesh43 <anweshthecool0@gmail.com>
2022-08-06 13:49:24 +0530

    line-sq-right-mover: adding DrawingUtil class

--- line-sq-right-mover.ts
+++ line-sq-right-mover.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSqRightMove(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(3 * rot * sc2)
+            DrawingUtil.drawLine(context, 0, 0, -size * sc1, 0)
+            context.fillRect(-size, 0, size, size * j * sc3)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLSRMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineSqRightMove(context, scale)
+    }
+}
