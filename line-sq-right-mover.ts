de5224b811802ff18d52ae6bb158100d1bde0bca
Anwesh43 <anweshthecool0@gmail.com>
2022-08-06 14:31:07 +0530

    line-sq-right-mover: adding potential fix 1

--- line-sq-right-mover.ts
+++ line-sq-right-mover.ts
@@ -80,6 +80,7 @@ class Stage {
         if (this.context) {
             this.context.fillStyle = backColor 
             this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
