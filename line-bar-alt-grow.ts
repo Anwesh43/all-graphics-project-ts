06084dbaa4fc70b5d41df0506a36c1988e208f90
Anwesh43 <anweshthecool0@gmail.com>
2022-08-08 18:26:53 +0530

    line-bar-alt-grow: adding LBAGNode class

--- line-bar-alt-grow.ts
+++ line-bar-alt-grow.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LBAGNode {
+
+    prev : LBAGNode 
+    next : LBAGNode
+    state : State = new State() 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBAGNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBAGNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBAGNode {
+        var curr : LBAGNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this
+    }
+}
