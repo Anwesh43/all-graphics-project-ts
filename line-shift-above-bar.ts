837f368fa8de31d1514231423d80f491455d2d25
Anwesh43 <anweshthecool0@gmail.com>
2022-08-09 13:32:32 +0530

    line-shift-above-bar: adding DrawingUtil class

--- line-shift-above-bar.ts
+++ line-shift-above-bar.ts
@@ -5,8 +5,8 @@ const colors : Array<string> = [
     "#D50000",
     "#43A047"
 ]
-const parts : number = 4
-const scGap : number = 0.04 / parts 
+const parts : number = 3
+const scGap : number = 0.03 / parts 
 const strokeFactor : number = 90 
 const delay : number = 20 
 const sizeFactor : number = 5.9 
@@ -23,5 +23,42 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineShiftAboveBar(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sc2)
+        context.save()
+        context.translate(-w / 2 - size / 2 + (w / 2 + size / 2) * sc1 - (h / 2 + size / 2) * sc3, 0)
+        DrawingUtil.drawLine(context, -size / 2, 0, size / 2, 0)
+        context.restore()
+        context.save()
+        context.translate(w / 2 + size / 2 - (w / 2 + size / 2) * sc3 + (h / 2 + size / 2) * sc3, 0)
+        context.fillRect(-size / 2, -size / 2, size, size)
+        context.restore()
+        context.restore()
+    }
+
+    static drawLSABNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineShiftAboveBar(context, scale)
     }
 }
