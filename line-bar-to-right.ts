faa6a22a1135728f9bf0714e230ae53702f4577f
Anwesh43 <anweshthecool0@gmail.com>
2022-08-10 11:29:01 +0530

    line-bar-to-right: adding DrawingUtil class

--- line-bar-to-right.ts
+++ line-bar-to-right.ts
@@ -26,3 +26,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBarToRight(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barSize : number = Math.min(w, h) / barFactor 
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2) * sc4)
+        context.rotate(rot * sc3)
+        DrawingUtil.drawLine(context, 0, 0, size * sc1, 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(size, 0)
+            context.scale(1, 1 - 2 * j)
+            context.save()
+            context.rotate(rot * sc2)
+            DrawingUtil.drawLine(context, 0, 0, -barSize * Math.floor(sc1), 0)
+            context.restore()
+            context.fillRect(-barSize, -barSize * sc3, barSize, barSize * sc3)
+            context.restore()
+        }
+        context.restore()
+    }
+    
+    static drawLBTRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineBarToRight(context, scale)
+    }
+}
