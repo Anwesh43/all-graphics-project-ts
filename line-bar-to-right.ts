d1ff196046a6f9d7df27626305379c6ed0c34677
Anwesh43 <anweshthecool0@gmail.com>
2022-08-10 12:33:27 +0530

    line-bar-to-right: adding LBTRNode class

--- line-bar-to-right.ts
+++ line-bar-to-right.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LBTRNode {
+
+    prev : LBTRNode 
+    next : LBTRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+    
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBTRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBTRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBTRNode {
+        var curr : LBTRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
