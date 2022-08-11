66d029a9fc70d6fbf72256096056b07cad8a154e
Anwesh43 <anweshthecool0@gmail.com>
2022-08-11 12:33:10 +0530

    line-multi-sq-holder: adding LMSHNode class

--- line-multi-sq-holder.ts
+++ line-multi-sq-holder.ts
@@ -145,3 +145,41 @@ class Animator {
         }
     }
 }
+
+class LMSHNode {
+
+    prev : LMSHNode 
+    next : LMSHNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LMSHNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLMSHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LMSHNode {
+        var curr : LMSHNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
