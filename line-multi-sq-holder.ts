b4b422f36dae569561fa4ce9f75f066b649c25a8
Anwesh43 <anweshthecool0@gmail.com>
2022-08-11 12:15:23 +0530

    line-multi-sq-holder: adding DrawingUtil class

--- line-multi-sq-holder.ts
+++ line-multi-sq-holder.ts
@@ -10,7 +10,7 @@ const scGap : number = 0.04 / parts
 const strokeFactor : number = 90 
 const sizeFactor : number = 11.9 
 const delay : number = 20 
-const rot : number = Math.PI / 2 
+const rot : number = Math.PI  
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
@@ -23,5 +23,48 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineMultiSqHolder(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sc4)
+        context.scale(1 - sc4, 1 - sc4)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.translate(size * 0.5 * sc2, 0)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * 0.5 * sc1)
+            context.restore()
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, 0, 0, size * 0.5 * sc1, 0)
+            context.restore()
+        }
+        context.fillRect(-size * 0.5 * sc3, -size / 2, size * sc3, size)
+        context.restore()
+    }
+
+    static drawLMSHNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineMultiSqHolder(context, scale)
     }
 }
