2e64dd4aedcbac6fbbbaa80a1e7eba03056e5db1
Anwesh43 <anweshthecool0@gmail.com>
2022-08-19 12:59:16 +0530

    line-parallel-joiner: adding Stage class

--- line-parallel-joiner.ts
+++ line-parallel-joiner.ts
@@ -72,3 +72,23 @@ class DrawingUtil {
         DrawingUtil.drawLineParallelJoiner(context, scale)
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+}
