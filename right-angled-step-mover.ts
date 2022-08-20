9e8de86d5bae578b101737eb6160417aa1f19026
Anwesh43 <anweshthecool0@gmail.com>
2022-08-20 14:30:54 +0530

    right-angled-step-mover: adding RASMNode class

--- right-angled-step-mover.ts
+++ right-angled-step-mover.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class RASMNode {
+
+    prev : RASMNode
+    next : RASMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RASMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRASMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RASMNode {
+        var curr : RASMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
