a26d24af8e191ebc87f02c0b3b34459a498e4f7f
Anwesh43 <anweshthecool0@gmail.com>
2022-08-20 14:10:57 +0530

    right-angled-step-mover: adding DrawingUtil class

--- right-angled-step-mover.ts
+++ right-angled-step-mover.ts
@@ -25,3 +25,38 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1  : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRightAngledStepMover(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(rot * sc2 * j)
+            context.translate(0, -(h / 2 + (w / 2 - h / 2) * j) * ScaleUtil.divideScale(scale, 2 + (1 - j), parts))
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * sc1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawRASMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawRightAngledStepMover(context, scale)
+    }
+}
