76383db168f0cba5b5a76b110580dd8031090e8c
Anwesh43 <anweshthecool0@gmail.com>
2022-08-22 14:23:43 +0530

    far-from-middle-t: adding DrawingUtil class

--- far-from-middle-t.ts
+++ far-from-middle-t.ts
@@ -24,3 +24,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawFarFromMiddle(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const divideSc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.save()
+            context.translate((w / 4 - size) * divideSc(2) + (w / 4 + size) * divideSc(3), 0)
+            DrawingUtil.drawLine(context, 0, 0, size * divideSc(0), 0)
+            context.restore()
+            context.save()
+            context.translate(w / 4 +(w / 4) * divideSc(3), 0)
+            DrawingUtil.drawLine(context, 0, -size * 0.5 * divideSc(1), 0, size * 0.5 * divideSc(1))
+            context.restore()
+            context.restore()
+
+        }
+        context.restore()
+    }
+
+    static drawFFMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawFarFromMiddle(context, scale)
+    }
+}
+
