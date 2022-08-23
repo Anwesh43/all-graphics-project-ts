cb976ee77826282ac281526854bf9487367f4ee9
Anwesh43 <anweshthecool0@gmail.com>
2022-08-23 15:54:42 +0530

    side-bar-graph-move: adding SNGMNode class

--- side-bar-graph-move.ts
+++ side-bar-graph-move.ts
@@ -126,3 +126,45 @@ class Animator {
         }
     }
 }
+
+class SBGMNode {
+
+    prev : SBGMNode 
+    next : SBGMNode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBGMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSBGMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SBGMNode {
+        var curr : SBGMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
