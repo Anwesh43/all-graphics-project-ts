3054adcd628287b7dd44768cc59d47c7e1d7ba29
Anwesh43 <anweshthecool0@gmail.com>
2022-08-24 12:04:05 +0530

    line-rot-sq-down: adding LineRotSqDown class

--- line-rot-sq-down.ts
+++ line-rot-sq-down.ts
@@ -179,3 +179,26 @@ class LRSDNode {
         return this 
     }
 }
+
+class LineRotSqDown {
+
+    curr : LRSDNode = new LRSDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
