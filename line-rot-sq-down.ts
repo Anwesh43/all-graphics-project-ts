102aadeee8e8069a36665ae5c2e6893f89f52736
Anwesh43 <anweshthecool0@gmail.com>
2022-08-24 12:02:43 +0530

    line-rot-sq-down: adding LRSDNode class

--- line-rot-sq-down.ts
+++ line-rot-sq-down.ts
@@ -138,3 +138,44 @@ class Animator {
     }
 }
 
+class LRSDNode {
+
+    prev : LRSDNode 
+    next : LRSDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRSDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRSDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRSDNode {
+        var curr : LRSDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
