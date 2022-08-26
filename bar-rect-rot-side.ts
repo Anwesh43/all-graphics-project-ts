933b4172b6da819b78710547db0d6e1f79f39758
Anwesh43 <anweshthecool0@gmail.com>
2022-08-26 13:17:36 +0530

    bar-rect-rot-side: adding BRRSNode class

--- bar-rect-rot-side.ts
+++ bar-rect-rot-side.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class BRRSNode {
+
+    next : BRRSNode 
+    prev : BRRSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BRRSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBRRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BRRSNode {
+        var curr : BRRSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
