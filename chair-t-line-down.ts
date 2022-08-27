e5a9737104b9ea52309016e987e47674c9da218d
Anwesh43 <anweshthecool0@gmail.com>
2022-08-27 19:13:11 +0530

    chair-t-line-down: adding ChairTLineDown class

--- chair-t-line-down.ts
+++ chair-t-line-down.ts
@@ -178,3 +178,26 @@ class CTLDNode {
         return this 
     }
 }
+
+class ChairTLineDown {
+
+    curr : CTLDNode = new CTLDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
