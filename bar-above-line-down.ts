967948325d1a40a9959a98251c333f943d27434d
Anwesh43 <anweshthecool0@gmail.com>
2022-08-30 12:44:49 +0530

    bar-above-line-down: adding BarAboveLineDown class

--- bar-above-line-down.ts
+++ bar-above-line-down.ts
@@ -175,3 +175,26 @@ class BALDNode {
         return this 
     }
 }
+
+class BarAboveLineDown {
+
+    curr : BALDNode = new BALDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
