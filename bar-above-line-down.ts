ef0d172d67a7d1aa854b4dd9f4c634f82e8cfef8
Anwesh43 <anweshthecool0@gmail.com>
2022-08-30 12:43:24 +0530

    bar-above-line-down: adding BALDNode class

--- bar-above-line-down.ts
+++ bar-above-line-down.ts
@@ -133,3 +133,45 @@ class State {
         } 
     }
 }
+
+class BALDNode {
+
+    prev : BALDNode
+    next : BALDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BALDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBALDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BALDNode {
+        var curr : BALDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr  
+        }
+        cb()
+        return this 
+    }
+}
