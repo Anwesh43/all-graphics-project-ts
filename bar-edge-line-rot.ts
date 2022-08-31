22fa3fd4b2c23081f57170dcbf2cef1be30c3b19
Anwesh43 <anweshthecool0@gmail.com>
2022-08-31 11:01:50 +0530

    bar-edge-line-rot: adding BarEdgeLineRot class

--- bar-edge-line-rot.ts
+++ bar-edge-line-rot.ts
@@ -176,3 +176,26 @@ class BELRNode {
         return this 
     }
 }
+
+class BarEdgeLineRot {
+
+    curr : BELRNode = new BELRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        }) 
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
