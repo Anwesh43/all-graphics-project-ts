39e33a4e506cbf34cefda3d69d8c1be4826560d1
Anwesh43 <anweshthecool0@gmail.com>
2022-08-31 10:38:02 +0530

    bar-edge-line-rot: adding DrawingUtil class

--- bar-edge-line-rot.ts
+++ bar-edge-line-rot.ts
@@ -26,3 +26,35 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarEdgeLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barW : number = Math.min(w, h) / barWFactor 
+        context.save()
+        context.translate(w / 2 - (w / 2 + size) * dsc(3), h / 2)
+        
+        DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+        context.save()
+        context.rotate(rot * dsc(2))
+        context.fillRect(-barW / 2, -size * dsc(1), barW, size * dsc(1))
+        context.restore()
+        context.restore()
+    }
+
+    static drawBELRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBarEdgeLineRot(context, scale)
+    }
+}
