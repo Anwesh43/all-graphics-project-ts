66e8705120e8a6071f5339b6a4f66db2f104a6ce
Anwesh43 <anweshthecool0@gmail.com>
2022-08-31 10:13:32 +0530

    bar-edge-line-rot: adding constants

---
+++ bar-edge-line-rot.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#03DAC6",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 4.9
+const barWFactor : number = 11.9 
+const rot : number = Math.PI / 2
+const strokeFactor : number = 90 
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
