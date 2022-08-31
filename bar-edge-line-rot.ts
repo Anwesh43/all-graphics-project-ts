09d4ff26b3ecbe5405ebb922bbf0b6ceb570ea89
Anwesh43 <anweshthecool0@gmail.com>
2022-08-31 11:00:26 +0530

    bar-edge-line-rot: adding BELRNode class

--- bar-edge-line-rot.ts
+++ bar-edge-line-rot.ts
@@ -135,3 +135,44 @@ class Animator {
         }
     }
 }
+
+class BELRNode {
+
+    prev : BELRNode 
+    next : BELRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BELRNode(this.i + 1) 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBELRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BELRNode {
+        var curr : BELRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
