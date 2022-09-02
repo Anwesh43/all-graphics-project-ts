1196d4717caf59df7e9de04bb003a9a4800522ab
Anwesh43 <anweshthecool0@gmail.com>
2022-09-02 20:41:43 +0530

    bar-on-line-jump: adding DrawingUtil class

--- bar-on-line-jump.ts
+++ bar-on-line-jump.ts
@@ -26,3 +26,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarOnLineJump(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sqSize : number = Math.min(w, h) / sqSizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, 0, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.save()
+            context.rotate(rot * dsc(1))
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * (dsc(0) - dsc(3)))
+            context.restore()
+            context.save()
+            context.translate(size / 2 - sqSize / 2, (h / 2 + sqSize) * dsc(4))
+            context.fillRect(0, -sqSize * dsc(2), sqSize, sqSize * dsc(2))
+            context.restore()
+            context.restore()
+            
+        }
+        context.restore()
+    }
+
+    static drawBOLJNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.fillStyle = backColor 
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawBarOnLineJump(context, scale)
+    }
+}
