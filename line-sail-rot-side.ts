23d6ec9532d9f56f32d4832e4d9774cb977cfffd
Anwesh43 <anweshthecool0@gmail.com>
2022-09-08 17:57:30 +0530

    line-sail-rot-side: adding LSRSNode class

--- line-sail-rot-side.ts
+++ line-sail-rot-side.ts
@@ -133,3 +133,45 @@ class Animator {
         }
     }
 }
+
+class LSRSNode {
+
+    prev : LSRSNode 
+    next : LSRSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSRSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSRSNode {
+        var curr : LSRSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
