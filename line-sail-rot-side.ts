c6c42642d47f9ca1d2268fe5cf73d814b9578418
Anwesh43 <anweshthecool0@gmail.com>
2022-09-08 17:27:24 +0530

    line-sail-rot-side: adding DrawingUtil class

--- line-sail-rot-side.ts
+++ line-sail-rot-side.ts
@@ -25,3 +25,34 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSailRotSide(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * dsc(3), h / 2)
+        context.rotate(rot * dsc(2))
+        DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+        DrawingUtil.drawLine(context, 0, -size, size * 0.5 * dsc(1), -size + size * dsc(1))
+        context.restore()
+    }
+
+    static drawLSRSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineSailRotSide(context, scale)
+    }
+}
