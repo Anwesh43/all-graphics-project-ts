01122c1eb14c8556771279d039a74723c08d9d11
Anwesh43 <anweshthecool0@gmail.com>
2022-09-11 02:55:37 +0530

    cross-line-square-holder: adding fixes 3

--- cross-line-square-holder.ts
+++ cross-line-square-holder.ts
@@ -5,8 +5,8 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4 
-const scGap : number = 0.04 / parts 
+const parts : number = 6 
+const scGap : number = 0.06 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 5.9 
 const delay : number = 20 
