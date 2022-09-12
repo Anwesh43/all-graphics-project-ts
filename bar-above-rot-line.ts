6741ffc91275792dba9c46d6a16941aa3a64980e
Anwesh43 <anweshthecool0@gmail.com>
2022-09-12 15:55:53 +0530

    bar-above-rot-line: adding BARLNode class

--- bar-above-rot-line.ts
+++ bar-above-rot-line.ts
@@ -136,3 +136,45 @@ class Animator {
         }
     }
 }
+
+class BARLNode {
+
+    prev : BARLNode
+    next : BARLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BARLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBARLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BARLNode {
+        var curr : BARLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
