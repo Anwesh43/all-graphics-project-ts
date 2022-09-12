6e02c7e9d4fc9a8c006039c93059963682d8ba8b
Anwesh43 <anweshthecool0@gmail.com>
2022-09-12 15:57:16 +0530

    bar-above-rot-line: adding BarAboveRotLine class

--- bar-above-rot-line.ts
+++ bar-above-rot-line.ts
@@ -178,3 +178,26 @@ class BARLNode {
         return this 
     }
 }
+
+class BarAboveRotLine {
+
+    curr : BARLNode = new BARLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
