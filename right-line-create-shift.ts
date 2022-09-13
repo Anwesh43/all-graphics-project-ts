96c3c0ee1ffa2d00703d494d2a2d6d953231c460
Anwesh43 <anweshthecool0@gmail.com>
2022-09-13 19:16:55 +0530

    right-line-create-shift: adding RLCSNode class

--- right-line-create-shift.ts
+++ right-line-create-shift.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class RLCSNode {
+
+    prev : RLCSNode 
+    next : RLCSNode
+    state : State = new State() 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLCSNode(this.i +1 )
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRLCSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RLCSNode {
+        var curr : RLCSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
