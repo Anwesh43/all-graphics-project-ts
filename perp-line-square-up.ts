c2f3bd3f1ed69e0cc8d8fa857a7830b79882f47f
Anwesh43 <anweshthecool0@gmail.com>
2022-09-16 15:08:53 +0530

    perp-line-square-up: adding PeepLineSquareUp class

--- perp-line-square-up.ts
+++ perp-line-square-up.ts
@@ -184,3 +184,26 @@ class PLSUNode {
         return this 
     }
 }
+
+class PerpLineSquareUp {
+
+    curr : PLSUNode = new PLSUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
