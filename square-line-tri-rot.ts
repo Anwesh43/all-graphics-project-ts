ad4f9ac382ee2ef20a8f40879613a50632006d0e
Anwesh43 <anweshthecool0@gmail.com>
2022-09-17 13:36:46 +0530

    square-line-tri-rot: adding SLTRNode class

--- square-line-tri-rot.ts
+++ square-line-tri-rot.ts
@@ -132,3 +132,45 @@ class Animator {
         }
     }
 }
+
+class SLTRNode {
+
+    prev : SLTRNode 
+    next : SLTRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+    
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLTRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLTRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SLTRNode {
+        var curr : SLTRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
