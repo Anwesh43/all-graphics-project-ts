c1d3bb44db555e2db69b56774fc11fb29c506e9c
Anwesh43 <anweshthecool0@gmail.com>
2022-09-18 05:55:54 +0530

    fan-mid-ball-rot-shift: adding constants

---
+++ fan-mid-ball-rot-shift.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const rFactor : number = 18.9 
+const barFactor : number = 4.9 
+const delay : number = 20 
+const deg : number = 2 * Math.PI / 3
+const rot : number = 2 * Math.PI 
+const backColor : string = "#bdbdbd"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
