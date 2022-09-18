2a598afb2dd9a30268e1d05b9d123b0d05537347
Anwesh43 <anweshthecool0@gmail.com>
2022-09-18 06:57:51 +0530

    fan-mid-ball-rot-shift: adding FanMidBallRotShift class

--- fan-mid-ball-rot-shift.ts
+++ fan-mid-ball-rot-shift.ts
@@ -176,3 +176,26 @@ class FMBRSNode {
         return this 
     }
 }
+
+class FanMidBallRotShift {
+
+    curr : FMBRSNode = new FMBRSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
