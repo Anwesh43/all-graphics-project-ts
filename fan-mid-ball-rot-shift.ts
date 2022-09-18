e5fd0606ac5b96c4202bc68430537ebdf7ea6805
Anwesh43 <anweshthecool0@gmail.com>
2022-09-18 06:22:21 +0530

    fan-mid-ball-rot-shift: adding DrawingUtil class

--- fan-mid-ball-rot-shift.ts
+++ fan-mid-ball-rot-shift.ts
@@ -26,3 +26,34 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawFanMidBallRotShift(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const barSize : number = Math.min(w, h) / barFactor 
+        const r : number = Math.min(w, h) / rFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + r + barSize) * dsc(3), h / 2)
+        context.rotate(rot * dsc(3))
+        DrawingUtil.drawCircle(context, 0, (h / 2 + r) * (dsc(1) - 1), r)
+        for (let j = 0; j < 3; j++) {
+            context.save()
+            context.rotate(deg * dsc(2) * j)
+            context.fillRect(r, -r / 2, barSize * dsc(0), r)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawFMBRSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawFanMidBallRotShift(context, scale)
+    }
+}
