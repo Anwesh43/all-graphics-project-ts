81bcf31f09951718f1e3818ab50887af46674d07
Anwesh43 <anweshthecool0@gmail.com>
2022-09-18 06:55:31 +0530

    fan-mid-ball-rot-shift: adding FMBRSNode class

--- fan-mid-ball-rot-shift.ts
+++ fan-mid-ball-rot-shift.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class FMBRSNode {
+
+    prev : FMBRSNode 
+    next : FMBRSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new FMBRSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawFMBRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : FMBRSNode {
+        var curr : FMBRSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
