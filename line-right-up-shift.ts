2f2aed3c6227f4f6de52f0842f09729d0897756a
Anwesh43 <anweshthecool0@gmail.com>
2022-09-19 17:49:58 +0530

    line-right-up-shift: adding LineRightUpShift class

--- line-right-up-shift.ts
+++ line-right-up-shift.ts
@@ -176,3 +176,26 @@ class LRUSNode {
     }
     
 }
+
+class LineRightUpShift {
+
+    curr : LRUSNode = new LRUSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
