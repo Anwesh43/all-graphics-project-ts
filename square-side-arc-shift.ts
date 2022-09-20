37bb07e8bccb35b5a5668935ec331e7a93092426
Anwesh43 <anweshthecool0@gmail.com>
2022-09-20 17:23:41 +0530

    square-side-arc-shift: adding DrawingUtil class

--- square-side-arc-shift.ts
+++ square-side-arc-shift.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#D50000",
     "#43A047"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const sizeFactor : number = 5.9 
 const rFactor : number = 22.2 
@@ -24,5 +24,33 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawSquareSideArcShift(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const r : number = Math.min(w, h) / rFactor  
+        const dsc : (number) => number = (scale : number) : number => ScaleUtil.divideScale(scale, 0, parts)
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2 + size) * dsc(3))
+        context.rotate(-rot * dsc(2))
+        context.fillRect(-size, -size / 2, size * dsc(0), size)
+        for (let j = 0; j < 2; j++) {
+            DrawingUtil.drawCircle(context, 0, -h / 2  -r + (h / 2 + size / 2) * dsc(0) - (size - r) * dsc(3), r)
+        }
+        context.restore()
+    }
+
+    static drawSSASNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i] 
+        DrawingUtil.drawSquareSideArcShift(context, scale)
     }
 }
