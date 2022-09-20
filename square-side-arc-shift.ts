95e648edf5ba375ea5017229a88f244f73209c9d
Anwesh43 <anweshthecool0@gmail.com>
2022-09-20 15:57:03 +0530

    square-side-arc-shift: adding constants

---
+++ square-side-arc-shift.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const sizeFactor : number = 5.9 
+const rFactor : number = 22.2 
+const dealy : number = 20 
+const backColor : string = "#BDBDBD"
+const strokeFactor : number = 90 
+const rot : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
