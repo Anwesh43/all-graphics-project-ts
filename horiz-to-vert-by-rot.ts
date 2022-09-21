a9eb77ba2cb4d5690628a3b00ad30a99d7124580
Anwesh43 <anweshthecool0@gmail.com>
2022-09-21 14:39:08 +0530

    horiz-to-vert-by-rot: adding HTVRNode class

--- horiz-to-vert-by-rot.ts
+++ horiz-to-vert-by-rot.ts
@@ -135,3 +135,45 @@ class State {
         }
     }
 }
+
+class HTVRNode {
+
+    next : HTVRNode 
+    prev : HTVRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new HTVRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawHTVRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : HTVRNode {
+        var curr : HTVRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
