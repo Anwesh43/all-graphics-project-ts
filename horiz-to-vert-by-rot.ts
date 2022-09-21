146724d03a120d3ee85be3677f8dddc579c57c29
Anwesh43 <anweshthecool0@gmail.com>
2022-09-21 14:41:22 +0530

    horiz-to-vert-by-rot: adding HorizToVertByRot class

--- horiz-to-vert-by-rot.ts
+++ horiz-to-vert-by-rot.ts
@@ -177,3 +177,26 @@ class HTVRNode {
         return this 
     }
 }
+
+class HorizToVertByRot {
+
+    curr : HTVRNode = new HTVRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
