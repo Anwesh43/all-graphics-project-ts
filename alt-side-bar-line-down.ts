aebc9aa19d4aa5ede656e2cc3bdaa171dcdd2628
Anwesh43 <anweshthecool0@gmail.com>
2022-09-22 13:37:53 +0530

    alt-side-bar-line-down: adding Stage class

--- alt-side-bar-line-down.ts
+++ alt-side-bar-line-down.ts
@@ -66,3 +66,34 @@ class DrawingUtil {
         DrawingUtil.drawAltSideBarLineDown(context, scale)
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
