39aa9f10d801475aa6f3fe7090fa8a145de72347
Anwesh43 <anweshthecool0@gmail.com>
2022-09-22 14:20:35 +0530

    alt-side-bar-line-down: adding ASBLDNode class

--- alt-side-bar-line-down.ts
+++ alt-side-bar-line-down.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class ASBLDNode {
+
+    prev : ASBLDNode 
+    next : ASBLDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ASBLDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawASBLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) {
+        var curr : ASBLDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
