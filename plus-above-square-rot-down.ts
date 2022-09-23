f66617c1e6ded9b6341b096c5b2ee98feaabe5f1
Anwesh43 <anweshthecool0@gmail.com>
2022-09-23 13:40:09 +0530

    plus-above-square-rot-down: adding DrawingUtil class

--- plus-above-square-rot-down.ts
+++ plus-above-square-rot-down.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+    
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawPlusAboveSquareRotDown(context : CanvasRenderingContext2D, scale : number)  {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3) , () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(j * (Math.PI / 2))
+                    DrawingUtil.drawLine(context, -size * 0.5 * dsc(0), 0, size * 0.5 * dsc(0), 0)
+                })
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    const a : number = size * dsc(1) * 0.5
+                    context.scale(1 - 2 * j, 1 - 2 * j)
+                    context.fillRect(-a, -a, a, a)
+                })
+            }
+        })       
+    }
+
+    static drawPASRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPlusAboveSquareRotDown(context, scale)
+    }
+}
