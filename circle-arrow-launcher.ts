f7bdc56b9a90926d078cd677ccf463353ee7150d
Anwesh43 <anweshthecool0@gmail.com>
2022-09-24 14:00:20 +0530

    circle-arrow-launcher: adding DrawingUtil class

--- circle-arrow-launcher.ts
+++ circle-arrow-launcher.ts
@@ -26,3 +26,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+    
+    static drawTriArrow(context : CanvasRenderingContext2D, x : number, y : number, size : number, sc1 : number, sc2 : number) {
+        context.save()
+        context.translate(x, y)
+        context.rotate(sc2 * Math.PI )
+        context.beginPath()
+        context.moveTo(0, -size / 2)
+        context.lineTo(size, 0)
+        context.lineTo(0, size / 2)
+        context.lineTo(0, -size / 2)
+        context.clip()
+        context.fillRect(0,  -size / 2, size * sc1, size)
+        context.restore()
+    }
+
+    
+
+    static drawCircleArrowLauncher(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, 0, parts)
+        const r : number = Math.min(w, h) / sizeFactor 
+        const size : number = Math.min(w, h) / triSizeFactor
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawCircle(context, 0, (h / 2 + r) * dsc(4), r * dsc(0))
+        for (let j = 0 ; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawTriArrow(context, -w / 2 + (w / 2 - r) * dsc(2) - (w / 2 + size) * dsc(3), 0, size, dsc(1), dsc(2))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawCALNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawCircleArrowLauncher(context, scale)
+    }
+}
