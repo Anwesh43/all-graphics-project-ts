903520e258267c95624171bda3028e3a4b91cfeb
Anwesh43 <anweshthecool0@gmail.com>
2022-09-27 20:06:23 +0530

    rect-bar-biside-exploder: adding RBBSENode class

--- rect-bar-biside-exploder.ts
+++ rect-bar-biside-exploder.ts
@@ -136,3 +136,45 @@ class Animator {
         }
     }
 }
+
+class RBBSENode {
+
+    prev : RBBSENode 
+    next : RBBSENode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RBBSENode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRBBSENode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RBBSENode {
+        var curr : RBBSENode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
