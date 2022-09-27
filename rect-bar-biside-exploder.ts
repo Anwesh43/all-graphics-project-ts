a1ab673e7b168554a1a32e382c7d3ced76657b44
Anwesh43 <anweshthecool0@gmail.com>
2022-09-27 19:40:40 +0530

    rect-bar-biside-exploder: adding DrawingUtil class

--- rect-bar-biside-exploder.ts
+++ rect-bar-biside-exploder.ts
@@ -24,3 +24,38 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawBar(context  : CanvasRenderingContext2D, size : number, sc1 : number, sc2 : number) {
+        context.save()
+        context.translate(0, -size / 2)
+        context.fillRect(-size * 0.5 * sc1, -size / 2, size * sc1, size)
+        context.rotate(sc2 * Math.PI / 2)
+        context.fillRect(-size * 0.5 * sc1, -1.5 * size, size * sc1, size)
+        context.restore()
+    }
+
+    static drawRectBarBiSideExploder(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number)  : number => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h)
+        for (let j = 0; j < 3; j++) {
+            const k : number = j % 2 
+            const n : number = Math.floor(j / 2)
+            const ki : number = (1 - k)
+            context.save()
+            context.translate((w / 2 + size / 2) * (1 - 2 * n) * ki * dsc(2), -(h / 2 + size / 2) * (1 - k) * dsc(3))
+            DrawingUtil.drawBar(context, size, dsc(0), dsc(1))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawRBBSENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i] 
+        context.fillRect(0, 0, w, h)
+        DrawingUtil.drawRectBarBiSideExploder(context, scale)
+    }
+}
