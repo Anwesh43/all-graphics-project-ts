5ddea2f494123c1ed967c68c27f8f523e132e416
Anwesh43 <anweshthecool0@gmail.com>
2022-10-01 13:45:35 +0530

    square-corner-circles: adding Stage class

--- square-corner-circles.ts
+++ square-corner-circles.ts
@@ -14,6 +14,7 @@ const delay : number = 20
 const deg : number = Math.PI / 2
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
     
@@ -56,5 +57,38 @@ class DrawingUtil {
     static drawSCCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
         context.fillStyle = colors[i]
         DrawingUtil.drawSquareCornerCircle(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
