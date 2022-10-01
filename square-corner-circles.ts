3a08435243bc43dfb5be12b3d84c9e62bfe98b5c
Anwesh43 <anweshthecool0@gmail.com>
2022-10-01 14:53:20 +0530

    square-corner-circles: adding SquareCornerCircle class

--- square-corner-circles.ts
+++ square-corner-circles.ts
@@ -178,3 +178,26 @@ class SCCNode {
         return this 
     }
 }
+
+class SquareCornerCircle {
+    curr : SCCNode = new SCCNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
+
