3dddf9c6c91e61239a3b44f20d4d6fd79669f216
Anwesh43 <anweshthecool0@gmail.com>
2022-10-03 13:35:55 +0530

    side-open-rect-left: adding DrawingUtil class

--- side-open-rect-left.ts
+++ side-open-rect-left.ts
@@ -25,3 +25,37 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSideOpenRectLeft(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 - (w / 2) * dsc(4), h / 2)
+        context.rotate(-rot * dsc(3))
+        DrawingUtil.drawLine(context, -size * 0.5 * dsc(0), 0, size * 0.5 * dsc(0), 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, -size * 0.5 * dsc(1))
+            context.restore()
+        }
+        DrawingUtil.drawLine(context, -size / 2, -size / 2, -size / 2 + size * dsc(2), -size / 2 + (size / 2) * dsc(2))
+        context.restore()
+    }
+
+    static drawSORLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSideOpenRectLeft(context, scale)
+    }
+}
