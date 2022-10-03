444b1dfd57d65425f67e557d2bafa2e133bc00d4
Anwesh43 <anweshthecool0@gmail.com>
2022-10-03 13:41:15 +0530

    side-open-rect-left: adding Animator class

--- side-open-rect-left.ts
+++ side-open-rect-left.ts
@@ -93,3 +93,23 @@ class Stage {
         stage.handleTap()
     }
 }
+
+class Animator {
+
+    animated : boolean = false 
+    interval : number 
+
+    start(cb : () => void) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            clearInterval(this.interval)
+            this.animated = true 
+        }
+    }
+}
