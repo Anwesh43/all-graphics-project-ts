76b30ec456a72858e468e6f91dd9409831fdf212
Anwesh43 <anweshthecool0@gmail.com>
2022-10-03 13:46:05 +0530

    side-open-rect-left: adding SORLNode class

--- side-open-rect-left.ts
+++ side-open-rect-left.ts
@@ -138,3 +138,44 @@ class State {
         }
     }
 }
+class SORLNode {
+
+    prev : SORLNode 
+    next : SORLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+    
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SORLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSORLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SORLNode {
+        var curr : SORLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
