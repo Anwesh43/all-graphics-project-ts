23a4a390a760cc7280df61f4829fe480ee4fdc18
Anwesh43 <anweshthecool0@gmail.com>
2022-10-05 14:41:51 +0530

    m-to-e-rot: adding fixes 2

--- m-to-e-rot.ts
+++ m-to-e-rot.ts
@@ -6,7 +6,7 @@ const colors : Array<string> = [
     "#8BC34A"
 ]
 const parts : number = 4
-const scGap : number = 0.04 / parts 
+const scGap : number = 0.02 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 5.9 
 const rot : number = Math.PI / 2
