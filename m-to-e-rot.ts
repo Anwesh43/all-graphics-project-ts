156298e1f15d998046dda4fb5062c56b36ea24fe
Anwesh43 <anweshthecool0@gmail.com>
2022-10-05 14:02:06 +0530

    m-to-e-rot: adding MTERNode class

--- m-to-e-rot.ts
+++ m-to-e-rot.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class MTERNode {
+
+    prev : MTERNode 
+    next : MTERNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new MTERNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawMTERNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : MTERNode {
+        var curr : MTERNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
