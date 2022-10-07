6a7557573699649f30deb27cc9a24d3ff005c06d
Anwesh43 <anweshthecool0@gmail.com>
2022-10-07 15:26:08 +0530

    line-block-square-mover: adding LineBlockSquaeMover class

--- line-block-square-mover.ts
+++ line-block-square-mover.ts
@@ -182,3 +182,26 @@ class LBSMNode {
         return this 
     }
 }
+
+class LineBlockSquareMover {
+
+    curr : LBSMNode = new LBSMNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
