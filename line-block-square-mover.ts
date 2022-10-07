64a53ed363e805e6184afb266e4e17c6ea7fca77
Anwesh43 <anweshthecool0@gmail.com>
2022-10-07 15:11:09 +0530

    line-block-square-mover: adding DrawingUtil class

--- line-block-square-mover.ts
+++ line-block-square-mover.ts
@@ -25,3 +25,42 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBlockSquareMover(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2 - (w / 2 + size) * dsc(3), h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+
+            context.scale(1, 1 - 2 * j)
+            context.translate(0, size / 2)
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawLine(context, 0, 0, size * dsc(1), 0)
+            context.restore()
+        }
+        context.fillRect(-size * dsc(0), -size / 2, size * dsc(0), size)
+        context.restore() 
+    }
+
+    static drawLBSMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineBlockSquareMover(context, scale)
+    }
+}
