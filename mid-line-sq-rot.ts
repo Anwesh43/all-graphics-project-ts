7bf285e124bd31a994323e07c5cf54905f6fc8c0
Anwesh43 <anweshthecool0@gmail.com>
2022-10-08 15:38:35 +0530

    mid-line-sq-rot: adding constants

---
+++ mid-line-sq-rot.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#F44336",
+    "#2196F3",
+    "#FF9800",
+    "#795548",
+    "#8BC34A"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 8.9 
+const rot : number = Math.PI / 2
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
