14000451a0e3212fc7f7fe5cfeca5c05f16566f1
Anwesh43 <anweshthecool0@gmail.com>
2022-10-08 15:47:54 +0530

    mid-line-sq-rot: adding DrawingUtil class

--- mid-line-sq-rot.ts
+++ mid-line-sq-rot.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawMidLineSqRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + 2 * size) * dsc(4))
+        context.rotate(rot * dsc(3))
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2 - size * 0.5 * dsc(1), 0)
+            context.restore()
+        }
+        context.save()
+        context.translate(0, size * 0.5 * dsc(2))
+        context.fillRect(-size / 2, -size * 0.5 * dsc(0), size, size * 0.5 * dsc(0))
+        context.restore()
+        context.restore()
+    }
+
+    static drawMLSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawMidLineSqRot(context, scale)
+    }
+}
