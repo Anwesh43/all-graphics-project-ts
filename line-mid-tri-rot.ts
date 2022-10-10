9f2f6deeb074f00b8de4531c9ff3939ce9ad122c
Anwesh43 <anweshthecool0@gmail.com>
2022-10-10 15:58:20 +0530

    line-mid-tri-rot: adding DrawingUtil class

--- line-mid-tri-rot.ts
+++ line-mid-tri-rot.ts
@@ -26,3 +26,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineMidTriRot(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const triSize : number = Math.min(w, h) / triSizeFactor
+        context.save()
+        context.translate(w / 2 + (w / 2 + triSize) * dsc(3), h / 2)
+        context.rotate(rot * dsc(1))
+        DrawingUtil.drawLine(context, -size * 0.5 * dsc(0), 0, size * 0.5 * dsc(0), 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, triSize, 0, triSize * (1 - dsc(2)), -triSize * dsc(2))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLMTRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLMTRNode(context, i, scale)
+    }
+}
