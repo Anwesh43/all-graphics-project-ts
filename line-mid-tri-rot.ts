4dc45b4725696f7976726de7bc659b2ea47fe871
Anwesh43 <anweshthecool0@gmail.com>
2022-10-10 16:16:47 +0530

    line-mid-tri-rot: adding LMTRNode class

--- line-mid-tri-rot.ts
+++ line-mid-tri-rot.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class LMTRNode {
+
+    prev : LMTRNode 
+    next : LMTRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LMTRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLMTRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    } 
+
+    getNext(dir : number, cb : () => void) : LMTRNode {
+        var curr : LMTRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
