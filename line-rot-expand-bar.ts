4f49880db3ec0c7fdaf079cb356b0e0861ad89bf
Anwesh43 <anweshthecool0@gmail.com>
2022-10-11 16:12:10 +0530

    line-rot-expand-bar: adding DrawingUtil class

--- line-rot-expand-bar.ts
+++ line-rot-expand-bar.ts
@@ -25,3 +25,36 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRotExpandBar(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2 - (w / 2) * dsc(3), h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            context.rotate(rot * (1 - j) * dsc(1))
+            DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLREBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineRotExpandBar(context, scale)
+    }
+}
