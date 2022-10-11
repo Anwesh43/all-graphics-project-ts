9a64de34f7e17379cdef1082ae4d67ed86619df3
Anwesh43 <anweshthecool0@gmail.com>
2022-10-11 16:23:36 +0530

    line-rot-expand-bar: adding LineRotExpand class

--- line-rot-expand-bar.ts
+++ line-rot-expand-bar.ts
@@ -158,3 +158,26 @@ class LREBNode {
         return this 
     }
 }
+
+class LineRotExpand {
+
+    curr : LREBNode = new LREBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
