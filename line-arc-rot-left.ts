3bd048173bff4e0db9dea96619d843b8cf78016d
Anwesh43 <anweshthecool0@gmail.com>
2022-10-12 14:16:17 +0530

    line-arc-rot-left: adding LARLNode class

--- line-arc-rot-left.ts
+++ line-arc-rot-left.ts
@@ -144,3 +144,46 @@ class Animator {
         }
     }
 }
+
+class LARLNode {
+
+    prev : LARLNode
+    next : LARLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LARLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLARLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LARLNode {
+        var curr : LARLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
+
