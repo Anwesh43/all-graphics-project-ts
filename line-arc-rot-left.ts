9c0fb28dc27053a1b9bd0e998b21dfa75d140889
Anwesh43 <anweshthecool0@gmail.com>
2022-10-12 13:20:55 +0530

    line-arc-rot-left: adding constants

---
+++ line-arc-rot-left.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const rot : number = Math.PI / 2
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
