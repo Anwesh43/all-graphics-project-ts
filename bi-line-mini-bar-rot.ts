dd72366be79730bc0e685fcdf79fbe1c70db346e
Anwesh43 <anweshthecool0@gmail.com>
2022-10-13 15:49:56 +0530

    bi-line-mini-bar-rot: adding DrawingUtil class

--- bi-line-mini-bar-rot.ts
+++ bi-line-mini-bar-rot.ts
@@ -14,6 +14,8 @@ const barWFactor : number = 21.2
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const rot : number = Math.PI 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
 
 class ScaleUtil {
 
@@ -23,5 +25,44 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBilineMiniBar(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        const barW : number = Math.min(w, h) / barWFactor 
+        const barH : number = Math.min(w, h) / barHFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(0, -(h / 2) * (1 - 2 * j) * dsc(3)) 
+            context.rotate(rot * dsc(1) * j)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+            context.fillRect(0, -barH * dsc(2), barW, barH * dsc(2))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBLMBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBilineMiniBar(context, scale)
     }
 }
