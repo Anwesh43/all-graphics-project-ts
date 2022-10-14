22b282da8cee2ce4b0a0ff9d6d9f1adf2e076e2b
Anwesh43 <anweshthecool0@gmail.com>
2022-10-14 14:16:52 +0530

    cone-line-arc: adding DrawingUtil class

--- cone-line-arc.ts
+++ cone-line-arc.ts
@@ -10,9 +10,9 @@ const scGap : number = 0.04 / parts
 const strokeFactor : number = 90 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2
-const sizeFactor : number = 5.9 
-const coneHFactor : number = 19.9 
+const rot : number = Math.PI
+const sizeFactor : number = 3.9 
+const coneHFactor : number = 23.9 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 
@@ -24,5 +24,53 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawEllipse(context : CanvasRenderingContext2D, cx : number, cy : number, r1 : number, r2 : number, sc : number) {
+        context.beginPath()
+        for (let j = 0; j < 360 * sc; j++) {
+            const x = cx + r1 * Math.cos(j * Math.PI / 180) 
+            const y = cx + r2 * Math.sin(j * Math.PI / 180)
+            if (j == 0) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+    }
+
+    static drawConeLineArc(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const ch : number = Math.min(w, h) / coneHFactor
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2) * dsc(3))
+        context.rotate(rot * dsc(2))
+        for (let j = 0; j < 2; j++) {
+            DrawingUtil.drawLine(context, 0, 0, -(size / 3) * dsc(0), -size * dsc(0))
+        }
+        DrawingUtil.drawEllipse(context, 0, 0, size / 3, ch, dsc(1))
+        context.restore()
+    }
+
+    static drawCLANode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawConeLineArc(context, scale)
     }
 }
