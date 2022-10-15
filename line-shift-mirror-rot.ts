bb2b6d92b873f71a00cc9df32de4dcf93cfffc55
Anwesh43 <anweshthecool0@gmail.com>
2022-10-15 15:36:02 +0530

    line-shift-mirror-rot: adding LineShiftMirrorRot class

--- line-shift-mirror-rot.ts
+++ line-shift-mirror-rot.ts
@@ -180,3 +180,26 @@ class LSMRNode {
         return this 
     }
 }
+
+class LineShiftMirrorRot {
+
+    curr : LSMRNode = new LSMRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
