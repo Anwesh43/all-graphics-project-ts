353d93c9ba9afaa9c995c58131f8bbd58826cb2f
Anwesh43 <anweshthecool0@gmail.com>
2022-10-15 15:24:05 +0530

    line-shift-mirror-rot: adding DrawingUtil class

--- line-shift-mirror-rot.ts
+++ line-shift-mirror-rot.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#D50000",
     "#43A047"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 4.9 
@@ -23,5 +23,41 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineShiftMirrorRot(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * dsc(4), h / 2)
+        context.rotate(rot * dsc(3))
+        DrawingUtil.drawLine(context, -size * 0.5 * dsc(0), 0, size * 0.5 * dsc(0), 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(-size / 2 + size * j * dsc(2), 0)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(1))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLSMRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLSMRNode(context, i, scale)
     }
 }
