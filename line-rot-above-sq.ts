4621df17587b6a963aaa63fae715de111805840a
Anwesh43 <anweshthecool0@gmail.com>
2022-10-19 15:07:58 +0530

    line-rot-above-sq: adding DrawingUtil class

--- line-rot-above-sq.ts
+++ line-rot-above-sq.ts
@@ -25,3 +25,42 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineRotAboveSq(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h  / 2 + (h / 2 + size) * dsc(3), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(deg * j * dsc(1))
+                    DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                })
+            }
+            context.fillRect(0, 0, size, size * dsc(2))
+        })
+    }
+
+    static drawaLRASNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawLineRotAboveSq(context, scale)
+    }
+}
