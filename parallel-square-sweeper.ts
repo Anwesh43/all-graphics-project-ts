0f4ab9db697fa8c0130e09ed71c6c22e58e73802
Anwesh43 <anweshthecool0@gmail.com>
2022-10-20 16:56:33 +0530

    parallel-square-sweeper: adding DrawingUtil class

--- parallel-square-sweeper.ts
+++ parallel-square-sweeper.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawParallelSquareSweeper(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(1), -size * 0.5 + size * j, () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+            DrawingUtil.drawXY(context, 0, -h / 2 + (h / 2 + size / 2) * dsc(0), () => {
+                context.fillRect(0, 0, size, size)
+            })
+        })
+    }
+
+    static drawPSSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawParallelSquareSweeper(context, scale)
+    }
+}
