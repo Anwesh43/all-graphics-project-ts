0885f58cefc8a8404db97560c881dc3b9838c889
Anwesh43 <anweshthecool0@gmail.com>
2022-10-22 12:26:07 +0530

    vert-horiz-line-to-t: adding DrawingUtil class

--- vert-horiz-line-to-t.ts
+++ vert-horiz-line-to-t.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawVertHorizLineToT(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const upSize : number = size * (1 - dsc(3))
+
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawXY(context, -size / 2 + (w / 2 + size / 2) * dsc(1), 0, () => {
+                DrawingUtil.drawLine(context, -upSize / 2, 0, upSize / 2, 0)
+            })
+            DrawingUtil.drawXY(context, 0, -h / 2 + (h / 2) * dsc(0), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -upSize)
+            })
+        })
+    }
+
+    static drawVHLTNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawVHLTNode(context, i, scale)
+    }
+}
