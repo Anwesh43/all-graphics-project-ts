0dad73257ea39552c3f153bad0ccd7aa0cfdaa6f
Anwesh43 <anweshthecool0@gmail.com>
2022-10-24 18:00:35 +0530

    rocket-launcher-line: adding RLLNode class

--- rocket-launcher-line.ts
+++ rocket-launcher-line.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class RLLNode {
+
+    prev : RLLNode 
+    next : RLLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRLLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RLLNode {
+        var curr : RLLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
