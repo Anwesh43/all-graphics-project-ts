b4008b45006b40807a4fe5f6b59052641ea41fb0
Anwesh43 <anweshthecool0@gmail.com>
2022-10-26 15:58:41 +0530

    square-up-bi-divide: adding SquareUpBiDivide class

--- square-up-bi-divide.ts
+++ square-up-bi-divide.ts
@@ -180,3 +180,26 @@ class SUBDNode {
         return this 
     }
 }
+
+class SquareUpBiDivide {
+
+    curr : SUBDNode = new SUBDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
