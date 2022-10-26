42503df3f7e609bc6a43d30f2b47c8db20c26f78
Anwesh43 <anweshthecool0@gmail.com>
2022-10-26 15:36:50 +0530

    square-up-bi-divide: adding DrawingUtil class

--- square-up-bi-divide.ts
+++ square-up-bi-divide.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context :CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+    
+    static drawSquareUpBiDivide(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1) 
+            context.translate(w * 0.5 * dsc(3), 0)
+            context.save()
+            context.translate((-size) * (1 - dsc(2)) * j, 0)
+            context.fillRect(0, -size * 0.5 * dsc(1), size / 2, size * 0.5 * dsc(1))
+            context.restore()
+            DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(0), 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSUBDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSUBDNode(context, i, scale)
+    }
+}
