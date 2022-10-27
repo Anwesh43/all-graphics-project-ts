4489dea8e76eb8916c99fe39ccc6633af1d4f4a1
Anwesh43 <anweshthecool0@gmail.com>
2022-10-27 13:41:04 +0530

    bar-rot-break-down: adding DrawingUtil class

--- bar-rot-break-down.ts
+++ bar-rot-break-down.ts
@@ -25,3 +25,36 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawBarRotBreakDown(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barH : number = Math.min(w, h) / barHFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < 2; j++) {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.scale(1 - 2 * j, 1)
+                DrawingUtil.drawXY(context, w * 0.5 * dsc(2), (h / 2 + size) * dsc(3), () => {
+                    context.rotate(rot * dsc(1))
+                    context.fillRect(-size * 0.5 * dsc(0), -barH, size * dsc(0), barH)
+                })
+            })
+        }       
+        context.restore()
+    }
+
+    static drawBRBDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarRotBreakDown(context, scale)
+    }
+}
