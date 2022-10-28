9f029dd58869c83c8d7f50dde7055f606dd0b92b
Anwesh43 <anweshthecool0@gmail.com>
2022-10-28 15:27:29 +0530

    line-sq-rot-down: adding LineSqRotDown class

--- line-sq-rot-down.ts
+++ line-sq-rot-down.ts
@@ -154,7 +154,7 @@ class LSRDNode {
         this.addNeighbor()
     }
 
-    drawLSRDNode(context : CanvasRenderingContext2D) {
+    draw(context : CanvasRenderingContext2D) {
         DrawingUtil.drawLSRDNode(context, this.i, this.state.scale)
     }
 
@@ -176,5 +176,28 @@ class LSRDNode {
         }
         cb()
         return this 
+    }
+}
+
+class LineSqRotDown {
+
+    curr : LSRDNode = new LSRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
     }
 }
