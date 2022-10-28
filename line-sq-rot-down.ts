801f3c1c5ac263d23f9f838274b61c0874638597
Anwesh43 <anweshthecool0@gmail.com>
2022-10-28 15:13:28 +0530

    line-sq-rot-down: adding DrawingUtil class

--- line-sq-rot-down.ts
+++ line-sq-rot-down.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#D50000",
     "#43A047"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
 const sqSizeFactor : number = 6.9 
@@ -24,4 +24,38 @@ class ScaleUtil {
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSqRotDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sqSizeFactor
+        const dsc : (number) => number =  (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate((w / 2) * dsc(1), h / 2 + (h / 2) * dsc(4))
+        DrawingUtil.drawLine(context, -size + size * dsc(3), 0, 0, 0)
+        context.save()
+        context.rotate(rot * dsc(2))
+        context.fillRect(0, -size, size * dsc(0), size)
+        context.restore()
+        context.restore() 
+    }
+
+    static drawLSRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawLineSqRotDown(context, scale)
+    } 
 }
