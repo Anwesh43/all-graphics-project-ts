b99eb6d60e86c4c1332bc8ab094249f9862b6df2
Anwesh43 <anweshthecool0@gmail.com>
2022-10-30 11:43:51 +0530

    bar-parallel-join-down: adding DrawingUtil class

--- bar-parallel-join-down.ts
+++ bar-parallel-join-down.ts
@@ -26,3 +26,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarParallelJoinDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const barW : number = Math.min(w, h) / barWFactor  
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + size) * dsc(3))
+        context.rotate(rot * dsc(2))
+        context.save()
+        context.translate(w * 0.5 * (1 - dsc(1)), 0)
+        for (let j = 0; j < 2; j++) {
+            context.save()
+            context.translate(0, (-size / 2 + size * j) * (1 - dsc(2)))
+            DrawingUtil.drawLine(context, 0, 0, size, 0)
+            context.restore()
+        }
+        context.restore()
+        context.fillRect(-barW, size / 2 - size * dsc(0), barW, size * dsc(0))
+        context.restore()
+    }
+
+    static drawBPJDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        context.lineCap = 'round'
+        DrawingUtil.drawBarParallelJoinDown(context, scale)
+    }
+}
