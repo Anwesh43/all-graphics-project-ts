5574cfa54d8c4e30a06b1027c0c8653248f0739b
Anwesh43 <anweshthecool0@gmail.com>
2022-10-31 16:40:14 +0530

    rect-side-up-rot: adding RectSideUpRect class

--- rect-side-up-rot.ts
+++ rect-side-up-rot.ts
@@ -179,3 +179,26 @@ class RSURNode {
         return this 
     }
 }
+
+class RectSideRectUp {
+
+    curr : RSURNode = new RSURNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
