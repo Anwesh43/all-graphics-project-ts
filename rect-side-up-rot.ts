4e9b4e1f0b04eae76ba880631b047ba1703f76a3
Anwesh43 <anweshthecool0@gmail.com>
2022-10-31 16:25:56 +0530

    rect-side-up-rot: adding DrawingUtil class

--- rect-side-up-rot.ts
+++ rect-side-up-rot.ts
@@ -25,3 +25,38 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+    static drawRectSideUpRot(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * dsc(4), h / 2)
+        context.rotate(2 * Math.PI * dsc(4))
+        context.fillRect(-size, 0, size, size * dsc(0))
+        context.save()
+        context.rotate(rot * dsc(2))
+        DrawingUtil.drawLine(context, 0, 0, size * dsc(1), 0)
+        context.restore()
+        context.fillRect(0, 0, size * dsc(3), size)
+        context.restore()
+    }
+
+    static drawRSURNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawRectSideUpRot(context, scale)
+    }
+}
