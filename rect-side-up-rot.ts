998547d02ae822610fc4f3dbd4138334103a48c6
Anwesh43 <anweshthecool0@gmail.com>
2022-10-31 16:38:57 +0530

    rect-side-up-rot: adding RSURNode class

--- rect-side-up-rot.ts
+++ rect-side-up-rot.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class RSURNode {
+
+    prev : RSURNode 
+    next : RSURNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RSURNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRSURNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RSURNode {
+        var curr : RSURNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
