6f55c955212b836872813ea7cd62cfb066b729d8
Anwesh43 <anweshthecool0@gmail.com>
2022-11-01 14:28:38 +0530

    bar-wing-down: adding constatns

---
+++ bar-wing-down.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const deg : number = Math.PI / 3 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 4.9 
+const lSizeFactor : number = 12.9
+const strokeFactor : number = 90 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+ 
