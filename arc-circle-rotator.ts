a1410220d8b3c1ff399ab355103af117b90d7af6
Anwesh43 <anweshthecool0@gmail.com>
2022-11-02 12:20:25 +0530

    arc-circle-rotator: adding ACRNode class

--- arc-circle-rotator.ts
+++ arc-circle-rotator.ts
@@ -161,3 +161,45 @@ class Animator {
     }
 
 }
+
+class ACRNode {
+
+    prev : ACRNode 
+    next : ACRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawACRNode(context, this.i, this.state.scale)
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ACRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : ACRNode {
+        var curr : ACRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
