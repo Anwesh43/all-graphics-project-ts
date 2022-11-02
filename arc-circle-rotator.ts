f50da53fd0b191c8d41cfb6381fcfaed6eece312
Anwesh43 <anweshthecool0@gmail.com>
2022-11-02 12:46:31 +0530

    arc-circle-rotator: adding ArcCircleRotator class

--- arc-circle-rotator.ts
+++ arc-circle-rotator.ts
@@ -203,3 +203,26 @@ class ACRNode {
         return this 
     }
 }
+
+class ArcCircleRotator {
+
+    curr : ACRNode = new ACRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
