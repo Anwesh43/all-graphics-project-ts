ddde38af773521e61991fca05921162834c34efd
Anwesh43 <anweshthecool0@gmail.com>
2022-11-04 01:20:39 +0530

    bar-shift-line-rot: adding BSLRNode class

--- bar-shift-line-rot.ts
+++ bar-shift-line-rot.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class BSLRNode {
+
+    prev : BSLRNode
+    next : BSLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNieghbor()
+    }
+
+    addNieghbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BSLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBSLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BSLRNode {
+        var curr : BSLRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
