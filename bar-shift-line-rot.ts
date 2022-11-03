53e704d8978eccb491340b44e0baf836d884906b
Anwesh43 <anweshthecool0@gmail.com>
2022-11-04 01:12:56 +0530

    bar-shift-line-rot: adding DrawingUtil class

--- bar-shift-line-rot.ts
+++ bar-shift-line-rot.ts
@@ -25,3 +25,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number)  {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawBarShiftLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size / 2) * dsc(4), h / 2, () => {
+            context.fillRect(-size / 2, -size / 4, size * dsc(0), size / 4)
+            DrawingUtil.drawXY(context, size / 2, -size * 0.25 * dsc(2), () => {
+                context.rotate(-rot * dsc(3))
+                DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(1), 0)
+            })
+        })
+    }
+
+    static drawBSLRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarShiftLineRot(context, scale)
+    }
+}
