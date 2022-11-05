f355530cba67a6c5e26a80cf639ea625e75113cc
Anwesh43 <anweshthecool0@gmail.com>
2022-11-05 14:13:35 +0530

    line-break-join-down: adding LBJDNode class

--- line-break-join-down.ts
+++ line-break-join-down.ts
@@ -145,3 +145,47 @@ class Animator {
         }
     }
 }
+
+class LBJDNode {
+
+    prev : LBJDNode
+    next : LBJDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBJDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBJDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb : () => void) : LBJDNode {
+        var curr : LBJDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+
+    }
+}
