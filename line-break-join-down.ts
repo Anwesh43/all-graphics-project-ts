dd69d218bafe24b72cd6ea8f3551e352700a8101
Anwesh43 <anweshthecool0@gmail.com>
2022-11-05 14:27:40 +0530

    line-break-join-down: adding logs

--- line-break-join-down.ts
+++ line-break-join-down.ts
@@ -64,6 +64,7 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
+        console.log("SCALE___SCALE", scale)
         DrawingUtil.drawLineBreakJoinDown(context, scale)
     }
 }
