34276b48c7b4bc1306186975e0bdb2203ebbb4c5
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 13:31:52 +0530

    divide-line-bounce: adding DLBNode class

--- divide-line-bounce.ts
+++ divide-line-bounce.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class DLBNode {
+
+    prev : DLBNode 
+    next : DLBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DLBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDLBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : DLBNode {
+        var curr : DLBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
