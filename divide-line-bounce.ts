e1216ab58fa9b6dd65905967dbb26933c845cb21
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 15:02:22 +0530

    divide-line-bounce: adding Renderer class

--- divide-line-bounce.ts
+++ divide-line-bounce.ts
@@ -211,3 +211,25 @@ class DivideLineBounce {
         this.curr.startUpdating(cb)
     }
 }
+
+class Renderer {
+
+    dlb : DivideLineBounce = new DivideLineBounce()
+    animator : Animator = new Animator()
+
+    render(context : CanvasRenderingContext2D) {
+        this.dlb.draw(context)
+    }
+
+    handleTap(cb : () => void) {
+        this.dlb.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.dlb.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
+    }
+}
