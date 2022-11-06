c6ba4a92f65c60b50c25fe118f240ddbf0cb541e
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 12:47:47 +0530

    divide-line-bounce: adding ScaleUtil class

--- divide-line-bounce.ts
+++ divide-line-bounce.ts
@@ -14,3 +14,18 @@ const sizeFactor : number = 5.9
 const strokeFactor : number = 90 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
