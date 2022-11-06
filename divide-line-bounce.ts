e0ef3846675cef9dcc15e5e3d5d411787411c78b
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 13:13:11 +0530

    divide-line-bounce: adding DrawingUtil class

--- divide-line-bounce.ts
+++ divide-line-bounce.ts
@@ -9,7 +9,7 @@ const parts : number = 4
 const scGap : number = 0.04 / parts 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2 
+const rot : number = Math.PI / 4 
 const sizeFactor : number = 5.9 
 const strokeFactor : number = 90 
 const w : number = window.innerWidth 
@@ -27,5 +27,45 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawDivideLineBounce(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(ScaleUtil.sinify(scale), i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * (1 - 2 * j) * dsc(1))
+                    DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                })
+            }
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(2)), 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, size, 0)
+            })
+        })       
+    }
+
+    static drawDLBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDivideLineBounce(context, scale)
     }
 }
