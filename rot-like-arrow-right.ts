39592148ff29b9217223a7e9e1937e57c9285c31
Anwesh43 <anweshthecool0@gmail.com>
2022-11-07 15:28:38 +0530

    rot-like-arrow-right: adding RotLineArrowRight class

--- rot-like-arrow-right.ts
+++ rot-like-arrow-right.ts
@@ -184,3 +184,26 @@ class RLARNode {
         return this 
     }
 }
+
+class RotLineArrowRight {
+
+    curr : RLARNode = new RLARNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
