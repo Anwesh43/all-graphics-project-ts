f1a4b454cb519c65ac558acac4fcd5b159a75b9a
Anwesh43 <anweshthecool0@gmail.com>
2022-11-07 15:04:22 +0530

    rot-like-arrow-right: adding DrawingUtil class

--- rot-like-arrow-right.ts
+++ rot-like-arrow-right.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#795548",
     "#8BC34A"
 ]
-const rot : number = Math.PI / 2 
+const rot : number = Math.PI / 2 - Math.PI / 12 
 const parts : number = 4
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRotLikeArrowRight(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(2)), 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, size, 0)
+            })
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * dsc(1) * j)
+                    DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+                })
+            }
+        })
+    }
+
+    static drawRLARNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRotLikeArrowRight(context, scale)
     }
 }
