1f94e2d0d784f865264e229c0bd423da09220faf
Anwesh43 <anweshthecool0@gmail.com>
2022-11-08 13:11:16 +0530

    line-rot-drop-square: adding DrawingUtil class

--- line-rot-drop-square.ts
+++ line-rot-drop-square.ts
@@ -25,3 +25,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineRotDropSquare(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1 - 2 * j)
+                    for (let k = 0; k < 2; k++) {
+                        DrawingUtil.drawXY(context, -size, -h * 0.5 * dsc(3), () => {
+                            context.save()
+                            context.rotate(rot * dsc(1))
+                            DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+                            context.restore()
+                            context.fillRect(0, -size, size * dsc(2), size)
+                        })
+                    }
+                })
+            }
+        })
+    }
+
+    static drawLRDSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLRDSNode(context, i, scale)
+    }
+}
