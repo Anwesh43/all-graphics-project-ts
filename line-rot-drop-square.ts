9168d05a15371bfd1c17e0eda65a10dd26ab5f29
Anwesh43 <anweshthecool0@gmail.com>
2022-11-08 14:27:38 +0530

    line-rot-drop-square: adding LRDSNode class

--- line-rot-drop-square.ts
+++ line-rot-drop-square.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class LRDSNode {
+
+    prev : LRDSNode 
+    next : LRDSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRDSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRDSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRDSNode {
+        var curr : LRDSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
