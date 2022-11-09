4f72d7cb8946d37908f022f0aab6401393a109ad
Anwesh43 <anweshthecool0@gmail.com>
2022-11-10 02:06:43 +0530

    line-rot-bar-expand: adding LineRotBarExpand class

--- line-rot-bar-expand.ts
+++ line-rot-bar-expand.ts
@@ -173,3 +173,26 @@ class LRBENode {
         return this 
     }   
 }
+
+class LineRotBarExpand {
+
+    curr : LRBENode = new LRBENode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
