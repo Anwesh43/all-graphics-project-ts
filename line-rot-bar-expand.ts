a36796ead866a625ff64a7465fb5e7067ff2736f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-10 00:37:21 +0530

    line-rot-bar-expand: adding constants

---
+++ line-rot-bar-expand.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 5
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const rot : number = Math.PI / 2
+const backColor : string = "#bdbdbd"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
