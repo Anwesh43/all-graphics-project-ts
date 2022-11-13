52f26e70cf9def3b18dba644b8611fdf7d2247e6
Anwesh43 <anweshthecool0@gmail.com>
2022-11-14 00:00:54 +0530

    line-rot-extend-bar: adding LREBNode  class

--- line-rot-extend-bar.ts
+++ line-rot-extend-bar.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LREBNode {
+
+    prev : LREBNode 
+    next : LREBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LREBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLREBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LREBNode {
+        var curr : LREBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
