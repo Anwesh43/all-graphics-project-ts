6add86dcb0a44024f03d912bc5fb211990779516
Anwesh43 <anweshthecool0@gmail.com>
2022-11-16 17:21:44 +0530

    parallel-line-to-drop: adding DrawingUtil class

--- parallel-line-to-drop.ts
+++ parallel-line-to-drop.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawParallelLineToDrop(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
+            context.rotate(-rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, -size * (1 - dsc(1)), () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+                })
+            }
+        }) 
+    }
+
+    static drawPLTDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawParallelLineToDrop(context, scale)
+    }
+}
