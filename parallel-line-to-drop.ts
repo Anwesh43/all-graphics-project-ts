50738e0bbd55ab7ba29bc506ba9850546b82e1d7
Anwesh43 <anweshthecool0@gmail.com>
2022-11-16 17:54:59 +0530

    parallel-line-to-drop: adding ParallelLineToDrop class

--- parallel-line-to-drop.ts
+++ parallel-line-to-drop.ts
@@ -181,3 +181,26 @@ class PLTDNode {
         return this 
     }
 }
+
+class ParallelLineToDrop {
+
+    curr : PLTDNode = new PLTDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
