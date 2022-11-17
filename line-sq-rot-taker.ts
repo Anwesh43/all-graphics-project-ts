8c0ef1642f77c3afce62f9ed44d6fd3e58543af3
Anwesh43 <anweshthecool0@gmail.com>
2022-11-17 13:36:28 +0530

    line-sq-rot-taker: adding DrawingUtil class

--- line-sq-rot-taker.ts
+++ line-sq-rot-taker.ts
@@ -26,3 +26,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+    
+    static drawLineSqRotTaker(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        const sqSize : number = Math.min(w, h) / sqFactor 
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(3), 0, () => {
+            context.rotate(-rot * dsc(2))
+            DrawingUtil.drawXY(context, 0, -h / 2 + h * 0.5 * dsc(0), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+            DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(1), 0, () => {
+                context.fillRect(-sqSize, 0, sqSize, sqSize)
+            })
+        })
+    }
+
+    static drawLSRTNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineSqRotTaker(context, scale)
+    }
+}
