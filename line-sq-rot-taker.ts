a19d253ba9a2a2be103348d734c50bcef97aaec2
Anwesh43 <anweshthecool0@gmail.com>
2022-11-17 13:26:20 +0530

    line-sq-rot-taker: adding constants

---
+++ line-sq-rot-taker.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const sqFactor : number = 8.9 
+const delay : number = 20 
+const rot : number = Math.PI / 2
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
