814077cfe4fe180802056777c418690e2aa95a0f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-18 17:46:17 +0530

    line-parallel-block: adding DrawingUtil class

--- line-parallel-block.ts
+++ line-parallel-block.ts
@@ -26,3 +26,34 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+    static drawLineParallelBlock(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barH : number = Math.min(w, h) / barHFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1 - 2 * j) 
+                    DrawingUtil.drawXY(context, 0, size * (1 - dsc(1)), () => {
+                        context.fillRect(0, -barH / 2, size * dsc(0), barH)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawLPBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineParallelBlock(context, scale)
+    }
+}
