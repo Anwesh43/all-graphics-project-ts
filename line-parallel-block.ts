851849af518e1fc1d755812e53bbd5e60b52060e
Anwesh43 <anweshthecool0@gmail.com>
2022-11-18 18:43:09 +0530

    line-parallel-block: adding LineParallelBlock class

--- line-parallel-block.ts
+++ line-parallel-block.ts
@@ -176,3 +176,25 @@ class LPBNode {
         return this 
     }
 }
+
+class LineParallelBlock {
+    curr : LPBNode = new LPBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
