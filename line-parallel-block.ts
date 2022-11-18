6634daf324656754efa90b6e1e1cb6993c2171d2
Anwesh43 <anweshthecool0@gmail.com>
2022-11-18 18:37:11 +0530

    line-parallel-block: adding LPBNode class

--- line-parallel-block.ts
+++ line-parallel-block.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class LPBNode {
+
+    prev : LPBNode 
+    next : LPBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LPBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLPBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LPBNode {
+        var curr : LPBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
