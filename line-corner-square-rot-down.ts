1f1a854c919743f0774b7c0b8f8eef43850e8fbd
Anwesh43 <anweshthecool0@gmail.com>
2022-11-19 13:08:05 +0530

    line-corner-square-rot-down: adding LineCornerSquareRotDown class

--- line-corner-square-rot-down.ts
+++ line-corner-square-rot-down.ts
@@ -183,3 +183,26 @@ class LCSRDNode {
         return this 
     }
 }
+
+class LineCornerSquareRotDown {
+
+    curr : LCSRDNode = new LCSRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
