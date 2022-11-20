3a40642a2bce01272bb54f0f9454efe38f0212c9
Anwesh43 <anweshthecool0@gmail.com>
2022-11-20 13:54:56 +0530

    line-break-rot-tri-line: adding Stage class

--- line-break-rot-tri-line.ts
+++ line-break-rot-tri-line.ts
@@ -74,3 +74,36 @@ class DrawingUtil {
         DrawingUtil.drawLineBreakRotTriLine(context, scale)
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCnavas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCnavas()
+        stage.render()
+        stage.handleTap()
+    }
+}
