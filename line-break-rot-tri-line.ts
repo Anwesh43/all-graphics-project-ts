889f4d403adde501d990dfdb78c930df873c0bbb
Anwesh43 <anweshthecool0@gmail.com>
2022-11-20 14:11:43 +0530

    line-break-rot-tri-line: adding LBRTLNode class

--- line-break-rot-tri-line.ts
+++ line-break-rot-tri-line.ts
@@ -151,3 +151,45 @@ class Animator {
         }
     }
 }
+
+class LBRTLNode {
+
+    prev : LBRTLNode 
+    next : LBRTLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBRTLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBRTLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBRTLNode {
+        var curr : LBRTLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
