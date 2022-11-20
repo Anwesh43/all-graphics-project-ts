aed21f4be20a57302381ec74844d07af8b868167
Anwesh43 <anweshthecool0@gmail.com>
2022-11-20 14:12:57 +0530

    line-break-rot-tri-line: adding LineBreakRotTriLine class

--- line-break-rot-tri-line.ts
+++ line-break-rot-tri-line.ts
@@ -193,3 +193,26 @@ class LBRTLNode {
         return this 
     }
 }
+
+class LineBreakRotTriLine {
+
+    curr : LBRTLNode = new LBRTLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
