678c9a62ba7c479c1a25f3aa7ed993ae512fabaa
Anwesh43 <anweshthecool0@gmail.com>
2022-11-21 15:07:47 +0530

    bar-vert-expand-rot: adding DrawingUtil classs

--- bar-vert-expand-rot.ts
+++ bar-vert-expand-rot.ts
@@ -25,3 +25,31 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawVertExpandRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barH : number = Math.min(w, h) / barHFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(4), () => {
+            context.rotate(rot * dsc(3))
+            context.fillRect(0, -size / 2, size * dsc(0), size)
+            DrawingUtil.drawXY(context, 0, (size / 2 - barH) - (size - barH / 2) * dsc(2), () => {
+                context.fillRect(-size, barH - barH * dsc(1), size, barH * dsc(1))
+            })
+        })
+    }
+
+    static drawVERNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawVertExpandRot(context, scale)
+    }
+}
