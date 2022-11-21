13fe18e6d169f231875bce892d8e57f9dec960ab
Anwesh43 <anweshthecool0@gmail.com>
2022-11-21 15:28:14 +0530

    bar-vert-expand-rot: adding BarVertExpandRot classs

--- bar-vert-expand-rot.ts
+++ bar-vert-expand-rot.ts
@@ -173,3 +173,26 @@ class BVERNode {
         return this 
     }
 }
+
+class BarVertExpandRot {
+
+    curr : BVERNode 
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
