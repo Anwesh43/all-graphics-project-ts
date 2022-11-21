ae328e959127426afd089cb3a3eb5766f22df530
Anwesh43 <anweshthecool0@gmail.com>
2022-11-21 15:26:54 +0530

    bar-vert-expand-rot: adding BVERNode classs

--- bar-vert-expand-rot.ts
+++ bar-vert-expand-rot.ts
@@ -130,3 +130,46 @@ class Animator {
         }
     }
 }
+
+class BVERNode {
+    
+    prev : BVERNode 
+    next : BVERNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BVERNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawVERNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BVERNode {
+        var curr : BVERNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
