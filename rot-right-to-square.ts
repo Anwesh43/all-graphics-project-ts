b071734ab8fe9ec96704dd796cb4bd8e898a41f6
Anwesh43 <anweshthecool0@gmail.com>
2022-11-23 15:23:32 +0530

    rot-right-to-square: adding RRTSNode class

--- rot-right-to-square.ts
+++ rot-right-to-square.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class RRTSNode {
+
+    prev : RRTSNode 
+    next : RRTSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RRTSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRRTSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RRTSNode {
+        var curr : RRTSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
