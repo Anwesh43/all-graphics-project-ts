beb4acdc7a390804c1e3a9d560efdd8bcf1d1b90
Anwesh43 <anweshthecool0@gmail.com>
2022-11-24 11:24:27 +0530

    t-line-break-down: adding renderer in stage

--- t-line-break-down.ts
+++ t-line-break-down.ts
@@ -73,6 +73,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D | null 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -85,12 +86,15 @@ class Stage {
         if (this.context) {
             this.context.fillStyle = backColor 
             this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
-        this.canvas.onmousedown = () => {
-
+        this.canvas.onmousedown = () => {   
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
