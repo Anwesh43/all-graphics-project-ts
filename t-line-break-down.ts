222c5cafed23f175f6320148c4e7be09b5066855
Anwesh43 <anweshthecool0@gmail.com>
2022-11-24 11:09:44 +0530

    t-line-break-down: adding TLineBreakDown class

--- t-line-break-down.ts
+++ t-line-break-down.ts
@@ -187,3 +187,26 @@ class TLBDNode {
         return this 
     }
 }
+
+class TLineBreakDown {
+
+    curr : TLBDNode = new TLBDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
