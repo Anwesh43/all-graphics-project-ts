76879524848d4261fcdd9d588d52b9b48bc601e4
Anwesh43 <anweshthecool0@gmail.com>
2022-11-26 13:43:09 +0530

    break-line-divide-rot: adding BreakLineDivideRot class

--- break-line-divide-rot.ts
+++ break-line-divide-rot.ts
@@ -184,3 +184,26 @@ class BLDRNode {
         return this 
     }
 }
+
+class BreakLineDivideRot {
+
+    curr : BLDRNode = new BLDRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
