8a42751a5de6e670a673539277105485097759ea
Anwesh43 <anweshthecool0@gmail.com>
2022-11-27 14:28:26 +0530

    rect-tail-line-rot: adding RectTailLineRot class

--- rect-tail-line-rot.ts
+++ rect-tail-line-rot.ts
@@ -184,3 +184,26 @@ class RTLRNode {
         return this 
     }
 }
+
+class RectTailLineRot {
+
+    curr : RTLRNode = new RTLRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
