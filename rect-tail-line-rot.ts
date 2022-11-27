a24c094e8d678a0a146b36ee79751d13d6b7d408
Anwesh43 <anweshthecool0@gmail.com>
2022-11-27 14:26:49 +0530

    rect-tail-line-rot: adding RTLRNode class

--- rect-tail-line-rot.ts
+++ rect-tail-line-rot.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class RTLRNode {
+
+    prev : RTLRNode 
+    next : RTLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RTLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRTLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RTLRNode {
+        var curr : RTLRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
