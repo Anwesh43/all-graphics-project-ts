fd8f977f7afa7969865d954db2ba38e75fbd6ef6
Anwesh43 <anweshthecool0@gmail.com>
2022-12-07 16:48:10 +0530

    circle-bar-extender: adding CircleBarExtender class

--- circle-bar-extender.ts
+++ circle-bar-extender.ts
@@ -177,3 +177,26 @@ class CBENode {
         return this 
     }
 }
+
+class CircleBarExtender {
+
+    curr : CBENode = new CBENode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
