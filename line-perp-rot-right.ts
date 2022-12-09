0ca70ac95b497df5d1a399ddd817bde26812b559
Anwesh43 <anweshthecool0@gmail.com>
2022-12-09 19:54:38 +0530

    line-perp-rot-right: adding DrawingUtil class

--- line-perp-rot-right.ts
+++ line-perp-rot-right.ts
@@ -25,3 +25,42 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLinePerpRotRight(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(0), 0, () => {
+                DrawingUtil.drawLine(context, -size, 0, 0, 0)
+            })
+            DrawingUtil.drawXY(context, 0, h / 2 * (1 - dsc(1)), () => {
+                context.rotate(rot * dsc(2))
+                DrawingUtil.drawLine(context, 0, 0, 0, size)
+            })
+            
+        })
+    }
+
+    static drawLPRRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLinePerpRotRight(context, scale)
+    }
+}
