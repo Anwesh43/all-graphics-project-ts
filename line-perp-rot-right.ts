2a7911a1a14fafe674dfde886b45fd810f1e4c81
Anwesh43 <anweshthecool0@gmail.com>
2022-12-09 20:15:43 +0530

    line-perp-rot-right: adding renderer in stage

--- line-perp-rot-right.ts
+++ line-perp-rot-right.ts
@@ -69,6 +69,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D | null 
+    renderer : Renderer = new Renderer()
     
     initCanvas() {
         this.canvas.width = w 
@@ -81,13 +82,16 @@ class Stage {
         if (this.context) {
             this.context.fillStyle = backColor 
             this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
-        this.canvas.onmousedown = () => {
-
-        }
+        this.canvas.onmousedown = () => {   
+            this.renderer.handleTap(() => {
+                this.render()
+            })
+        }   
     }
 
     static init() {
