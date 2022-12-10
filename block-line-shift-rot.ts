f3e725513c5ce5d97f9ba8e355dd8703728c810a
Anwesh43 <anweshthecool0@gmail.com>
2022-12-10 15:31:33 +0530

    block-line-shift-rot: adding DrawingUtil class

--- block-line-shift-rot.ts
+++ block-line-shift-rot.ts
@@ -25,3 +25,37 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBlockLineShiftRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number =>  ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h / 2 + (h / 2 + 2 * size) * dsc(4))
+        context.rotate(-rot * dsc(3))
+        context.fillRect(-size / 2, -size * dsc(0), size, size * dsc(0))
+        context.save()
+        context.translate(size / 2, -size * 0.5 * dsc(2))
+        DrawingUtil.drawLine(context, 0, 0, size * dsc(1), 0)
+        context.restore()
+        context.restore()
+    }
+
+    static drawBLSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBlockLineShiftRot(context, scale)
+    }
+}
