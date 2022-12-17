ac99253794c4079ae5acbac0e9daa2d58a049aba
Anwesh43 <anweshthecool0@gmail.com>
2022-12-17 11:49:16 +0530

    line-rot-bar-right: adding LRBRNode class

--- line-rot-bar-right.ts
+++ line-rot-bar-right.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class LRBRNode  {
+
+    next : LRBRNode 
+    prev : LRBRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRBRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRBRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRBRNode {
+        var curr : LRBRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
