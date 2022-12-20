217ef84335ca4bbecbeaf1cfa95f208506b9b2d3
Anwesh43 <anweshthecool0@gmail.com>
2022-12-20 10:16:39 +0530

    plus-in-bottom-sq: adding PIBSNode class

--- plus-in-bottom-sq.ts
+++ plus-in-bottom-sq.ts
@@ -146,3 +146,45 @@ class State {
         }
     }
 }
+
+class PIBSNode {
+
+    prev : PIBSNode 
+    next : PIBSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PIBSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPIBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : PIBSNode {
+        var curr : PIBSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
