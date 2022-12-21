b9ada40c2273bb81cf2289a4cd4429b83c6407dc
Anwesh43 <anweshthecool0@gmail.com>
2022-12-21 19:45:02 +0530

    t-line-to-single: adding TLineToSingle class

--- t-line-to-single.ts
+++ t-line-to-single.ts
@@ -190,3 +190,26 @@ class TLTSNode {
         return this 
     }
 }
+
+class TLineToSingle {
+
+    curr : TLTSNode = new TLTSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
