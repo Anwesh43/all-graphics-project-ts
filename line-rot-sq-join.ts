20cb0ea9dbdb836389e3ae25995cda554a584cb3
Anwesh43 <anweshthecool0@gmail.com>
2022-12-25 11:04:57 +0530

    line-rot-sq-join: adding constants

---
+++ line-rot-sq-join.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 6
+const scGap : number = 0.05 / parts 
+const delay : number = 20 
+const rot : number = Math.PI / 2
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 4.9 
+const strokeFactor : number = 90 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
