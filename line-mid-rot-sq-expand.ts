544656a9fa316f039c4939588536215abdec8d11
Anwesh43 <anweshthecool0@gmail.com>
2022-12-27 18:57:59 +0530

    line-mid-rot-sq-expand: adding LMRSENode class

--- line-mid-rot-sq-expand.ts
+++ line-mid-rot-sq-expand.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LMRSENode {
+
+    prev : LMRSENode 
+    next : LMRSENode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LMRSENode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLMRSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LMRSENode {
+        var curr : LMRSENode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
