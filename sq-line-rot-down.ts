f2dc2f3effbe9a2f89b4fe17228fc2a238d04276
Anwesh43 <anweshthecool0@gmail.com>
2022-12-28 17:28:55 +0530

    sq-line-rot-down: adding SqLineRotDown class

--- sq-line-rot-down.ts
+++ sq-line-rot-down.ts
@@ -186,3 +186,26 @@ class SLRDNode {
         return this 
     }
 }
+
+class SqLineRotDown {
+
+    curr : SLRDNode = new SLRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
