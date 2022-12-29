e844a5aac6ecd20217456c885b960d785e2d7a5a
Anwesh43 <anweshthecool0@gmail.com>
2022-12-29 11:30:51 +0530

    square-up-line-rot: adding SquareUpLineRot class

--- square-up-line-rot.ts
+++ square-up-line-rot.ts
@@ -190,3 +190,26 @@ class SULRNode {
         return this 
     }
 }
+
+class SquareUpLineRot {
+
+    curr : SULRNode = new SULRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
