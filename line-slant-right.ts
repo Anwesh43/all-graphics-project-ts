b24a3bf7dabc2b3923818126ea2d3a93c39e806d
Anwesh43 <anweshthecool0@gmail.com>
2022-12-31 11:26:06 +0530

    line-slant-right: adding LSRNode class

--- line-slant-right.ts
+++ line-slant-right.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LSRNode {
+
+    prev : LSRNode 
+    next : LSRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSRNode {
+        var curr : LSRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
