918c388d3f6a9cd81b1849b53dabace3e2e6ab40
Anwesh43 <anweshthecool0@gmail.com>
2022-12-31 10:16:03 +0530

    line-slant-right: adding constants

---
+++ line-slant-right.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const parts : number = 4 
+const scGap : number = 0.04 / parts 
+const deg : number = Math.PI / 4 
+const rot : number = Math.PI / 2
+const sizeFactor : number = 4.9 
+const strokeFactor : number = 90 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
