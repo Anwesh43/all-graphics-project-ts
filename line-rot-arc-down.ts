b2a343225b09f2ba0e3d0ad68bf5a56b018ddc13
Anwesh43 <anweshthecool0@gmail.com>
2023-01-02 13:14:52 +0530

    line-rot-arc-down: adding LineRotArcDown class

--- line-rot-arc-down.ts
+++ line-rot-arc-down.ts
@@ -203,3 +203,26 @@ class LRADNode {
         return this 
     }
 }
+
+class LineRotArcDown {
+
+    curr : LRADNode = new LRADNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
