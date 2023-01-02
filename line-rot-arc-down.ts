33fe0bcb450cafe8f599192e99311686ac0820d4
Anwesh43 <anweshthecool0@gmail.com>
2023-01-02 12:43:01 +0530

    line-rot-arc-down: adding DrawingUtil class

--- line-rot-arc-down.ts
+++ line-rot-arc-down.ts
@@ -9,11 +9,14 @@ const parts : number = 5
 const scGap : number = 0.04 / parts 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2
+const rot : number = -Math.PI / 4
 const sizeFactor : number = 4.9 
 const strokeFactor : number = 90 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const deg : number = 270
+const start : number = 180 
+const rad : number = Math.PI / 180 
 
 class ScaleUtil {
 
@@ -23,5 +26,60 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2) 
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawArc(context : CanvasRenderingContext2D, x : number, y : number, r : number, start : number, deg : number) {
+        context.save()
+        context.translate(x, y)
+        context.beginPath()
+        context.moveTo(0, 0)
+        for (let j = start; j <= start + deg; j++) {
+            const a : number = r * Math.cos(j * rad)
+            const b : number = r * Math.sin(j * rad)
+            context.lineTo(a, b)
+        }
+        context.fill()
+        context.restore()
+    }
+
+    static drawLineRotArcDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2 + size) * dsc(5), () => {
+            context.rotate(rot * dsc(4))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(deg * rad * dsc(1) * j)
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+                })
+            }
+            DrawingUtil.drawArc(context, 0, 0, size / 5, start, deg * dsc(2))
+            context.fillRect(-size * dsc(3), 0, size * dsc(3), size)
+        })
+    }
+    
+    static drawLRADNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineRotArcDown(context, scale)
     }
 }
