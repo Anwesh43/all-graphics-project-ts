8d5a117b4023ae3735be555928b1e1cd71a840d1
Anwesh43 <anweshthecool0@gmail.com>
2023-01-02 13:13:10 +0530

    line-rot-arc-down: adding LRADNode class

--- line-rot-arc-down.ts
+++ line-rot-arc-down.ts
@@ -161,3 +161,45 @@ class Animator {
         }
     }
 }
+
+class LRADNode {
+
+    next : LRADNode 
+    prev : LRADNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRADNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRADNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRADNode {
+        var curr : LRADNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        } 
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
