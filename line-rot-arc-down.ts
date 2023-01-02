9d4f8894533d8486ce214e749670c02527d79bae
Anwesh43 <anweshthecool0@gmail.com>
2023-01-02 12:55:32 +0530

    line-rot-arc-down: adding State class

--- line-rot-arc-down.ts
+++ line-rot-arc-down.ts
@@ -105,7 +105,7 @@ class Stage {
 
     handleTap() {
         this.canvas.onmousedown = () => {
-            
+
         }
     }
 
@@ -115,5 +115,29 @@ class Stage {
         stage.initCanvas()
         stage.render()
         stage.handleTap()
+    }
+}
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0 
+    prevScale : number = 0 
+
+    update(cb : () => void) {
+        this.scale += scGap * this.dir 
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir 
+            this.dir = 0 
+            this.prevScale = this.scale 
+            cb()
+        }
+    }
+
+    startUpdating(cb : () => void) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        }
     }
 }
