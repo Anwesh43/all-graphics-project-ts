c328a2801678febf5f35523f2f58cfcb6203824a
Anwesh43 <anweshthecool0@gmail.com>
2023-01-03 09:12:22 +0530

    line-half-circ-right: adding LineHalfCircRight class

--- line-half-circ-right.ts
+++ line-half-circ-right.ts
@@ -197,3 +197,26 @@ class LHCRNode {
         return this 
     }
 }
+
+class LineHalfCircRight {
+
+    curr : LHCRNode = new LHCRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
