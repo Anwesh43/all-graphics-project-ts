7f98dac65b0950937a1ea945d69219e071baf49d
Anwesh43 <anweshthecool0@gmail.com>
2023-01-03 09:00:00 +0530

    line-half-circ-right: adding DrawingUtil class

--- line-half-circ-right.ts
+++ line-half-circ-right.ts
@@ -14,6 +14,7 @@ const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 2 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const RADIANS = Math.PI / 180
 
 class ScaleUtil {
 
@@ -23,5 +24,57 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - y1) < 0.1 && Math.abs(x2 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArc(context : CanvasRenderingContext2D, x : number, y : number, r : number, start : number, sweep : number) {
+        context.save()
+        context.translate(x, y)
+        context.beginPath()
+        context.moveTo(0, 0)
+        for (let j = start; j <= start + sweep; j++) {
+            const a : number = r * Math.cos(j * RADIANS)
+            const b : number = r * Math.sin(j * RADIANS)
+            context.lineTo(a, b)
+        }
+        context.fill()
+        context.restore()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineHalfCircRight(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawLine(context, -size * 0.5 * dsc(0), 0, size * 0.5 * dsc(0), 0)
+            DrawingUtil.drawArc(context, 0, 0, size / 2, 180, 180)
+        }) 
+    }
+
+    static drawLHCRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineHalfCircRight(context, scale)
     }
 }
