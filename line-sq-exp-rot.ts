bb3af7cb7560c7b720ea85193b60e6c5aaafcd65
Anwesh43 <anweshthecool0@gmail.com>
2023-01-07 21:41:14 +0530

    line-sq-exp-rot: adding LineSqExpRot class

--- line-sq-exp-rot.ts
+++ line-sq-exp-rot.ts
@@ -190,3 +190,26 @@ class LSERNode {
         return this 
     }
 }
+
+class LineSqExpRot {
+
+    curr : LSERNode = new LSERNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
