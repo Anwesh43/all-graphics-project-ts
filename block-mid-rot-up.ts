2f4602b5b12bebc5892f2736e0a654fa5a80f886
Anwesh43 <anweshthecool0@gmail.com>
2023-01-28 12:38:24 +0530

    block-mid-rot-up: adding BMRUNode class

--- block-mid-rot-up.ts
+++ block-mid-rot-up.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class BMRUNode {
+
+    state : State = new State()
+    prev : BMRUNode 
+    next : BMRUNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BMRUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBMRUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BMRUNode {
+        var curr : BMRUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
