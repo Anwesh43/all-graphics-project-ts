fc4d682b328bfe5f1947002117e206e3f5bc3e56
Anwesh43 <anweshthecool0@gmail.com>
2023-01-28 10:29:22 +0530

    block-mid-rot-up: adding DrawingUtil class

--- block-mid-rot-up.ts
+++ block-mid-rot-up.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawBlockMidRotUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / strokeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2 + size) * dsc(5), () => {
+            DrawingUtil.drawXY(context, 0, -h / 2 + (h / 2) * dsc(1), () => {
+                context.rotate(-rot * dsc(3))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+            DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(0) + size * 0.5 * dsc(2), 0, () => {
+                context.rotate(-rot * dsc(4))
+                context.fillRect(-size / 2, 0, size, size)
+            })
+        })
+    }
+
+    static drawBMRUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBlockMidRotUp(context, scale)
+    }
+}
