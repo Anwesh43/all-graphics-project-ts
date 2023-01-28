fff3d990f455490437a333af0d39f0194181f7f7
Anwesh43 <anweshthecool0@gmail.com>
2023-01-28 12:58:03 +0530

    block-mid-rot-up: adding square y fix

--- block-mid-rot-up.ts
+++ block-mid-rot-up.ts
@@ -48,7 +48,7 @@ class DrawingUtil {
     static drawBlockMidRotUp(context : CanvasRenderingContext2D, scale : number) {
         const size : number = Math.min(w, h) / sizeFactor 
         const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2 + size) * dsc(5), () => {
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(5), h / 2, () => {
             DrawingUtil.drawXY(context, 0, -h / 2 + (h / 2) * dsc(1), () => {
                 context.rotate(-rot * dsc(3))
                 DrawingUtil.drawLine(context, 0, 0, 0, -size)
