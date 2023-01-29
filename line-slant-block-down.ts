1d8167ce0a7765a5eadaa99c4c71bb937453af2c
Anwesh43 <anweshthecool0@gmail.com>
2023-01-29 12:37:22 +0530

    line-slant-block-down: adding LSBDNode class

--- line-slant-block-down.ts
+++ line-slant-block-down.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LSBDNode {
+
+    prev : LSBDNode 
+    next : LSBDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSBDNode(this.i + 1)
+            this.next.prev = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSBDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSBDNode {
+        var curr : LSBDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
