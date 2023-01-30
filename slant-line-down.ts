4db01f6a43a5fed12c3ab7055428ab60a0794dfb
Anwesh43 <anweshthecool0@gmail.com>
2023-01-30 22:35:08 +0530

    slant-line-down: adding SLDNode class

--- slant-line-down.ts
+++ slant-line-down.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class SLDNode {
+
+    prev : SLDNode
+    next : SLDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SLDNode {
+        var curr : SLDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
