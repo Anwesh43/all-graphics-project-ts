c55dea543ae2260d5c5c492bfc5a21ed1a8d4114
Anwesh43 <anweshthecool0@gmail.com>
2023-02-03 17:42:07 +0530

    square-bi-side-mover: adding DrawingUtil class

--- square-bi-side-mover.ts
+++ square-bi-side-mover.ts
@@ -24,3 +24,33 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawSquareBiSideMover(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawXY(context, (w / 2) * dsc(j * 2 + 1), -h / 2 + (h / 2) * dsc(j * 2), () => {
+                        context.fillRect(0, -size, size, size)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawSBSMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = backColor 
+        DrawingUtil.drawSquareBiSideMover(context, scale)
+    } 
+}
