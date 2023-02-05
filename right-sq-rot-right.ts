80471c37f4e71012f44e4e083a7e5864ba1fbd84
Anwesh43 <anweshthecool0@gmail.com>
2023-02-05 12:15:25 +0530

    right-sq-rot-right: adding RightSqRotRight class

--- right-sq-rot-right.ts
+++ right-sq-rot-right.ts
@@ -169,3 +169,26 @@ class RSRRNode {
         return this 
     }
 }
+
+class RightSqRotRight {
+
+    curr : RSRRNode = new RSRRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
