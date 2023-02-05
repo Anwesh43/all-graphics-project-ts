c59d8c831807a886fd3bb67e8bf396162e8f71e1
Anwesh43 <anweshthecool0@gmail.com>
2023-02-05 12:05:21 +0530

    right-sq-rot-right: adding RSRRNode class

--- right-sq-rot-right.ts
+++ right-sq-rot-right.ts
@@ -127,3 +127,45 @@ class Animator {
         }
     }
 }
+
+class RSRRNode {
+
+    prev : RSRRNode 
+    next : RSRRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RSRRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRSRRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RSRRNode {
+        var curr : RSRRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
