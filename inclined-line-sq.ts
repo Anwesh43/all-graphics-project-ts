b04d4684a819c30095a91627ba04f730857cb707
Anwesh43 <anweshthecool0@gmail.com>
2023-02-09 23:10:26 +0530

    inclined-line-sq: adding ILSNode class

--- inclined-line-sq.ts
+++ inclined-line-sq.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class ILSNode {
+
+    next : ILSNode 
+    prev : ILSNode
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ILSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawILSNode(context, this.i, this.state.scale)
+    }  
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : ILSNode {
+        var curr : ILSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
