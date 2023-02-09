20ce20a14a1cbec527c03b6473e22ab9fbe0aeba
Anwesh43 <anweshthecool0@gmail.com>
2023-02-09 15:14:35 +0530

    inclined-line-sq: adding constants

---
+++ inclined-line-sq.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const rot : number = Math.PI / 4
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+
