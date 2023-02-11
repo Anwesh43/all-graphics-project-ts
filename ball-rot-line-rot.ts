ff2a7f5e57d71e9622d445359b90499800722f88
Anwesh43 <anweshthecool0@gmail.com>
2023-02-11 11:33:54 +0530

    ball-rot-line-rot: adding BRLNode class

--- ball-rot-line-rot.ts
+++ ball-rot-line-rot.ts
@@ -181,3 +181,45 @@ class Animator {
         }
     }
 }
+
+class BRLNode {
+
+    prev : BRLNode 
+    next : BRLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BRLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BRLNode {
+        var curr : BRLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
