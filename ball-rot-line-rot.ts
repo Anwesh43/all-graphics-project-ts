b61b6bebdf499a3d84b3cb95d20b0d4c35320e7d
Anwesh43 <anweshthecool0@gmail.com>
2023-02-11 11:16:57 +0530

    ball-rot-line-rot: adding DrawingUtil class

--- ball-rot-line-rot.ts
+++ ball-rot-line-rot.ts
@@ -49,3 +49,55 @@ class DimensionController {
         }
     }
 }
+
+class DrawingUtil {
+   
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawBallRotLine(context : CanvasRenderingContext2D, w : number, h : number, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const r : number = Math.min(w, h) / rFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(4), () => {
+            DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(0), 0, () => {
+               context.rotate(rot * dsc(3))
+               DrawingUtil.drawLine(context, 0, 0, -size, 0) 
+            })
+
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(1)), 0, () => {
+                context.rotate(rot * dsc(2))
+                DrawingUtil.drawCircle(context, r, 0, r)
+            })
+        })        
+    }
+
+    static drawBRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        const {w, h} = DimensionController.getDimension()
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBallRotLine(context, w, h, scale)
+    }
+}
