46bf481ac2d99ebc1a67437f14d42858d0b255ba
Anwesh43 <anweshthecool0@gmail.com>
2023-02-11 11:23:24 +0530

    ball-rot-line-rot: adding Stage class

--- ball-rot-line-rot.ts
+++ ball-rot-line-rot.ts
@@ -101,3 +101,39 @@ class DrawingUtil {
         DrawingUtil.drawBallRotLine(context, w, h, scale)
     }
 }
+
+class Stage {
+    
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        const {w, h} = DimensionController.getDimension()
+        this.canvas.width = w
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas) 
+        DimensionController.handleResize()
+    }
+
+    render() {
+        if (this.context) {
+            const {w, h} = DimensionController.getDimension()
+            this.context.fillStyle = backColor
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
