f29f69e9582f5f9e553a331471083072d0c85f9d
Anwesh43 <anweshthecool0@gmail.com>
2023-02-11 11:37:15 +0530

    ball-rot-line-rot: adding BallRotLineRot class

--- ball-rot-line-rot.ts
+++ ball-rot-line-rot.ts
@@ -223,3 +223,26 @@ class BRLNode {
         return this 
     }
 }
+
+class BallRotLineRot {
+
+    curr : BRLNode = new BRLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb  : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
