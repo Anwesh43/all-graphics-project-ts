de324e1ed4193ef1302b747cedb07c24e0cba951
Anwesh43 <anweshthecool0@gmail.com>
2023-02-12 14:05:17 +0530

    sq-rot-line-up: adding SqRotLineUp class

--- sq-rot-line-up.ts
+++ sq-rot-line-up.ts
@@ -186,3 +186,26 @@ class SRLUNode {
         return this 
     }
  }
+
+ class SqRotLineUp {
+
+    curr : SRLUNode = new SRLUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+ }
