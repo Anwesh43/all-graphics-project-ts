91376bde5c393438cf992f9b8089afb51ce013ac
Anwesh43 <anweshthecool0@gmail.com>
2023-02-17 16:38:14 +0530

    line-touch-sq-rot: adding constants

---
+++ line-touch-sq-rot.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#00C853",
+    "#0091EA",
+    "#AA00FF",
+    "#FFAB00"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const rot : number = window.innerWidth 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
