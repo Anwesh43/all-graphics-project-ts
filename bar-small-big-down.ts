8db4af234288b55f895a4b32b8449fb4d594ba31
Anwesh43 <anweshthecool0@gmail.com>
2023-02-19 10:59:22 +0530

    bar-small-big-down: adding constants

---
+++ bar-small-big-down.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#B71C1C",
+    "#00C853",
+    "#0091EA",
+    "#AA00FF",
+    "#FFAB00"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const rot : number = Math.PI / 2
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
