e84e099a99b4f738763ea0195801296e6fdc4d64
Anwesh43 <anweshthecool0@gmail.com>
2023-02-20 17:54:58 +0530

    bar-divide-separate: adding BarDivideSeparate class

--- bar-divide-separate.ts
+++ bar-divide-separate.ts
@@ -174,3 +174,26 @@ class BDSNode {
         return this 
     }
 }
+
+class BarDivideSeparate {
+
+    curr : BDSNode = new BDSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
