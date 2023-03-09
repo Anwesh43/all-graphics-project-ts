b2d19a9ae8befa11377fd91ce7e3f3112e356235
Anwesh43 <anweshthecool0@gmail.com>
2023-03-09 08:47:08 +0530

    line-rot-shrink-left: adding Drawing'Util class

--- line-rot-shrink-left.ts
+++ line-rot-shrink-left.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRotShrinkLeft(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w * 0.5 * (dsc(0) - dsc(3)), h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(-rot * j * dsc(1))
+                    DrawingUtil.drawLine(context, 0, 0, -size + size * 0.5 * j * dsc(2), 0)
+                })
+            }
+        })
+    }
+
+    static drawLRSLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineRotShrinkLeft(context, scale)
+    }
+}
