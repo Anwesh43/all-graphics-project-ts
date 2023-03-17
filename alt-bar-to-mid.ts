0d64c6930cc10459422ea186258e0d3266077a97
Anwesh43 <anweshthecool0@gmail.com>
2023-03-17 20:59:46 +0530

    alt-bar-to-mid: adding DrawingUtil class

--- alt-bar-to-mid.ts
+++ alt-bar-to-mid.ts
@@ -14,4 +14,55 @@ const rot : number = Math.PI / 2
 const hBarFactor : number = 11.2 
 const backColor : string = "#bdbdbd"
 const w : number = window.innerWidth 
-const h : number = window.innerHeight 
+const h : number = window.innerHeight
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawAllBarToMid(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barH : number = Math.min(w, h) / hBarFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1 - 2 * j)
+                    context.fillRect(0, -barH * 0.5 * dsc(1), size * dsc(0), barH)
+                })
+            }
+        })
+    }
+
+    static drawABTMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawAllBarToMid(context, scale)
+    }
+}
