a2bc43822449bd8b12aca8596309710e8d01ad2c
Anwesh43 <anweshthecool0@gmail.com>
2023-03-20 21:41:04 +0530

    rot-divide-line-down: adding RotDivideLineDown class

--- rot-divide-line-down.ts
+++ rot-divide-line-down.ts
@@ -182,3 +182,26 @@ class RDLDNode {
         return this 
     }
 }
+
+class RotDivideLineDown {
+
+    curr : RDLDNode = new RDLDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
