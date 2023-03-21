431d0f85ed9f02512d03272bf4223e52a275201b
Anwesh43 <anweshthecool0@gmail.com>
2023-03-21 22:36:00 +0530

    square-left-down-rot: adding SquareLeftDownRot class

--- square-left-down-rot.ts
+++ square-left-down-rot.ts
@@ -170,3 +170,26 @@ class SLDRNode {
         return this 
     }
 }
+
+class SquareLeftDownRot {
+
+    curr : SLDRNode = new SLDRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
