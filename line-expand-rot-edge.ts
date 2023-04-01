7facfd555f8319e2abe4a0cf1cd60ca26b1a845a
Anwesh43 <anweshthecool0@gmail.com>
2023-04-01 16:23:22 +0530

    line-expand-rot-edge: adding DrawingUtil class

--- line-expand-rot-edge.ts
+++ line-expand-rot-edge.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineExpandRotEdge(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, (h / 2) * dsc(1), () => {
+            context.rotate(rot * (1 - dsc(2)))
+            DrawingUtil.drawLine(context, 0, 0, size * (dsc(0) - dsc(3)), 0)
+        })
+    }
+
+    static drawLERENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineExpandRotEdge(context, scale)
+    }
+}
