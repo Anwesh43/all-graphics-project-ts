bd99bdef6b774300e8b38615f2cb13c18bf23ce2
Anwesh43 <anweshthecool0@gmail.com>
2023-04-02 12:38:22 +0530

    line-perp-to-arrow: adding DrawingUtil class

--- line-perp-to-arrow.ts
+++ line-perp-to-arrow.ts
@@ -6,7 +6,7 @@ const colors : Array<string> = [
     "#00C853"
 ]
 const rot : number = Math.PI / 4 
-const deg : number = Math.PI / 3
+const deg : number = Math.PI / 6
 const strokeFactor : number = 90 
 const sizeFactor : number = 4.9 
 const delay : number = 20 
@@ -24,5 +24,48 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLinePerpToArrow(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2 + size) * dsc(3), h / 2, () => {
+            context.rotate(deg * dsc(2))
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(0)), 0, () => {
+                context.rotate(-rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, size, 0)
+            })
+            DrawingUtil.drawXY(context, 0, -(h / 2) * (1 - dsc(0)), () => {
+                context.rotate(rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+        })       
+    }
+
+    static drawLPTANode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.lineCap = 'round'
+        DrawingUtil.drawLinePerpToArrow(context, scale)
     }
 }
