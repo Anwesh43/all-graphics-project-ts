18368b26430b1712a7c5649b860c7598695fb1d5
Anwesh43 <anweshthecool0@gmail.com>
2023-04-02 11:48:31 +0530

    line-perp-to-arrow: adding constants

---
+++ line-perp-to-arrow.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const rot : number = Math.PI / 4 
+const deg : number = Math.PI / 3
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
