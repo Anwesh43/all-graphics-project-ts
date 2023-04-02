f0a4d60811597eae0f31b1f70d069b4b1a98d3ba
Anwesh43 <anweshthecool0@gmail.com>
2023-04-02 12:50:21 +0530

    line-perp-to-arrow: adding LPTANode class

--- line-perp-to-arrow.ts
+++ line-perp-to-arrow.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LPTANode {
+
+    prev : LPTANode 
+    next : LPTANode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LPTANode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLPTANode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LPTANode {
+        var curr : LPTANode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
