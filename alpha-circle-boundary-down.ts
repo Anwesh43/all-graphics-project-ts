812ea591606be50b1a77823efe970d2b24cf84d7
Anwesh43 <anweshthecool0@gmail.com>
2023-04-03 14:28:56 +0530

    alpha-circle-boundary-down: adding constants

---
+++ alpha-circle-boundary-down.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const backcolor : string = "#BDBDBD"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const alphaDec : number = 0.6
+
