253982b3d2066b3828e4e5eba505454e1ac5dc03
Anwesh43 <anweshthecool0@gmail.com>
2023-04-07 11:42:29 +0530

    line-arc-down-shift: adding constants

---
+++ line-arc-down-shift.ts
@@ -0,0 +1,16 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 0.04 
+const scGap : number = 5
+const rot : number = Math.PI / 2 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const sizeFactor : number = 4.9 
+const strokeFactor : number = 90 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
