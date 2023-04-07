73b80c53234b3843fa821e75a8d84f37f9214fef
Anwesh43 <anweshthecool0@gmail.com>
2023-04-07 17:36:43 +0530

    line-arc-down-shift: adding fix 3

--- line-arc-down-shift.ts
+++ line-arc-down-shift.ts
@@ -126,6 +126,7 @@ class State {
 
     update(cb : () => void) {
         this.scale += scGap * this.dir 
+        console.log(this.scale)
         if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir 
             this.dir = 0 
