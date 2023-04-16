f40170417e4b2ee72a25a096c3802caa0ac934a9
Anwesh43 <anweshthecool0@gmail.com>
2023-04-16 13:46:45 +0530

    line-bar-down-rot-shift: adding LBDRSNode class

--- line-bar-down-rot-shift.ts
+++ line-bar-down-rot-shift.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LBDRSNode {
+
+    prev : LBDRSNode 
+    next : LBDRSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBDRSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBDRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBDRSNode {
+        var curr : LBDRSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
