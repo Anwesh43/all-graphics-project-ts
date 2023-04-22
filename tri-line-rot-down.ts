06a8245c1ff11243ad732e4e17797c32c821b791
Anwesh43 <anweshthecool0@gmail.com>
2023-04-22 13:46:31 +0530

    tri-line-rot-down: adding TriLineRotDown class

--- tri-line-rot-down.ts
+++ tri-line-rot-down.ts
@@ -189,3 +189,26 @@ class TLRDNode {
         return this 
     }
 }
+
+class TriLineRotDown {
+
+    curr : TLRDNode = new TLRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
