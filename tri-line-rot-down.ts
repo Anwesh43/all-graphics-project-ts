e597cf5c134b8a23416312423ca91867394aefda
Anwesh43 <anweshthecool0@gmail.com>
2023-04-22 13:11:08 +0530

    tri-line-rot-down: adding DrawingUtil class

--- tri-line-rot-down.ts
+++ tri-line-rot-down.ts
@@ -25,3 +25,48 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n  
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () =>  void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawTriLineRotDown(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate((1 - 2 * j) * (Math.PI / 6))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+                })
+            }
+            DrawingUtil.drawXY(context, size * dsc(1), 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, -size * dsc(0), 0)
+            })
+            DrawingUtil.drawLine(context, 0, 0, -size * dsc(0), 0)
+        })
+    }
+
+    static drawTLRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.strokeStyle = colors[i]
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawTriLineRotDown(context, scale)
+    }
+}
