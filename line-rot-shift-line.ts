4b7117876087608fc057af05b33e4e2129f6438b
Anwesh43 <anweshthecool0@gmail.com>
2023-04-23 11:05:31 +0530

    line-rot-shift-line: adding LRSLNode class

--- line-rot-shift-line.ts
+++ line-rot-shift-line.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LRSLNode {
+
+    next : LRSLNode 
+    prev : LRSLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRSLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRSLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRSLNode {
+        var curr : LRSLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        } 
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
