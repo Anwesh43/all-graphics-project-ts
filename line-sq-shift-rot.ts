6bb8a0a5d11ef0740c054e2eb5de57d3705acf53
Anwesh43 <anweshthecool0@gmail.com>
2023-04-29 17:25:12 +0530

    line-sq-shift-rot: adding DrawingUtil class

--- line-sq-shift-rot.ts
+++ line-sq-shift-rot.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#D50000",
     "#43A047"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 4.9 
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineSqShiftRot(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(4), () => {
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+            DrawingUtil.drawXY(context, 0, -size / 2 + size * 0.5 * dsc(2), () => {
+                context.rotate(rot * dsc(3))
+                context.fillRect(0, -size / 2, size * dsc(1), size)
+            })
+        })
+    }
+
+    static drawLSSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLSSRNode(context, i, scale)
     }
 }
