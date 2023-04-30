9d45a0b10f497f22a606edc4eb7dfe7907ddbb76
Anwesh43 <anweshthecool0@gmail.com>
2023-04-30 10:11:06 +0530

    break-bar-line-down: adding BreakBarLineDown class

--- break-bar-line-down.ts
+++ break-bar-line-down.ts
@@ -187,3 +187,26 @@ class BBLDNode {
         return this 
     }
 }
+
+class BreakBarLineDown {
+
+    curr : BBLDNode = new BBLDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
