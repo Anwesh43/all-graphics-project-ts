6f814c2f2b096f21566b638b6c0b1318e3ad6482
Anwesh43 <anweshthecool0@gmail.com>
2023-05-05 19:28:28 +0530

    circle-line-taker: adding DrawingUtil class

--- circle-line-taker.ts
+++ circle-line-taker.ts
@@ -24,3 +24,50 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - y1) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircleLineTaker(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            DrawingUtil.drawXY(context, - w / 2 + (w / 2 - size) * dsc(2) + (w / 2 + size) * dsc(3), 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+            })
+            DrawingUtil.drawXY(context, (w / 2 + size) * dsc(3), -h / 2 + size / 3 + (h / 2 - size / 3) * dsc(2), () => {
+                DrawingUtil.drawCircle(context, 0, 0, (size / 3) * dsc(1))
+            })
+        })
+    }
+    
+    static drawCLTNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawCircleLineTaker(context, scale)
+    }
+}
