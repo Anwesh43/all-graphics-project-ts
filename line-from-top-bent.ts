6bfea585211727f44167f6ae577df223c76ae142
Anwesh43 <anweshthecool0@gmail.com>
2023-05-07 22:23:15 +0530

    line-from-top-bent: adding DrawingUtil class

--- line-from-top-bent.ts
+++ line-from-top-bent.ts
@@ -11,7 +11,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2 
+const rot : number = -Math.PI / 4
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 
@@ -23,5 +23,41 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineFromTopBent(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts) 
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), -h / 2 + (h / 2) * dsc(1), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+        })
+    }
+
+    static drawLFTBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineFromTopBent(context, scale)
     }
 }
