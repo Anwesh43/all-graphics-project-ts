e4c0d0ea478a3f9d42132e4bd803cf4920878ffa
Anwesh43 <anweshthecool0@gmail.com>
2023-05-11 22:28:55 +0530

    three-line-cross-stage: adding DrawingUtil class

--- three-line-cross-stage.ts
+++ three-line-cross-stage.ts
@@ -25,3 +25,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawThreeLineCrossStage(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 3; j++) {
+                DrawingUtil.drawXY(context, -size / 2, -size / 2 + size * 0.5 * j, () => {
+                    context.rotate(rot * dsc(1))
+                    DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                })
+            }
+        })
+    }
+
+    static drawTLCSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawTLCSNode(context, i, scale)
+    }
+}
