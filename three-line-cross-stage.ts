e42c440f0cf52c0c22cef788b7ca445c571639a4
Anwesh43 <anweshthecool0@gmail.com>
2023-05-12 01:45:31 +0530

    three-line-cross-stage: adding TLCSNode class

--- three-line-cross-stage.ts
+++ three-line-cross-stage.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class TLCSNode {
+
+    prev : TLCSNode 
+    next : TLCSNode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TLCSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTLCSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : TLCSNode {
+        var curr : TLCSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
