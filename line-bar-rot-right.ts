5a7402bd15b8a1fb417ba16ea6a8b5ff7bca410c
Anwesh43 <anweshthecool0@gmail.com>
2023-05-13 11:47:16 +0530

    line-bar-rot-right: adding DrawingUtil class

--- line-bar-rot-right.ts
+++ line-bar-rot-right.ts
@@ -8,7 +8,7 @@ const colors : Array<string> = [
 const parts : number = 4
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
-const rot : number = Math.PI / 2
+const rot : number = Math.PI
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const sizeFactor : number = 4.9 
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineBarRotRight(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(3), h / 2, () => {
+            DrawingUtil.drawLine(context, 0, 0, -size * dsc(0), 0)
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * dsc(2))
+                context.fillRect(0, -size * dsc(1), size / 2, size * dsc(1))
+            })
+        })
+    }
+
+    static drawLBRRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineBarRotRight(context, scale)
     }
 }
