c8d4ddced2b8aaaf65d27fe447435b7b3430ccb7
Anwesh43 <anweshthecool0@gmail.com>
2023-05-13 12:24:58 +0530

    line-bar-rot-right: adding LBRRNode class

--- line-bar-rot-right.ts
+++ line-bar-rot-right.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LBRRNode {
+
+    prev : LBRRNode
+    next : LBRRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBRRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBRRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBRRNode {
+        var curr : LBRRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
