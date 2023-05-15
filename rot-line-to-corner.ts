90cb98d8ea5999c1d00aa1990bc690e677839523
Anwesh43 <anweshthecool0@gmail.com>
2023-05-15 17:41:44 +0530

    rot-line-to-corner: adding DrawingUtil class

--- rot-line-to-corner.ts
+++ rot-line-to-corner.ts
@@ -9,7 +9,7 @@ const parts : number = 4
 const scGap : number = 0.04 / parts 
 const sizeFactor : number = 4.9 
 const delay : number = 20 
-const rot : number = Math.PI / 2 
+const rot : number = Math.PI / 4
 const strokeFactor : number = 90 
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawRotLineToCorner(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, (w / 2) * dsc(2 + j), (h / 2) * dsc(2 + j) * j, () => {
+                    context.rotate(rot * j * dsc(1))
+                    DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                })
+            }
+        })
+    }
+
+    static drawRLTCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRotLineToCorner(context, scale)
     }
 }
