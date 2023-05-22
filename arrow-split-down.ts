81fe56bfa712ab98c6d4291c758b16101d717e31
Anwesh43 <anweshthecool0@gmail.com>
2023-05-22 15:48:12 +0530

    arrow-split-down: adding DrrawingUtil class

--- arrow-split-down.ts
+++ arrow-split-down.ts
@@ -26,3 +26,48 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArrowSplitDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(4), () => {
+            context.rotate(rot * dsc(3))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(splitDeg * dsc(1))
+                    DrawingUtil.drawLine(context, 0, 0, 0, size * 0.1 * dsc(0))
+                })
+            }
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, -size * dsc(2), 0)
+            })
+        
+        })
+    }
+
+    static drawASLDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawArrowSplitDown(context, scale)
+    }
+}
