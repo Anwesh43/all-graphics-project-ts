f3f780816a338b186910db2a732f4bafb9b47fee
Anwesh43 <anweshthecool0@gmail.com>
2023-05-22 12:54:42 +0530

    arrow-split-down: adding constants

---
+++ arrow-split-down.ts
@@ -0,0 +1,17 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 5
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const splitDeg : number = Math.PI / 4
+const rot : number = Math.PI / 2 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
