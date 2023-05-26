8f0f20ba3fc8869cd65e09b8f83cdd371f84e84e
Anwesh43 <anweshthecool0@gmail.com>
2023-05-26 16:40:43 +0530

    line-into-t-right: adding LITRNode class

--- line-into-t-right.ts
+++ line-into-t-right.ts
@@ -144,3 +144,42 @@ class Animator {
         }
     }
 }
+
+class LITRNode {
+
+    prev : LITRNode 
+    next : LITRNode 
+    state : State = new State()
+
+    addNeighbor() {
+
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLITRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LITRNode {
+        var curr : LITRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
