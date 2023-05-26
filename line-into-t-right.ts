affb150682c609804116c41eb994427f66a3b299
Anwesh43 <anweshthecool0@gmail.com>
2023-05-26 16:08:04 +0530

    line-into-t-right: adding DrawingUtil class

--- line-into-t-right.ts
+++ line-into-t-right.ts
@@ -25,3 +25,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineIntoTRight(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawLine(context, 0, 0, size * 0.5 * ScaleUtil.divideScale(dsc(0), 0, 2), 0)
+        for (let j = 0; j < 2; j++) {
+            DrawingUtil.drawXY(context, size / 2, 0, () => {
+                context.rotate(rot * (1 - 2 * j) * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, size * 0.5 * ScaleUtil.divideScale(dsc(0), 1, 2), 0)
+            })
+        }
+        context.restore()
+    }
+
+    static drawLITRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineIntoTRight(context, scale)
+    }
+}
