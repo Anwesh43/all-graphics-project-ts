ebecf13b30d647e81bb573de5739b07df2ca3670
Anwesh43 <anweshthecool0@gmail.com>
2023-05-28 01:26:49 +0530

    line-sq-expand-rot: adding DrawingUtil class

--- line-sq-expand-rot.ts
+++ line-sq-expand-rot.ts
@@ -25,3 +25,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+    
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineSqExpandRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, -w * 0.5 * (1 - dsc(0)), 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, -size, 0)
+            })
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(-rot * dsc(2))
+                context.fillRect(0, 0, size * dsc(1), size / 2)
+            })
+        })
+    }
+
+    static drawLSERNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineSqExpandRot(context, scale)
+    }
+}
