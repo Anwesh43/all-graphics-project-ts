8751e8155403ed496b46eada66c97f5089ad88f2
Anwesh43 <anweshthecool0@gmail.com>
2023-05-28 12:38:47 +0530

    line-sq-expand-rot: adding LSERNode class

--- line-sq-expand-rot.ts
+++ line-sq-expand-rot.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LSERNode {
+
+    next : LSERNode 
+    prev : LSERNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSERNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSERNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSERNode {
+        var curr : LSERNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
