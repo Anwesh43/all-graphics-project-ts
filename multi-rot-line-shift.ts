9aceb4453b6fe1edd5f9c27f4df8f845834bbf8d
Anwesh43 <anweshthecool0@gmail.com>
2023-06-13 17:51:38 +0530

    multi-rot-line-shift: adding MultiRotLineShift class

--- multi-rot-line-shift.ts
+++ multi-rot-line-shift.ts
@@ -185,3 +185,26 @@ class MRLSNode {
         return this 
     }
 }
+
+class MultiRotLineShift {
+
+    curr : MRLSNode = new MRLSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
