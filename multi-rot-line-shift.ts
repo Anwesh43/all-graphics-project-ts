e02fc8f10b4243631d3e438b8099abc42b69cd45
Anwesh43 <anweshthecool0@gmail.com>
2023-06-13 17:23:02 +0530

    multi-rot-line-shift: adding DrawingUtil class

--- multi-rot-line-shift.ts
+++ multi-rot-line-shift.ts
@@ -10,6 +10,7 @@ const scGap : number = 0.04 / parts
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 2
+const rot2 : number = Math.PI / 3
 const sizeFactor : number  = 4.9 
 const strokeFactor : number = 90 
 const w : number = window.innerWidth 
@@ -23,5 +24,45 @@ class ScaleUtil {
     
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+    
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - y1) < 0.1 && Math.abs(x2 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawMultiRotLineShift(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2, () => {
+            for (let j = 0; j < 3; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(Math.min(j, 1) * rot * dsc(1) + Math.min(j - 1, 1) * rot2 * dsc(2))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+                })
+            }
+        })
+    }
+
+    static drawMRLSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawMultiRotLineShift(context, scale)
     }
 }
