3df146787aafde02ae7704a4abc15e52388f673a
Anwesh43 <anweshthecool0@gmail.com>
2023-06-14 17:39:48 +0530

    line-break-sq-down: adding LBSDNode class

--- line-break-sq-down.ts
+++ line-break-sq-down.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LBSDNode {
+
+    prev : LBSDNode 
+    next : LBSDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBSDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBSDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBSDNode {
+        var curr : LBSDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
