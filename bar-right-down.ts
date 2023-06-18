5083649034dcbd3fe46da7e061d0569e90e4c759
Anwesh43 <anweshthecool0@gmail.com>
2023-06-18 20:33:09 +0530

    bar-right-down: adding BarRightDown class

--- bar-right-down.ts
+++ bar-right-down.ts
@@ -183,3 +183,26 @@ class BRDNode {
         return this 
     }
 }
+
+class BarRightDown {
+
+    curr : BRDNode = new BRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
