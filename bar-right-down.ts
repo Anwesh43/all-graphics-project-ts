772b5786c45c3bf51e9c9916617326f260761540
Anwesh43 <anweshthecool0@gmail.com>
2023-06-18 18:55:11 +0530

    bar-right-down: adding DrawingUtil class

--- bar-right-down.ts
+++ bar-right-down.ts
@@ -13,7 +13,7 @@ const delay : number = 20
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
-const rot : number = Math.PI / 2
+const rot : number = Math.PI / 4
 
 class ScaleUtil {
 
@@ -23,5 +23,46 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+    
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarRightDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            context.fillRect(0, - size / 2, size * dsc(0), size)
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(1), 0, () => {
+                    context.rotate(rot * dsc(2) * (1 -  2 * j))
+                    DrawingUtil.drawLine(context, 0, 0, -size, 0)
+                })
+            }
+        })
+    }
+
+    static drawBRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBarRightDown(context, scale)
     }
 }
