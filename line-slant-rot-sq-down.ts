f8ec66f36cbce81904f69c5e9d4062d4a0309a89
Anwesh43 <anweshthecool0@gmail.com>
2023-06-20 17:53:16 +0530

    line-slant-rot-sq-down: adding LineSlantRotSqDown class

--- line-slant-rot-sq-down.ts
+++ line-slant-rot-sq-down.ts
@@ -183,3 +183,26 @@ class LSRSDNode {
         return this 
     }
 }
+
+class LineSlantRotSqDown {
+
+    curr : LSRSDNode = new LSRSDNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
