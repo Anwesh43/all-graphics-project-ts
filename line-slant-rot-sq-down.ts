a02143fed322b1eb42b889c275f6ecfc23173f2c
Anwesh43 <anweshthecool0@gmail.com>
2023-06-20 17:37:28 +0530

    line-slant-rot-sq-down: adding DrawingUtil class

--- line-slant-rot-sq-down.ts
+++ line-slant-rot-sq-down.ts
@@ -13,6 +13,7 @@ const delay : number = 20
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const rot : number = Math.PI
 
 class ScaleUtil {
 
@@ -22,5 +23,44 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSlantRotSqDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * 0.75 * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, -size * dsc(0), 0)
+            })
+            context.fillRect(0, 0, size, size * dsc(2))
+        })
+    }
+
+    static drawLSRSDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineSlantRotSqDown(context, scale)
     }
 }
