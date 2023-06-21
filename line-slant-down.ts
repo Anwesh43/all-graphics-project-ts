aa35574e184e3cd1a0a4f31a7a090d45eccbce8e
Anwesh43 <anweshthecool0@gmail.com>
2023-06-21 19:13:28 +0530

    line-slant-down: adding LSDNode class

--- line-slant-down.ts
+++ line-slant-down.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LSDNode {
+
+    prev : LSDNode
+    next : LSDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb : () => void) : LSDNode {
+        var curr : LSDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
