247f8abc5f052f7ed9ddbb21e2e7285dcfc6abd0
Anwesh43 <anweshthecool0@gmail.com>
2023-06-22 15:59:29 +0530

    size-sq-down-rot: adding Renderer in Stage

--- size-sq-down-rot.ts
+++ size-sq-down-rot.ts
@@ -69,6 +69,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D | null 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
@@ -81,12 +82,15 @@ class Stage {
         if (this.context) {
             this.context.fillStyle = backColor 
             this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
