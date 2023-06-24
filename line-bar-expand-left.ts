94db26a38c026866d592213ea9db5276583817b8
Anwesh43 <anweshthecool0@gmail.com>
2023-06-24 12:23:05 +0530

    line-bar-expand-left: adding LBELNode class

--- line-bar-expand-left.ts
+++ line-bar-expand-left.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class LBELNode {
+
+    prev : LBELNode 
+    next : LBELNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBELNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBELNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBELNode {
+        var curr : LBELNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
