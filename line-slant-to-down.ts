e4f7a9fa545f1daaf27291e2f0b2bbffafac640d
Anwesh43 <anweshthecool0@gmail.com>
2023-06-26 14:16:24 +0530

    line-slant-to-down: adding LSTDNode class

--- line-slant-to-down.ts
+++ line-slant-to-down.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LSTDNode {
+
+    next : LSTDNode 
+    prev : LSTDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSTDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSTDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSTDNode {
+        var curr : LSTDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
