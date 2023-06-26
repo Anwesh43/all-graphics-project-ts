0c72d90d629de05e4345ed426ab511b74d82ef3a
Anwesh43 <anweshthecool0@gmail.com>
2023-06-26 14:17:58 +0530

    line-slant-to-down: adding LineSlantToDown class

--- line-slant-to-down.ts
+++ line-slant-to-down.ts
@@ -184,3 +184,26 @@ class LSTDNode {
         return this 
     }
 }
+
+class LineSlantToDown {
+
+    curr : LSTDNode = new LSTDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
