2c75ee181089b690b9a79ee63b5c471f0cd68bd1
Anwesh43 <anweshthecool0@gmail.com>
2023-06-27 12:34:52 +0530

    single-sq-to-big-sq: adding SSTBSNode class

--- single-sq-to-big-sq.ts
+++ single-sq-to-big-sq.ts
@@ -131,3 +131,45 @@ class Animator {
         }
     }
 }
+
+class SSTBSNode {
+
+    prev : SSTBSNode 
+    next : SSTBSNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SSTBSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSSTBSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SSTBSNode {
+        var curr : SSTBSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
