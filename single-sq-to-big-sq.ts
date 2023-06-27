501e0ee16bd895fd9942c21a6a1bee23b2267d4b
Anwesh43 <anweshthecool0@gmail.com>
2023-06-27 12:29:37 +0530

    single-sq-to-big-sq: adding Stage class

--- single-sq-to-big-sq.ts
+++ single-sq-to-big-sq.ts
@@ -54,3 +54,36 @@ class DrawingUtil {
         DrawingUtil.drawSingleSqToBigSq(context, scale)
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
