42ce16b4f3d767235f7f80e0273617030b5dd655
Anwesh43 <anweshthecool0@gmail.com>
2023-06-28 11:56:27 +0530

    mirror-rot-line-up: adding MRLUNode class

--- mirror-rot-line-up.ts
+++ mirror-rot-line-up.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class MRLUNode {
+
+    prev : MRLUNode 
+    next : MRLUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new MRLUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawMRLUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : MRLUNode {
+        var curr : MRLUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
