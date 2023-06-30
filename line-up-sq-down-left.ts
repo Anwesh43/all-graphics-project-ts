1751075bda6a7041cab79190e522a284f7f9b322
Anwesh43 <anweshthecool0@gmail.com>
2023-06-30 13:19:15 +0530

    line-up-sq-down-left: adding LUSDLNode class

--- line-up-sq-down-left.ts
+++ line-up-sq-down-left.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LUSDLNode {
+
+    prev : LUSDLNode 
+    next : LUSDLNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LUSDLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLUSDLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LUSDLNode {
+        var curr : LUSDLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
