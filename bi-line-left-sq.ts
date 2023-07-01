b9169a830be60f081ab5cc18e32ca99baa0a04b4
Anwesh43 <anweshthecool0@gmail.com>
2023-07-01 18:28:25 +0530

    bi-line-left-sq: adding BLLSNode class

--- bi-line-left-sq.ts
+++ bi-line-left-sq.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class BLLSNode {
+
+    prev : BLLSNode 
+    next : BLLSNode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLLSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLLSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BLLSNode {
+        var curr : BLLSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
