093460b68ffb228c083fa536d62443ae662d485d
Anwesh43 <anweshthecool0@gmail.com>
2023-07-01 18:34:23 +0530

    bi-line-left-sq: adding fix

--- bi-line-left-sq.ts
+++ bi-line-left-sq.ts
@@ -51,9 +51,9 @@ class DrawingUtil {
         DrawingUtil.drawXY(context, w / 2 - (w / 2 + size / 2) * dsc(3), h / 2, () => {
             context.fillRect(-size * 0.5 * dsc(0), -size / 2, size * dsc(0), size)
             for (let j = 0; j < 2; j++) {
-                DrawingUtil.drawXY(context, 0, -size / 2, () => {
+                DrawingUtil.drawXY(context, 0, -size * 0.5 * (1 - 2 * j), () => {
                     context.scale(1, 1 - 2 * j)
-                    context.rotate(rot * dsc(2))
+                    context.rotate(rot * dsc(2) * (1 - 2 * j))
                     DrawingUtil.drawLine(context, 0, 0, -size * dsc(1), 0)
                 })
             }
