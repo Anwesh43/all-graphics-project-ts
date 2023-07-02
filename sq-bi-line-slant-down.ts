53224b53080a186db198c1ee00c279ab57e0c546
Anwesh43 <anweshthecool0@gmail.com>
2023-07-02 13:20:09 +0530

    sq-bi-line-slant-down: adding SBLSDNode class

--- sq-bi-line-slant-down.ts
+++ sq-bi-line-slant-down.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class SBLSDNode {
+
+    prev : SBLSDNode 
+    next : SBLSDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBLSDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSBLSDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SBLSDNode {
+        var curr : SBLSDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this
+    }
+}
