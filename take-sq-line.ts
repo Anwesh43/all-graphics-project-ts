fb32ea1168e7e2f3ddd01613499f15670f09a1d1
Anwesh43 <anweshthecool0@gmail.com>
2023-07-03 12:20:11 +0530

    take-sq-line: adding DrawingUtil class

--- take-sq-line.ts
+++ take-sq-line.ts
@@ -25,3 +25,50 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawTakeSqLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const dsc21 : number = ScaleUtil.divideScale(dsc(2), 0, 2)
+        const dsc22 : number = ScaleUtil.divideScale(dsc(2), 1, 2)
+
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2 + size) * dsc(3), () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * dsc22)
+                context.fillRect(-size, -size * dsc(0), size, size * dsc(0))
+            })
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * (dsc21 + dsc22) * j)
+                    DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(1))
+                })
+            }
+        })
+    }
+
+    static drawTSLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawTakeSqLine(context, scale)
+    }
+}
