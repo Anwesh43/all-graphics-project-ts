f0d5105981a9070de656ad4db8335648567abd5c
Anwesh43 <anweshthecool0@gmail.com>
2023-07-04 13:37:48 +0530

    z-line-sq-rot: adding ZLSRNode class

--- z-line-sq-rot.ts
+++ z-line-sq-rot.ts
@@ -147,3 +147,41 @@ class Animator {
         }
     }
 }
+
+class ZLSRNode {
+
+    prev : ZLSRNode 
+    next : ZLSRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ZLSRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : ZLSRNode {
+        var curr : ZLSRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
