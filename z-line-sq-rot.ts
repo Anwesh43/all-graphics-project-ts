b480a35852e84bd4505e74ab3c13dfd8e672df97
Anwesh43 <anweshthecool0@gmail.com>
2023-07-04 13:30:51 +0530

    z-line-sq-rot: adding DrawingUtil class

--- z-line-sq-rot.ts
+++ z-line-sq-rot.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 4.9 
@@ -26,3 +26,47 @@ class ScaleUtil {
     }
 }
 
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawZLineSqRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            context.fillRect(-size * 0.5 * dsc(0), -size / 2, size * dsc(0), size)
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1 - 2 * j)
+                    DrawingUtil.drawXY(context, 0, size / 2 + (h / 2 - size / 2) * (1 - dsc(1)), () => {
+                        context.rotate(-rot * dsc(2))
+                        DrawingUtil.drawLine(context, 0, 0, 0, size) 
+                    })
+                })
+            }
+        })
+    }
+
+    static drawZLSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawZLineSqRot(context, scale)
+    }
+}
