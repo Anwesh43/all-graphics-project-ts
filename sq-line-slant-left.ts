888ece16f248c3d73f1b8a6e4b8ed94697d497da
Anwesh43 <anweshthecool0@gmail.com>
2023-07-06 12:48:30 +0530

    sq-line-slant-left: adding SqLineSlantLeft class

--- sq-line-slant-left.ts
+++ sq-line-slant-left.ts
@@ -184,3 +184,26 @@ class SLSLNode {
         return this 
     }
 }
+
+class SqLineSlantLeft {
+
+    curr : SLSLNode = new SLSLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
