a867388322740643d9be8052bc7ca225c665a1fc
Anwesh43 <anweshthecool0@gmail.com>
2023-07-06 12:46:09 +0530

    sq-line-slant-left: adding SLSLNode class

--- sq-line-slant-left.ts
+++ sq-line-slant-left.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class SLSLNode {
+
+    prev : SLSLNode 
+    next : SLSLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLSLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLSLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SLSLNode {
+        var curr : SLSLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
