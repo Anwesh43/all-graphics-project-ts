244bd11d7b6463d37f5b295f83f340f3493ef9a1
Anwesh43 <anweshthecool0@gmail.com>
2023-07-08 13:18:48 +0530

    line-plus-bent-up: adding Renderer in Stage

--- line-plus-bent-up.ts
+++ line-plus-bent-up.ts
@@ -76,24 +76,29 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D | null 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
         this.canvas.height = h 
         this.context = this.canvas.getContext('2d')
         document.body.appendChild(this.canvas)
+        
     }
 
     render() {
         if (this.context) {
             this.context.fillStyle = backColor 
             this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
