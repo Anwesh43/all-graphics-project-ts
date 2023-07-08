5e49094a2182cf5ade4f6bd2d99ed11201488b62
Anwesh43 <anweshthecool0@gmail.com>
2023-07-08 11:47:56 +0530

    line-plus-bent-up: adding DrawingUtil class

--- line-plus-bent-up.ts
+++ line-plus-bent-up.ts
@@ -25,3 +25,48 @@ class ScaleUtil {
         return Math.min(1 / n)
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLinePlusBentUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            DrawingUtil.drawLine(context, 0, -size / 2, 0, -size / 2 + size * dsc(0))
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(-rot * (1 - dsc(1)))
+                for (let j = 0; j < 2; j++) {
+                    DrawingUtil.drawXY(context, 0, 0, () => {
+                        context.scale(1 - 2 * j, 1)
+                        context.rotate(deg * dsc(2))
+                        DrawingUtil.drawLine(context, 0, 0, size * Math.floor(dsc(0)), 0)
+                    })
+                }
+            })
+        })
+    }
+
+    static drawLPBUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLinePlusBentUp(context, scale)
+    }
+}
