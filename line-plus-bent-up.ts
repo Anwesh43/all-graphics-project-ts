39946caed6e78a836c674c325098242c6e54a13a
Anwesh43 <anweshthecool0@gmail.com>
2023-07-08 11:53:25 +0530

    line-plus-bent-up: adding Stage class

--- line-plus-bent-up.ts
+++ line-plus-bent-up.ts
@@ -14,6 +14,7 @@ const rot : number = Math.PI / 2
 const deg : number = Math.PI / 12 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -68,5 +69,38 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor
         context.strokeStyle = colors[i]
         DrawingUtil.drawLinePlusBentUp(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
