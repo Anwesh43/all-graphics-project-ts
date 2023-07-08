3309f0e054e8b4ca23b0faca8f44f0c4d401fbc8
Anwesh43 <anweshthecool0@gmail.com>
2023-07-08 12:42:15 +0530

    line-plus-bent-up: adding LinePlusBentUp class

--- line-plus-bent-up.ts
+++ line-plus-bent-up.ts
@@ -190,3 +190,26 @@ class LPBNode {
         return this 
     }
 }
+
+class LinePlusBentUp {
+
+    curr : LPBNode = new LPBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
