c549e37572a53c6cace1ce17adc94a26125542cc
Anwesh43 <anweshthecool0@gmail.com>
2023-07-09 13:54:14 +0530

    base-expand-line-up: adding BELUNode class

--- base-expand-line-up.ts
+++ base-expand-line-up.ts
@@ -146,3 +146,44 @@ class Animator {
         }
     }
 }
+
+class BELUNode {
+
+    prev : BELUNode
+    next: BELUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BELUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBELUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BELUNode {
+        var curr = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
