478cb65c7a4b13196023a9f6b28eba860b385f01
Anwesh43 <anweshthecool0@gmail.com>
2023-07-12 09:42:57 +0530

    drop-bi-sq-rot-right: adding DrawingUtil class

--- drop-bi-sq-rot-right.ts
+++ drop-bi-sq-rot-right.ts
@@ -26,3 +26,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2)  < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawDropBiSqRotRight(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const sqSize : number = Math.min(w, h) / sqSizeFactor
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawXY(context, (size - sqSize) * (1 - dsc(2)), -h * 0.5 * (1 - dsc(1)), () => {
+                        context.fillRect(0, -2 * sqSize, sqSize, 2 * sqSize)
+                    })
+                    DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+                })
+            }
+        })
+    }
+
+    static drawDBSRRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawDropBiSqRotRight(context, scale)
+    }
+}
