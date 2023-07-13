0e6b1fc8f31dcd158fc3407caf7d735e5b15844a
Anwesh43 <anweshthecool0@gmail.com>
2023-07-13 11:22:36 +0530

    cross-line-angle-down: adding constants

---
+++ cross-line-angle-down.ts
@@ -0,0 +1,18 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const rot : number = -Math.PI / 4
+const deg : number = Math.PI 
+const scGap : number = 0.04 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const w : number = window.innerWidth 
+const h : number = window.innerHeight  
+
