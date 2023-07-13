f1b59da22920d4f01869c34a1119a6022f4369f0
Anwesh43 <anweshthecool0@gmail.com>
2023-07-13 12:05:46 +0530

    cross-line-angle-down: adding CrossLineAngleDown class

--- cross-line-angle-down.ts
+++ cross-line-angle-down.ts
@@ -191,3 +191,26 @@ class CLADNode {
         return this 
     }
 }
+
+class CrossLineAngleDown {
+
+    curr : CLADNode = new CLADNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
