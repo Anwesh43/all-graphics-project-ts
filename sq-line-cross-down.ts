4721c711fac83f45200a07705482e8800c586018
Anwesh43 <anweshthecool0@gmail.com>
2023-07-14 10:08:21 +0530

    sq-line-cross-down: adding DrawingUtil class

--- sq-line-cross-down.ts
+++ sq-line-cross-down.ts
@@ -11,7 +11,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2
+const rot : number = Math.PI / 6
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 
@@ -23,5 +23,48 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawSqLineCrossDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
+            DrawingUtil.drawXY(context, 0, h * 0.5 * (1 - dsc(0)), () => {
+                context.fillRect(-size, 0, size, size)
+            })
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * (1 - 2 * j) * dsc(2))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(1))
+                })
+            }
+        })
+    }
+
+    static drawSLCDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSqLineCrossDown(context, scale)
     }
 }
