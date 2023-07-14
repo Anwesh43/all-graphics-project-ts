6d02086933a47d0f956a72eb6cbd5fc7680983bf
Anwesh43 <anweshthecool0@gmail.com>
2023-07-14 10:18:06 +0530

    sq-line-cross-down: adding SqLineCrossDown class

--- sq-line-cross-down.ts
+++ sq-line-cross-down.ts
@@ -187,3 +187,26 @@ class SLCDNode {
         return this 
     }
 }
+
+class SqLineCrossDown {
+
+    curr : SLCDNode = new SLCDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
