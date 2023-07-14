d03ea4498b20375f7991055f09f44bc43cfb59c4
Anwesh43 <anweshthecool0@gmail.com>
2023-07-14 10:16:56 +0530

    sq-line-cross-down: adding SLCDNode class

--- sq-line-cross-down.ts
+++ sq-line-cross-down.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class SLCDNode {
+
+    prev : SLCDNode
+    next : SLCDNode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLCDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLCDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SLCDNode {
+        var curr : SLCDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
