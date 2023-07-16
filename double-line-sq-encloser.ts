7467ec7ada7339166a1ab61db92e5f28f29741fb
Anwesh43 <anweshthecool0@gmail.com>
2023-07-16 11:58:02 +0530

    double-line-sq-encloser: adding DoubleLineSqEncloser class

--- double-line-sq-encloser.ts
+++ double-line-sq-encloser.ts
@@ -188,3 +188,26 @@ class DLSENode {
         return this 
     }
 }
+
+class DoubleLineSqEncloser {
+
+    curr : DLSENode = new DLSENode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
