b279d14be77ce4c233281e100d6618808f450ff5
Anwesh43 <anweshthecool0@gmail.com>
2023-07-16 11:45:11 +0530

    double-line-sq-encloser: adding DrawingUtil class

--- double-line-sq-encloser.ts
+++ double-line-sq-encloser.ts
@@ -25,3 +25,47 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawDoubleLineSqEncloser(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size / 2) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, -h / 2 + (h / 2) * dsc(0), () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+            DrawingUtil.drawXY(context, (-w / 2 - size / 2) * (1 - dsc(1)), 0, () => {
+                context.fillRect(-size / 2, 0, size, size)
+            })
+        })
+    }
+
+    static drawDLSENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDoubleLineSqEncloser(context, scale)
+    }
+}
