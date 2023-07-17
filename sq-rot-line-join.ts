717f755f4b407816275d7bcc01da8afa020a87f8
Anwesh43 <anweshthecool0@gmail.com>
2023-07-17 13:55:08 +0530

    sq-rot-line-join: adding SRLJNode class

--- sq-rot-line-join.ts
+++ sq-rot-line-join.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class SRLJNode {
+
+    prev : SRLJNode 
+    next : SRLJNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SRLJNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSRLJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SRLJNode {
+        var curr : SRLJNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
