593c600c505776474c5d0cba515a09bbb1c006e0
Anwesh43 <anweshthecool0@gmail.com>
2023-07-19 12:13:10 +0530

    bi-line-drop-down: adding BiLineDropDown class

--- bi-line-drop-down.ts
+++ bi-line-drop-down.ts
@@ -189,3 +189,26 @@ class BLDDNode {
         return this 
     }
 }
+
+class BiLineDropDown {
+
+    curr : BLDDNode = new BLDDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
