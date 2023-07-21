9a52ae58756e279b7b3f0d2ff435b8b57c06f48d
Anwesh43 <anweshthecool0@gmail.com>
2023-07-21 11:22:54 +0530

    sq-rot-line-left: adding fixe

--- sq-rot-line-left.ts
+++ sq-rot-line-left.ts
@@ -50,10 +50,11 @@ class DrawingUtil {
         const dsc : (number) => number = (i : number) => ScaleUtil.divideScale(scale, i, parts)
         DrawingUtil.drawXY(context, w / 2 - (w / 2 + size) * dsc(3), h / 2, () => {
             DrawingUtil.drawXY(context, 0, h * 0.5 * (1 - dsc(0)), () => {
+                context.rotate(rot * dsc(2))
                 context.fillRect(0, 0, size, size)
             })
             DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(1)), 0, () => {
-                context.rotate(rot * dsc(2))
+                DrawingUtil.drawLine(context, 0, 0, 0, size)
             })
         })
     }
