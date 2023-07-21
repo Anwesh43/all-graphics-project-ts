68fe38f6915d1775f5ebf889295a508c186be163
Anwesh43 <anweshthecool0@gmail.com>
2023-07-21 10:52:02 +0530

    sq-rot-line-left: adding State class

--- sq-rot-line-left.ts
+++ sq-rot-line-left.ts
@@ -13,6 +13,7 @@ const rot : number = Math.PI / 4
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight
+const scGap : number = 0.04 / parts 
 
 class ScaleUtil {
 
@@ -96,5 +97,29 @@ class Stage {
         stage.initCanvas()
         stage.render()
         stage.handleTap()
+    }
+}
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0 
+    prevScale : number = 0 
+
+    update(cb : () => void) {
+        this.scale += scGap * this.dir 
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir 
+            this.dir = 0 
+            this.prevScale = this.scale 
+            cb()
+        }
+    }
+
+    startUpdating(cb : () => void) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        }
     }
 }
