5dfd26acc65c2ff3fb0ffc7dc544510e772c9d51
Anwesh43 <anweshthecool0@gmail.com>
2023-07-26 10:09:02 +0530

    big-short-line-down: adding BSLDNode class

--- big-short-line-down.ts
+++ big-short-line-down.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class BSLDNode {
+
+    prev : BSLDNode 
+    next : BSLDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BSLDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBSLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BSLDNode {
+        var curr : BSLDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
