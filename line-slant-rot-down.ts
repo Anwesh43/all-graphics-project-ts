e6768870cbd3b2aad9074ad7244b5534d2628d44
Anwesh43 <anweshthecool0@gmail.com>
2023-07-28 01:40:10 +0530

    line-slant-rot-down: adding fix 5

--- line-slant-rot-down.ts
+++ line-slant-rot-down.ts
@@ -48,7 +48,7 @@ class DrawingUtil {
     static drawLineSlantRotDown(context : CanvasRenderingContext2D, scale : number) {
         const size : number = Math.min(w, h) / sizeFactor 
         const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
             for (let j = 0; j < 2; j++) {
                 DrawingUtil.drawXY(context, 0, 0 , () => {
                     context.scale(1 - 2 * j, 1)
