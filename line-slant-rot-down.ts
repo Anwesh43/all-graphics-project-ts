d1eb54b25cefb64b82a0efe956b09ce61ef9f275
Anwesh43 <anweshthecool0@gmail.com>
2023-07-28 01:00:24 +0530

    line-slant-rot-down: adding Renderer class

--- line-slant-rot-down.ts
+++ line-slant-rot-down.ts
@@ -211,3 +211,25 @@ class LineSlantRotDown {
         this.curr.startUpdating(cb)
     }
 }
+
+class Renderer {
+
+    lsrd : LineSlantRotDown = new LineSlantRotDown()
+    animator : Animator = new Animator()
+
+    render(context : CanvasRenderingContext2D) {
+        this.lsrd.draw(context)
+    }
+
+    handleTap(cb : () => void) {
+        this.lsrd.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.lsrd.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
+    }
+}
