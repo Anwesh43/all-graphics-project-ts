fe87d10a95b4fd6da3e3ce1ebb91ef9c16fbce98
Anwesh43 <anweshthecool0@gmail.com>
2023-07-29 09:46:11 +0530

    bi-line-sq-down: adding DrawingUtil class

--- bi-line-sq-down.ts
+++ bi-line-sq-down.ts
@@ -11,7 +11,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2 
+const rot : number = Math.PI
 const w : number = window.innerWidth 
 const h : number = window.innerHeight  
 
@@ -23,5 +23,50 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiLineSqDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h  / 2 + (h / 2) * dsc(3), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(0), 0)
+                    DrawingUtil.drawXY(context, size / 2, 0, () => {
+                        context.rotate(rot * dsc(2))
+                        context.fillRect(0, -size * 0.5 * dsc(1), size * 0.5, size * 0.5 * dsc(0))
+                    })
+                })
+            }
+        })
+    }
+
+    static drawBLSDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBiLineSqDown(context, scale)
     }
 }
