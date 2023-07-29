bc87d0b031e32c0e06c12230fd5958a128212120
Anwesh43 <anweshthecool0@gmail.com>
2023-07-29 10:11:32 +0530

    bi-line-sq-down: adding BiLineSqDown class

--- bi-line-sq-down.ts
+++ bi-line-sq-down.ts
@@ -189,3 +189,26 @@ class BLSDNode {
         return this 
     }
 }
+
+class BiLineSqDown {
+
+    curr : BLSDNode = new BLSDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
