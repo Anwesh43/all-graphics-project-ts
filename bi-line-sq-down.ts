c0d039a7365b3319b9883c83c433056b8301ae84
Anwesh43 <anweshthecool0@gmail.com>
2023-07-29 10:07:51 +0530

    bi-line-sq-down: adding BLSDNode class

--- bi-line-sq-down.ts
+++ bi-line-sq-down.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class BLSDNode {
+
+    prev : BLSDNode 
+    next : BLSDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLSDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLSDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BLSDNode {
+        var curr : BLSDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
