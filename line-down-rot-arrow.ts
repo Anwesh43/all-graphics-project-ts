66304b7991a258339e98a9a425a513666b2bc000
Anwesh43 <anweshthecool0@gmail.com>
2023-07-31 14:23:39 +0530

    line-down-rot-arrow: addign DrawingUtil class

--- line-down-rot-arrow.ts
+++ line-down-rot-arrow.ts
@@ -14,6 +14,7 @@ const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 4
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const deg : number = Math.PI
 
 class ScaleUtil {
 
@@ -23,5 +24,46 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineDownRotArrow(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2 + size) * dsc(3), () => {
+            context.rotate(deg * dsc(1))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, (h / 2) * (1 - dsc(0)), () => {
+                    context.rotate(rot * dsc(2))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+        })
+    }
+
+    static drawLDRANode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineDownRotArrow(context, scale)
     }
 }
