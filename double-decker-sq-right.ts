32a02467387554bac18b4d035e8cf8451e43ac12
Anwesh43 <anweshthecool0@gmail.com>
2023-08-01 12:50:43 +0530

    double-decker-sq-right: adding Renderer in stage

--- double-decker-sq-right.ts
+++ double-decker-sq-right.ts
@@ -58,6 +58,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D | null 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
@@ -70,12 +71,15 @@ class Stage {
         if (this.context) {
             this.context.fillStyle = backColor 
             this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
