ef9ec5c38ac60d28359e8a4ca3e57c023f6acf2a
Anwesh43 <anweshthecool0@gmail.com>
2023-08-04 13:58:19 +0530

    line-sq-middle-up: adding LSMUNode class

--- line-sq-middle-up.ts
+++ line-sq-middle-up.ts
@@ -142,3 +142,42 @@ class Animator {
         }
     }
 }
+
+class LSMUNode {
+
+    prev : LSMUNode 
+    next : LSMUNode 
+    state : State = new State()
+
+    addNeighbor() {
+
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSMUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSMUNode {
+        var curr : LSMUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
