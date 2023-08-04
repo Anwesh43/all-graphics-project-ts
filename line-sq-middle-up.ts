d6bd704a4136354e4475b06ee633904493f20e29
Anwesh43 <anweshthecool0@gmail.com>
2023-08-04 17:53:10 +0530

    line-sq-middle-up: adding LineSqMiddleUp class

--- line-sq-middle-up.ts
+++ line-sq-middle-up.ts
@@ -181,3 +181,26 @@ class LSMUNode {
         return this 
     }
 }
+
+class LineSqMiddleUp {
+
+    curr : LSMUNode = new LSMUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
