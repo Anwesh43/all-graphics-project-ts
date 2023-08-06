b8f24e7a6bc5e4edf9bffe1a41e972c05754cb6c
Anwesh43 <anweshthecool0@gmail.com>
2023-08-06 16:27:16 +0530

    drop-rot-line-holder: adding DRLHNode class

--- drop-rot-line-holder.ts
+++ drop-rot-line-holder.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class DRLHNode {
+
+    prev : DRLHNode 
+    next : DRLHNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DRLHNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDRLHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : DRLHNode {
+        var curr : DRLHNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
