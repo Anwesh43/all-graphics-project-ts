2ecc6dc532a1008367d8a96f65e7ef8f58c8657f
Anwesh43 <anweshthecool0@gmail.com>
2023-08-06 16:28:32 +0530

    drop-rot-line-holder: adding DropRotLineHolder class

--- drop-rot-line-holder.ts
+++ drop-rot-line-holder.ts
@@ -186,3 +186,26 @@ class DRLHNode {
         return this 
     }
 }
+
+class DropRotLineHolder {
+
+    curr : DRLHNode = new DRLHNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
