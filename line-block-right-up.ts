d546fb212028a8fa768d4aa58e44288cd9c9e9ba
Anwesh43 <anweshthecool0@gmail.com>
2023-08-07 15:23:31 +0530

    line-block-right-up: adding LineBlockRightUp class

--- line-block-right-up.ts
+++ line-block-right-up.ts
@@ -190,3 +190,26 @@ class LBRUNode {
         return this 
     }
 }
+
+class LineBlockRightUp {
+
+    curr : LBRUNode = new LBRUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
