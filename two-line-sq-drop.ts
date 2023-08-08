80cb232c4fb97e69a3c5e9a7b327d79214188312
Anwesh43 <anweshthecool0@gmail.com>
2023-08-08 11:57:12 +0530

    two-line-sq-drop: adding DrawingUtil class

--- two-line-sq-drop.ts
+++ two-line-sq-drop.ts
@@ -24,3 +24,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawTwoLineSqDrop(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, 0, 0, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, (w / 2) * j + w / 4 , h * dsc(2), () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+                })
+            }
+            DrawingUtil.drawXY(context, w / 2, h * dsc(3), () => {
+                context.fillRect(-size / 4, 0, size / 2, size * 0.5 * dsc(1))
+            })
+        })
+    }
+
+    static drawTLSDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        DrawingUtil.drawTwoLineSqDrop(context, scale)
+    }
+}
