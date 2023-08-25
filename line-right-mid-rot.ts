e291592729183c01125fe317e30f84e07a615169
Anwesh43 <anweshthecool0@gmail.com>
2023-08-25 15:58:50 +0530

    line-right-mid-rot: adding LRMRNode class

--- line-right-mid-rot.ts
+++ line-right-mid-rot.ts
@@ -150,3 +150,45 @@ class Animator {
         }
     }
 }
+
+class LRMRNode {
+
+    prev : LRMRNode 
+    next : LRMRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRMRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRMRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRMRNode {
+        var curr : LRMRNode = this.next 
+        if (dir == -1) {
+            curr = this.prev 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
