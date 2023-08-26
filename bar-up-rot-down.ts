1c219910920237be7d8ba41b4cfe401aa8cb19d4
Anwesh43 <anweshthecool0@gmail.com>
2023-08-26 14:00:18 +0530

    bar-up-rot-down: adding BarUpRotDown class

--- bar-up-rot-down.ts
+++ bar-up-rot-down.ts
@@ -167,3 +167,26 @@ class BURDNode {
         return this 
     }
 }
+
+class BarUpRotDown {
+
+    curr : BURDNode = new BURDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
