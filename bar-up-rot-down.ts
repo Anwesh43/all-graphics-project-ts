6d4c5b0c1293070e5ad5b2f7612b6c35859df7c6
Anwesh43 <anweshthecool0@gmail.com>
2023-08-26 13:16:17 +0530

    bar-up-rot-down: adding constants

---
+++ bar-up-rot-down.ts
@@ -0,0 +1,15 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
+const rot : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
