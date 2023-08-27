7be60497fdb5964065a5c427747dac3ee35ed08f
Anwesh43 <anweshthecool0@gmail.com>
2023-08-27 14:21:43 +0530

    line-rot-up-left: adding size fix

--- line-rot-up-left.ts
+++ line-rot-up-left.ts
@@ -11,7 +11,7 @@ const strokeFactor : number = 90
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 2
-const sizeFactor : number = 90 
+const sizeFactor : number = 4.9 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 
