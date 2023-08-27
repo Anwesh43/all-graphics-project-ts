82512ce520910cb670055fdb48a22d67982a6431
Anwesh43 <anweshthecool0@gmail.com>
2023-08-27 14:09:35 +0530

    line-rot-up-left: adding DrawingUtil class modifications

--- line-rot-up-left.ts
+++ line-rot-up-left.ts
@@ -28,11 +28,36 @@ class ScaleUtil {
 
 class DrawingUtil {
 
-    static maxScale(scale : number, i : number, n : number) : number {
-        return Math.max(0, scale - i / n)
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
     }
 
-    static divideScale(scale : number, i : number, n : number) : number {
-        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineRotUpLeft(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(3)), h * 0.5 * dsc(1), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+        })
+    }
+
+    static drawLRULNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineRotUpLeft(context, scale)
     }
 }
