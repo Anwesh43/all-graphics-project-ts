eb6a01578068405ab225149dbe6124df8c511bc7
Anwesh43 <anweshthecool0@gmail.com>
2023-09-07 19:15:35 +0530

    right-line-angle-up: adding RLAUNode class

--- right-line-angle-up.ts
+++ right-line-angle-up.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class RLAUNode {
+
+    prev : RLAUNode 
+    next : RLAUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLAUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRLAUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RLAUNode {
+        var curr : RLAUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
