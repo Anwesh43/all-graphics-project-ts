fd20bd2ab54ecbc3dac3e5fbfb0c6a246810b35e
Anwesh43 <anweshthecool0@gmail.com>
2023-09-07 19:16:58 +0530

    right-line-angle-up: adding RightLineAngleUp class

--- right-line-angle-up.ts
+++ right-line-angle-up.ts
@@ -184,3 +184,26 @@ class RLAUNode {
         return this 
     }
 }
+
+class RightLineAngleUp {
+
+    curr : RLAUNode = new RLAUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+    
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
