2789cc11b807980148b46557f0c16597efdd3c70
Anwesh43 <anweshthecool0@gmail.com>
2023-09-09 21:46:29 +0530

    line-bi-sq-rot: adding fix

--- line-bi-sq-rot.ts
+++ line-bi-sq-rot.ts
@@ -65,6 +65,7 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
         DrawingUtil.drawLineBiSqRot(context, scale)
     }
 }
