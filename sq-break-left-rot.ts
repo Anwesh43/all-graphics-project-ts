c7800983bc14e32a740302744be107c7837dc6ae
Anwesh43 <anweshthecool0@gmail.com>
2023-09-17 13:30:59 +0530

    sq-break-left-rot: adding SBLRNode class

--- sq-break-left-rot.ts
+++ sq-break-left-rot.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class SBLRNode {
+
+    prev : SBLRNode 
+    next : SBLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSBLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SBLRNode {
+        var curr : SBLRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
