d23508fbf891db0deaacf4bfd8ade3b89f45479d
Anwesh43 <anweshthecool0@gmail.com>
2023-10-02 15:24:59 +0530

    rot-line-left-move: adding RLLMNode class

--- rot-line-left-move.ts
+++ rot-line-left-move.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class RLLMNode {
+
+    prev : RLLMNode 
+    next : RLLMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLLMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRLLMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RLLMNode {
+        var curr : RLLMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
