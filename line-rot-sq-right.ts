6202f5cdd014632552e0074adba0016d366b7da7
Anwesh43 <anweshthecool0@gmail.com>
2023-10-06 18:40:27 +0530

    line-rot-sq-right: adding LRSRNode class

--- line-rot-sq-right.ts
+++ line-rot-sq-right.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LRSRNode {
+
+    prev : LRSRNode 
+    next : LRSRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRSRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    } 
+
+    getNext(dir : number, cb : () => void) : LRSRNode {
+        var curr : LRSRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
