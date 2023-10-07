c54d739baa2c2c1d2a233ce9b791b6a55feb0690
Anwesh43 <anweshthecool0@gmail.com>
2023-10-07 18:13:00 +0530

    sq-shrink-rot-up: adding SSRUNode class

--- sq-shrink-rot-up.ts
+++ sq-shrink-rot-up.ts
@@ -126,3 +126,45 @@ class Animator {
         }
     }
 }
+
+class SSRUNode {
+
+    prev : SSRUNode 
+    next : SSRUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SSRUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSSRUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SSRUNode {
+        var curr : SSRUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
