3190b7f65fb2154d07c69ef79e4bb33a00e5caee
Anwesh43 <anweshthecool0@gmail.com>
2023-10-11 21:42:31 +0530

    sq-shrink-line-right: adding SSLRNode class

--- sq-shrink-line-right.ts
+++ sq-shrink-line-right.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class SSLRNode {
+
+    prev : SSLRNode 
+    next : SSLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SSLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSSLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) {
+        var curr : SSLRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
