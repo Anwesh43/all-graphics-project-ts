7292aeb2776ebd5cfc77f2809cfb4c2290929e2e
Anwesh43 <anweshthecool0@gmail.com>
2023-10-12 23:23:28 +0530

    bent-line-to-straight-right: adding BentLineToStraightRight class

--- bent-line-to-straight-right.ts
+++ bent-line-to-straight-right.ts
@@ -187,3 +187,27 @@ class BLSRNode {
         return this 
     }
 }
+
+class BentLineToStraightRight {
+
+    curr : BLSRNode = new BLSRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+
+}
