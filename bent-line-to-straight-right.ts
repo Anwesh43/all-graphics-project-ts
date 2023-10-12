6c5fcbc3e547c004e5e62dcb801bba6038c9b38e
Anwesh43 <anweshthecool0@gmail.com>
2023-10-12 23:21:25 +0530

    bent-line-to-straight-right: adding BLSRNode class

--- bent-line-to-straight-right.ts
+++ bent-line-to-straight-right.ts
@@ -144,3 +144,46 @@ class Animator {
         }
     }
 }
+
+class BLSRNode {
+
+    prev : BLSRNode 
+    next : BLSRNode 
+
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLSRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BLSRNode {
+        var curr : BLSRNode = this.prev
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
