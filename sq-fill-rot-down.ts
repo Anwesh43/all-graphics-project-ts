b512584d1423536990ededc8afdf8752235aefda
Anwesh43 <anweshthecool0@gmail.com>
2023-10-13 15:13:33 +0530

    sq-fill-rot-down: adding DrawingUtil class

--- sq-fill-rot-down.ts
+++ sq-fill-rot-down.ts
@@ -23,3 +23,27 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawSqFillRotDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(2), () => {
+            context.rotate(rot * dsc(1))
+            context.fillRect(0, -size * dsc(0), size * dsc(0), size * dsc(0))
+        })
+    }
+
+    static drawSFRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSqFillRotDown(context, scale)
+    }
+}
