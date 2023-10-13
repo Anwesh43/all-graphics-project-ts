22c0f010f75dca1cf798f966a0581f98cb78db2d
Anwesh43 <anweshthecool0@gmail.com>
2023-10-13 15:01:49 +0530

    sq-fill-rot-down: adding ScaleUtil class

--- sq-fill-rot-down.ts
+++ sq-fill-rot-down.ts
@@ -12,3 +12,14 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight 
 const parts : number = 3
 const scGap : number = 0.03 / parts 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
