2d43d93a74282c4d02493423e0f88e6489bef389
Anwesh43 <anweshthecool0@gmail.com>
2023-10-19 14:51:47 +0530

    line-perp-rot-down: adding ScaleUtil class

--- line-perp-rot-down.ts
+++ line-perp-rot-down.ts
@@ -14,3 +14,14 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight 
 const parts : number = 4
 const scGap : number = 0.04 / parts 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+    
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
