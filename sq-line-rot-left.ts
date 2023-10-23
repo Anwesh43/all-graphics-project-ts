bb3590e6d86a8c41c8c80547aa45783b55a800b8
Anwesh43 <anweshthecool0@gmail.com>
2023-10-23 21:14:04 +0530

    sq-line-rot-left: adding DrawingUtil class

--- sq-line-rot-left.ts
+++ sq-line-rot-left.ts
@@ -13,7 +13,8 @@ const delay : number = 20
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
-const rot : number = -Math.PI / 4
+const rot : number = Math.PI / 2
+const deg : number = Math.PI / 4
 
 class ScaleUtil {
 
@@ -23,5 +24,51 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSqLineRotLeft(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawXY(context, -size / 2, 0, () => {
+                        context.rotate(deg)
+                        DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(1))
+                    })
+                })
+            }
+            context.fillRect(-size / 2, -size * dsc(1), size, size * dsc(1))
+        })
+    }
+
+    static drawSLRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawSqLineRotLeft(context, scale)
     }
 }
