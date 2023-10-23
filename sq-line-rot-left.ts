147962da9c1b46da7029ddbf9209d38b868aa711
Anwesh43 <anweshthecool0@gmail.com>
2023-10-23 21:20:46 +0530

    sq-line-rot-left: adding SLRLNode class

--- sq-line-rot-left.ts
+++ sq-line-rot-left.ts
@@ -149,3 +149,45 @@ class Animator {
         }
     }
 }
+
+class SLRLNode {
+
+    prev : SLRLNode 
+    next : SLRLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLRLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SLRLNode {
+        var curr : SLRLNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
