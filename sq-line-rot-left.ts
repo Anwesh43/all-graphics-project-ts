d749075646a8941a10e918a4620842294cc1ea5a
Anwesh43 <anweshthecool0@gmail.com>
2023-10-23 21:22:02 +0530

    sq-line-rot-left: adding SqLineRotLeft class

--- sq-line-rot-left.ts
+++ sq-line-rot-left.ts
@@ -191,3 +191,26 @@ class SLRLNode {
         return this 
     }
 }
+
+class SqLineRotLeft {
+
+    curr : SLRLNode = new SLRLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+                cb()
+            })
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
