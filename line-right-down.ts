b28ca5e204f9324c894b39802f6b397676fce579
Anwesh43 <anweshthecool0@gmail.com>
2023-10-29 16:22:07 +0530

    line-right-down: adding LineRightDown class

--- line-right-down.ts
+++ line-right-down.ts
@@ -184,3 +184,26 @@ class LRDNode {
         return this 
     }
 }
+
+class LineRightDown {
+
+    curr : LRDNode = new LRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
