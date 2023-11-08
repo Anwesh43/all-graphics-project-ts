5167247ee3e8d4b3fea71cc750f7ceaeba78b884
Anwesh43 <anweshthecool0@gmail.com>
2023-11-08 14:32:20 +0530

    line-rot-sq-left-up: adding LineRotSqLeftUp class

--- line-rot-sq-left-up.ts
+++ line-rot-sq-left-up.ts
@@ -187,3 +187,26 @@ class LRSLUNode {
         return this 
     }
 }
+
+class LineRotSqLeftUp {
+
+    curr : LRSLUNode = new LRSLUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
