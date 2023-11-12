86b1ded1746d9833e0151823f904bf7d6ba75e34
Anwesh43 <anweshthecool0@gmail.com>
2023-11-12 17:11:44 +0530

    arrow-from-perp-line: adding DrawingUtil class

--- arrow-from-perp-line.ts
+++ arrow-from-perp-line.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90 
 const delay : number = 20 
@@ -25,5 +25,51 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArrowFromPerpLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / strokeFactor 
+        const lSize : number = size / lSizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + lSize) * dsc(4), h / 2, () => {
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(0)), () => {
+                context.rotate(rot * dsc(3))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, -w * 0.5 * (1 - dsc(1)), 0, () => {
+                    context.rotate(deg * (1 - 2 * j) * dsc(2))
+                    DrawingUtil.drawLine(context, 0, 0, -lSize, 0)
+                })
+            }
+            
+        })
+    }
+
+    static drawAFPLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawArrowFromPerpLine(context, scale)
     }
 }
