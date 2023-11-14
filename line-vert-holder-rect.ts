dd4fa835fb47ed9adb09a91c21b3e8430adecdea
Anwesh43 <anweshthecool0@gmail.com>
2023-11-14 20:20:19 +0530

    line-vert-holder-rect: adding DrawingUtil class

--- line-vert-holder-rect.ts
+++ line-vert-holder-rect.ts
@@ -25,3 +25,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineVertHolderRect(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawXY(context, 0, (-h * 0.5 - size / 2) * (1 - dsc(0)), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, size)
+            })
+            DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(1)), 0, () => {
+                context.fillRect(0, -size / 4, size / 2, size / 2)
+            })
+        })
+    }
+
+    static drawLVHRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineVertHolderRect(context, scale)
+    }
+}
