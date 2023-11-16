18444c7e2c8a13434deebcac102efc129b0e66f2
Anwesh43 <anweshthecool0@gmail.com>
2023-11-17 01:28:14 +0530

    divide-line-rect-up: adding DLRUNode class

--- divide-line-rect-up.ts
+++ divide-line-rect-up.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class DLRUNode {
+    
+    prev : DLRUNode 
+    next : DLRUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DLRUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDLRUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : DLRUNode {
+        var curr : DLRUNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this
+    }
+}
