c8304e519f8febc5d096f21caa5f764f28f8d51c
Anwesh43 <anweshthecool0@gmail.com>
2023-12-23 14:26:41 +0530

    sq-extend-line-left: adding fixes 3

--- sq-extend-line-left.ts
+++ sq-extend-line-left.ts
@@ -45,7 +45,7 @@ class DrawingUtil {
     static drawSqExtendLineLeft(context : CanvasRenderingContext2D, scale : number) {
         const size : number = Math.min(w, h) / sizeFactor 
         const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2 - (w  / 2 + size / 2) * dsc(3), h / 2, () => {
+        DrawingUtil.drawXY(context, w / 2 + (w  / 2 + size) * dsc(3), h / 2, () => {
             for (let j = 0; j < 2; j++) {
                 DrawingUtil.drawXY(context, 0, 0, () => {
                     context.scale(1, 1 - 2 * j)
