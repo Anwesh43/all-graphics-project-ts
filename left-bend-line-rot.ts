c6a04c0fe8412731766e953fa402a2f60dae19e5
Anwesh43 <anweshthecool0@gmail.com>
2023-12-24 17:36:50 +0530

    left-bend-line-rot: adding LBLRNode class

--- left-bend-line-rot.ts
+++ left-bend-line-rot.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LBLRNode {
+
+    prev : LBLRNode 
+    next : LBLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBLRNode {
+        var curr : LBLRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
