0dec45c907b707b5a518884332c1a28e5bea43cc
Anwesh43 <anweshthecool0@gmail.com>
2023-12-25 18:10:42 +0530

    sq-all-line-rot: adding DrawingUtil class

--- sq-all-line-rot.ts
+++ sq-all-line-rot.ts
@@ -5,8 +5,8 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4
-const scGap : number = 0.04 / parts 
+const parts : number = 3
+const scGap : number = 0.03 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
 const delay : number = 20
@@ -23,5 +23,52 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSqAllLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 4; j++) {
+                const dsc0j : number = ScaleUtil.divideScale(dsc(0), j, 4)
+                const dsc1j : number = ScaleUtil.divideScale(dsc(1), j, 4)
+                const dsc2j : number = ScaleUtil.divideScale(dsc(2), j, 4)
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    const ik : number = j % 2
+                    context.rotate(rot * j)
+                    DrawingUtil.drawXY(context, -size / 2, size / 2 + dsc2j * ((h / 2) * (1 - ik) + (w / 2) * ik) , () => {
+                        context.rotate(rot * dsc1j)
+                        DrawingUtil.drawLine(context, 0, 0, size * dsc0j, 0)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawSALRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSqAllLineRot(context, scale)
     }
 }
