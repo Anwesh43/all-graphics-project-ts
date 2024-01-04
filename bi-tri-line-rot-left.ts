9e1f33a256746570c7f3dce7f8170eaae3db0a05
Anwesh43 <anweshthecool0@gmail.com>
2024-01-05 02:46:25 +0530

    bi-tri-line-rot-left: adding BiTriLineRotLeft class

--- bi-tri-line-rot-left.ts
+++ bi-tri-line-rot-left.ts
@@ -187,3 +187,26 @@ class BTLRNode {
         return this 
     }
 }
+
+class BiTriLineRotLeft {
+
+    curr : BTLRNode = new BTLRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
