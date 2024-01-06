65e8c8f556d75325b926f03b86c20a544b3a250e
Anwesh43 <anweshthecool0@gmail.com>
2024-01-06 10:59:58 +0530

    side-bar-diag-move: adding DrawingUtil class

--- side-bar-diag-move.ts
+++ side-bar-diag-move.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSideBarDiagMove(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(2), h / 2 + (h / 2) * dsc(2), () => {
+            context.rotate(rot * dsc(1))
+            DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(0)), 0, () => {
+                context.fillRect(0, -size / 4, size, size / 4)
+            })
+        })
+    }
+
+    static drawSBDMNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSideBarDiagMove(context, scale)
+    }
+}
