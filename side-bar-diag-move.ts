9e0b6510238ec54e44ed8249e37055ec084c2973
Anwesh43 <anweshthecool0@gmail.com>
2024-01-06 11:06:53 +0530

    side-bar-diag-move: adding SBDMNode class

--- side-bar-diag-move.ts
+++ side-bar-diag-move.ts
@@ -138,3 +138,46 @@ class Animator {
         }
     }
 }
+
+
+class SBDMNode {
+
+    prev : SBDMNode 
+    next : SBDMNode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBDMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSBDMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SBDMNode {
+        var curr : SBDMNode = this.prev
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
