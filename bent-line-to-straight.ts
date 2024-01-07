ef4eb9d44cbf78e8f7b4ce0593a6709dd27b1e9a
Anwesh43 <anweshthecool0@gmail.com>
2024-01-07 15:19:29 +0530

    bent-line-to-straight: adding BLTSNode class

--- bent-line-to-straight.ts
+++ bent-line-to-straight.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class BLTSNode {
+
+    prev : BLTSNode 
+    next : BLTSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLTSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLTSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BLTSNode {
+        var curr : BLTSNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
