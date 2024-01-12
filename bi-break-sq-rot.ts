0730fd97fd43f86b11e1fc6951dd0365031cc9ad
Anwesh43 <anweshthecool0@gmail.com>
2024-01-12 22:11:13 +0530

    bi-break-sq-rot: adding BBSRNode class

--- bi-break-sq-rot.ts
+++ bi-break-sq-rot.ts
@@ -152,3 +152,45 @@ class Animator {
         }
     }
 }
+
+class BBSRNode {
+
+    prev : BBSRNode 
+    next : BBSRNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BBSRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBBSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BBSRNode {
+        var curr : BBSRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
