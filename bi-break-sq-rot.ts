0d99ec3a6d9b3c2612478903fe66020fb171f88d
Anwesh43 <anweshthecool0@gmail.com>
2024-01-12 22:19:32 +0530

    bi-break-sq-rot: adding fix 2

--- bi-break-sq-rot.ts
+++ bi-break-sq-rot.ts
@@ -46,9 +46,9 @@ class DrawingUtil {
     }
 
     static drawBiBreakSq(context : CanvasRenderingContext2D, size : number) {
-        DrawingUtil.drawLine(context, 0, 0, -size / 2, 0)
-        DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, -size)
-        DrawingUtil.drawLine(context, -size / 2, -size, 0, -size)
+        DrawingUtil.drawLine(context, 0, 0, size / 2, 0)
+        DrawingUtil.drawLine(context, size / 2, 0, size / 2, -size)
+        DrawingUtil.drawLine(context, size / 2, -size, 0, -size)
     }
 
     static drawBiBreakSqRot(context : CanvasRenderingContext2D, scale : number) {
