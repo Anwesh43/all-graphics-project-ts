87fd8385bc1cda76db36db59ee1036732cd53458
Anwesh43 <anweshthecool0@gmail.com>
2024-01-20 16:01:20 +0530

    alt-line-to-right: adding AltLineToRight class

--- alt-line-to-right.ts
+++ alt-line-to-right.ts
@@ -187,3 +187,26 @@ class ALTRNode {
         return this 
     }
 }
+
+class AltLineToRight {
+
+    curr : ALTRNode = new ALTRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
