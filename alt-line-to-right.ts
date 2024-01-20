78b41937e2d8c75a5840d95d4833eac13546072e
Anwesh43 <anweshthecool0@gmail.com>
2024-01-20 15:58:50 +0530

    alt-line-to-right: adding ALTRNode class

--- alt-line-to-right.ts
+++ alt-line-to-right.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class ALTRNode {
+
+    prev : ALTRNode 
+    next : ALTRNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ALTRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawALTRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : ALTRNode {
+        var curr : ALTRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
