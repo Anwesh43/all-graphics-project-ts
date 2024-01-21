e929763b6bc31545e618077d9d3f62c78773c506
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 13:11:58 +0530

    line-drop-square: adding LineDropSquare class

--- line-drop-square.ts
+++ line-drop-square.ts
@@ -186,3 +186,27 @@ class LDSNode {
         return this 
     }
 }
+
+class LineDropSquare {
+
+    curr : LDSNode = new LDSNode(0)
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+
+}
