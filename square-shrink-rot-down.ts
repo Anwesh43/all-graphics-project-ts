3aafa85b4a9696252a15a8da198549d699857974
Anwesh43 <anweshthecool0@gmail.com>
2024-01-22 19:30:39 +0530

    square-shrink-rot-down: adding SSRDNode class

--- square-shrink-rot-down.ts
+++ square-shrink-rot-down.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class SSRDNode {
+
+    prev : SSRDNode 
+    next : SSRDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SSRDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSSRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SSRDNode {
+        var curr : SSRDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
