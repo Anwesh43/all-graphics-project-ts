9ecd7649dfad1106c3b85c690e41eaa819b4d911
Anwesh43 <anweshthecool0@gmail.com>
2024-01-22 16:05:58 +0530

    square-shrink-rot-down: adding DrawingUtil class

--- square-shrink-rot-down.ts
+++ square-shrink-rot-down.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n  
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSquareShrinkRotDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(0)), () => {
+                context.fillRect(-size / 2, -size, size, size - size * 0.5 * dsc(0))
+            })
+        })
+    }
+
+    static drawSSRDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSquareShrinkRotDown(context, scale)
+    }
+}
