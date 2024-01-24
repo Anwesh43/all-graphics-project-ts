d296fcdce2e86bc6b016926200a30e50ea3cb85f
Anwesh43 <anweshthecool0@gmail.com>
2024-01-25 02:06:29 +0530

    line-sq-up-line: adding LSULNode class

--- line-sq-up-line.ts
+++ line-sq-up-line.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class LSULNode {
+
+    prev : LSULNode 
+    next : LSULNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSULNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSULNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSULNode {
+        var curr : LSULNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
