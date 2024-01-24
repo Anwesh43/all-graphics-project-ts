28e76f9ceb6821d0aad00ad1487452cc6fbf727a
Anwesh43 <anweshthecool0@gmail.com>
2024-01-25 01:58:38 +0530

    line-sq-up-line: adding DrawingUtil class

--- line-sq-up-line.ts
+++ line-sq-up-line.ts
@@ -25,3 +25,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSqUpLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2 + size) *  dsc(3), () => {
+            DrawingUtil.drawXY(context, (w / 2 + size / 2) * (1 - dsc(0)), 0, () => {
+                for (let j = 0; j < 2; j++) {
+                    DrawingUtil.drawXY(context, 0, 0, () => {
+                        context.rotate(rot * dsc(2) * (1 - 2 * j))
+                        DrawingUtil.drawLine(context, 0, 0, size * 0.5 * (1 - 2 * j), 0)
+                    })
+                }
+            })
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(1)), () => {
+                context.fillRect(-size / 4, -size / 2, size / 2, size / 2)
+            })
+        })
+    }
+
+    static drawLSULNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineSqUpLine(context, scale)
+    }
+}
