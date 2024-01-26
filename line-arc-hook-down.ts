157a540e8628dbcde67470dee44c5387bb7070d3
Anwesh43 <anweshthecool0@gmail.com>
2024-01-27 02:22:58 +0530

    line-arc-hook-down: adding DrawingUtil class

--- line-arc-hook-down.ts
+++ line-arc-hook-down.ts
@@ -25,3 +25,54 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArc(context : CanvasRenderingContext2D, r : number, start : number, sweep : number) {
+        context.beginPath()
+        for (let i = start; i <= start + sweep; i++) {
+            const x : number = r * Math.cos(i * Math.PI / 180)
+            const y : number = r * Math.sin(i * Math.PI / 180)
+            if (i === start) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+    }
+
+    static drawLineArcHookDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+            DrawingUtil.drawArc(context, size, -rot * dsc(1), rot * dsc(1))
+        })
+    }
+
+    static drawLAHDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.strokeStyle = colors[i]
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawLineArcHookDown(context, scale)
+    }
+}
