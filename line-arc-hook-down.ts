859046ffc99703ca3828400ab4fb926438879431
Anwesh43 <anweshthecool0@gmail.com>
2024-01-27 12:29:09 +0530

    line-arc-hook-down: adding LAHDNode class

--- line-arc-hook-down.ts
+++ line-arc-hook-down.ts
@@ -151,3 +151,45 @@ class Animator {
         }
     }
 }
+
+class LAHDNode {
+    
+    next : LAHDNode 
+    prev : LAHDNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LAHDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLAHDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LAHDNode {
+        var curr : LAHDNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        } 
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
