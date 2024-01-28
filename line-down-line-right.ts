e25094fc3e05904ee3243548f396ab28f67ba20f
Anwesh43 <anweshthecool0@gmail.com>
2024-01-29 02:59:57 +0530

    line-down-line-right: adding fix 2

--- line-down-line-right.ts
+++ line-down-line-right.ts
@@ -48,7 +48,7 @@ class DrawingUtil {
     static drawLineDownLineRight(context : CanvasRenderingContext2D, scale : number) {
         const size : number = Math.min(w, h) / sizeFactor 
         const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2, () => {
             context.rotate(-rot * dsc(2))
             DrawingUtil.drawXY(context, 0, h * 0.5 * (1 - dsc(0)), () => {
                 DrawingUtil.drawLine(context, 0, 0, 0, size)
