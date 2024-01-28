edff9c0c1107b48319cdec3695d0caf2c3864c8e
Anwesh43 <anweshthecool0@gmail.com>
2024-01-29 02:55:25 +0530

    line-down-line-right: adding LDLRNode class

--- line-down-line-right.ts
+++ line-down-line-right.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LDLRNode {
+
+    prev : LDLRNode 
+    next : LDLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLDLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LDLRNode {
+        var curr : LDLRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
