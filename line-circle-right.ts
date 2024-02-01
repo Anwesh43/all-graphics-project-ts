00a6716431de9fc7c0f7f6bca07068f31c6efd76
Anwesh43 <anweshthecool0@gmail.com>
2024-02-01 18:18:49 +0530

    line-circle-right: adding LCRNode class

--- line-circle-right.ts
+++ line-circle-right.ts
@@ -150,3 +150,45 @@ class Animator {
         }
     }
 }
+
+class LCRNode {
+
+    prev : LCRNode 
+    next : LCRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LCRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLCRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LCRNode {
+        var curr : LCRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
