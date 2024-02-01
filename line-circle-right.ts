15fe0d618b65f3fc67dfd0321f0887b99f631965
Anwesh43 <anweshthecool0@gmail.com>
2024-02-01 18:20:56 +0530

    line-circle-right: adding LineCircleRight class

--- line-circle-right.ts
+++ line-circle-right.ts
@@ -192,3 +192,26 @@ class LCRNode {
         return this 
     }
 }
+
+class LineCircleRight {
+
+    curr : LCRNode = new LCRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
