6278179be1459b090bf1706b2b0cd415a74f7d16
Anwesh43 <anweshthecool0@gmail.com>
2024-02-03 18:45:05 +0530

    alternate-line-rot-down: adding AlternateLineRotDown class

--- alternate-line-rot-down.ts
+++ alternate-line-rot-down.ts
@@ -187,3 +187,26 @@ class ALRDNode {
         return this 
     }
 }
+
+class AlternateLineRotDown {
+
+    curr : ALRDNode = new ALRDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
