5e411abe518db3f21f2cf9c536919fa8035d07af
Anwesh43 <anweshthecool0@gmail.com>
2024-02-06 00:00:39 +0530

    line-bend-sq-down: adding fixes

--- line-bend-sq-down.ts
+++ line-bend-sq-down.ts
@@ -51,7 +51,7 @@ class DrawingUtil {
         DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
             for (let j = 0; j < 2; j++) {
                 DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(0)), () => {
-                    context.rotate(-rot * dsc(1))
+                    context.rotate(-rot * dsc(1) * j)
                     DrawingUtil.drawLine(context, 0, 0, size, 0)
                 })
             }
