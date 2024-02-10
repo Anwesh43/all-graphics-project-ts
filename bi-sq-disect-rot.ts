440e95b77ea6358f15aeee7fec69fbb199e9522a
Anwesh43 <anweshthecool0@gmail.com>
2024-02-10 15:21:56 +0530

    bi-sq-disect-rot: adding DrawingUtil class

--- bi-sq-disect-rot.ts
+++ bi-sq-disect-rot.ts
@@ -25,3 +25,38 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawSqDisectRot(context : CanvasRenderingContext2D, x : number,  wBar : number, hBar : number, rot : number) {
+        DrawingUtil.drawXY(context, x, 0, () => {
+            context.rotate(rot)
+            context.fillRect(0, 0, wBar, hBar)
+        })
+    }
+
+    static drawBiSqDisectRot(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (i : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawSqDisectRot(context, -size, size * dsc(0), size / 2 , rot * dsc(1 + j))
+                })
+            }
+        }) 
+    }
+
+    static drawBSDRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBiSqDisectRot(context, scale)
+    }
+}
