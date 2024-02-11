43ac917f5454e0d1effc407f547e61a715dc651f
Anwesh43 <anweshthecool0@gmail.com>
2024-02-11 14:23:35 +0530

    line-to-line-down: adding LTLDNode class

--- line-to-line-down.ts
+++ line-to-line-down.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LTLDNode {
+
+    prev : LTLDNode 
+    next : LTLDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LTLDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLTLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LTLDNode {
+        var curr : LTLDNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
