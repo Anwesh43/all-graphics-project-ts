5f95f224a6e0ff94351a6e32820a1b43efedb063
Anwesh43 <anweshthecool0@gmail.com>
2024-02-14 02:53:24 +0530

    bar-rot-down-bar: adding fix

--- bar-rot-down-bar.ts
+++ bar-rot-down-bar.ts
@@ -38,7 +38,7 @@ class DrawingUtil {
     static drawBarRotDownBar(context : CanvasRenderingContext2D, scale : number) {
         const size : number = Math.min(w, h) / sizeFactor 
         const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size / 2) * dsc(3), () => {
             DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(0)), 0, () => {
                 context.rotate(-rot * dsc(1))
                 context.fillRect(0, -size / 2, size * 0.5, size)
