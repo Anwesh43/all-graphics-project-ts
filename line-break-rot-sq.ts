74c8884a94345f553c2329c2380f6216c4426170
Anwesh43 <anweshthecool0@gmail.com>
2024-02-15 15:33:31 +0530

    line-break-rot-sq: adding DrawingUtil class

--- line-break-rot-sq.ts
+++ line-break-rot-sq.ts
@@ -25,3 +25,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBreakRotSq(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h /2 + size) * dsc(3), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * dsc(1))
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(0)), 0, () => {
+                        DrawingUtil.drawLine(context, 0, 0, size, 0)
+                    })
+                })
+            }
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(2)), () => {
+                context.fillRect(-size / 2, -size, size, size)
+            })
+        })
+    }
+
+    static drawLBRSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineBreakRotSq(context, scale)
+    }
+}
