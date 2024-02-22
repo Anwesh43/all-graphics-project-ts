8dc1a21970a83c498b2a783df81e368efe5970fe
Anwesh43 <anweshthecool0@gmail.com>
2024-02-23 01:11:26 +0530

    line-rot-alternate-side: adding Stage class

--- line-rot-alternate-side.ts
+++ line-rot-alternate-side.ts
@@ -13,6 +13,7 @@ const delay : number = 20
 const rot : number = Math.PI / 2
 const w : number = window.innerWidth
 const h : number = window.innerHeight 
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -65,5 +66,38 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         DrawingUtil.drawLineRotAlternateSide(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null 
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
