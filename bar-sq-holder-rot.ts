0495ef42f2e99576420b6b57108fb5f17245e298
Anwesh43 <anweshthecool0@gmail.com>
2024-02-25 18:27:09 +0530

    bar-sq-holder-rot: adding BarSqHolderRot class

--- bar-sq-holder-rot.ts
+++ bar-sq-holder-rot.ts
@@ -173,3 +173,26 @@ class BSHRNode {
         return this 
     }
 }
+
+class BarSqHolderRot {
+
+    curr : BSHRNode = new BSHRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
