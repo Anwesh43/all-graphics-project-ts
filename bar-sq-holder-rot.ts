70997b33601687da6b5d0030341159e884f77bdc
Anwesh43 <anweshthecool0@gmail.com>
2024-02-25 18:25:13 +0530

    bar-sq-holder-rot: adding BSHRNode class

--- bar-sq-holder-rot.ts
+++ bar-sq-holder-rot.ts
@@ -131,3 +131,45 @@ class Animator {
         }
     }
 }
+
+class BSHRNode {
+
+    prev : BSHRNode 
+    next : BSHRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length) {
+            this.next = new BSHRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBSHRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BSHRNode {
+        var curr : BSHRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
