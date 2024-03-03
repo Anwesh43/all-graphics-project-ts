b0f37b2e8b98d822769d4e452c0110877e42e783
Anwesh43 <anweshthecool0@gmail.com>
2024-03-04 00:56:47 +0530

    line-bent-extend-line: adding LBELNode class

--- line-bent-extend-line.ts
+++ line-bent-extend-line.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LBELNode {
+
+    prev : LBELNode 
+    next : LBELNode
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBELNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBELNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBELNode {
+        var curr : LBELNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
