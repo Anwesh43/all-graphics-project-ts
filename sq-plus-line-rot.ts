2df9af5353e7fb69aa61e4d5706aa3c5f205a915
Anwesh43 <anweshthecool0@gmail.com>
2024-04-07 21:21:00 +0530

    sq-plus-line-rot: adding SPLRNode class

--- sq-plus-line-rot.ts
+++ sq-plus-line-rot.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class SPLRNode {
+    
+    prev : SPLRNode 
+    next : SPLRNode 
+    state : State = new State()
+
+    constructor(private i  : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SPLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSPLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SPLRNode {
+        var curr : SPLRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
