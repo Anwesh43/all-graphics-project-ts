18decde7288d56ae0bb54cc4611d825c20b57e02
Anwesh43 <anweshthecool0@gmail.com>
2024-04-15 21:07:32 +0530

    alt-line-bent-down: adding ALBDNode class

--- alt-line-bent-down.ts
+++ alt-line-bent-down.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class ALBDNode {
+
+    prev : ALBDNode 
+    next : ALBDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ALBDNode(this.i + 1)
+            this.next.prev = this 
+        }    
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawALBDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : ALBDNode {
+        var curr : ALBDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
