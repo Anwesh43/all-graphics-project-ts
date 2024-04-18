1955b0407efb88c6b7e7eab3f8bd3db3fae78a72
Anwesh43 <anweshthecool0@gmail.com>
2024-04-18 20:44:21 +0530

    sq-expand-line-drop: adding DrawingUtil class

--- sq-expand-line-drop.ts
+++ sq-expand-line-drop.ts
@@ -25,3 +25,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSqExpandLineDrop(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (arg1 : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2 + size) * dsc(3), h / 2, () => {
+            context.rotate(rot * dsc(2))
+            context.fillRect(0, 0, size * dsc(0), size * dsc(0))
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(1)), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+        })
+    }
+
+    static drawSELDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSqExpandLineDrop(context, scale)
+    }
+}
