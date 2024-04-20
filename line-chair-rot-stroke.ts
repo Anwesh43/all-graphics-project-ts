1d46f15322f938e43b490722b0b616fbca5bdefd
Anwesh43 <anweshthecool0@gmail.com>
2024-04-20 23:08:35 +0530

    line-chair-rot-stroke: adding log fix

--- line-chair-rot-stroke.ts
+++ line-chair-rot-stroke.ts
@@ -48,6 +48,7 @@ class DrawingUtil {
         const size : number = Math.min(w, h) / sizeFactor 
         const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
         DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
+            context.fillRect(0, 0, size * dsc(0), size * dsc(0))
             DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
             for (let j = 0; j < 2 * Math.floor(dsc(1)); j++) {
                 DrawingUtil.drawXY(context, size * j, 0, () => {
