15d2f87f4bb3bb47e73d2aa9e7aca95f751e4473
Anwesh43 <anweshthecool0@gmail.com>
2024-04-27 22:51:55 +0530

    bent-line-left: adding BLLNode class

--- bent-line-left.ts
+++ bent-line-left.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class BLLNode {
+
+    prev : BLLNode 
+    next : BLLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNegihbor()
+    }
+
+    addNegihbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    } 
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BLLNode {
+        var curr : BLLNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
