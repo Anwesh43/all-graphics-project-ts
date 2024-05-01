c6786b090fbb3589b0e9feb5db7513195c9e640c
Anwesh43 <anweshthecool0@gmail.com>
2024-05-01 21:36:05 +0530

    follow-rect-line: adding DrawingUtil class

--- follow-rect-line.ts
+++ follow-rect-line.ts
@@ -25,3 +25,42 @@ class ScaleUtil {
         return Math.max(0, scale - i / n)
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+    static drawFollowRectLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(0)), () => {
+                context.rotate(rot * dsc(2))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+            context.fillRect(0, -size / 4, size * dsc(1), size / 2)
+        })
+    }
+
+    static drawFRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawFollowRectLine(context, scale)
+    }
+}
