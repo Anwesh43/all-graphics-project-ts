20df7b7f505a51b33b3a7a96f209ef7b2e0cf336
Anwesh43 <anweshthecool0@gmail.com>
2024-05-19 17:01:29 +0530

    block-up-line-bent: adding fix 3

--- block-up-line-bent.ts
+++ block-up-line-bent.ts
@@ -48,7 +48,7 @@ class DrawingUtil {
     static drawBlockUpLineBent(context : CanvasRenderingContext2D, scale : number) {
         const size : number = Math.min(w, h) / sizeFactor
         const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2 + (w / 2 + size) * dsc(4), h / 2, () => {
+        DrawingUtil.drawXY(context, w / 2 - (w / 2 + size) * dsc(4), h / 2, () => {
             context.rotate(rot * dsc(3))
             DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(0)), () => {
                 DrawingUtil.drawLine(context, 0, 0, 0, -size)
