bbf38d5d23e8bc12bd7c271c2af845ee95cf5a8a
Anwesh43 <anweshthecool0@gmail.com>
2024-05-19 16:41:46 +0530

    block-up-line-bent: adding BULBNode class

--- block-up-line-bent.ts
+++ block-up-line-bent.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class BULBNode {
+
+    prev : BULBNode 
+    next : BULBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BULBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBULBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BULBNode {
+        var curr : BULBNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
