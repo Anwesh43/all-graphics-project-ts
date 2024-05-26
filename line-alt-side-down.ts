6ee8483c262adc5678fa428548360e5e123dbe84
Anwesh43 <anweshthecool0@gmail.com>
2024-05-26 17:35:19 +0530

    line-alt-side-down: adding DrawingUtil class

--- line-alt-side-down.ts
+++ line-alt-side-down.ts
@@ -25,3 +25,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineAltSideDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                for (let j = 0; j < 2; j++) {
+                    DrawingUtil.drawXY(context, 0, 0, () => {
+                        context.scale(1 - 2 * j, 1)
+                        DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(2)), size * j, () => {
+                            DrawingUtil.drawLine(context, 0, 0, size, 0)
+                        })
+                    })
+                }
+            })
+        })
+    }
+
+    static drawLASDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineAltSideDown(context, scale)
+    }
+}
