49c17aa404024cee5abd222eee8e692e3d461cba
Anwesh43 <anweshthecool0@gmail.com>
2024-05-26 18:19:30 +0530

    line-alt-side-down: adding fix 2

--- line-alt-side-down.ts
+++ line-alt-side-down.ts
@@ -51,8 +51,8 @@ class DrawingUtil {
         DrawingUtil.drawXY(context, w / 2, h / 2, () => {
             DrawingUtil.drawXY(context, 0, 0, () => {
                 context.rotate(rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
             })
-            DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
             for (let j = 0; j < 2; j++) {
                 DrawingUtil.drawXY(context, 0, 0, () => {
                     context.scale(1 - 2 * j, 1)
