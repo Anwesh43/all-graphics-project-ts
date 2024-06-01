29017936a4c8f7b5dabb56176e9981e5d45166ca
Anwesh43 <anweshthecool0@gmail.com>
2024-06-02 00:15:14 +0530

    line-shift-sq-drop: adding DrawingUtil class

--- line-shift-sq-drop.ts
+++ line-shift-sq-drop.ts
@@ -11,7 +11,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
 const delay : number = 20
 const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2
+const rot : number = Math.PI
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 
@@ -23,5 +23,39 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineShiftSqDrop(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawXY(context, size * dsc(1), -h * 0.5 * (1 - dsc(0)), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(2)), () => {
+                context.fillRect(0, -size, size, size)
+            })
+        }) 
     }
 }
