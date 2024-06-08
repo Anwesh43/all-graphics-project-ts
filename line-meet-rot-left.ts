a91c7ca302b4ae07ff533af40bfb3b804972601e
Anwesh43 <anweshthecool0@gmail.com>
2024-06-08 15:07:29 +0530

    line-meet-rot-left: adding DrawingUtil class

--- line-meet-rot-left.ts
+++ line-meet-rot-left.ts
@@ -5,8 +5,8 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4
-const scGap : number = 0.04 / parts 
+const parts : number = 3
+const scGap : number = 0.03 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 8.6
 const delay : number = 20
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineMeetRotLeft(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(2), h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, w * 0.5 * (1 - 2 * j) * (1 - dsc(0)), 0, () => {
+                    context.rotate(rot * dsc(1) * (1 - j))
+                    DrawingUtil.drawLine(context, 0, 0, size * (1 - 2 * j), 0)
+                })
+            }
+        })
+    }
+
+    static drawLMRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineMeetRotLeft(context, scale)
     }
 }
