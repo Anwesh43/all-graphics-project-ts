4e97678b322d581c24de9cd3917efd153a8b3eae
Anwesh43 <anweshthecool0@gmail.com>
2024-06-16 14:50:03 +0530

    peak-alt-line-rot: adding DrawingUtil class

--- peak-alt-line-rot.ts
+++ peak-alt-line-rot.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4
+const parts : number = 6
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
@@ -23,5 +23,48 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawPeakAltLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(5), h / 2, () => {
+            context.rotate(rot * dsc(4))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, size * j, 0, () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(j * 3))
+                })
+                DrawingUtil.drawXY(context, size * 0.5 * j, -size * j, () => {
+                    DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(1 + j), -size * (1 - 2 * j) * dsc(1 + j))
+                })
+            }
+        })
+    }
+
+    static drawPALRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPeakAltLineRot(context, scale)
     }
 }
