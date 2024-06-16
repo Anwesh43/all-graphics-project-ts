2e7c1a07ddfc74b296cda355c912559fa46c92d4
Anwesh43 <anweshthecool0@gmail.com>
2024-06-16 14:58:09 +0530

    peak-alt-line-rot: adding PALRNode class

--- peak-alt-line-rot.ts
+++ peak-alt-line-rot.ts
@@ -145,3 +145,46 @@ class Animator {
         }
     }
 }
+
+class PALRNode {
+
+    prev : PALRNode 
+    next : PALRNode 
+
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    } 
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PALRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPALRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : PALRNode {
+        var curr : PALRNode = this.prev
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
