320a785a80f505c96089788b9fdf246ede044ed7
Anwesh43 <anweshthecool0@gmail.com>
2024-06-22 18:58:46 +0530

    line-sq-rot-catch: adding LSRCNode class

--- line-sq-rot-catch.ts
+++ line-sq-rot-catch.ts
@@ -146,8 +146,46 @@ class Animator {
     }
 }
 
+class LSRCNode {
 
+    next : LSRCNode 
+    prev : LSRCNode
+    state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
 
+    addNeighbor() { 
+        if (this.i < colors.length - 1) {
+            this.next = new LSRCNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSRCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSRCNode {
+        var curr : LSRCNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
 
 
 
