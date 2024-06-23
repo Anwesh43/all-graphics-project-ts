9574218a2aec94c850aa317e1ec73107cd35a364
Anwesh43 <anweshthecool0@gmail.com>
2024-06-23 17:08:07 +0530

    open-box-line-rot: adding OpenBoxLineRot class

--- open-box-line-rot.ts
+++ open-box-line-rot.ts
@@ -196,3 +196,26 @@ class OBLRNode {
         return this 
     }
 }
+
+class OpenBoxLineRot {
+
+    curr : OBLRNode = new OBLRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
