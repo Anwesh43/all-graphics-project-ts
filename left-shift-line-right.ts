e5244eeeeac83c58a1e52e8abfad87b2897a6fef
Anwesh43 <anweshthecool0@gmail.com>
2024-06-29 14:36:57 +0530

    left-shift-line-right: adding LSLRNode class

--- left-shift-line-right.ts
+++ left-shift-line-right.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class LSLRNode {
+
+    prev : LSLRNode 
+    next : LSLRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSLRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSLRNode {
+        var curr : LSLRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
