a7758c7829c1b9a85d8bc353124b084108ec0ac3
Anwesh43 <anweshthecool0@gmail.com>
2024-06-30 16:56:01 +0530

    bi-line-right-slant: adding BLRSNode class

--- bi-line-right-slant.ts
+++ bi-line-right-slant.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class BLRSNode {
+
+    prev : BLRSNode 
+    next : BLRSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLRSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BLRSNode {
+        var curr : BLRSNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
