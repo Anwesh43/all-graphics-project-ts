08dbed49a9946c5e944256b541fd866cc295ce9b
Anwesh43 <anweshthecool0@gmail.com>
2024-07-20 12:56:39 +0530

    line-rot-extend: adding LRENode class

--- line-rot-extend.ts
+++ line-rot-extend.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LRENode {
+    
+    prev : LRENode 
+    next : LRENode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRENode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRENode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRENode {
+        var curr : LRENode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
