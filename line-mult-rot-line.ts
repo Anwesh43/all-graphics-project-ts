d10439d0c398a8a89341c152bb581ae76ec71684
Anwesh43 <anweshthecool0@gmail.com>
2024-07-28 18:56:35 +0530

    line-mult-rot-line: adding Stage class

--- line-mult-rot-line.ts
+++ line-mult-rot-line.ts
@@ -9,7 +9,7 @@ const parts : number = 4
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 4.9
-const backClor : string = "#BDBDBD"
+const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 2
 const delay : number = 20 
 const w : number = window.innerWidth 
@@ -63,5 +63,38 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         DrawingUtil.drawLineMultRotLine(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D | null
+    
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor 
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+    
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
