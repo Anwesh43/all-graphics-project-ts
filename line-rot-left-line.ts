f34e644fb4ff2b137b8f358196727a181d2d9ca6
Anwesh43 <anweshthecool0@gmail.com>
2024-08-01 22:39:12 +0530

    line-rot-left-line: adding Renderer in Stage

--- line-rot-left-line.ts
+++ line-rot-left-line.ts
@@ -70,6 +70,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D | null 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
@@ -80,13 +81,15 @@ class Stage {
 
     render() {
         if (this.context) {
-
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
