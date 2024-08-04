a596cb3f929f1b07a6589eb212f4d1928afe8a3b
Anwesh43 <anweshthecool0@gmail.com>
2024-08-04 16:11:29 +0530

    line-from-up-rot: adding LineFromUpRot class

--- line-from-up-rot.ts
+++ line-from-up-rot.ts
@@ -182,3 +182,26 @@ class LFURNode {
         return this 
     }
 }
+
+class LineFromUpRot {
+
+    curr : LFURNode = new LFURNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
