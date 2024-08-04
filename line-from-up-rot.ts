b00bdf6116cfac28ac0b61909f31d66bad751835
Anwesh43 <anweshthecool0@gmail.com>
2024-08-04 15:10:49 +0530

    line-from-up-rot: adding DrawingUtil class

--- line-from-up-rot.ts
+++ line-from-up-rot.ts
@@ -13,6 +13,7 @@ const backColor : string = "#BDBDBD"
 const sizeFactor : number = 4.9 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const rot : number = Math.PI * 1.5 
 
 class ScaleUtil {
 
@@ -22,5 +23,43 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineFromUpRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(2), h / 2, () => {
+            DrawingUtil.drawXY(context, size, -h * 0.5 * (1 - dsc(0)), () => {
+                context.rotate(rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+        })
+    }
+
+    static drawLFURNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineFromUpRot(context, scale)
     }
 }
