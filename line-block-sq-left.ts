d19d4fa40372584018d963c520e6cda20f9f4728
Anwesh43 <anweshthecool0@gmail.com>
2024-08-11 14:22:27 +0530

    line-block-sq-left: adding LBSLNode class

--- line-block-sq-left.ts
+++ line-block-sq-left.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LBSLNode {
+
+    prev : LBSLNode 
+    next : LBSLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBSLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBSLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBSLNode {
+        var curr : LBSLNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
