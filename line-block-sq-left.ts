e90f836f21c54cb1a857e6c55e155cb9ff2cdf80
Anwesh43 <anweshthecool0@gmail.com>
2024-08-11 13:44:16 +0530

    line-block-sq-left: adding DrawingUtil class

--- line-block-sq-left.ts
+++ line-block-sq-left.ts
@@ -7,7 +7,7 @@ const colors : Array<string> = [
 ]
 const parts : number = 4
 const scGap : number = 0.04 / parts 
-const sizeFactor : number = 11.9 
+const sizeFactor : number = 7.9 
 const delay : number= 20
 const rot : number = Math.PI / 2
 const backColor : string = "#BDBDBD"
@@ -23,5 +23,49 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBlockSqLeft(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * dsc(2))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+            })
+            DrawingUtil.drawXY(context, size, 0, () => {
+                const upSize : number = size * dsc(1)
+                context.rotate(rot * dsc(3))
+                context.fillRect(upSize, -upSize, upSize, upSize)
+            })
+        })
+    }
+
+    static drawLBSLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineBlockSqLeft(context, scale)
     }
 }
