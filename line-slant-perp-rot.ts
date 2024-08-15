eca007c60667236319b95b326bca8cc98a282742
Anwesh43 <anweshthecool0@gmail.com>
2024-08-15 05:59:39 +0530

    line-slant-perp-rot: adding LSPRNode class

--- line-slant-perp-rot.ts
+++ line-slant-perp-rot.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LSPRNode {
+
+    prev : LSPRNode 
+    next : LSPRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSPRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSPRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LSPRNode {
+        var curr : LSPRNode = this.next 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
