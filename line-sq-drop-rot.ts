153d53950ca3786a3ba71bd8b1096a7b7ed7569b
Anwesh43 <anweshthecool0@gmail.com>
2024-08-25 13:43:00 +0530

    line-sq-drop-rot: adding DrawingUtil class

--- line-sq-drop-rot.ts
+++ line-sq-drop-rot.ts
@@ -10,9 +10,10 @@ const scGap : number = 0.04 / parts
 const delay : number = 20 
 const sizeFactor : number = 4.9 
 const strokeFactor : number = 20 
-const rot : number = Math.PI / 2
+const rot : number = Math.PI
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const deg : number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -22,5 +23,49 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineSqDropRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(5), () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(deg * dsc(4))
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+            })
+            DrawingUtil.drawXY(context, size, -h * 0.5 * (1 - dsc(1) - dsc(3)), () => {
+                context.rotate(rot * dsc(2))
+                context.fillRect(-size, -size, size, size)
+            })
+        })
+    }
+
+    static drawLSDRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.lineCap = 'round'
+        DrawingUtil.drawLineSqDropRot(context, scale)
     }
 }
