202c18538c4c32b8a0b2da7e77cb3b5aa8b52e1b
Anwesh43 <anweshthecool0@gmail.com>
2024-08-30 14:38:30 +0530

    line-right-to-sq: adding LineRightToSq class

--- line-right-to-sq.ts
+++ line-right-to-sq.ts
@@ -190,3 +190,25 @@ class LRTSNode {
         return this 
     }
 }
+
+class LineRightToSq {
+
+    curr : LRTSNode = new LRTSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
