ec3288e28e4fa8440ee5eab2bb9caf5e36cbf826
Anwesh43 <anweshthecool0@gmail.com>
2024-09-01 14:02:38 +0530

    line-extend-from-point: adding LineExtendFromPoint class

--- line-extend-from-point.ts
+++ line-extend-from-point.ts
@@ -187,3 +187,26 @@ class LEFPNode {
         return this 
     }
 }
+
+class LineExtendFromPoint {
+
+    curr : LEFPNode = new LEFPNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
