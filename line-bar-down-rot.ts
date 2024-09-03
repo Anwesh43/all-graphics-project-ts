7e2904bd21998beb9f2f33d300ce6bf1a1b5699f
Anwesh43 <anweshthecool0@gmail.com>
2024-09-03 20:42:50 +0530

    line-bar-down-rot: adding LineBarDownRot class

--- line-bar-down-rot.ts
+++ line-bar-down-rot.ts
@@ -183,5 +183,27 @@ class LBDRNode {
         cb()
         return this 
     }
+}
 
+class LineBarDownRot {
+
+    curr : LBDRNode = new LBDRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
 }
