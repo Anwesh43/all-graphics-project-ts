a1953ffe247ff97960129c57a6d7e154226a89ee
Anwesh43 <anweshthecool0@gmail.com>
2024-09-03 20:41:29 +0530

    line-bar-down-rot: adding LBDRNode class

--- line-bar-down-rot.ts
+++ line-bar-down-rot.ts
@@ -142,3 +142,46 @@ class Animator {
         }
     }
 }
+
+class LBDRNode {
+
+    prev : LBDRNode 
+    next : LBDRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBDRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBDRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBDRNode {
+        var curr : LBDRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+
+}
