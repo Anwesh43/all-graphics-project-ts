6db8a19adcc847875c0556b7f5dcd97e6a2fd737
Anwesh43 <anweshthecool0@gmail.com>
2024-09-03 20:34:23 +0530

    line-bar-down-rot: adding DrawingUtil class

--- line-bar-down-rot.ts
+++ line-bar-down-rot.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"    
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBarDownRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawLine(context, 0, 0, size * (dsc(0) - dsc(2)), 0)
+            DrawingUtil.drawXY(context, size * (1 - dsc(2)), 0, () => {
+                context.fillRect(0, 0, size * 0.5 * dsc(2), size * 0.5)
+            })
+        })
+    }
+
+    static drawLBDRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawLineBarDownRot(context, scale)
     }
 }
