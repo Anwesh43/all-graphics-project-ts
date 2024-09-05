78bf1230b34ba152629b2d4f0c7d32980554e3f9
Anwesh43 <anweshthecool0@gmail.com>
2024-09-05 12:57:17 +0530

    sq-rot-hold-line: adding SqRotHoldLine class

--- sq-rot-hold-line.ts
+++ sq-rot-hold-line.ts
@@ -189,3 +189,28 @@ class SRHLNode {
         return this 
     }
 }
+
+class SqRotHoldLine {
+
+    curr : SRHLNode = new SRHLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.curr = this.curr.getNext(this.dir, () => {
+                    this.dir *= -1
+                })
+                cb()
+            })
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
