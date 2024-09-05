c698182b8169bdcc1a8e4f53080b9ad1550966f7
Anwesh43 <anweshthecool0@gmail.com>
2024-09-05 12:38:46 +0530

    sq-rot-hold-line: adding DrawingUtil class

--- sq-rot-hold-line.ts
+++ sq-rot-hold-line.ts
@@ -5,8 +5,8 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 3
-const scGap : number = 0.03 / parts
+const parts : number = 4
+const scGap : number = 0.04 / parts
 const strokeFactor : number = 90
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
@@ -14,6 +14,7 @@ const rot : number = -Math.PI
 const sizeFactor : number = 4.9 
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const deg : number = Math.PI / 4
 
 class ScaleUtil {
 
@@ -24,4 +25,48 @@ class ScaleUtil {
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawSqRotHoldLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(-rot * dsc(1))
+                context.fillRect(-size * dsc(0), -size, size * dsc(0), size)
+            })
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(1)), () => {
+                    context.rotate(deg * (1 - 2 * j) * dsc(2))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+        })
+    }
+
+    static drawSRHLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSqRotHoldLine(context, scale)
+    }   
 }
