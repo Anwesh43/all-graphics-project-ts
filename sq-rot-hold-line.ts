8c9a0be8d696036aad101b5ad18a717269299eb4
Anwesh43 <anweshthecool0@gmail.com>
2024-09-05 12:52:34 +0530

    sq-rot-hold-line: adding SRHLNode class

--- sq-rot-hold-line.ts
+++ sq-rot-hold-line.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class SRHLNode {
+
+    prev : SRHLNode 
+    next : SRHLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SRHLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSRHLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) {
+        var curr : SRHLNode = this.next 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
