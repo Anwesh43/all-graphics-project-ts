4a9c6cdf351e346b0822737c037b30f4c8e7892a
Anwesh43 <anweshthecool0@gmail.com>
2024-09-13 16:10:34 +0530

    rot-sq-from-left: adding RSFLNode  class

--- rot-sq-from-left.ts
+++ rot-sq-from-left.ts
@@ -127,3 +127,45 @@ class Animator {
         }
     }
 }
+
+class RSFLNode {
+
+    prev : RSFLNode 
+    next : RSFLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RSFLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRSFLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) {
+        var curr : RSFLNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
