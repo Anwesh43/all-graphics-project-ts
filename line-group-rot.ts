e5790b48f656113f85e8c5ee689d5612825c5e8c
Anwesh43 <anweshthecool0@gmail.com>
2024-09-14 10:44:48 +0530

    line-group-rot: adding DrawingUtil class

--- line-group-rot.ts
+++ line-group-rot.ts
@@ -8,7 +8,7 @@ const colors : Array<string> = [
 const rot : number = Math.PI / 2
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const parts : number = 4
+const parts : number = 6
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
@@ -23,5 +23,50 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineGroupRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(3), h / 2, () => {
+            context.rotate(rot * dsc(4))
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * dsc(2))
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(1)), () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            })
+            DrawingUtil.drawXY(context, 0, -h / 2 + (h / 2 + size) * dsc(3), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+        })
+    }
+    
+    static drawLGRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        DrawingUtil.drawLineGroupRot(context, scale)
     }
 }
