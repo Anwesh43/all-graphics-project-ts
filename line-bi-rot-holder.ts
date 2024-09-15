b130ed0251c2994af8bbb77a57ceed3c94bc7897
Anwesh43 <anweshthecool0@gmail.com>
2024-09-15 13:18:45 +0530

    line-bi-rot-holder: adding LBRHNode class

--- line-bi-rot-holder.ts
+++ line-bi-rot-holder.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LBRHNode {
+
+    prev : LBRHNode 
+    next : LBRHNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBRHNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBRHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBRHNode {
+        var curr : LBRHNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
