d1e73dc92654970a8de51aba5179e184077a3e2e
Anwesh43 <anweshthecool0@gmail.com>
2024-09-15 13:21:16 +0530

    line-bi-rot-holder: adding LineBiRotHolder class

--- line-bi-rot-holder.ts
+++ line-bi-rot-holder.ts
@@ -187,3 +187,26 @@ class LBRHNode {
         return this 
     }
 }
+
+class LineBiRotHolder {
+
+    curr : LBRHNode = new LBRHNode(0)
+    dir : number = 1 
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })        
+            cb()
+        })
+    }
+}
