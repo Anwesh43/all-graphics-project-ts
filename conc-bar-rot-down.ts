20b7c3b23e7269666fbd909e245e9c37443387d7
Anwesh43 <anweshthecool0@gmail.com>
2024-09-18 17:16:00 +0530

    conc-bar-rot-down: adding CBRDNode class

--- conc-bar-rot-down.ts
+++ conc-bar-rot-down.ts
@@ -136,3 +136,45 @@ class Animator {
         }
     }
 }
+
+class CBRDNode {
+
+    prev : CBRDNode 
+    next : CBRDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new CBRDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawCBRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : CBRDNode {
+        var curr : CBRDNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
