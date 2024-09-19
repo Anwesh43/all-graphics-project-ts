72944ad4f6ec97af9b19d969e2b7620ff3e6415a
Anwesh43 <anweshthecool0@gmail.com>
2024-09-19 18:08:59 +0530

    line-rot-sq-fill: adding LRSFNode class

--- line-rot-sq-fill.ts
+++ line-rot-sq-fill.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LRSFNode {
+
+    prev : LRSFNode 
+    next : LRSFNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRSFNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRSFNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LRSFNode {
+        var curr : LRSFNode = this.prev
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
