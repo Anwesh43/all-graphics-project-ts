7f90a95361f02912f8ded0696ca7d0bdf2c78599
Anwesh43 <anweshthecool0@gmail.com>
2024-09-22 16:06:22 +0530

    line-either-side-up: adding fix 2

--- line-either-side-up.ts
+++ line-either-side-up.ts
@@ -56,7 +56,7 @@ class DrawingUtil {
                     DrawingUtil.drawXY(context, 0, 0, () => {
                         context.scale(1 - 2 * j, 1)
                         context.rotate(rot * dsc(2))
-                        DrawingUtil.drawXY(context, (w / 2) * (1 - 2 * j) * (1 - dsc(0)), 0, () => {
+                        DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(0)), 0, () => {
                             DrawingUtil.drawLine(context, 0, 0, size, 0)
                         })
                        
