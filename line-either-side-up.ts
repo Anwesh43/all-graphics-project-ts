2562ef6f87cd2eec8defdf02d6ef0db96577c193
Anwesh43 <anweshthecool0@gmail.com>
2024-09-22 14:21:53 +0530

    line-either-side-up: adding DrawingUtil class

--- line-either-side-up.ts
+++ line-either-side-up.ts
@@ -14,6 +14,7 @@ const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 2
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
+const deg : number = Math.PI 
 
 class ScaleUtil {
 
@@ -23,5 +24,51 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    } 
+
+    static drawLineEitherSideUp(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2) * dsc(4), () => {
+            context.rotate(deg * dsc(3))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, size * dsc(1), () => {
+                    DrawingUtil.drawXY(context, 0, 0, () => {
+                        context.rotate(rot * dsc(2))
+                        DrawingUtil.drawXY(context, (w / 2) * (1 - 2 * j) * dsc(0), 0, () => {
+                            DrawingUtil.drawLine(context, 0, 0, size, 0)
+                        })
+                       
+                    })
+                })
+            }
+        })
+    }
+
+    static drawLESUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineEitherSideUp(context, scale)
     }
 }
