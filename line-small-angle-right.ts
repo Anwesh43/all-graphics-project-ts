af3e91b76203f3aabbb4761df7ba6eff4d58ba1d
Anwesh43 <anweshthecool0@gmail.com>
2024-09-26 15:41:38 +0530

    line-small-angle-right: adding constants

---
+++ line-small-angle-right.ts
@@ -0,0 +1,27 @@
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853" 
+]
+const parts : number = 4
+const scGap : number = 0.04 / parts 
+const delay : number = 20
+const backColor : string = "#BDBDBD"
+const strokeFactor : number = 90
+const sizeFactor : number = 4.9 
+const rot : number = Math.PI / 2
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
