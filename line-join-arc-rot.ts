691177196266cf631cfde6f12870e5705e5f5bdd
Anwesh43 <anweshthecool0@gmail.com>
2024-09-28 18:48:26 +0530

    line-join-arc-rot: adding LJARNode class

--- line-join-arc-rot.ts
+++ line-join-arc-rot.ts
@@ -155,3 +155,45 @@ class Animator {
         }
     }
 }
+
+class LJARNode {
+
+    next : LJARNode 
+    prev : LJARNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LJARNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLJARNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LJARNode {
+        var curr : LJARNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
