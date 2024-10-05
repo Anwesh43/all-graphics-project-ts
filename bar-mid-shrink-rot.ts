2031e340026635633d6f0e40f560fbf79bc6f1ce
Anwesh43 <anweshthecool0@gmail.com>
2024-10-05 13:27:53 +0530

    bar-mid-shrink-rot: adding DrawingUtil clas

--- bar-mid-shrink-rot.ts
+++ bar-mid-shrink-rot.ts
@@ -24,3 +24,29 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawBarMidShrinkRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2, () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawXY(context, - size / 2 + size * 0.5 * dsc(1), -size * dsc(0), () => {
+                context.fillRect(0, 0, size - size * 0.5 * dsc(1), size * dsc(0))
+            })
+        })
+    }
+
+    static drawBMSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarMidShrinkRot(context, scale)
+    }
+}
