1457725cd8d49394cc7bab64ac5f7dcab7aeea1e
Anwesh43 <anweshthecool0@gmail.com>
2024-10-05 21:56:07 +0530

    bar-mid-shrink-rot: adding BMSRNode class

--- bar-mid-shrink-rot.ts
+++ bar-mid-shrink-rot.ts
@@ -127,3 +127,45 @@ class Animator {
         }
     }
 }
+
+class BMSRNode {
+
+    prev : BMSRNode 
+    next : BMSRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BMSRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBMSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BMSRNode {
+        var curr : BMSRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
