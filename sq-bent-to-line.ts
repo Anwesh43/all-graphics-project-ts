b40a0235f04de09f87ae5fcc9f385a60cc07f978
Anwesh43 <anweshthecool0@gmail.com>
2024-10-17 01:00:28 +0530

    sq-bent-to-line: adding SqBentToLine class

--- sq-bent-to-line.ts
+++ sq-bent-to-line.ts
@@ -185,3 +185,24 @@ class SBTLNode {
         return this 
     }
 }
+
+class SqBentToLine {
+
+    curr : SBTLNode = new SBTLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr = this.curr.getNext(this.dir, () => {
+            this.dir *= -1
+        })
+        cb()
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
