64df45bf2e7e60d3a3752c9cd76777f4b41ffa25
Anwesh43 <anweshthecool0@gmail.com>
2024-10-17 00:58:18 +0530

    sq-bent-to-line: adding SBTLNode class

--- sq-bent-to-line.ts
+++ sq-bent-to-line.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class SBTLNode {
+
+    prev : SBTLNode 
+    next : SBTLNode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBTLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSBTLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SBTLNode {
+        var curr : SBTLNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
