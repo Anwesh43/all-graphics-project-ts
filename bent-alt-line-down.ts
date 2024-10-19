1da039d7b69d5120e8a091fc8822d7c84a05ace3
Anwesh43 <anweshthecool0@gmail.com>
2024-10-19 14:13:33 +0530

    bent-alt-line-down: adding DrawingUtil class

--- bent-alt-line-down.ts
+++ bent-alt-line-down.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
@@ -23,5 +23,48 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBentAllLineDown(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (i : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(4), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    context.rotate(rot * (1 - 2 * j) * dsc(2 * j + 1))
+                    DrawingUtil.drawXY(context, 0, (h / 2) * (1 - dsc(2 * j)), () => {
+                        DrawingUtil.drawLine(context, 0, 0, 0, size)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawBALDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.lineCap = 'round'
+        DrawingUtil.drawBentAllLineDown(context, scale)
     }
 }
