e9794c2c8d602d7fb21fa856d033fc879023bdf6
Anwesh43 <anweshthecool0@gmail.com>
2024-10-20 14:40:22 +0530

    line-sq-down-right: adding DrawingUtil class

--- line-sq-down-right.ts
+++ line-sq-down-right.ts
@@ -25,3 +25,51 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static rotateXY(context : CanvasRenderingContext2D, x : number, y : number, deg : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        context.rotate(deg)
+        cb()
+        context.restore()
+    }
+
+    static drawLineSqDownRight(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            DrawingUtil.rotateXY(context, (w / 2) * dsc(4), -h * 0.5 * (1 - dsc(0)), rot * dsc(2), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+            DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(1)), h * 0.5 * dsc(3), () => {
+                context.fillRect(0, 0, size, size)
+            })
+        })
+    }
+
+    static drawLSDRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineSqDownRight(context, scale)
+    }
+}
