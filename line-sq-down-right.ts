0ec212bc62fbdbc8ce49267e73d5372f7d9af039
Anwesh43 <anweshthecool0@gmail.com>
2024-10-20 15:07:41 +0530

    line-sq-down-right: adding fillStyle fix

--- line-sq-down-right.ts
+++ line-sq-down-right.ts
@@ -70,6 +70,7 @@ class DrawingUtil {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
         DrawingUtil.drawLineSqDownRight(context, scale)
     }
 }
