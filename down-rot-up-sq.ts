2cb719a5a3c996ede5d890bec2abbc5d74044201
Anwesh43 <anweshthecool0@gmail.com>
2024-10-26 13:44:32 +0530

    down-rot-up-sq: adding DownRotUp class

--- down-rot-up-sq.ts
+++ down-rot-up-sq.ts
@@ -188,3 +188,26 @@ class DRUNode {
         return this 
     }
 }
+
+class DownRotUp {
+
+    curr : DRUNode = new DRUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
