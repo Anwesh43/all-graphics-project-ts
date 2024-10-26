819e2237fa63a54107f10fdf0a47da829761412b
Anwesh43 <anweshthecool0@gmail.com>
2024-10-26 13:32:39 +0530

    down-rot-up-sq: adding DrawingUtil class

--- down-rot-up-sq.ts
+++ down-rot-up-sq.ts
@@ -25,3 +25,47 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        } 
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDownRotUp(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawXY(context, (w / 2 - size) * (1 - dsc(1)), 0, () => {
+                DrawingUtil.drawXY(context, 0, (h / 2) * (1 - dsc(0)), () => {
+                    context.fillRect(-size / 2, 0, size, size)
+                })
+            })
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(0)), () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+        })
+    }
+
+    static drawDRUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        DrawingUtil.drawDownRotUp(context, scale)
+    }
+}
