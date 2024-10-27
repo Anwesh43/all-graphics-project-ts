9291d91fb1d94a63533254a5146ea9e1d58a9835
Anwesh43 <anweshthecool0@gmail.com>
2024-10-27 17:30:20 +0530

    sq-vert-drop-right: adding SVDRNode class

--- sq-vert-drop-right.ts
+++ sq-vert-drop-right.ts
@@ -49,7 +49,7 @@ class DrawingUtil {
         })
     }
 
-    static drawSCDRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+    static drawSVDRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
         context.fillStyle = colors[i]
         DrawingUtil.drawSqVertDropRight(context, scale)
 
@@ -130,5 +130,47 @@ class Animator {
             this.animated = false 
             clearInterval(this.interval)
         }
+    }
+}
+
+class SVDRNode {
+
+    prev : SVDRNode 
+    next : SVDRNode 
+    state : State = new State() 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SVDRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSVDRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : SVDRNode {
+        var curr : SVDRNode = this.prev
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
     }
 }
