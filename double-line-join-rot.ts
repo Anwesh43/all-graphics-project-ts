e8d81b7b2a2ef857ee57084baf7a4ce74a292dd1
Anwesh43 <anweshthecool0@gmail.com>
2024-11-01 14:43:35 +0530

    double-line-join-rot: adding DLJRNode class

--- double-line-join-rot.ts
+++ double-line-join-rot.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class DLJRNode {
+
+    prev : DLJRNode 
+    next : DLJRNode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length  - 1) {
+            this.next = new DLJRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDLJRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : DLJRNode {
+        var curr : DLJRNode = this.prev
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
