170843e3ba9b529eb5214bdf0df8e27ca5f45380
Anwesh43 <anweshthecool0@gmail.com>
2024-11-01 14:52:01 +0530

    double-line-join-rot: adding fix

--- double-line-join-rot.ts
+++ double-line-join-rot.ts
@@ -49,6 +49,7 @@ class DrawingUtil {
     static drawDoubleLineJoinRot(context : CanvasRenderingContext2D, scale : number) {
         const size : number = Math.min(w, h) / sizeFactor 
         const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        console.log(`SIZE ${size} SCALE ${scale}`)
         DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(3), h / 2, () => {
             context.rotate(deg * dsc(2))
             for (let j = 0; j < 2; j++) {
