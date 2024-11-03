2c6cb633a7ddc8cb8dbc200b4fb549c9c3027a41
Anwesh43 <anweshthecool0@gmail.com>
2024-11-03 16:16:17 +0530

    right-line-join-bent: adding RLJBNode class

--- right-line-join-bent.ts
+++ right-line-join-bent.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class RLJBNode {
+
+    prev : RLJBNode 
+    next : RLJBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLJBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRLJBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : RLJBNode {
+        var curr : RLJBNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
