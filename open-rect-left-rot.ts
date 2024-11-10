021566b54621e8f57d41d78be82c8c758e4f8b7c
Anwesh43 <anweshthecool0@gmail.com>
2024-11-10 14:38:52 +0530

    open-rect-left-rot: adding OpenRectLeftRot class

--- open-rect-left-rot.ts
+++ open-rect-left-rot.ts
@@ -186,3 +186,26 @@ class ORLRNode {
         return this 
     }
 }
+
+class OpenRectLeftRot {
+
+    curr : ORLRNode = new ORLRNode(0)
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
