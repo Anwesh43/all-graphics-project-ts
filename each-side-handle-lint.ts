6dc61b9aaa4039efc3306b665a07172a9b6bef53
Anwesh43 <anweshthecool0@gmail.com>
2024-11-16 16:22:57 +0530

    each-side-handle-lint: adding DrawingUtil class

--- each-side-handle-lint.ts
+++ each-side-handle-lint.ts
@@ -5,7 +5,7 @@ const colors : Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts : number = 4
+const parts : number = 5
 const scGap : number = 0.04 / parts 
 const strokeFactor : number = 90
 const sizeFactor : number = 4.9 
@@ -25,3 +25,47 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return 
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawEachSideHandleLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawXY(context, -size / 2, -size, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+            })
+            DrawingUtil.drawXY(context, -size / 2, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(1), 0)
+            })
+            DrawingUtil.drawXY(context, size / 2, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.5 * dsc(2))
+            })
+        })
+    }
+
+    static drawESHLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawEachSideHandleLine(context, scale)
+    }
+ }
