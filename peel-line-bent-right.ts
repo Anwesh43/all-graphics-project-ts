a3b66c997921d5a138b151223830079f1910d2fd
Anwesh43 <anweshthecool0@gmail.com>
2024-11-17 20:58:31 +0530

    peel-line-bent-right: adding PLBRNode class

--- peel-line-bent-right.ts
+++ peel-line-bent-right.ts
@@ -145,3 +145,47 @@ class Animator {
         }
     }
 }
+
+class PLBRNode {
+
+    prev : PLBRNode 
+    next : PLBRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PLBRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPLBRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : PLBRNode {
+        var curr : PLBRNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+
+    
+}
