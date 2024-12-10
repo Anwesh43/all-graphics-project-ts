4677ea332a883ac73031435a8c437afdfcc48f36
Anwesh43 <anweshthecool0@gmail.com>
2024-12-10 14:26:04 +0530

    line-bar-rot-catcher: adding LineBarRotCatcher class

--- line-bar-rot-catcher.ts
+++ line-bar-rot-catcher.ts
@@ -188,6 +188,27 @@ class LBRCNode {
         cb()
         return this 
     }
+}
 
+class LineBarRotCatcher {
 
+    curr : LBRCNode = new LBRCNode(0)
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : () => void) {
+        this.curr.startUpdating(cb)
+    }
 }
