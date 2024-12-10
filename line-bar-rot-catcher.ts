0ff17dcd06352c5ed3792340df20cf8e60a07a81
Anwesh43 <anweshthecool0@gmail.com>
2024-12-10 13:42:51 +0530

    line-bar-rot-catcher: adding LBRCNode class

--- line-bar-rot-catcher.ts
+++ line-bar-rot-catcher.ts
@@ -147,3 +147,47 @@ class Animator {
         }
     }
 }
+
+class LBRCNode {
+
+    prev : LBRCNode 
+    next : LBRCNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBRCNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLBRCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : LBRCNode {
+        var curr : LBRCNode = this.prev 
+        if (dir === 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+
+
+}
