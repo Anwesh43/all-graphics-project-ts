67f25d421b42ae11617eb458e981d1759b71bcca
Anwesh43 <anweshthecool0@gmail.com>
2024-12-14 15:46:40 +0530

    block-from-up-rot-left: adding DrawingUtil class

--- block-from-up-rot-left.ts
+++ block-from-up-rot-left.ts
@@ -24,3 +24,27 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawBlockFromUpRotLeft(context : CanvasRenderingContext2D, scale : number) {
+        const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+        const size : number = Math.min(w, h) / sizeFactor
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(3), h * 0.5 * dsc(1), () => {
+            context.rotate(rot * dsc(2))
+            context.fillRect(-size / 4, 0, size / 2, size * 0.5 * dsc(0))
+        })
+    }
+
+    static drawBFURLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBlockFromUpRotLeft(context, scale)
+    }
+}
