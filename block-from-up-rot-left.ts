ac6040fcf9c8e01525d6e420e104bc202115e462
Anwesh43 <anweshthecool0@gmail.com>
2024-12-14 16:38:28 +0530

    block-from-up-rot-left: adding BFURLNode class

--- block-from-up-rot-left.ts
+++ block-from-up-rot-left.ts
@@ -125,3 +125,45 @@ class Animator {
         }
     }
 }
+
+class BFURLNode {
+
+    prev : BFURLNode 
+    next : BFURLNode 
+    state : State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BFURLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBFURLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : () => void) : BFURLNode {
+        var curr : BFURLNode = this.prev 
+        if (dir === 1) {
+             curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
