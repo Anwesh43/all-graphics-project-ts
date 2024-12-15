4b83ef04b1245ffa92aaf5be1a173219786c5b4c
Anwesh43 <anweshthecool0@gmail.com>
2024-12-15 13:28:20 +0530

    right-angle-left-rot: adding RALRNode class

--- right-angle-left-rot.ts
+++ right-angle-left-rot.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class RALRNode {
+
+    prev: RALRNode
+    next: RALRNode
+    state: State = new State()
+
+    addNeighbor() {
+        this.addNeighbor()
+    }
+
+    constructor(private i: number) {
+        if (this.i < colors.length - 1) {
+            this.next = new RALRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRALRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RALRNode {
+        var curr: RALRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
