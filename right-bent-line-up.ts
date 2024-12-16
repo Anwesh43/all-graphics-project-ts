8cb5dd0ac2b2cd80575ab5424f34ac82db8090ea
Anwesh43 <anweshthecool0@gmail.com>
2024-12-17 01:18:13 +0530

    right-bent-line-up: adding RBLUNode class

--- right-bent-line-up.ts
+++ right-bent-line-up.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class RBLUNode {
+
+    prev: RBLUNode
+    next: RBLUNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RBLUNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRBLUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RBLUNode {
+        var curr: RBLUNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
