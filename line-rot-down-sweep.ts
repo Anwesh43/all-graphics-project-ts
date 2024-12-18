04185f223090c8d2c7fe74674ea806526ff03301
Anwesh43 <anweshthecool0@gmail.com>
2024-12-18 10:22:16 +0530

    line-rot-down-sweep: adding ScaleUtil class

--- line-rot-down-sweep.ts
+++ line-rot-down-sweep.ts
@@ -18,4 +18,12 @@ const deg: number = Math.PI
 
 class ScaleUtil {
 
+    static maxScale(scale: number, i: number, n: number): number {
+        return Math.max(0, scale - i / n)
+    }
+
+
+    static divideScale(scale: number, i: number, n: number): number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
 }
