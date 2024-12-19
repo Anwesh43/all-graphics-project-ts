207d3574469135240d46f75c9c9cb0a2967dc69f
Anwesh43 <anweshthecool0@gmail.com>
2024-12-19 17:54:03 +0530

    line-bent-to-left: adding fix 5

--- line-bent-to-left.ts
+++ line-bent-to-left.ts
@@ -38,18 +38,19 @@ class DrawingUtil {
 
     static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
         if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
-            context.beginPath()
-            context.moveTo(x1, y1)
-            context.lineTo(x2, y2)
-            context.stroke()
+            return
         }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
     }
 
     static drawLineBentToLeft(context: CanvasRenderingContext2D, scale: number) {
         const size: number = Math.min(w, h) / sizeFactor
         const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
         console.log("SC", scale)
-        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(3), h / 2, () => {
             context.rotate(deg * dsc(2))
             DrawingUtil.drawXY(context, 0, 0, () => {
                 context.rotate(rot * dsc(1))
