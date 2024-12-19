499f377413cdd218e76ba52d7c34752aa56ccfaf
Anwesh43 <anweshthecool0@gmail.com>
2024-12-19 17:50:55 +0530

    line-bent-to-left: adding fix

--- line-bent-to-left.ts
+++ line-bent-to-left.ts
@@ -110,6 +110,7 @@ class State {
 
     update(cb: () => void) {
         this.scale += scGap * this.dir
+        console.log("SCALE", this.scale)
         if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir
             this.dir = 0
