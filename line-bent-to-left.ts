2452e6d09a4c5a1d20dbfd3a5db48878b2fdade2
Anwesh43 <anweshthecool0@gmail.com>
2024-12-19 17:18:47 +0530

    line-bent-to-left: adding DrawingUtil class

--- line-bent-to-left.ts
+++ line-bent-to-left.ts
@@ -11,9 +11,10 @@ const strokeFactor: number = 90
 const sizeFactor: number = 4.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
-const rot: number = (5 * Math.PI) / 12
+const rot: number = -(5 * Math.PI) / 12
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const deg: number = -Math.PI
 
 class ScaleUtil {
 
@@ -23,5 +24,43 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            context.beginPath()
+            context.moveTo(x1, y1)
+            context.lineTo(x2, y2)
+            context.stroke()
+        }
+    }
+
+    static drawLineBentToLeft(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(3), h / 2, () => {
+            context.rotate(deg * dsc(2))
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+            })
+        })
+    }
+
+    static drawLBTLNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineBentToLeft(context, scale)
     }
 }
