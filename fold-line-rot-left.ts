44fb76847d1a4c397780330f045f561cdfa347c3
Anwesh43 <anweshthecool0@gmail.com>
2024-12-21 11:31:17 +0530

    fold-line-rot-left: adding DrawingUtil class

--- fold-line-rot-left.ts
+++ fold-line-rot-left.ts
@@ -25,3 +25,43 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawFoldLineRotLeft(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(4), h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(0), (h / 2 - size) * (1 - 2 * j) * (1 - dsc(2)), () => {
+                    context.rotate(-rot * (1 - 2 * j) * (dsc(1) - dsc(3)))
+                    DrawingUtil.drawLine(context, 0, 0, -size * dsc(0), 0)
+                })
+            }
+        })
+    }
+
+    static drawFLRLNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawFoldLineRotLeft(context, scale)
+    }
+}
