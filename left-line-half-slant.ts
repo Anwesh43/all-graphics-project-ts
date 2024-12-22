47d9b12ce38ff6d5b2884b67b94236c39f3b1ca9
Anwesh43 <anweshthecool0@gmail.com>
2024-12-22 13:09:00 +0530

    left-line-half-slant: adding LeftLineHalfSlant class

--- left-line-half-slant.ts
+++ left-line-half-slant.ts
@@ -185,3 +185,26 @@ class LLHSNode {
         return this
     }
 }
+
+class LeftLineHalfSlant {
+
+    curr: LLHSNode = new LLHSNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
