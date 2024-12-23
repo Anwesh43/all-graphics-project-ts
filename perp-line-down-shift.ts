454de3e2b03fa34d1574289810c9f3081b29cdac
Anwesh43 <anweshthecool0@gmail.com>
2024-12-23 11:49:01 +0530

    perp-line-down-shift: adding PLDSNode class

--- perp-line-down-shift.ts
+++ perp-line-down-shift.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class PLDSNode {
+
+    prev: PLDSNode
+    next: PLDSNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PLDSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawPLDSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): PLDSNode {
+        let curr: PLDSNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
