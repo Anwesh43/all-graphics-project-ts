79f85fab2b12b4087afd5963eb2f481e84b86d07
Anwesh43 <anweshthecool0@gmail.com>
2024-12-24 09:41:01 +0530

    rot-sq-down-left: adding RSDLNode class

--- rot-sq-down-left.ts
+++ rot-sq-down-left.ts
@@ -126,3 +126,46 @@ class Animator {
         }
     }
 }
+
+class RSDLNode {
+
+    prev: RSDLNode
+    next: RSDLNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RSDLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRSDLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RSDLNode {
+        var curr: RSDLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+}
