38a79d537a3e62ddf2a096c06b92374723e0be6b
Anwesh43 <anweshthecool0@gmail.com>
2024-12-24 09:43:06 +0530

    rot-sq-down-left: adding RotSqDownLeft class

--- rot-sq-down-left.ts
+++ rot-sq-down-left.ts
@@ -167,5 +167,27 @@ class RSDLNode {
         cb()
         return this
     }
+}
 
+class RotSqDownLeft {
+
+    curr: RSDLNode = new RSDLNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update((() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        }))
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
 }
