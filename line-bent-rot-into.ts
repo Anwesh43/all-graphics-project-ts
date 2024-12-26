9a30a8ac847379e4bd28ef0de25b06f8936c9d02
Anwesh43 <anweshthecool0@gmail.com>
2024-12-26 11:12:54 +0530

    line-bent-rot-into: adding fix

--- line-bent-rot-into.ts
+++ line-bent-rot-into.ts
@@ -8,7 +8,7 @@ const colors: Array<string> = [
 const parts: number = 6
 const scGap: number = 0.05 / parts
 const strokeFactor: number = 90
-const sizeFactor: number = 4.9
+const sizeFactor: number = 5.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
 const rot: number = -Math.PI / 4
@@ -63,7 +63,7 @@ class DrawingUtil {
 
     static drawLBRINode(context: CanvasRenderingContext2D, i: number, scale: number) {
         context.lineCap = 'round'
-        context.lineWidth = Math.min(w, h) / sizeFactor
+        context.lineWidth = Math.min(w, h) / strokeFactor
         context.strokeStyle = colors[i]
         DrawingUtil.drawLineBentRotInto(context, scale)
     }
