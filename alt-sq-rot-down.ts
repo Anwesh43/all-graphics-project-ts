1813b8d6cd2fe31db9ded24067dcf188332c4114
Anwesh43 <anweshthecool0@gmail.com>
2024-12-27 09:16:50 +0530

    alt-sq-rot-down: adding DrawingUtil class

--- alt-sq-rot-down.ts
+++ alt-sq-rot-down.ts
@@ -24,3 +24,34 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawAltSqRotDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1 - 2 * j)
+                    DrawingUtil.drawXY(context, 0, (h / 2 - size) * (1 - dsc(1 + j) + (h / 2) * dsc(5)), () => {
+                        context.rotate(rot * dsc(1 + j))
+                        context.fillRect(0, -size * dsc(0), size, size * dsc(0))
+                    })
+                })
+            }
+        })
+    }
+
+    static drawASRDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawAltSqRotDown(context, scale)
+    }
+}
