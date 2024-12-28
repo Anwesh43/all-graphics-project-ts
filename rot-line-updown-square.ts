381ba0476bd1abf3c90e5103c339b26983420c90
Anwesh43 <anweshthecool0@gmail.com>
2024-12-28 11:19:17 +0530

    rot-line-updown-square: adding Stage class

--- rot-line-updown-square.ts
+++ rot-line-updown-square.ts
@@ -65,3 +65,33 @@ class DrawingUtil {
         DrawingUtil.drawRotLineUpDownSquare(context, scale)
     }
 }
+
+class Stage {
+
+    canvas: HTMLCanvasElement = document.createElement('canvas')
+    context: CanvasRenderingContext2D | null
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+
+    }
+}
