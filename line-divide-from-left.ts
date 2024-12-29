7a4cbff42c3bef5ac900d0a762c35ec31f440784
Anwesh43 <anweshthecool0@gmail.com>
2024-12-29 19:41:13 +0530

    line-divide-from-left: adding fix 2

--- line-divide-from-left.ts
+++ line-divide-from-left.ts
@@ -51,7 +51,7 @@ class DrawingUtil {
         DrawingUtil.drawXY(context, w * dsc(3), h / 2, () => {
             for (let j = 0; j < 2; j++) {
                 DrawingUtil.drawXY(context, 0, 0, () => {
-                    context.rotate(rot * dsc(1 + j))
+                    context.rotate(rot * dsc(1 + j) * (1 - 2 * j))
                     DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
                 })
             }
