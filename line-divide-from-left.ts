e823833b2d6c19109bca4ffcb0db0cb2c7a6046b
Anwesh43 <anweshthecool0@gmail.com>
2024-12-29 10:42:13 +0530

    line-divide-from-left: adding LineDivideFromLeft class

--- line-divide-from-left.ts
+++ line-divide-from-left.ts
@@ -184,3 +184,26 @@ class LDFLNode {
         return this
     }
 }
+
+class LineDivideFromLeft {
+
+    curr: LDFLNode = new LDFLNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
