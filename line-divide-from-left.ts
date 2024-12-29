120554e2b33d00f8491bab7b6dfda5819b21d993
Anwesh43 <anweshthecool0@gmail.com>
2024-12-29 10:37:23 +0530

    line-divide-from-left: adding LDFLNode class

--- line-divide-from-left.ts
+++ line-divide-from-left.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LDFLNode {
+
+    prev: LDFLNode
+    next: LDFLNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDFLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLDFLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LDFLNode {
+        var curr: LDFLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
