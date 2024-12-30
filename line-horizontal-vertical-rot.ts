c6864bf03b644523be226581cea5654a566dda44
Anwesh43 <anweshthecool0@gmail.com>
2024-12-30 09:28:49 +0530

    line-horizontal-vertical-rot: adding LHVRNode class

--- line-horizontal-vertical-rot.ts
+++ line-horizontal-vertical-rot.ts
@@ -138,3 +138,46 @@ class Animator {
         }
     }
 }
+
+class LHVRNode {
+
+    prev: LHVRNode
+    next: LHVRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LHVRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLHVRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LHVRNode {
+        var curr: LHVRNode = this.next
+        if (dir === -1) {
+            curr = this.prev
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+}
