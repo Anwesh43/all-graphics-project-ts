2bc7f8e4797375164bd8d11fa116e4b383642263
Anwesh43 <anweshthecool0@gmail.com>
2024-12-31 14:04:23 +0530

    bar-down-rot-right: adding BDRRNode class

--- bar-down-rot-right.ts
+++ bar-down-rot-right.ts
@@ -127,3 +127,45 @@ class Animator {
         }
     }
 }
+
+class BDRRNode {
+
+    prev: BDRRNode
+    next: BDRRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BDRRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBDRRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BDRRNode {
+        var curr: BDRRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
