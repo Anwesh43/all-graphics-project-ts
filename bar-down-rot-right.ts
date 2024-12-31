8cd7489bab57977fae66a62c32f37e5f2e8fbeb7
Anwesh43 <anweshthecool0@gmail.com>
2024-12-31 13:50:50 +0530

    bar-down-rot-right: adding DrawingUtil class

--- bar-down-rot-right.ts
+++ bar-down-rot-right.ts
@@ -7,8 +7,8 @@ const colors: Array<string> = [
 ]
 const parts: number = 4
 const scGap: number = 0.04 / parts
-const barHFactor: number = 4.9
-const barWFactor: number = 11.2
+const barHFactor: number = 5.9
+const barWFactor: number = 14.2
 const rot: number = Math.PI / 2
 const delay: number = 20
 const backColor: string = "#BDBDBD"
@@ -23,5 +23,30 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawBarDownRotRight(context: CanvasRenderingContext2D, scale: number) {
+        const barW: number = Math.min(w, h) / barWFactor
+        const barH: number = Math.min(w, h) / barHFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), (h / 2) * dsc(2), () => {
+            context.rotate(rot * dsc(1))
+            context.fillRect(-barW, 0, barW, barH * dsc(0))
+        })
+    }
+
+    static drawBDRRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarDownRotRight(context, scale)
     }
 }
