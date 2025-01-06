9e5cc137469b42c731eac0a2bb54322649d29783
Anwesh43 <anweshthecool0@gmail.com>
2025-01-06 13:15:07 +0530

    bent-from-left-down: adding ScaleUtil class

--- bent-from-left-down.ts
+++ bent-from-left-down.ts
@@ -9,8 +9,19 @@ const parts: number = 4
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 6.2
-const rot: number = Math.PI / 2
+const rot: number = Math.PI / 4
 const delay: number = 20
 const backColor: string = "#BDBDBD"
 const w: number = window.innerWidth
-const h: number = window.innerHeight 
+const h: number = window.innerHeight
+
+class ScaleUtil {
+
+    static maxScale(scale: number, i: number, n: number): number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale: number, i: number, n: number): number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
