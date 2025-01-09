7a30a750f53a3cb1284225f8746108d9ef4bf3ab
Anwesh43 <anweshthecool0@gmail.com>
2025-01-09 17:04:02 +0530

    bar-drop-rot-left: adding BarDropRotLeft class

--- bar-drop-rot-left.ts
+++ bar-drop-rot-left.ts
@@ -165,3 +165,26 @@ class BDRLNode {
         return this
     }
 }
+
+class BarDropRotLeft {
+
+    curr: BDRLNode = new BDRLNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
