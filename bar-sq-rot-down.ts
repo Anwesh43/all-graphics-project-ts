8774dad623c37730a250032f4eb60cd7948b458f
Anwesh43 <anweshthecool0@gmail.com>
2025-01-14 13:43:31 +0530

    bar-sq-rot-down: adding BarSqRotDown class

--- bar-sq-rot-down.ts
+++ bar-sq-rot-down.ts
@@ -167,3 +167,26 @@ class BSRDNode {
         return this
     }
 }
+
+class BarSqRotDown {
+
+    curr: BSRDNode = new BSRDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
