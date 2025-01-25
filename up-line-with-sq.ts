fab693b6f6bbe6b7546238187d65a6ac90508f35
Anwesh43 <anweshthecool0@gmail.com>
2025-01-25 16:30:55 +0530

    up-line-with-sq: adding UpLineWithSq class

--- up-line-with-sq.ts
+++ up-line-with-sq.ts
@@ -187,3 +187,26 @@ class ULWSNode {
         return this
     }
 }
+
+class UpLineWithSq {
+
+    curr: ULWSNode = new ULWSNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
