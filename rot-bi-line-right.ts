7205605b47db28b4a8047aae49439de814fe4512
Anwesh43 <anweshthecool0@gmail.com>
2025-01-28 22:59:18 +0530

    rot-bi-line-right: adding RBLRNode class

--- rot-bi-line-right.ts
+++ rot-bi-line-right.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class RBLRNode {
+
+    prev: RBLRNode
+    next: RBLRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RBLRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRBLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RBLRNode {
+        var curr: RBLRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
