f940c7ad2aad40e3a08031edf3da2bd14235e763
Anwesh43 <anweshthecool0@gmail.com>
2025-01-28 23:03:57 +0530

    rot-bi-line-right: adding RotBiLineRight class

--- rot-bi-line-right.ts
+++ rot-bi-line-right.ts
@@ -183,3 +183,26 @@ class RBLRNode {
         return this
     }
 }
+
+class RotBiLineRight {
+
+    curr: RBLRNode = new RBLRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
