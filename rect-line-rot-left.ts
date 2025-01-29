fc5633a6fc30d4cfa79a1bf7ea3745e131cc858e
Anwesh43 <anweshthecool0@gmail.com>
2025-01-30 01:01:56 +0530

    rect-line-rot-left: adding RLRLNode class

--- rect-line-rot-left.ts
+++ rect-line-rot-left.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class RLRLNode {
+
+    prev: RLRLNode
+    next: RLRLNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLRLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRLRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RLRLNode {
+        let curr: RLRLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
