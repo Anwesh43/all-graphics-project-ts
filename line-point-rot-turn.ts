794831eeecc0632806a5f4d731d6408e4dea2e6e
Anwesh43 <anweshthecool0@gmail.com>
2025-02-04 23:59:43 +0530

    line-point-rot-turn: adding LPRTNode class

--- line-point-rot-turn.ts
+++ line-point-rot-turn.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LPRTNode {
+
+    prev: LPRTNode
+    next: LPRTNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LPRTNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLPRTNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LPRTNode {
+        var curr: LPRTNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
