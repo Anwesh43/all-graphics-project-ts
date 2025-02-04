eb9ca17014ac72dbb90379e29e7fc8404c686a98
Anwesh43 <anweshthecool0@gmail.com>
2025-02-04 23:42:18 +0530

    line-point-rot-turn: adding ScaleUtil class

--- line-point-rot-turn.ts
+++ line-point-rot-turn.ts
@@ -16,3 +16,13 @@ const deg: number = Math.PI
 const sizeFactor: number = 4.9
 const strokeFactor: number = 90
 
+class ScaleUtil {
+
+    static maxScale(scale: number, i: number, n: number): number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale: number, i: number, n: number): number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
