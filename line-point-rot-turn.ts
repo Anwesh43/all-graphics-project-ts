1940ef6bcb8a1a41b0700a8f6544d524a2f6fbd8
Anwesh43 <anweshthecool0@gmail.com>
2025-02-04 23:41:19 +0530

    line-point-rot-turn: adding constants

---
+++ line-point-rot-turn.ts
@@ -0,0 +1,18 @@
+const colors: Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts: number = 4
+const scGap: number = 0.4 / parts
+const delay: number = 20
+const backColor: string = "#BDBDBD"
+const w: number = window.innerWidth
+const h: number = window.innerHeight
+const initRot: number = Math.PI / 4
+const deg: number = Math.PI
+const sizeFactor: number = 4.9
+const strokeFactor: number = 90
+
