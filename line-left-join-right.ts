2c5d86b2e805b21b066951e91424acd3ac013cc5
Anwesh43 <anweshthecool0@gmail.com>
2025-02-16 13:02:13 +0530

    line-left-join-right: adding DrawingUtil class

--- line-left-join-right.ts
+++ line-left-join-right.ts
@@ -1,17 +1,17 @@
-const colors: Array<String> = [
+const colors: Array<string> = [
     "#1A237E",
     "#EF5350",
     "#AA00FF",
     "#C51162",
     "#00C853"
 ]
-const parts: number = 4
+const parts: number = 5
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 4.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
-const rot: number = -Math.PI / 2
+const rot: number = Math.PI / 2
 const w: number = window.innerWidth
 const h: number = window.innerHeight
 
@@ -23,5 +23,47 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineLeftJoinRight(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(4), () => {
+            DrawingUtil.drawXY(context, -w / 2 + (w / 2) * dsc(0), 0, () => {
+                context.rotate(-rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, -size * 0.5, 0)
+            })
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(2)), size / 2, () => {
+                context.rotate(rot * dsc(3))
+                DrawingUtil.drawLine(context, 0, 0, size / 2, 0)
+            })
+        })
+    }
+
+    static drawLLJRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineLeftJoinRight(context, scale)
     }
 }
