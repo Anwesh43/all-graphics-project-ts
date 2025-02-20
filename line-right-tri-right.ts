ee70950dcde4b83286ca2a99f5a1443622588f60
Anwesh43 <anweshthecool0@gmail.com>
2025-02-20 12:17:35 +0530

    line-right-tri-right: adding LRTRNode class

--- line-right-tri-right.ts
+++ line-right-tri-right.ts
@@ -149,3 +149,45 @@ class Animator {
         }
     }
 }
+
+class LRTRNode {
+
+    prev: LRTRNode
+    next: LRTRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRTRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLRTRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LRTRNode {
+        var curr: LRTRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
