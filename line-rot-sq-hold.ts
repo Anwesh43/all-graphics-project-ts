9bc96bb3da639aabe965df0d3c9997aa031d8d5a
Anwesh43 <anweshthecool0@gmail.com>
2025-02-21 08:50:14 +0530

    line-rot-sq-hold: adding DrawingUtil class

--- line-rot-sq-hold.ts
+++ line-rot-sq-hold.ts
@@ -25,3 +25,50 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRotSqHold(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (i: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(0)), 0, () => {
+                    DrawingUtil.drawXY(context, size, 0, () => {
+                        context.rotate(-rot * 0.5 * dsc(1) * j)
+                        DrawingUtil.drawLine(context, 0, 0, -size, 0)
+                    })
+                })
+            }
+            DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(2)), () => {
+                context.rotate(rot * dsc(3))
+                context.fillRect(-size, -size, size, size)
+            })
+        })
+    }
+
+    static drawLRSHNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineRotSqHold(context, scale)
+    }
+}
