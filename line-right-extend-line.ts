8b5b77312da5400d8d35e33b7f9450a2f55b1c71
Anwesh43 <anweshthecool0@gmail.com>
2025-02-23 12:20:00 +0530

    line-right-extend-line: adding LRELNode class

--- line-right-extend-line.ts
+++ line-right-extend-line.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LRELNode {
+
+    prev: LRELNode
+    next: LRELNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRELNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLRELNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LRELNode {
+        let curr: LRELNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
