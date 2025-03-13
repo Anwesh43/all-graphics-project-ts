83b4cb4237577e967d2470997425103810548675
Anwesh43 <anweshthecool0@gmail.com>
2025-03-13 15:15:35 +0530

    tri-step-line-rot: adding DrawingUtil class

--- tri-step-line-rot.ts
+++ tri-step-line-rot.ts
@@ -5,7 +5,7 @@ const colors: Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts: number = 4
+const parts: number = 5
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 4.9
@@ -23,5 +23,44 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            context.beginPath()
+            context.moveTo(x1, y1)
+            context.lineTo(x2, y2)
+            context.stroke()
+        }
+    }
+
+    static drawTriStepLineRot(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            for (let j = 0; j < 3; j++) {
+                DrawingUtil.drawXY(context, size * 0.5 * j, 0, () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, -(size * 0.5 + size * 0.25 * j) * dsc(j))
+                })
+            }
+        })
+    }
+
+    static drawTSLRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        DrawingUtil.drawTriStepLineRot(context, scale)
     }
 }
