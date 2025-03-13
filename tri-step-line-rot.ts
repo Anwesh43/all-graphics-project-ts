bb90bde6b9b0da9ef2fb934173017bfa83e63544
Anwesh43 <anweshthecool0@gmail.com>
2025-03-13 16:24:43 +0530

    tri-step-line-rot: adding TSLRNode class

--- tri-step-line-rot.ts
+++ tri-step-line-rot.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class TSLRNode {
+
+    prev: TSLRNode
+    next: TSLRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TSLRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawTSLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): TSLRNode {
+        var curr: TSLRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
