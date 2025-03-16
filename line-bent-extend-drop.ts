aa3d547a0085c2b2e89b31999319fd958e563d49
Anwesh43 <anweshthecool0@gmail.com>
2025-03-16 12:19:27 +0530

    line-bent-extend-drop: adding  LBEDNode class

--- line-bent-extend-drop.ts
+++ line-bent-extend-drop.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LBEDNode {
+
+    prev: LBEDNode
+    next: LBEDNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBEDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLBEDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LBEDNode {
+        var curr: LBEDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
