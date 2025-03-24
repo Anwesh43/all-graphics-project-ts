4685abc1b5ff2921edbf6e8329c6ce26e7e6357b
Anwesh43 <anweshthecool0@gmail.com>
2025-03-24 17:05:19 +0530

    line-bent-from-right: adding LineBarRotDownUp class

--- line-bent-from-right.ts
+++ line-bent-from-right.ts
@@ -186,3 +186,26 @@ class LBFRNode {
     }
 
 }
+
+class LineBarRotDownUp {
+
+    curr: LBFRNode = new LBFRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
