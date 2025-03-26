2a5cc599bc91d820c7070c291cfa9fb989808b4d
Anwesh43 <anweshthecool0@gmail.com>
2025-03-26 17:56:21 +0530

    double-pie-line-down: addin DrawingUtil class

--- double-pie-line-down.ts
+++ double-pie-line-down.ts
@@ -8,7 +8,7 @@ const colors: Array<string> = [
 const parts: number = 4
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
-const sizeFactor: number = 5.9
+const sizeFactor: number = 8.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
 const rot: number = Math.PI
@@ -23,5 +23,50 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawDoublePieLineDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number) => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                    DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(1)), () => {
+                        DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawDPLDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDoublePieLineDown(context, scale)
     }
 }
