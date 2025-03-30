0217029dff641bc65d5c8909301b27597100c494
Anwesh43 <anweshthecool0@gmail.com>
2025-03-30 14:00:29 +0530

    line-extend-perp-rot: adding LEPRNode class

--- line-extend-perp-rot.ts
+++ line-extend-perp-rot.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class LEPRNode {
+
+    prev: LEPRNode
+    next: LEPRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LEPRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLEPRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LEPRNode {
+        var curr: LEPRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
