a756ba7664f5d40935e8a21327a4cf9f29cd1879
Anwesh43 <anweshthecool0@gmail.com>
2025-04-02 21:46:05 +0530

    line-bent-join-right: adding DrawingUtil class

--- line-bent-join-right.ts
+++ line-bent-join-right.ts
@@ -26,3 +26,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBentJoinRight(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (i: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot1 * (1 - dsc(2)))
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(1), 0)
+            })
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot2 * (1 - dsc(2)))
+                DrawingUtil.drawLine(context, size * (1 - dsc(0)), 0, size, 0)
+            })
+        })
+    }
+
+    static drawLBJRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineBentJoinRight(context, scale)
+    }
+}
