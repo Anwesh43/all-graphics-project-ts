a43b98f650d4af3cb7723ba388cfd72a35e4b273
Anwesh43 <anweshthecool0@gmail.com>
2025-04-02 22:22:28 +0530

    line-bent-join-right: adding LineBentJoinRight class

--- line-bent-join-right.ts
+++ line-bent-join-right.ts
@@ -187,3 +187,26 @@ class LBJRNode {
         return this
     }
 }
+
+class LineBentJoinRight {
+
+    curr: LBJRNode = new LBJRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
