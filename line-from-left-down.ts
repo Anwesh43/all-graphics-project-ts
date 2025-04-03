2e6194e24d4d48216d4adffb579898dac8371ce5
Anwesh43 <anweshthecool0@gmail.com>
2025-04-03 21:17:07 +0530

    line-from-left-down: adding LFLDNode class

--- line-from-left-down.ts
+++ line-from-left-down.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LFLDNode {
+
+    prev: LFLDNode
+    next: LFLDNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LFLDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLFLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LFLDNode {
+        var curr: LFLDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
