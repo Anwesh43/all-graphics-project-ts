7da1464498c45c536a3595889cf18d37c8f17ae2
Anwesh43 <anweshthecool0@gmail.com>
2025-04-07 16:34:21 +0530

    line-bent-left-up: adding LineBentLeftUp class

--- line-bent-left-up.ts
+++ line-bent-left-up.ts
@@ -180,3 +180,26 @@ class LBLUNode {
         return this
     }
 }
+
+class LineBentLeftUp {
+
+    curr: LBLUNode = new LBLUNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
