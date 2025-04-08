579e8090b14962283c0096e4374d5d49c940f49b
Anwesh43 <anweshthecool0@gmail.com>
2025-04-08 17:24:17 +0530

    bisect-line-rot-up: adding BLRUNode class

--- bisect-line-rot-up.ts
+++ bisect-line-rot-up.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class BLRUNode {
+
+    prev: BLRUNode
+    next: BLRUNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLRUNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BLRUNode {
+        var curr: BLRUNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
