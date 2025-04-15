73f188ccca1c8bf430f7a4421e8a270dce8862ac
Anwesh43 <anweshthecool0@gmail.com>
2025-04-15 16:16:13 +0530

    line-rot-into-left: adding LineRotIntoLeft class

--- line-rot-into-left.ts
+++ line-rot-into-left.ts
@@ -183,3 +183,28 @@ class LRILNode {
         return this
     }
 }
+
+class LineRotIntoLeft {
+
+    curr: LRILNode = new LRILNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(() => {
+            cb()
+        })
+    }
+}
