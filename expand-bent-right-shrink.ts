2d1ed175f3a4725bb95911bad017404e1b17a39c
Anwesh43 <anweshthecool0@gmail.com>
2025-04-22 09:24:31 +0530

    expand-bent-right-shrink: adding EBRSNode class

--- expand-bent-right-shrink.ts
+++ expand-bent-right-shrink.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class EBRSNode {
+
+    prev: EBRSNode
+    next: EBRSNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new EBRSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawEBRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): EBRSNode {
+        var curr: EBRSNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
