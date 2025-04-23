75be3f3ffebc0c5f48b870013972540f8dda07c4
Anwesh43 <anweshthecool0@gmail.com>
2025-04-23 19:49:33 +0530

    half-box-rot-complete: adding HBRCNode class

--- half-box-rot-complete.ts
+++ half-box-rot-complete.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class HBRCNode {
+
+    prev: HBRCNode
+    next: HBRCNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new HBRCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawHBRCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): HBRCNode {
+        var curr: HBRCNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
