39d10e870cbe515fade7e2803db32ca2a967b74e
Anwesh43 <anweshthecool0@gmail.com>
2025-04-24 21:13:21 +0530

    right-right-line-rot: adding RRLRNode class

--- right-right-line-rot.ts
+++ right-right-line-rot.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class RRLRNode {
+
+    prev: RRLRNode
+    next: RRLRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RRLRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRRLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RRLRNode {
+        var curr: RRLRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
