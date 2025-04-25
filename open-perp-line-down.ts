bb456efc5ec995cfbc1262a854eb8dd6f7bbc641
Anwesh43 <anweshthecool0@gmail.com>
2025-04-26 02:18:18 +0530

    open-perp-line-down: adding DrawingUtil class

--- open-perp-line-down.ts
+++ open-perp-line-down.ts
@@ -8,7 +8,7 @@ const colors: Array<string> = [
 const parts: number = 4
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
-const sizeFactor: number = 4.9
+const sizeFactor: number = 5.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
 const rot: number = Math.PI / 4
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawOpenPerpLineDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h * 0.5 * (dsc(0) - dsc(3)), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * dsc(j + 1) * (1 - 2 * j))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+        })
+    }
+
+    static drawOPLDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        context.lineCap = 'round'
+        DrawingUtil.drawOpenPerpLineDown(context, scale)
     }
 }
