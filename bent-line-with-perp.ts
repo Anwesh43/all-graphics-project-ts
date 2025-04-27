66919174952199e4cc27c4bce1455e3743d13c33
Anwesh43 <anweshthecool0@gmail.com>
2025-04-27 13:03:12 +0530

    bent-line-with-perp: adding constants

---
+++ bent-line-with-perp.ts
@@ -0,0 +1,18 @@
+const colors: Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts: number = 5
+const scGap: number = 0.04 / parts
+const strokeFactor: number = 90
+const sizeFactor: number = 5.9
+const backColor: string = "#BDBDBD"
+const rot: number = Math.PI / 2
+const delay: number = 2
+const bentRot: number = Math.PI / 4
+const w: number = window.innerWidth
+const h: number = window.innerHeight
+
