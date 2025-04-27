2fde2e3823e609e94cdf2c35fcc468ae0ab17c5f
Anwesh43 <anweshthecool0@gmail.com>
2025-04-27 13:09:54 +0530

    bent-line-with-perp: adding DrawingUtil class

--- bent-line-with-perp.ts
+++ bent-line-with-perp.ts
@@ -10,7 +10,7 @@ const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 5.9
 const backColor: string = "#BDBDBD"
-const rot: number = Math.PI / 2
+const rot: number = Math.PI
 const delay: number = 2
 const bentRot: number = Math.PI / 4
 const w: number = window.innerWidth
@@ -24,5 +24,38 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBentLineWithPerp(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number) => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(bentRot * dsc(2))
+            DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+            DrawingUtil.drawXY(context, size, 0, () => {
+                context.rotate(-rot * dsc(3))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(1))
+            })
+        })
     }
 }
