716c4b8db893151a371fdd0aed724b7acf30a3b5
Anwesh43 <anweshthecool0@gmail.com>
2025-04-30 09:55:13 +0530

    line-up-mid-right: adding LUMRNode class

--- line-up-mid-right.ts
+++ line-up-mid-right.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class LUMRNode {
+
+    prev: LUMRNode
+    next: LUMRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LUMRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLUMRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LUMRNode {
+        var curr: LUMRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
