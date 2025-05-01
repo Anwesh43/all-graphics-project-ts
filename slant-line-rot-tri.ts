fe251b372af01b43ebfa7c383303352a19b6e67a
Anwesh43 <anweshthecool0@gmail.com>
2025-05-01 11:19:03 +0530

    slant-line-rot-tri: adding SLRTNode class

--- slant-line-rot-tri.ts
+++ slant-line-rot-tri.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class SLRTNode {
+
+    prev: SLRTNode
+    next: SLRTNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLRTNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSlantLineRotTri(context, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SLRTNode {
+        var curr: SLRTNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
