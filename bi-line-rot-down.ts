7fff5bb442fd1af826a6a92921a59a430e54b5be
Anwesh43 <anweshthecool0@gmail.com>
2025-05-04 11:05:39 +0530

    bi-line-rot-down: adding DrawingUtil class

--- bi-line-rot-down.ts
+++ bi-line-rot-down.ts
@@ -25,3 +25,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiLineRotDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, -size, -h * 0.5 * (1 - dsc(0)), () => {
+                    context.rotate(rot * (1 - 2 * j) * dsc(1))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+        })
+    }
+
+    static drawBLRDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBiLineRotDown(context, scale)
+    }
+}
