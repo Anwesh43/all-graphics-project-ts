38ac41cab1eee765b83f31a720705fe486f6ddb9
Anwesh43 <anweshthecool0@gmail.com>
2025-05-05 11:17:30 +0530

    line-rot-into-box: adding DrawingUtil class

--- line-rot-into-box.ts
+++ line-rot-into-box.ts
@@ -9,7 +9,7 @@ const parts: number = 4
 const scGap: number = 0.04 / parts
 const delay: number = 20
 const backColor: string = "#BDBDBD"
-const rot: number = Math.PI / 2
+const rot: number = -1.5 * Math.PI
 const strokeFactor: number = 90
 const sizeFactor: number = 4.9
 const w: number = window.innerWidth
@@ -23,5 +23,51 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLineRotIntoBox(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h * 0.5 * (1 + dsc(3)), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.scale(1 - 2 * j, 1)
+                    DrawingUtil.drawXY(context, -size / 2, 0, () => {
+                        context.rotate(rot * dsc(1))
+                        DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+                    })
+                    DrawingUtil.drawXY(context, -size / 2, size / 2, () => {
+                        DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(2), 0)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawLRIBNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.strokeStyle = colors[i]
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        DrawingUtil.drawLineRotIntoBox(context, scale)
     }
 }
