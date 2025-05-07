918bda217207e10818d78e0b397c5b20beb74c71
Anwesh43 <anweshthecool0@gmail.com>
2025-05-07 16:15:34 +0530

    diag-big-small-line: adding DiagBigSmallLine class

--- diag-big-small-line.ts
+++ diag-big-small-line.ts
@@ -186,3 +186,26 @@ class DBSLNode {
         return this
     }
 }
+
+class DiagBigSmallLine {
+
+    curr: DBSLNode = new DBSLNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
