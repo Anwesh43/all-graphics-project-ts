41ae7559ba2c8d40f480bcce30a7d1fbb7cf8f0f
Anwesh43 <anweshthecool0@gmail.com>
2025-05-07 16:14:20 +0530

    diag-big-small-line: adding DBSLNode class

--- diag-big-small-line.ts
+++ diag-big-small-line.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class DBSLNode {
+
+    prev: DBSLNode
+    next: DBSLNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DBSLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawDBSLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): DBSLNode {
+        var curr: DBSLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
