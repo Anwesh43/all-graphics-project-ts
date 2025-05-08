12e294dd0fd0e8a58cca4a932f76d1a0c930b200
Anwesh43 <anweshthecool0@gmail.com>
2025-05-08 12:10:22 +0530

    bi-line-join-rot: adding BLJRNode class

--- bi-line-join-rot.ts
+++ bi-line-join-rot.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class BLJRNode {
+
+    prev: BLJRNode
+    next: BLJRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLJRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBLJRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BLJRNode {
+        var curr: BLJRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
