2e5aa7cf4eeba5e54e1ccf4ce43814d0a71e8d0a
Anwesh43 <anweshthecool0@gmail.com>
2025-05-09 09:34:36 +0530

    square-move-rot-right: adding SMRRNode class

--- square-move-rot-right.ts
+++ square-move-rot-right.ts
@@ -130,3 +130,45 @@ class Animator {
         }
     }
 }
+
+class SMRRNode {
+
+    prev: SMRRNode
+    next: SMRRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SMRRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSMRRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SMRRNode {
+        var curr: SMRRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
