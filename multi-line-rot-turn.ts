90a4fd2ccafaf5e8a67c7c410eb3fce920c9cc7b
Anwesh43 <anweshthecool0@gmail.com>
2025-05-12 15:01:31 +0530

    multi-line-rot-turn: adding MultiLineRotTurn class

--- multi-line-rot-turn.ts
+++ multi-line-rot-turn.ts
@@ -185,3 +185,26 @@ class MLRTNode {
         return this
     }
 }
+
+class MultiLineRotTurn {
+
+    curr: MLRTNode = new MLRTNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
