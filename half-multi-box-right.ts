c804e27ff1f51dfd0bbe866aaa9f875745f7f5da
Anwesh43 <anweshthecool0@gmail.com>
2025-05-14 11:36:43 +0530

    half-multi-box-right: adding HalfMulitBoxRight class

--- half-multi-box-right.ts
+++ half-multi-box-right.ts
@@ -194,3 +194,26 @@ class HMBRNode {
         return this
     }
 }
+
+class HalfMultiBoxRight {
+
+    curr: HMBRNode = new HMBRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
