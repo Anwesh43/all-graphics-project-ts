6410b08facbadab5becdedc830a41f127329b1eb
Anwesh43 <anweshthecool0@gmail.com>
2025-05-14 11:44:14 +0530

    half-multi-box-right: adding Renderer in Stage

--- half-multi-box-right.ts
+++ half-multi-box-right.ts
@@ -80,6 +80,7 @@ class Stage {
 
     canvas: HTMLCanvasElement = document.createElement('canvas')
     context: CanvasRenderingContext2D | null
+    renderer: Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -92,12 +93,15 @@ class Stage {
         if (this.context) {
             this.context.fillStyle = backColor
             this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
