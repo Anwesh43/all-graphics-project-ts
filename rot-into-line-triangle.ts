5e5b1f5b832af0799e54e66afc5215fe078281ec
Anwesh43 <anweshthecool0@gmail.com>
2025-05-15 16:58:58 +0530

    rot-into-line-triangle: adding RILTNode class

--- rot-into-line-triangle.ts
+++ rot-into-line-triangle.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class RILTNode {
+
+    prev: RILTNode
+    next: RILTNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RILTNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRILTNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RILTNode {
+        var curr: RILTNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
