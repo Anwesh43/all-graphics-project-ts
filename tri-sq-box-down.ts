825bd539a614fab6e42e331cc31869b39b2b4728
Anwesh43 <anweshthecool0@gmail.com>
2025-05-16 13:07:28 +0530

    tri-sq-box-down: adding DrawingUtil class

--- tri-sq-box-down.ts
+++ tri-sq-box-down.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawTriSqBoxDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        const dsk: (a: number, b: number, parts: number) => number = (i: number, j: number, parts: number) => ScaleUtil.divideScale(dsc(i), j, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawLine(context, 0, 0, size * dsk(0, 0, 3), 0)
+            DrawingUtil.drawXY(context, size, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, -size * dsk(0, 1, 3))
+            })
+            DrawingUtil.drawXY(context, size + size * dsc(1), -size, () => {
+                DrawingUtil.drawLine(context, 0, 0, -size * dsk(0, 2, 3), 0)
+            })
+        })
+    }
+
+    static drawTSBDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawTriSqBoxDown(context, scale)
+    }
+}
