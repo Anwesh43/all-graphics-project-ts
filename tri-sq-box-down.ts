e1a1632feb218532ffce619a17cdb1403c31efcb
Anwesh43 <anweshthecool0@gmail.com>
2025-05-16 14:20:16 +0530

    tri-sq-box-down: adding TSBDNode class

--- tri-sq-box-down.ts
+++ tri-sq-box-down.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class TSBDNode {
+
+    prev: TSBDNode
+    next: TSBDNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TSBDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawTSBDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): TSBDNode {
+        var curr: TSBDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
