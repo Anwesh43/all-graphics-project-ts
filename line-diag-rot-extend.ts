4bd487cc8b2e070506316da9519c3016a9f061b9
Anwesh43 <anweshthecool0@gmail.com>
2025-05-17 11:00:16 +0530

    line-diag-rot-extend: adding LDRENode class

--- line-diag-rot-extend.ts
+++ line-diag-rot-extend.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LDRENode {
+
+    prev: LDRENode
+    next: LDRENode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDRENode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLDRENode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LDRENode {
+        var curr: LDRENode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
