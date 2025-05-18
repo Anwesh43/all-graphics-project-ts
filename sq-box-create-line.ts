30cdef2e281b2414a70db4d3e742b7cde543a202
Anwesh43 <anweshthecool0@gmail.com>
2025-05-18 10:26:53 +0530

    sq-box-create-line: adding SBCLNode class

--- sq-box-create-line.ts
+++ sq-box-create-line.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class SBCLNode {
+
+    prev: SBCLNode
+    next: SBCLNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBCLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSBCLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SBCLNode {
+        var curr: SBCLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
