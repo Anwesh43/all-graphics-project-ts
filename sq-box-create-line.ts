d1cd002291555eacbe4bee422677f4cc98141363
Anwesh43 <anweshthecool0@gmail.com>
2025-05-18 10:28:09 +0530

    sq-box-create-line: adding SqBoxCreateLine class

--- sq-box-create-line.ts
+++ sq-box-create-line.ts
@@ -184,3 +184,26 @@ class SBCLNode {
         return this
     }
 }
+
+class SqBoxCreateLine {
+
+    curr: SBCLNode = new SBCLNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
