eb50ac31551aa34792031c6fa878ac7135dad1f2
Anwesh43 <anweshthecool0@gmail.com>
2025-05-19 15:49:05 +0530

    up-line-right-rot: adding ULRRNode class

--- up-line-right-rot.ts
+++ up-line-right-rot.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class ULRRNode {
+
+    prev: ULRRNode
+    next: ULRRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ULRRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawULRRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): ULRRNode {
+        var curr: ULRRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
