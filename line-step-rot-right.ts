dd03f6098c3cfde15395b433cd022030cbc431d5
Anwesh43 <anweshthecool0@gmail.com>
2025-05-20 12:38:04 +0530

    line-step-rot-right: adding LSRRNode class

--- line-step-rot-right.ts
+++ line-step-rot-right.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LSRRNode {
+
+    prev: LSRRNode
+    next: LSRRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSRRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLSRRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LSRRNode {
+        var curr: LSRRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
