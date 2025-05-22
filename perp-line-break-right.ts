6657a980cddce9f112d4e897bba39a859cece006
Anwesh43 <anweshthecool0@gmail.com>
2025-05-22 15:43:20 +0530

    perp-line-break-right: adding DrawingUtil class

--- perp-line-break-right.ts
+++ perp-line-break-right.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawPerpLineBreakRight(context: CanvasRenderingContext2D, scale: number) {
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        const size: number = Math.min(w, h) / sizeFactor
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            DrawingUtil.drawXY(context, w * 0.5 * (1 - dsc(0)), 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, size, 0)
+            })
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, size * 0.5 * dsc(2), -h * 0.5 * (1 - dsc(1)), () => {
+                    context.rotate(rot * (1 - 2 * j) * dsc(3))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+        })
+    }
+
+    static drawPLBRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPerpLineBreakRight(context, scale)
+    }
+}
