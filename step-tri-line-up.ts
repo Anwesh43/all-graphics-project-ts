7c5e0d2b52e06b867ba59fb7abe6730f824e4cd0
Anwesh43 <anweshthecool0@gmail.com>
2025-05-31 16:25:58 +0530

    step-tri-line-up: adding DrawingUtil class

--- step-tri-line-up.ts
+++ step-tri-line-up.ts
@@ -25,3 +25,49 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawStepTriLineUp(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        const dsk: (i: number, j: number, k: number) => number = (i: number, j: number, k: number): number => ScaleUtil.divideScale(dsc(i), j, k)
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2) * dsc(4), () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawXY(context, -size, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, size * dsk(0, 0, 3), 0)
+            })
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, size * 0.5 * (dsk(0, 1, 3) - dsc(2)))
+            })
+            DrawingUtil.drawXY(context, 0, size * 0.5 * (1 - dsc(2)), () => {
+                context.rotate(rot * 2 * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, size * dsk(0, 2, 3), 0)
+            })
+        })
+    }
+
+    static drawSTLUNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawStepTriLineUp(context, scale)
+    }
+}
