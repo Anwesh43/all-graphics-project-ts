15ab2cf2539f8462a9a550c5ef92522fe60d32a6
Anwesh43 <anweshthecool0@gmail.com>
2025-05-31 19:32:32 +0530

    step-tri-line-up: adding STLUNode class

--- step-tri-line-up.ts
+++ step-tri-line-up.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class STLUNode {
+
+    prev: STLUNode
+    next: STLUNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new STLUNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSTLUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): STLUNode {
+        var curr: STLUNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
