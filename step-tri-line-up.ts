a66bf05b3669008c3064f555de24cbe2f8792b0a
Anwesh43 <anweshthecool0@gmail.com>
2025-05-31 19:38:04 +0530

    step-tri-line-up: adding StepTriLineUp class

--- step-tri-line-up.ts
+++ step-tri-line-up.ts
@@ -190,3 +190,26 @@ class STLUNode {
         return this
     }
 }
+
+class StepTriLineUp {
+
+    curr: STLUNode = new STLUNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
