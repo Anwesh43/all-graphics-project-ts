889c452d47d5911f04c86cf34fe22beb471a1b9a
Anwesh43 <anweshthecool0@gmail.com>
2025-06-02 12:22:55 +0530

    arc-cap-line-right: adding ACLRNode class

--- arc-cap-line-right.ts
+++ arc-cap-line-right.ts
@@ -157,3 +157,45 @@ class Animator {
         }
     }
 }
+
+class ACLRNode {
+
+    prev: ACLRNode
+    next: ACLRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ACLRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawACLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): ACLRNode {
+        var curr: ACLRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
