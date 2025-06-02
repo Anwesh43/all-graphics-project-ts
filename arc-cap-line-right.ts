a9f942b79ee9f861a204a253880094d7ab789aa3
Anwesh43 <anweshthecool0@gmail.com>
2025-06-02 12:24:04 +0530

    arc-cap-line-right: adding ArcCapLineRight class

--- arc-cap-line-right.ts
+++ arc-cap-line-right.ts
@@ -199,3 +199,26 @@ class ACLRNode {
         return this
     }
 }
+
+class ArcCapLineRight {
+
+    curr: ACLRNode = new ACLRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
