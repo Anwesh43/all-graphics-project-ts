1d2b24e069e31a7bae9c0520d4052290d4a51cbf
Anwesh43 <anweshthecool0@gmail.com>
2025-06-08 10:26:51 +0530

    line-arc-join-perp: adding DrawingUtil class

--- line-arc-join-perp.ts
+++ line-arc-join-perp.ts
@@ -25,3 +25,60 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArc(context: CanvasRenderingContext2D, r: number, sweep: number) {
+        const endDeg: number = Math.floor(sweep) + 180
+        context.beginPath()
+        for (let j = 180; j <= endDeg; j++) {
+            const x: number = r * Math.cos(j * Math.PI / 180)
+            const y: number = r * Math.sin(j * Math.PI / 180)
+            if (j === 180) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+    }
+
+    static drawLineArcJoinPerp(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2) * dsc(4), () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawLine(context, 0, 0, -size * dsc(0), 0)
+            DrawingUtil.drawXY(context, -size / 2, 0, () => {
+                DrawingUtil.drawArc(context, size / 2, 90 * dsc(1))
+            })
+            DrawingUtil.drawXY(context, 0, -size / 2, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, size * 0.5 * dsc(2))
+            })
+        })
+    }
+
+    static drawLAJPNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineArcJoinPerp(context, scale)
+    }
+}
