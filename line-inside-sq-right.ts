e7234cf69dc353cd7ff58e615acd263c12f4913f
Anwesh43 <anweshthecool0@gmail.com>
2025-06-12 16:11:15 +0530

    line-inside-sq-right: adding LineInsideSqRight class

--- line-inside-sq-right.ts
+++ line-inside-sq-right.ts
@@ -184,3 +184,26 @@ class LISRNode {
         return this
     }
 }
+
+class LineInsideSqRight {
+
+    curr: LISRNode = new LISRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
