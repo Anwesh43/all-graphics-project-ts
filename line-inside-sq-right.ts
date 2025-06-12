8070a696062ea45e7c3586a2fbbbc772babb016c
Anwesh43 <anweshthecool0@gmail.com>
2025-06-12 16:10:09 +0530

    line-inside-sq-right: adding LISRNode class

--- line-inside-sq-right.ts
+++ line-inside-sq-right.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LISRNode {
+
+    prev: LISRNode
+    next: LISRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LISRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLISRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LISRNode {
+        var curr: LISRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
