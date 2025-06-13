d3809142e2b32fe5725564012ae1748f3f4652c4
Anwesh43 <anweshthecool0@gmail.com>
2025-06-13 10:38:32 +0530

    arc-quarter-partial-full: adding DrawingUtil class

--- arc-quarter-partial-full.ts
+++ arc-quarter-partial-full.ts
@@ -11,9 +11,10 @@ const strokeFactor: number = 90
 const sizeFactor: number = 4.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
-const rot: number = Math.PI / 4
+const rot: number = 45
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const deg: number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -23,5 +24,52 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawArc(context: CanvasRenderingContext2D, r: number, start: number, sweep: number) {
+        const startInt: number = Math.floor(start)
+        const sweepInt: number = Math.floor(sweep)
+        context.beginPath()
+        for (let i = startInt; i <= sweepInt; i++) {
+            const x: number = r * Math.cos(i * Math.PI / 180)
+            const y: number = r * Math.sin(i * Math.PI / 180)
+            if (i === startInt) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+    }
+
+    static drawArcQuarterPartial(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size / 2) * dsc(3), () => {
+            context.rotate(deg * dsc(1))
+            for (let j = 0; j < 4; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * 2 * j)
+                    DrawingUtil.drawArc(context, size * 0.5, -rot * 0.5 * (dsc(0) + dsc(2)), rot * (dsc(0) + dsc(2)))
+                })
+            }
+        })
+    }
+
+    static drawAQPNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawArcQuarterPartial(context, scale)
     }
 }
