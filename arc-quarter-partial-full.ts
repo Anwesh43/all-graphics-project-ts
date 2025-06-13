fcd426581c8b021c73112da214e7c88e531fec7e
Anwesh43 <anweshthecool0@gmail.com>
2025-06-13 11:12:59 +0530

    arc-quarter-partial-full: adding AQPFNode class

--- arc-quarter-partial-full.ts
+++ arc-quarter-partial-full.ts
@@ -150,3 +150,45 @@ class Animator {
         }
     }
 }
+
+class AQPFNode {
+
+    prev: AQPFNode
+    next: AQPFNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new AQPFNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawAQPNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): AQPFNode {
+        var curr: AQPFNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
