f958d6acbb581dd038632d603e30467b93444d67
Anwesh43 <anweshthecool0@gmail.com>
2025-06-21 11:59:59 +0530

    start-arc-right-line: adding StartArcRightLine class

--- start-arc-right-line.ts
+++ start-arc-right-line.ts
@@ -204,3 +204,26 @@ class SARLNode {
         return this
     }
 }
+
+class StartArcRightLine {
+
+    curr: SARLNode = new SARLNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
