f56833f7f2edc41a4440fe617be8ee0eb19b01c6
Anwesh43 <anweshthecool0@gmail.com>
2025-06-21 11:48:47 +0530

    start-arc-right-line: adding DrawingUtil class

--- start-arc-right-line.ts
+++ start-arc-right-line.ts
@@ -11,6 +11,7 @@ const strokeFactor: number = 90
 const sizeFactor: number = 5.9
 const rot: number = Math.PI / 2
 const deg: number = Math.PI
+const sweep: number = 90
 const w: number = window.innerWidth
 const h: number = window.innerHeight
 
@@ -22,5 +23,63 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArc(context: CanvasRenderingContext2D, r: number, start: number, sweep: number) {
+        const end: number = start + Math.floor(sweep)
+        context.beginPath()
+        for (let i = start; i <= end; i++) {
+            const x: number = r * Math.cos(i * Math.PI / 180)
+            const y: number = r * Math.sin(i * Math.PI / 180)
+            if (i == start) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+
+    }
+
+    static drawStartArcRightLine(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(4), () => {
+            context.rotate(deg * dsc(3))
+            DrawingUtil.drawArc(context, size, 2 * sweep, sweep * dsc(0))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * dsc(2))
+                    DrawingUtil.drawLine(context, 0, -size, 0, -size * (1 - dsc(1)))
+                })
+            }
+        })
+    }
+
+    static drawSARLNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawStartArcRightLine(context, scale)
     }
 }
