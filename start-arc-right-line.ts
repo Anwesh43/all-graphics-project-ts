7f8b47f2a3efb5ec010ac1246b80a2c196dfbf43
Anwesh43 <anweshthecool0@gmail.com>
2025-06-21 11:58:45 +0530

    start-arc-right-line: adding SARLNode class

--- start-arc-right-line.ts
+++ start-arc-right-line.ts
@@ -162,3 +162,45 @@ class Animator {
         }
     }
 }
+
+class SARLNode {
+
+    prev: SARLNode
+    next: SARLNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SARLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSARLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SARLNode {
+        var curr: SARLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
