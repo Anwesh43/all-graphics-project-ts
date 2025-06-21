22a2f75559b30395cd166aec6bf958385cb3fe76
Anwesh43 <anweshthecool0@gmail.com>
2025-06-21 11:52:27 +0530

    start-arc-right-line: adding Stage class

--- start-arc-right-line.ts
+++ start-arc-right-line.ts
@@ -14,6 +14,7 @@ const deg: number = Math.PI
 const sweep: number = 90
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const backColor: string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -81,5 +82,38 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor
         context.strokeStyle = colors[i]
         DrawingUtil.drawStartArcRightLine(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas: HTMLCanvasElement = document.createElement('canvas')
+    context: CanvasRenderingContext2D | null
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage: Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
