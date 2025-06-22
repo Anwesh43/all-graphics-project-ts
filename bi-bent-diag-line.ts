5a38f3baf8cbe7448372ad952161c0bed7d269c6
Anwesh43 <anweshthecool0@gmail.com>
2025-06-22 09:02:31 +0530

    bi-bent-diag-line: adding BBDLNode class

--- bi-bent-diag-line.ts
+++ bi-bent-diag-line.ts
@@ -144,3 +144,43 @@ class Animator {
         }
     }
 }
+
+class BBDLNode {
+
+    prev: BBDLNode
+    next: BBDLNode
+    state: State = new State()
+
+    addNeighbor() {
+        this.next = new BBDLNode(this.i + 1)
+        this.next.prev = this
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBBDLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BBDLNode {
+        var curr: BBDLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
