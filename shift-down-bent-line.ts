6aa5ac099429ba20c9f00cd53ef5aa62198a289b
Anwesh43 <anweshthecool0@gmail.com>
2025-06-23 13:22:56 +0530

    shift-down-bent-line: adding SDBLNode  class

--- shift-down-bent-line.ts
+++ shift-down-bent-line.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class SDBLNode {
+
+    prev: SDBLNode
+    next: SDBLNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SDBLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSDBLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SDBLNode {
+        var curr: SDBLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
