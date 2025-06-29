c4dc3efd2c75117e566c636acbb17882297750fe
Anwesh43 <anweshthecool0@gmail.com>
2025-06-29 17:10:22 +0530

    multi-rot-sq-down: adding MultiRotSqDown class

--- multi-rot-sq-down.ts
+++ multi-rot-sq-down.ts
@@ -167,3 +167,26 @@ class MRSDNode {
         return this
     }
 }
+
+class MultiRotSqDown {
+
+    curr: MRSDNode = new MRSDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
