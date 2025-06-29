a1573105ca6974fb06d76d1826bc355d0f780a92
Anwesh43 <anweshthecool0@gmail.com>
2025-06-29 16:58:49 +0530

    multi-rot-sq-down: adding DrawingUtil class

--- multi-rot-sq-down.ts
+++ multi-rot-sq-down.ts
@@ -10,10 +10,11 @@ const strokeFactor: number = 90
 const sizeFactor: number = 4.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
-const rot: number = Math.PI / 2
+const rot: number = Math.PI
 const scGap: number = 0.04 / parts
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const deg: number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -23,5 +24,32 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawMultiRotSqDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(3), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawXY(context, -w * 0.5 * (1 - dsc(0)), -size, () => {
+                context.rotate(deg * dsc(1))
+                context.fillRect(0, -size, size, size)
+            })
+        })
+    }
+
+    static drawMRSDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawMultiRotSqDown(context, scale)
     }
 }
