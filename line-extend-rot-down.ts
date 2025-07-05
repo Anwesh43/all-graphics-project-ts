f6d46d7e58a092d9237f7a4ae218b12b8268e81f
Anwesh43 <anweshthecool0@gmail.com>
2025-07-05 10:49:02 +0530

    line-extend-rot-down: adding LERDNode class

--- line-extend-rot-down.ts
+++ line-extend-rot-down.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class LERDNode {
+
+    prev: LERDNode
+    next: LERDNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LERDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLERDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startupdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void) {
+        var curr: LERDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
