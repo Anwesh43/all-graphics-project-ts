e148691589a0e6bc41975bfe436a017d77db57b4
Anwesh43 <anweshthecool0@gmail.com>
2025-07-06 14:56:19 +0530

    horiz-line-bent-vert: adding HLBVNode class

--- horiz-line-bent-vert.ts
+++ horiz-line-bent-vert.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class HLBVNode {
+
+    prev: HLBVNode
+    next: HLBVNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new HLBVNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawHLBVNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): HLBVNode {
+        var curr: HLBVNode = this.next
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
