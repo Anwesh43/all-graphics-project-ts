2fe77ccba4a670ab55bfc25f8d1e168db99dd52f
Anwesh43 <anweshthecool0@gmail.com>
2025-07-26 15:51:59 +0530

    bi-line-rot-right: adding DrawingUtil class

--- bi-line-rot-right.ts
+++ bi-line-rot-right.ts
@@ -8,8 +8,8 @@ const colors: Array<string> = [
 const parts: number = 5
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
-const rot: number = Math.PI / 2
-const deg: number = -Math.PI / 4
+const sizeFactor: number = 5.9
+const rot: number = -Math.PI / 4
 const backColor: string = "#BDBDBD"
 const delay: number = 20
 const h: number = window.innerHeight
@@ -23,5 +23,45 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiLineRotRight(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * (1 + j) * (1 - dsc(2 + j)))
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(1 - j))
+                })
+            }
+        })
+    }
+
+    static drawBLRRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBiLineRotRight(context, scale)
     }
 }
