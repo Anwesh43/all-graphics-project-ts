59c22b1a28aa4af41220d70a4fab4f2082e0cdee
Anwesh43 <anweshthecool0@gmail.com>
2025-07-26 15:33:06 +0530

    bi-line-rot-right: adding constants

---
+++ bi-line-rot-right.ts
@@ -0,0 +1,16 @@
+const colors: Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const parts: number = 5
+const scGap: number = 0.04 / parts
+const strokeFactor: number = 90
+const rot: number = Math.PI / 2
+const deg: number = -Math.PI / 4
+const backColor: string = "#BDBDBD"
+const delay: number = 20
+const h: number = window.innerHeight
+const w: number = window.innerWidth
