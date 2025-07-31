b211fd261e7e5c126a246aa1411bd0647d440823
Anwesh43 <anweshthecool0@gmail.com>
2025-07-31 12:59:41 +0530

    join-bent-line-down: adding JBLDNode class

--- join-bent-line-down.ts
+++ join-bent-line-down.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class JBLDNode {
+
+    prev: JBLDNode
+    next: JBLDNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new JBLDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawJBLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): JBLDNode {
+        var curr: JBLDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
