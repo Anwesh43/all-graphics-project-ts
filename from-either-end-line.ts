76af6845f4f2fd3cb1c60dd3c23e625db6e03c36
Anwesh43 <anweshthecool0@gmail.com>
2025-08-01 16:42:16 +0530

    from-either-end-line: adding FromEitherEndLine class

--- from-either-end-line.ts
+++ from-either-end-line.ts
@@ -187,3 +187,25 @@ class FEELNode {
     }
 }
 
+class FromEitherEndLine {
+
+    curr: FEELNode = new FEELNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
