39b24a3094d379538c823f6a3aa47fd43a27ae2a
Anwesh43 <anweshthecool0@gmail.com>
2025-08-01 17:05:55 +0530

    from-either-end-line: adding fix 2

--- from-either-end-line.ts
+++ from-either-end-line.ts
@@ -46,7 +46,7 @@ class DrawingUtil {
     }
 
     static drawFromEitherEndLine(context: CanvasRenderingContext2D, scale: number) {
-        const size: number = Math.min(w, h) / strokeFactor
+        const size: number = Math.min(w, h) / sizeFactor
         const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
         DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(4), () => {
             context.rotate(rot * dsc(3))
