47351f1b43d6ce35a78869916cc0d415e4a09885
Anwesh43 <anweshthecool0@gmail.com>
2025-08-03 11:58:13 +0530

    small-sq-big-hide: adding SSBHNode class

--- small-sq-big-hide.ts
+++ small-sq-big-hide.ts
@@ -151,3 +151,45 @@ class Animator {
         }
     }
 }
+
+class SSBHNode {
+
+    prev: SSBHNode
+    next: SSBHNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SSBHNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSSBHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SSBHNode {
+        var curr: SSBHNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
