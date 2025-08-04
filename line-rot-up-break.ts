96565525ead1d6293ddd236de4f486191e03f225
Anwesh43 <anweshthecool0@gmail.com>
2025-08-04 14:05:29 +0530

    line-rot-up-break: adding DrawingUtil class

--- line-rot-up-break.ts
+++ line-rot-up-break.ts
@@ -11,7 +11,7 @@ const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 4.9
 const delay: number = 20
-const rot: number = Math.PI / 2
+const rot: number = -Math.PI / 2
 const w: number = window.innerWidth
 const h: number = window.innerHeight
 
@@ -23,5 +23,50 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBrokenLine(context: CanvasRenderingContext2D, i: number, size: number, sc1: number, sc2: number) {
+        const dSize: number = (size / lines)
+        DrawingUtil.drawXY(context, dSize * i, (w / 2) * sc2, () => {
+            DrawingUtil.drawLine(context, 0, 0, dSize * ScaleUtil.divideScale(sc2, i, lines), 0)
+        })
+    }
+
+    static drawLineRotUpBreak(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            context.rotate(rot * dsc(1))
+            for (let j = 0; j < lines; j++) {
+                DrawingUtil.drawBrokenLine(context, j, size, dsc(0), dsc(2 + (lines - 1 - j)))
+            }
+        })
+    }
+
+    static drawLRUBNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineRotUpBreak(context, scale)
     }
 }
