1abc8051075384707f0e3a702adee391ee406e46
Anwesh43 <anweshthecool0@gmail.com>
2025-08-06 13:02:47 +0530

    shift-drop-line-rot: adding SDLRNode class

--- shift-drop-line-rot.ts
+++ shift-drop-line-rot.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class SDLRNode {
+
+    prev: SDLRNode
+    next: SDLRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SDLRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSDLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SDLRNode {
+        var curr: SDLRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
