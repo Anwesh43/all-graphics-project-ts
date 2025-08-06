75e6d9c551200340528ecc953a37206fa4b8ae66
Anwesh43 <anweshthecool0@gmail.com>
2025-08-06 15:54:43 +0530

    shift-drop-line-rot: adding fix

--- shift-drop-line-rot.ts
+++ shift-drop-line-rot.ts
@@ -5,7 +5,7 @@ const colors: Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const parts: number = 4
+const parts: number = 5
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 5.9
