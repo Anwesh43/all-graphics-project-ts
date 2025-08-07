727b0abc2d08cd427e503e8cd74a10bdd2575981
Anwesh43 <anweshthecool0@gmail.com>
2025-08-07 15:29:06 +0530

    line-extend-up-down: adding DrawaingUtil class

--- line-extend-up-down.ts
+++ line-extend-up-down.ts
@@ -20,4 +20,47 @@ class ScaleUtil {
     static maxScale(scale: number, i: number, n: number): number {
         return Math.max(0, scale - i / n)
     }
+
+    static divideScale(scale: number, i: number, n: number): number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineExtendUpDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h * dsc(3), () => {
+            DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(1)), 0, () => {
+                context.rotate(rot * dsc(2))
+                DrawingUtil.drawLine(context, 0, 0, 0, size)
+            })
+        })
+    }
+
+    static drawLEUDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineExtendUpDown(context, scale)
+    }
 }
