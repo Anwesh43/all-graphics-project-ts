03da71aaba4448f3e8282d42115cd6e6a8830679
Anwesh43 <anweshthecool0@gmail.com>
2025-08-07 16:10:33 +0530

    line-extend-up-down: adding LineExtendUpDown class

--- line-extend-up-down.ts
+++ line-extend-up-down.ts
@@ -183,3 +183,25 @@ class LEUDNode {
         return this
     }
 }
+
+class LineExtendUpDown {
+
+    curr: LEUDNode = new LEUDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
