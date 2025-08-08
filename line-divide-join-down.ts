3d71a792397ab3d1e4d12b4c0566a33ba2eb0cf4
Anwesh43 <anweshthecool0@gmail.com>
2025-08-08 12:00:18 +0530

    line-divide-join-down: adding LDJDNode class

--- line-divide-join-down.ts
+++ line-divide-join-down.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LDJDNode {
+
+    prev: LDJDNode
+    next: LDJDNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDJDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLDJDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LDJDNode {
+        var curr: LDJDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
