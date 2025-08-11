628297d40bc3de5b4c16d753ba14cff813df4265
Anwesh43 <anweshthecool0@gmail.com>
2025-08-11 14:00:14 +0530

    bar-shrink-expand-down: adding DrawingUtil class

--- bar-shrink-expand-down.ts
+++ bar-shrink-expand-down.ts
@@ -24,3 +24,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarShrinkExpandDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (i: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(4), () => {
+            context.rotate(rot * dsc(2))
+            DrawingUtil.drawXY(context, (w / 4) * (1 - dsc(1)), 0, () => {
+                context.fillRect(0, -size, size * 0.5 * (dsc(0) + dsc(3)), size)
+            })
+        })
+    }
+
+    static drawBSEDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarShrinkExpandDown(context, scale)
+    }
+}
