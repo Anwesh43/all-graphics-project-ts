05e71614082fd2944f0569e64b0cae72bd971385
Anwesh43 <anweshthecool0@gmail.com>
2025-08-16 10:53:37 +0530

    line-bent-rot-down: adding deg constant

--- line-bent-rot-down.ts
+++ line-bent-rot-down.ts
@@ -13,4 +13,6 @@ const delay: number = 20
 const backColor: string = "#BDBDBD"
 const rot: number = Math.PI / 2
 const w: number = window.innerWidth
-const h: number = window.innerHeight 
+const h: number = window.innerHeight
+const deg: number = -Math.PI / 4
+
