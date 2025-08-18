03fa95f38095270f5c8a5d0f6493738bee49e616
Anwesh43 <anweshthecool0@gmail.com>
2025-08-18 10:30:34 +0530

    right-cap-line-rot: adding RLCRNode class

--- right-cap-line-rot.ts
+++ right-cap-line-rot.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class RLCRNode {
+
+    prev: RLCRNode
+    next: RLCRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLCRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRightCapLineRot(context, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RLCRNode {
+        var curr: RLCRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
