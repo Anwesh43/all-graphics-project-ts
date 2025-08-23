eb5ffc80b9482717fee5e9d4f6c01a19bbabe781
Anwesh43 <anweshthecool0@gmail.com>
2025-08-23 17:18:48 +0530

    line-extend-fold-down: adding LEFDNode class

--- line-extend-fold-down.ts
+++ line-extend-fold-down.ts
@@ -150,3 +150,38 @@ class Animator {
         }
     }
 }
+
+class LEFDNode {
+
+    prev: LEFDNode
+    next: LEFDNode
+    state: State = new State()
+
+    addNeighbor() {
+
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LEFDNode {
+        var curr: LEFDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
