78a3c8f1b7fc2eff26a3a275d08d83c3dfd165e4
Anwesh43 <anweshthecool0@gmail.com>
2025-08-23 17:43:39 +0530

    line-extend-fold-down: adding LineExtendFoldDown class

--- line-extend-fold-down.ts
+++ line-extend-fold-down.ts
@@ -192,3 +192,26 @@ class LEFDNode {
         return this
     }
 }
+
+class LineExtendFoldDown {
+
+    curr: LEFDNode = new LEFDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
