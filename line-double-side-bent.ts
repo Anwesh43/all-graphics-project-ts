796469fa1b7dfd0c29306d03fe6b5d102fd5f332
Anwesh43 <anweshthecool0@gmail.com>
2025-08-31 08:12:03 +0530

    line-double-side-bent: adding LDSBNode class

--- line-double-side-bent.ts
+++ line-double-side-bent.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LDSBNode {
+
+    prev: LDSBNode
+    next: LDSBNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDSBNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLDSBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LDSBNode {
+        var curr: LDSBNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
