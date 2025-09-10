619af189d2a8da70b12ab3fcdbc8819a880acd64
Anwesh43 <anweshthecool0@gmail.com>
2025-09-10 14:00:26 +0530

    line-rot-twice-right: adding LRTRNode class

--- line-rot-twice-right.ts
+++ line-rot-twice-right.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LRTRNode {
+
+    prev: LRTRNode
+    next: LRTRNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRTRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLRTRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LRTRNode {
+        var curr: LRTRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
