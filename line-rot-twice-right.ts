026ee61e5eb5f8f2f883841f4cb6d7fcc544889e
Anwesh43 <anweshthecool0@gmail.com>
2025-09-10 13:48:30 +0530

    line-rot-twice-right: adding DrawingUtil class

--- line-rot-twice-right.ts
+++ line-rot-twice-right.ts
@@ -28,11 +28,40 @@ class ScaleUtil {
 
 class DrawingUtil {
 
-    static maxScale(scale: number, i: number, n: number): number {
-        return Math.max(0, scale - i / n)
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
     }
 
-    static divideScale(scale: number, i: number, n: number): number {
-        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRotTwiceRight(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawXY(context, 0, -size * dsc(2), () => {
+                context.rotate(-rot * dsc(1))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+            })
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(2))
+        })
+    }
+
+    static drawLRTRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineRotTwiceRight(context, scale)
     }
 }
