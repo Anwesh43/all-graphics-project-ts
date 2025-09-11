1f730f4a7c9327e6e592699ff0cfcc71082f7598
Anwesh43 <anweshthecool0@gmail.com>
2025-09-11 11:40:25 +0530

    down-break-line-join: adding DrawingUtil class

--- down-break-line-join.ts
+++ down-break-line-join.ts
@@ -14,6 +14,7 @@ const backColor: string = "#BDBDBD"
 const rot: number = Math.PI / 2
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const deg: number = Math.PI / 4
 
 class ScaleUtil {
 
@@ -23,5 +24,46 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDownBreakLineJoin(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(4), () => {
+            context.rotate(rot * dsc(3))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(deg * (j * (1 - 2 * j) + dsc(2) * (1 - j)))
+                    DrawingUtil.drawLine(context, size * (1 - j) * (1 - dsc(j)), 0, size * ((1 - j) + j * dsc(j)), 0)
+                })
+            }
+        })
+    }
+
+    static drawDBLJNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDownBreakLineJoin(context, scale)
     }
 }
