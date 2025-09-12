165cc7307e90cad4963082b93e30244c4b9fb4b8
Anwesh43 <anweshthecool0@gmail.com>
2025-09-12 18:33:53 +0530

    right-line-rot-enclose: adding RightLineRotEnclose class

--- right-line-rot-enclose.ts
+++ right-line-rot-enclose.ts
@@ -189,3 +189,26 @@ class RLRENode {
         return this
     }
 }
+
+class RightLineRotEnclose {
+
+    curr: RLRENode = new RLRENode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
