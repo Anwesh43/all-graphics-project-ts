844b011ac14665208fbae2dd6f608ae4c58fb2b4
Anwesh43 <anweshthecool0@gmail.com>
2025-09-12 18:32:21 +0530

    right-line-rot-enclose: adding RLRENode class

--- right-line-rot-enclose.ts
+++ right-line-rot-enclose.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class RLRENode {
+
+    prev: RLRENode
+    next: RLRENode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLRENode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRLRENode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RLRENode {
+        var curr: RLRENode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
