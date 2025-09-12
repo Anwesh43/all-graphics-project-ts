4a01c641880f52a2682c34215fcba903b914e407
Anwesh43 <anweshthecool0@gmail.com>
2025-09-12 18:37:22 +0530

    right-line-rot-enclose: adding fix

--- right-line-rot-enclose.ts
+++ right-line-rot-enclose.ts
@@ -78,10 +78,10 @@ class Stage {
     renderer: Renderer = new Renderer()
 
     initCanvas() {
-        const stage: Stage = new Stage()
-        stage.initCanvas()
-        stage.render()
-        stage.handleTap()
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
     }
 
     render() {
