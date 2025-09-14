1d5dac0bb6e8b493b4bc28c21ca7508ea8c8eb6b
Anwesh43 <anweshthecool0@gmail.com>
2025-09-14 12:20:16 +0530

    line-bent-join-rot: adding LineBentJoinRot class

--- line-bent-join-rot.ts
+++ line-bent-join-rot.ts
@@ -197,3 +197,26 @@ class LBJRNode {
         return this
     }
 }
+
+class LineBentJoinRot {
+
+    curr: LBJRNode = new LBJRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
