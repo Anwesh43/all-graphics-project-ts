5ecffa1551ced558742d8989c6595a7907f461a0
Anwesh43 <anweshthecool0@gmail.com>
2025-09-17 12:50:49 +0530

    bi-slant-line-join: adding BSLJNode class

--- bi-slant-line-join.ts
+++ bi-slant-line-join.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class BSLJNode {
+
+    next: BSLJNode
+    prev: BSLJNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BSLJNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBSLJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BSLJNode {
+        var curr: BSLJNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
