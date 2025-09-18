c1b255f03238605e3d9a85fd7f90ff3e4161278b
Anwesh43 <anweshthecool0@gmail.com>
2025-09-18 13:54:46 +0530

    line-left-rot-up: adding DrawingUtil class

--- line-left-rot-up.ts
+++ line-left-rot-up.ts
@@ -14,6 +14,7 @@ const strokeFactor: number = 90
 const sizeFactor: number = 5.9
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const deg: number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -23,5 +24,51 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineLeftRotUp(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        const lSize: number = size / 3
+        DrawingUtil.drawXY(context, w * 0.5 * (dsc(0) - dsc(4)), h / 2, () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+                for (let j = 0; j < 3; j++) {
+                    const ds1j: number = ScaleUtil.divideScale(dsc(1), j, 3)
+                    const ds2j: number = ScaleUtil.divideScale(dsc(2), j, 3)
+                    DrawingUtil.drawXY(context, lSize * j, -h * 0.5 * (1 - ds1j), () => {
+                        context.rotate(deg * ds2j)
+                    })
+                }
+            })
+
+        })
+    }
+
+    static drawLLRUNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineLeftRotUp(context, scale)
     }
 }
