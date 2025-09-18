bdcf4e72e952b451b8463d2ba25b5bb1cf3b9381
Anwesh43 <anweshthecool0@gmail.com>
2025-09-18 13:45:55 +0530

    line-left-rot-up: adding ScaleUtil class

--- line-left-rot-up.ts
+++ line-left-rot-up.ts
@@ -13,4 +13,15 @@ const rot: number = -Math.PI
 const strokeFactor: number = 90
 const sizeFactor: number = 5.9
 const w: number = window.innerWidth
-const h: number = window.innerHeight 
+const h: number = window.innerHeight
+
+class ScaleUtil {
+
+    static maxScale(scale: number, i: number, n: number): number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale: number, i: number, n: number): number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
