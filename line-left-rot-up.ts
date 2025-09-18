d4770fec8f99f38056eb30b360338f802dd6d317
Anwesh43 <anweshthecool0@gmail.com>
2025-09-18 14:03:33 +0530

    line-left-rot-up: adding LLRUNode class

--- line-left-rot-up.ts
+++ line-left-rot-up.ts
@@ -149,3 +149,43 @@ class Animator {
         }
     }
 }
+
+class LLRUNode {
+
+    prev: LLRUNode
+    next: LLRUNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        this.next = new LLRUNode(this.i + 1)
+        this.next.prev = this
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLLRUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LLRUNode {
+        var curr: LLRUNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
