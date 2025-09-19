630e91292cc933f01a2ec2c0f20cdcf18074a4fd
Anwesh43 <anweshthecool0@gmail.com>
2025-09-19 11:31:43 +0530

    line-flap-rot-down: adding LFRDNode class

--- line-flap-rot-down.ts
+++ line-flap-rot-down.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LFRDNode {
+
+    prev: LFRDNode
+    next: LFRDNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LFRDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLFRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LFRDNode {
+        var curr: LFRDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
