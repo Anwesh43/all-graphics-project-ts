0b35b4e9baa8b2eab2ab3fd44dbb26bd3d677da0
Anwesh43 <anweshthecool0@gmail.com>
2025-09-20 12:03:24 +0530

    block-line-catcher: adding BlockLineCatcher class

--- block-line-catcher.ts
+++ block-line-catcher.ts
@@ -189,3 +189,26 @@ class BLCNode {
         return this
     }
 }
+
+class BlockLineCatcher {
+
+    curr: BLCNode = new BLCNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
