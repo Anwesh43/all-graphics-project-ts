e2cc82d814383ad0623c0fec97eae5d2eb069b68
Anwesh43 <anweshthecool0@gmail.com>
2025-09-21 09:05:59 +0530

    line-bisect-rot-line: adding LBRLNode class

--- line-bisect-rot-line.ts
+++ line-bisect-rot-line.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LBRLNode {
+
+    prev: LBRLNode
+    next: LBRLNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBRLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLBRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LBRLNode {
+        var curr: LBRLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
