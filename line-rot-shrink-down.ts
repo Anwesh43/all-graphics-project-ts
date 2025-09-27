e264bab2c0fb6bf2183a02647450a7cda41be2f3
Anwesh43 <anweshthecool0@gmail.com>
2025-09-27 11:25:56 +0530

    line-rot-shrink-down: adding ScaleUtil class

--- line-rot-shrink-down.ts
+++ line-rot-shrink-down.ts
@@ -14,3 +14,14 @@ const parts: number = 5
 const scGap: number = 0.04 / parts
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+
+class ScaleUtil {
+
+    static maxScale(scale: number, i: number, n: number): number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale: number, i: number, n: number): number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
