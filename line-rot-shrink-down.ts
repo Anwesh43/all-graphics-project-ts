9652d05b0effc884a023d596608493f4dfb10a4f
Anwesh43 <anweshthecool0@gmail.com>
2025-09-27 11:42:48 +0530

    line-rot-shrink-down: adding Stage class

--- line-rot-shrink-down.ts
+++ line-rot-shrink-down.ts
@@ -110,3 +110,36 @@ class Animator {
         }
     }
 }
+
+class Stage {
+
+    context: CanvasRenderingContext2D | null
+    canvas: HTMLCanvasElement
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage: Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
