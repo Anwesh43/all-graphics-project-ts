f82174f24a176f9f64a43160b661203d54de79d6
Anwesh43 <anweshthecool0@gmail.com>
2025-10-08 11:47:38 +0530

    partial-line-rot-joiner: adding PLRJNode class

--- partial-line-rot-joiner.ts
+++ partial-line-rot-joiner.ts
@@ -149,3 +149,44 @@ class Animator {
         }
     }
 }
+
+class PLRJNode {
+
+    next: PLRJNode
+    prev: PLRJNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PLRJNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawPLRJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): PLRJNode {
+        var curr: PLRJNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
