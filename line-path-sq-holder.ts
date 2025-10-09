b65ff3656ee8793c1a118ae55cbfdde056db85b2
Anwesh43 <anweshthecool0@gmail.com>
2025-10-09 09:04:41 +0530

    line-path-sq-holder: adding constants

---
+++ line-path-sq-holder.ts
@@ -0,0 +1,18 @@
+const colors: Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const delay: number = 20
+const backColor: string = "#BDBDBD"
+const rot: number = Math.PI / 2
+const parts: number = 6
+const scGap: number = 0.04 / parts
+const sizeFactor: number = 5.9
+const sqFactor: number = 5.2
+const strokeFactor: number = 90
+const w: number = window.innerWidth
+const h: number = window.innerHeight
+
