d031b92d4fdbe80c041e676f58bf1b0dc5e3f456
Anwesh43 <anweshthecool0@gmail.com>
2025-10-09 09:23:28 +0530

    line-path-sq-holder: adding LPSHNode class

--- line-path-sq-holder.ts
+++ line-path-sq-holder.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class LPSHNode {
+
+    prev: LPSHNode
+    next: LPSHNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LPSHNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLPSHNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LPSHNode {
+        var curr: LPSHNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
