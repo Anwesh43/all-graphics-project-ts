ebc35ece0594120a6769ac8168fa73699eda3979
Anwesh43 <anweshthecool0@gmail.com>
2025-10-16 09:52:09 +0530

    line-right-rot-move: adding LineRightRotMove class

--- line-right-rot-move.ts
+++ line-right-rot-move.ts
@@ -185,3 +185,27 @@ class LRRMNode {
         return this
     }
 }
+
+class LineRightRotMove {
+
+    curr: LRRMNode = new LRRMNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+
+}
