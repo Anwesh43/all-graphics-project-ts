f9cbcc83109ca4a047da0ed01aa0519f1043ba13
Anwesh43 <anweshthecool0@gmail.com>
2025-10-17 09:15:13 +0530

    rot-shift-line-down: adding RLSDNode class

--- rot-shift-line-down.ts
+++ rot-shift-line-down.ts
@@ -125,3 +125,45 @@ class State {
         }
     }
 }
+
+class RLSDNode {
+
+    prev: RLSDNode
+    next: RLSDNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLSDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRSLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RLSDNode {
+        var curr: RLSDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
