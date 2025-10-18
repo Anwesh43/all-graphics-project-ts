c55916e0c4ea50e8f5ecc940ea2635a97491a1a2
Anwesh43 <anweshthecool0@gmail.com>
2025-10-18 16:41:17 +0530

    alt-line-rot-same: adding ALRSNode class

--- alt-line-rot-same.ts
+++ alt-line-rot-same.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class ALRSNode {
+
+    prev: ALRSNode
+    next: ALRSNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ALRSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawALRSLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): ALRSNode {
+        var curr: ALRSNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
