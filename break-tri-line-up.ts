210a77976ec201353d22c4a9c0d7931fb18cd57c
Anwesh43 <anweshthecool0@gmail.com>
2025-10-19 09:45:02 +0530

    break-tri-line-up: adding BreakTriLineUp class

--- break-tri-line-up.ts
+++ break-tri-line-up.ts
@@ -151,3 +151,26 @@ class BTLUNode {
         return this
     }
 }
+
+class BreakTriLineUp {
+
+    curr: BTLUNode = new BTLUNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
