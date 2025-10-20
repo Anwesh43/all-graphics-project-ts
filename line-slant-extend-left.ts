6d3fc3ef1b020a27a63cbc9d731bf61f099c7ee7
Anwesh43 <anweshthecool0@gmail.com>
2025-10-20 15:33:06 +0530

    line-slant-extend-left: adding LineSlantExtendLeft class

--- line-slant-extend-left.ts
+++ line-slant-extend-left.ts
@@ -184,3 +184,26 @@ class LSELNode {
         return this
     }
 }
+
+class LineSlantExtendLeft {
+
+    curr: LSELNode = new LSELNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
