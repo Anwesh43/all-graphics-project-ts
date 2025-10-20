7b6cd5bf84d6a6022af553c6cc2a2b84cbcf01ce
Anwesh43 <anweshthecool0@gmail.com>
2025-10-20 15:31:23 +0530

    line-slant-extend-left: adding LSELNode class

--- line-slant-extend-left.ts
+++ line-slant-extend-left.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LSELNode {
+
+    prev: LSELNode
+    next: LSELNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSELNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLSELNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LSELNode {
+        var curr: LSELNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
