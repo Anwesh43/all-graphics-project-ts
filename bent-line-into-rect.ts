102d41a6561efaf730cd3b7b24143eba936aecf7
Anwesh43 <anweshthecool0@gmail.com>
2025-10-24 14:48:27 +0530

    bent-line-into-rect: adding BentLineIntoRect class

--- bent-line-into-rect.ts
+++ bent-line-into-rect.ts
@@ -195,3 +195,26 @@ class BLIRNode {
         return this
     }
 }
+
+class BentLineIntoRect {
+
+    curr: BLIRNode = new BLIRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.startUpdating(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
