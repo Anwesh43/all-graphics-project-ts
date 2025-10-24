d21b7f2d00186d9b0a3a9bd32b06c916a48497b9
Anwesh43 <anweshthecool0@gmail.com>
2025-10-24 14:45:01 +0530

    bent-line-into-rect: adding BLIRNode class

--- bent-line-into-rect.ts
+++ bent-line-into-rect.ts
@@ -153,3 +153,45 @@ class Animator {
         }
     }
 }
+
+class BLIRNode {
+
+    prev: BLIRNode
+    next: BLIRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLIRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBLIRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BLIRNode {
+        var curr: BLIRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
