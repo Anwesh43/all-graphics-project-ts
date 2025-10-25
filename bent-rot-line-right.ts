9921a2370aa7d70046ca5b81a03ad484208d3881
Anwesh43 <anweshthecool0@gmail.com>
2025-10-25 12:36:15 +0530

    bent-rot-line-right: adding BRLRNode class

--- bent-rot-line-right.ts
+++ bent-rot-line-right.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class BRLRNode {
+
+    prev: BRLRNode
+    next: BRLRNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BRLRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBRLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BRLRNode {
+        var curr: BRLRNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
