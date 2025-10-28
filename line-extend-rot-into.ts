9564643388add25a785aa935212e48c1cd6da6ee
Anwesh43 <anweshthecool0@gmail.com>
2025-10-28 16:55:50 +0530

    line-extend-rot-into: adding LERINode class

--- line-extend-rot-into.ts
+++ line-extend-rot-into.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LERINode {
+
+    prev: LERINode
+    next: LERINode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LERINode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLERINode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LERINode {
+        var curr: LERINode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
