6efb09bbec219960a6c399bad4fbc949c830fa7f
Anwesh43 <anweshthecool0@gmail.com>
2025-10-29 17:42:44 +0530

    line-partial-join-down: adding LJPDNode class

--- line-partial-join-down.ts
+++ line-partial-join-down.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LPJDNode {
+
+    prev: LPJDNode
+    next: LPJDNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LPJDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLPJDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LPJDNode {
+        var curr: LPJDNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
