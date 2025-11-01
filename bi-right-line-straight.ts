ba3a5f457cdcd381aa24edf382f899988c05189f
Anwesh43 <anweshthecool0@gmail.com>
2025-11-01 17:37:34 +0530

    bi-right-line-straight: adding DrawingUtil class

--- bi-right-line-straight.ts
+++ bi-right-line-straight.ts
@@ -14,6 +14,7 @@ const gapDeg: number = Math.PI / 2
 const rot: number = Math.PI / 4
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const sizeFactor: number = 5.9
 
 class ScaleUtil {
 
@@ -23,5 +24,48 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiRightLineStraight(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2 + size) * dsc(4), () => {
+            context.rotate(rot * dsc(3))
+            for (let k = 0; k < 2; k++) {
+                for (let j = 0; j < 2; j++) {
+                    DrawingUtil.drawXY(context, 0, 0, () => {
+                        context.rotate(gapDeg * j + rot * (1 - 2 * j) * dsc(2))
+                        DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(k))
+                    })
+                }
+            }
+        })
+    }
+
+    static drawBRLSNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBiRightLineStraight(context, scale)
     }
 }
