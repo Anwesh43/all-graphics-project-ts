ec210c72e18a9342a1b0c67a4725803707fcb698
Anwesh43 <anweshthecool0@gmail.com>
2025-11-02 11:50:29 +0530

    line-right-expand-shrink: adding LineRightExpandShrink class

--- line-right-expand-shrink.ts
+++ line-right-expand-shrink.ts
@@ -193,3 +193,26 @@ class LRESNode {
         return this
     }
 }
+
+class LineRightExpandShrink {
+
+    curr: LRESNode = new LRESNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
