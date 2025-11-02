ebff0554daeb7c6c86fc51c80a31571d07d8c3de
Anwesh43 <anweshthecool0@gmail.com>
2025-11-02 11:49:02 +0530

    line-right-expand-shrink: adding LRESNode class

--- line-right-expand-shrink.ts
+++ line-right-expand-shrink.ts
@@ -151,3 +151,45 @@ class Animator {
         }
     }
 }
+
+class LRESNode {
+
+    prev: LRESNode
+    next: LRESNode
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRESNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLRESNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LRESNode {
+        var curr: LRESNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
