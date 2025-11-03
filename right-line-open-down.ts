e9874fdb17b6ae45ced11c134a80ee9d4726bcb7
Anwesh43 <anweshthecool0@gmail.com>
2025-11-03 09:30:17 +0530

    right-line-open-down: addign DrawingUtil class

--- right-line-open-down.ts
+++ right-line-open-down.ts
@@ -14,6 +14,7 @@ const backColor: string = "#BDBDBD"
 const rot: number = Math.PI / 4
 const w: number = window.innerWidth
 const h: number = window.innerHeight
+const deg: number = Math.PI / 2
 
 class ScaleUtil {
 
@@ -23,5 +24,50 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRightLineOpenDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(deg * j)
+                    for (let k = 0; k < 2; k++) {
+                        DrawingUtil.drawXY(context, 0, 0, () => {
+                            context.rotate((rot) * (1 - 2 * k) * j * dsc(1))
+                            DrawingUtil.drawLine(context, -size * dsc(3) * (1 - j), 0, -size * dsc(0) + size * dsc(2) * (1 - j), 0)
+                        })
+                    }
+                })
+            }
+        })
+    }
+
+    static drawRLODNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRightLineOpenDown(context, scale)
     }
 }
