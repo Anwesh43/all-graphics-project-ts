7d2dc093b735c5cd701241d5a11f0917cd687ae2
Anwesh43 <anweshthecool0@gmail.com>
2025-11-03 09:57:29 +0530

    right-line-open-down: addign RightLineOpenDown class

--- right-line-open-down.ts
+++ right-line-open-down.ts
@@ -190,3 +190,26 @@ class RLODNode {
         return this
     }
 }
+
+class RightLineOpenDown {
+
+    curr: RLODNode = new RLODNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
