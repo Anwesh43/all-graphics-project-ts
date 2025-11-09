b2b66a3dc2cda552dffac386ca9a955b2e8ccb73
Anwesh43 <anweshthecool0@gmail.com>
2025-11-09 15:48:01 +0530

    line-peak-rot-fold: adding LinePeakRotFold class

--- line-peak-rot-fold.ts
+++ line-peak-rot-fold.ts
@@ -192,3 +192,26 @@ class LPRFNode {
         return this
     }
 }
+
+class LinePeakRotFold {
+
+    curr: LPRFNode = new LPRFNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
