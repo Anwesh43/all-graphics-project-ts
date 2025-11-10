00711a39910bae8efd4283452dcceca37886d1cc
Anwesh43 <anweshthecool0@gmail.com>
2025-11-10 22:58:41 +0530

    line-hook-join-left: adding LHJLNode class

--- line-hook-join-left.ts
+++ line-hook-join-left.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LHJLNode {
+
+    prev: LHJLNode
+    next: LHJLNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LHJLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLHJLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LHJLNode {
+        var curr: LHJLNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
