b76807c0a2e078cffa745bdde305a72f49829ef7
Anwesh43 <anweshthecool0@gmail.com>
2025-11-10 21:05:00 +0530

    line-hook-join-left: adding DrawingUtil class

--- line-hook-join-left.ts
+++ line-hook-join-left.ts
@@ -25,3 +25,45 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineHookJoinLeft(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(4), () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawLine(context, 0, 0, -size * dsc(0), 0)
+            DrawingUtil.drawXY(context, -size, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, 0, size * 0.5 * dsc(1))
+                DrawingUtil.drawXY(context, -size, size / 2, () => {
+                    DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(2), 0)
+                })
+            })
+        })
+    }
+
+    static drawLHJLNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineHookJoinLeft(context, scale)
+    }
+}
