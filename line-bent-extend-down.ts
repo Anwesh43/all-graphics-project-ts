adaf3a3cd8bca9ca1e96208f8d7001ba748271ea
Anwesh43 <anweshthecool0@gmail.com>
2025-11-15 11:33:51 +0530

    line-bent-extend-down: adding DrawingUtil class

--- line-bent-extend-down.ts
+++ line-bent-extend-down.ts
@@ -25,3 +25,41 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBentExtendDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(5), () => {
+            context.rotate(rot * dsc(3))
+            DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(0), 0)
+            DrawingUtil.drawXY(context, size / 2, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(1), -size * 0.5 * dsc(1))
+                DrawingUtil.drawXY(context, size / 2, -size / 2, () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, size * 0.5 * dsc(2))
+                    DrawingUtil.drawXY(context, 0, size / 2, () => {
+                        DrawingUtil.drawLine(context, 0, 0, -size * 0.5 * dsc(4), 0)
+                    })
+                })
+            })
+        })
+    }
+}
