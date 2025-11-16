bd7285d313578c664d8690bd27b04026304172cd
Anwesh43 <anweshthecool0@gmail.com>
2025-11-16 12:25:46 +0530

    step-right-join-line: adding DrawingUtil class

--- step-right-join-line.ts
+++ step-right-join-line.ts
@@ -25,3 +25,52 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawStepRightJoinLine(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2 + (h / 2) * dsc(5), () => {
+            context.rotate(rot * dsc(4))
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(0))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, size * 0.5 * j, size * 0.5 * j, () => {
+                    for (let k = 0; k < 2; k++) {
+                        const dsjk: number = ScaleUtil.divideScale(dsc(j + 1), k, 2)
+                        DrawingUtil.drawXY(context, size * 0.5 * k, 0, () => {
+                            DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsjk * (1 - k), size * 0.5 * dsjk * k)
+                        })
+                    }
+                })
+            }
+            DrawingUtil.drawXY(context, size, 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, -size * dsc(3), 0)
+            })
+        })
+    }
+
+    static drawSRJLNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawStepRightJoinLine(context, scale)
+    }
+}
