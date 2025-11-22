f0835c4ab285df611cca04c6184d9f00f9601cfb
Anwesh43 <anweshthecool0@gmail.com>
2025-11-22 11:51:05 +0530

    line-tilt-box-down: adding LineTileBoxDown class

--- line-tilt-box-down.ts
+++ line-tilt-box-down.ts
@@ -192,3 +192,26 @@ class LTBDNode {
         return this
     }
 }
+
+class LineTiltBoxDown {
+
+    curr: LTBDNode = new LTBDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
