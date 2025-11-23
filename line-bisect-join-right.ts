a0db1f8fc27f2b50853394a7b8c41b6fe9274fb6
Anwesh43 <anweshthecool0@gmail.com>
2025-11-23 09:22:22 +0530

    line-bisect-join-right: adding DrawingUtil class

--- line-bisect-join-right.ts
+++ line-bisect-join-right.ts
@@ -25,3 +25,53 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineBisectJoinRot(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(0)), 0, () => {
+                DrawingUtil.drawLine(context, 0, 0, size * 0.5, 0)
+                DrawingUtil.drawXY(context, size / 2, 0, () => {
+                    for (let j = 0; j < 2; j++) {
+                        const deg: number = rot * dsc(2)
+                        const upSize: number = size * 0.5 * Math.cos(deg)
+                        DrawingUtil.drawXY(context, 0, 0, () => {
+                            context.rotate(-deg)
+                            DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(1), 0)
+                        })
+                        DrawingUtil.drawXY(context, size / 2, 0, () => {
+                            DrawingUtil.drawLine(context, 0, -upSize, 0, upSize)
+                        })
+                    }
+                })
+            })
+        })
+    }
+
+    static drawLBJRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineBisectJoinRot(context, scale)
+    }
+}
