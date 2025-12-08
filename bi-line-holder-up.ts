63324849c5bc934fd16941f0a4e2ad457b6f18ca
Anwesh43 <anweshthecool0@gmail.com>
2025-12-08 23:10:03 +0530

    bi-line-holder-up: adding BLHUNode class

--- bi-line-holder-up.ts
+++ bi-line-holder-up.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class BLHUNode {
+
+    prev: BLHUNode
+    next: BLHUNode
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLHUNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBLHUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BLHUNode {
+        var curr: BLHUNode = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
