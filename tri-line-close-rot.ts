c47086c5362c45f1adf860098547b3db49b454b5
Anwesh43 <anweshthecool0@gmail.com>
2025-12-14 12:26:09 +0530

    tri-line-close-rot: adding TriLineCloseRot class

--- tri-line-close-rot.ts
+++ tri-line-close-rot.ts
@@ -187,4 +187,28 @@ class TLCRNode {
         cb()
         return this
     }
-}   
+}
+
+class TriLineCloseRot {
+
+    curr: TLCRNode = new TLCRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+
+}
