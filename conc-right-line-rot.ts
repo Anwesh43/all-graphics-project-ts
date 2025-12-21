de99c65ee4e96edb4533b4377c68d2449b3cd1ee
Anwesh43 <anweshthecool0@gmail.com>
2025-12-21 15:16:57 +0530

    conc-right-line-rot: adding ScaleUtil class

--- conc-right-line-rot.ts
+++ conc-right-line-rot.ts
@@ -1,9 +1,9 @@
 const colors : Array<string> = [
-  "#1A237E",
-  "#EF5350",
-  "#AA00FF",
-  "#C51162",
-  "#00C853"
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
 ]
 const parts : number = 6
 const scGap : number = 0.05 / parts 
@@ -15,4 +15,13 @@ const rot : number = Math.PI
 const w : number = window.innerWidth 
 const h : number = window.innerHeight
 
+class ScaleUtil {
 
+    static maxScale(scale : number, i : number, n : number) {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
