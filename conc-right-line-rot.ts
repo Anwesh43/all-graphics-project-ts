ff53a179be32626855aa33a58cf9267ff66dce2d
Anwesh43 <anweshthecool0@gmail.com>
2025-12-21 15:43:49 +0530

    conc-right-line-rot: adding Drawingtil class

--- conc-right-line-rot.ts
+++ conc-right-line-rot.ts
@@ -25,3 +25,48 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+	
+	static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+    	context.save()
+		context.translate(x, y)
+		cb()
+		context.restore()
+    }
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2: number) {
+		if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+			return 
+		}
+		context.beginPath()
+		context.moveTo(x1, y1)
+		context.lineTo(x2, y2)
+		context.stroke()
+	}
+
+	static drawConcRightLineRot(context : CanvasRenderingContext2D, scale : number) {
+		const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+		const size : number = Math.min(w, h) / sizeFactor
+		const dscij : (a : number, b : number, c : number) => number = (i : number, j : number, n : number) : number => ScaleUtil.divideScale(dsc(i), j, n)
+		DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(5), h / 2, () => {
+			for (let j = 0; j < 2; j++) {
+				DrawingUtil.drawXY(context, 0, 0, () => {
+					context.rotate(rot * dsc(4 - j))
+					DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.5 * (dscij(0, 0, 2) + j * dscij(0, 1, 2)))
+					DrawingUtil.drawXY(context, 0, -size * 0.5 * (1 + j), () => {
+						DrawingUtil.drawLine(context, 0, 0, -size * 0.5 * dsc(2 - j), 0)
+					})
+				})
+			}
+		})
+	}
+
+	static drawCRLRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+		context.lineCap = 'round'
+		context.lineWidth = Math.min(w, h) / strokeFactor 
+		context.strokeStyle = colors[i]
+		DrawingUtil.drawConcRightLineRot(context, scale)
+	}
+}
+   
