d8ee541d4a0689ff0041fbb0eba585bde1fa2785
Anwesh43 <anweshthecool0@gmail.com>
2025-12-27 01:35:00 +0530

    right-line-slight-rot: adding RLSRNode class

--- right-line-slight-rot.ts
+++ right-line-slight-rot.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class RLSRNode {
+
+    prev: RLSRNode | null = null
+    next: RLSRNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RLSRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRLSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RLSRNode {
+        var curr: RLSRNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
