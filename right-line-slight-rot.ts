0bef68fcd7a8b9fbaf0bb8df9517a330ce3cb5bb
Anwesh43 <anweshthecool0@gmail.com>
2025-12-27 01:27:26 +0530

    right-line-slight-rot: adding DrawingUtil class

--- right-line-slight-rot.ts
+++ right-line-slight-rot.ts
@@ -12,6 +12,7 @@ const sizeFactor: number = 5.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
 const rot: number = Math.PI / 9
+const deg: number = Math.PI / 2
 const w: number = window.innerWidth
 const h: number = window.innerHeight
 
@@ -23,5 +24,45 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRightLineSlightRot(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(4), h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(deg * j + rot * (1 - 2 * j) * dsc(2 + j))
+                    DrawingUtil.drawLine(context, 0, size * (1 - dsc(j)), 0, size)
+                })
+            }
+        })
+    }
+
+    static drawRLSRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRightLineSlightRot(context, scale)
     }
 }
