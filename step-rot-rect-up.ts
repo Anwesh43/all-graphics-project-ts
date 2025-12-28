b869a20c0f166d90dc22edfce6632c15f0b830ca
Anwesh43 <anweshthecool0@gmail.com>
2025-12-28 16:45:57 +0530

    step-rot-rect-up: adding DrawingUtil class

--- step-rot-rect-up.ts
+++ step-rot-rect-up.ts
@@ -26,4 +26,50 @@ class ScaleUtil {
 	}
 }
 
+class DrawingUtil {
+	
+	static drawXY(context : CanvasRenderingContext2D, x : number, y : number, cb : () => void) {
+		context.save()
+		context.translate(x, y)
+		cb()
+		context.restore()
+	}
+
+	static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+		if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+			return 
+		}
+		context.beginPath()
+		context.moveTo(x1, y1)
+		context.lineTo(x2, y2)
+		context.stroke()
+	}
+
+	static drawStepRotRectUp(context : CanvasRenderingContext2D, scale : number) {
+		const size : number = Math.min(w, h) / sizeFactor 
+		const dsc : (a : number) => number = (i : number) : number => ScaleUtil.divideScale(scale, i, parts)
+		DrawingUtil.drawXY(context, w / 2, h / 2 - h * 0.5 * dsc(5), () => {
+			DrawingUtil.drawXY(context, -size, 0, () => {
+				DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.5 * dsc(0))
+				DrawingUtil.drawXY(context, 0, -size / 2, () => {
+					DrawingUtil.drawLine(context, 0, 0, size * dsc(1), 0)
+				})
+			})
+			DrawingUtil.drawXY(context, 0, -size / 2, () => {
+				context.rotate(rot * dsc(4))
+				DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.5 * dsc(2))
+				DrawingUtil.drawXY(context, 0, -size / 2, () => {
+					DrawingUtil.drawLine(context, 0, 0, size * dsc(3), 0)
+				})
+			})
+		})
+	}
+	
+	static drawSRRUNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+		context.lineCap = 'round'
+		context.lineWidth = Math.min(w, h) / strokeFactor 
+		context.strokeStyle = colors[i]
+		DrawingUtil.drawStepRotRectUp(context, scale)
+	}	
+}
 
