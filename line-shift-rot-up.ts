435a21c7d43e94af337bf5dccd0ec5947a3e7979
Anwesh43 <anweshthecool0@gmail.com>
2025-12-30 17:34:05 +0530

    line-shift-rot-up: adding fix

--- line-shift-rot-up.ts
+++ line-shift-rot-up.ts
@@ -49,7 +49,7 @@ class DrawingUtil {
         const size: number = Math.min(w, h) / sizeFactor
         const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
         DrawingUtil.drawXY(context, w / 2, h / 2 - (h / 2) * dsc(5), () => {
-            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(1)) - w * 0.5 * dsc(3), 0, () => {
+            DrawingUtil.drawXY(context, (w / 4) * (1 - dsc(1)) - w * 0.25 * dsc(3), 0, () => {
                 context.rotate(rot * (dsc(2) + dsc(4)))
                 DrawingUtil.drawLine(context, 0, 0, 0, size * dsc(0))
             })
