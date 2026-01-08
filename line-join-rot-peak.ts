029fbba7cbdb1346dd8b5654ab857c6a2a2c4d5a
Anwesh43 <anweshthecool0@gmail.com>
2026-01-09 01:42:42 +0530

    line-join-rot-peak: adding DrawingUtil class

--- line-join-rot-peak.ts
+++ line-join-rot-peak.ts
@@ -5,11 +5,11 @@ const colors: Array<string> = [
     "#C51162",
     "#00C853"
 ]
-const rot: number = Math.PI / 2
+const rot: number = -Math.PI
 const backColor: string = "#BDBDBD"
 const sizeFactor: number = 5.9
 const strokeFactor: number = 90
-const parts: number = 5
+const parts: number = 4
 const scGap: number = 0.04 / parts
 const delay: number = 20
 const w: number = window.innerWidth
@@ -23,5 +23,47 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineRotPeak(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(3), h / 2, () => {
+            DrawingUtil.drawXY(context, -size / 2, 0, () => {
+                DrawingUtil.drawXY(context, 0, 0, () => {
+                    context.rotate(rot * dsc(2))
+                    DrawingUtil.drawLine(context, -size * 0.5 * (1 - dsc(0)), 0, -size * 0.5, 0)
+                })
+                DrawingUtil.drawLine(context, 0, 0, size * 0.5 * dsc(1), -size * 0.5 * dsc(1))
+            })
+
+        })
+    }
+
+    static drawLRPNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineRotPeak(context, scale)
     }
 }
