5542d164586514ed26de600bb2944c8f2d29db7b
Anwesh43 <anweshthecool0@gmail.com>
2026-01-11 01:17:43 +0530

    step-line-bent-left: adding DrawingUtil class

--- step-line-bent-left.ts
+++ step-line-bent-left.ts
@@ -1,12 +1,12 @@
-const colors: Array<String> = [
+const colors: Array<string> = [
     "#1A237E",
     "#EF5350",
     "#AA00FF",
     "#C51162",
     "#00C853"
 ]
-const parts: number = 6
-const scGap: number = 0.05 / parts
+const parts: number = 7
+const scGap: number = 0.06 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 5.9
 const backColor: string = "#BDBDBD"
@@ -24,5 +24,50 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawStepLineBentLeft(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(6), h / 2, () => {
+            context.rotate(rot * dsc(5))
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, -size * j, () => {
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size * dsc(2 * j))
+                    DrawingUtil.drawXY(context, 0, 0, () => {
+                        context.rotate(bentRot * dsc(4))
+                        DrawingUtil.drawLine(context, 0, 0, size * dsc(2 * j + 1), 0)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawSLBLNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        context.lineCap = 'round'
+        DrawingUtil.drawStepLineBentLeft(context, scale)
     }
 }
