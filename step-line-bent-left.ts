47f212ac180f8ed86bda296ee47e5c6cfb072fac
Anwesh43 <anweshthecool0@gmail.com>
2026-01-11 01:09:56 +0530

    step-line-bent-left: adding ScaleUtil class

--- step-line-bent-left.ts
+++ step-line-bent-left.ts
@@ -14,4 +14,15 @@ const rot: number = Math.PI / 2
 const bentRot: number = Math.PI / 4
 const delay: number = 20
 const w: number = window.innerWidth
-const h: number = window.innerHeight 
+const h: number = window.innerHeight
+
+class ScaleUtil {
+
+    static maxScale(scale: number, i: number, n: number): number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale: number, i: number, n: number): number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
