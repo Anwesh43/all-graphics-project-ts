f52088dedff097e68b8998f5e538607b04ed0a31
Anwesh43 <anweshthecool0@gmail.com>
2026-01-11 01:34:13 +0530

    step-line-bent-left: adding SLBLNode class

--- step-line-bent-left.ts
+++ step-line-bent-left.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class SLBLNode {
+
+    prev: SLBLNode | null = null
+    next: SLBLNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLBLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawSLBLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): SLBLNode {
+        var curr: SLBLNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
