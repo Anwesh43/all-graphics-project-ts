969a4cb0570ef79f5f549bab575ba2f9c8a16d40
Anwesh43 <anweshthecool0@gmail.com>
2026-01-15 10:41:38 +0530

    line-bent-rot-left: adding LBRLNode class

--- line-bent-rot-left.ts
+++ line-bent-rot-left.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LBRLNode {
+
+    prev: LBRLNode | null = null
+    next: LBRLNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBRLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLBRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LBRLNode {
+        var curr: LBRLNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
