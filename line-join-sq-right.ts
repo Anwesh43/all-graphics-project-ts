b07fa20495c160ded20065944d48d3ea47e788d4
Anwesh43 <anweshthecool0@gmail.com>
2026-01-17 11:04:31 +0530

    line-join-sq-right: adding LJSRNode class

--- line-join-sq-right.ts
+++ line-join-sq-right.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class LJSRNode {
+
+    prev: LJSRNode | null = null
+    next: LJSRNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LJSRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLJSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LJSRNode {
+        var curr: LJSRNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
