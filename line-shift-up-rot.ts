5fafa21cf2bb11d4374a36dbf6b57d5753f562ff
Anwesh43 <anweshthecool0@gmail.com>
2026-01-21 19:42:53 +0530

    line-shift-up-rot: adding LineShiftRotUp class

--- line-shift-up-rot.ts
+++ line-shift-up-rot.ts
@@ -182,3 +182,26 @@ class LSRUNode {
         return this
     }
 }
+
+class LineShiftRotUp {
+
+    curr: LSRUNode = new LSRUNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
