b73210b09f155035033dee1e52db90272937611c
Anwesh43 <anweshthecool0@gmail.com>
2026-01-26 17:23:49 +0530

    line-join-center-down: adding DrawingUtil class

--- line-join-center-down.ts
+++ line-join-center-down.ts
@@ -1,26 +1,65 @@
-const colors : Array<string> = [
+const colors: Array<string> = [
 	"#1A237E",
 	"#EF5350",
 	"#AA00FF",
 	"#C51162",
-	"#00C853"	
+	"#00C853"
 ]
-const parts : number = 4
-const scGap : number = 0.04 / parts 
-const strokeFactor : number = 90
-const sizeFactor : number = 5.9
-const delay : number = 20 
-const backColor : string = "#BDBDBD"
-const rot : number = Math.PI / 2
-const w : number = window.innerWidth 
-const h : number = window.innerHeight 
+const parts: number = 4
+const scGap: number = 0.04 / parts
+const strokeFactor: number = 90
+const sizeFactor: number = 5.9
+const delay: number = 20
+const backColor: string = "#BDBDBD"
+const rot: number = Math.PI / 2
+const w: number = window.innerWidth
+const h: number = window.innerHeight
 
 class ScaleUtil {
-	static maxScale(scale : number, i : number, n : number) : number {
+	static maxScale(scale: number, i: number, n: number): number {
 		return Math.max(0, scale - i / n)
 	}
 
-	static divideScale(scale : number, i : number, n : number) : number {
-		return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+	static divideScale(scale: number, i: number, n: number): number {
+		return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
 	}
 }
+
+class DrawingUtil {
+
+	static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+		context.save()
+		context.translate(x, y)
+		cb()
+		context.restore()
+	}
+
+	static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+		context.beginPath()
+		context.moveTo(x1, y1)
+		context.lineTo(x2, y2)
+		context.stroke()
+	}
+
+	static drawLineJoinCenterDown(context: CanvasRenderingContext2D, scale: number) {
+		const size: number = Math.min(w, h) / sizeFactor
+		const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+		DrawingUtil.drawXY(context, w / 2 + (w / 2) * dsc(3), h / 2 + (h / 2) * dsc(3), () => {
+			context.rotate(rot * dsc(2))
+			DrawingUtil.drawXY(context, -size, -h * 0.5 * (1 - dsc(0)), () => {
+				DrawingUtil.drawLine(context, 0, 0, 0, -size)
+			})
+			DrawingUtil.drawXY(context, -size, -size, () => {
+				DrawingUtil.drawLine(context, 0, 0, size * dsc(1), size * dsc(1))
+			})
+		})
+	}
+
+	static drawLJCDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+		context.lineCap = 'round'
+		context.lineWidth = Math.min(w, h) / strokeFactor
+		context.strokeStyle = colors[i]
+		DrawingUtil.drawLineJoinCenterDown(context, scale)
+	}
+}
+
