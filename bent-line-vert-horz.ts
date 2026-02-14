9c7e8070f292fe01b6783b6f570d46471ae8c98f
Anwesh43 <anweshthecool0@gmail.com>
2026-02-14 12:44:47 +0530

    bent-line-vert-horz: adding DrawingUtil class

--- bent-line-vert-horz.ts
+++ bent-line-vert-horz.ts
@@ -26,3 +26,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBentLineVertHorz(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 + w * 0.25 * (1 - dsc(2)), h / 2 - h * 0.5 * (1 - dsc(0)), () => {
+            context.rotate((rot) * dsc(1) + deg * (dsc(1) - dsc(3)))
+            DrawingUtil.drawLine(context, 0, 0, 0, -size)
+        })
+    }
+
+    static drawBLVHNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBentLineVertHorz(context, scale)
+    }
+}
