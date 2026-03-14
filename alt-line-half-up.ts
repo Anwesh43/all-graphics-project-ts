a1dd1564498705d44e0feb1110b1ac6b6e41e115
Anwesh43 <anweshthecool0@gmail.com>
2026-03-14 11:57:17 +0530

    alt-line-half-up: adding ALHUNode class

--- alt-line-half-up.ts
+++ alt-line-half-up.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class ALHUNode {
+
+    prev: ALHUNode | null = null
+    next: ALHUNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ALHUNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawALHUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): ALHUNode {
+        var curr: ALHUNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
