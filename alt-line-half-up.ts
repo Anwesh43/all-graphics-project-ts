b4b0f25b40ec4172199d2b50b022378b1f0bc85a
Anwesh43 <anweshthecool0@gmail.com>
2026-03-14 12:10:49 +0530

    alt-line-half-up: adding AltLineHalfUp class

--- alt-line-half-up.ts
+++ alt-line-half-up.ts
@@ -187,3 +187,26 @@ class ALHUNode {
         return this
     }
 }
+
+class AltLineHalfUp {
+
+    curr: ALHUNode = new ALHUNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
