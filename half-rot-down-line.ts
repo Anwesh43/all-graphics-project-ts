1c736f6301f425bd9ac9bf339c879c3bb4b6e807
Anwesh43 <anweshthecool0@gmail.com>
2026-04-03 15:53:51 +0530

    half-rot-down-line: adding fix

--- half-rot-down-line.ts
+++ half-rot-down-line.ts
@@ -48,7 +48,7 @@ class DrawingUtil {
     static drawHalfRotDownLine(context: CanvasRenderingContext2D, scale: number) {
         const size: number = Math.min(w, h) / sizeFactor
         const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2 + (w / 4) * (1 - dsc(2)), -h * 0.5 * ((1 - dsc(0)) - dsc(4)), () => {
+        DrawingUtil.drawXY(context, w / 2 + (w / 4) * (1 - dsc(2)), h / 2 - h * 0.5 * ((1 - dsc(0)) - dsc(4)), () => {
             context.rotate(-rot * (dsc(1) + dsc(3)))
             DrawingUtil.drawLine(context, 0, 0, 0, -size)
         })
