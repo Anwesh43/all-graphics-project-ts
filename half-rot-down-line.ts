f87f81db377b673770e54ef34dfeba09e10f3629
Anwesh43 <anweshthecool0@gmail.com>
2026-04-03 15:48:18 +0530

    half-rot-down-line: adding HRDLNode class

--- half-rot-down-line.ts
+++ half-rot-down-line.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class HRDLNode {
+
+    prev: HRDLNode | null = null
+    next: HRDLNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new HRDLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawHRDLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): HRDLNode {
+        var curr: HRDLNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
