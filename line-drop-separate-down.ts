5614dd1b1f394b8cc8cbc9fa1421eaf2200ba8a8
Anwesh43 <anweshthecool0@gmail.com>
2026-04-08 00:51:12 +0530

    line-drop-separate-down: adding LineDropSeparateDown class

--- line-drop-separate-down.ts
+++ line-drop-separate-down.ts
@@ -184,3 +184,26 @@ class LDSDNode {
         return this
     }
 }
+
+class LineDropSeparateDown {
+
+    curr: LDSDNode = new LDSDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
