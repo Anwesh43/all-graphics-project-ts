e91b060dd3151541783e023a7612d10c084d9919
Anwesh43 <anweshthecool0@gmail.com>
2026-04-08 03:04:41 +0530

    line-drop-separate-down: adding fix

--- line-drop-separate-down.ts
+++ line-drop-separate-down.ts
@@ -200,7 +200,7 @@ class LineDropSeparateDown {
 
     update(cb: () => void) {
         this.curr.update(() => {
-            this.curr.getNext(this.dir, () => {
+            this.curr = this.curr.getNext(this.dir, () => {
                 this.dir *= -1
             })
             cb()
