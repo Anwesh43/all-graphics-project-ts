4b03632c140eb2b89b09d585c15aba42fe72192f
Anwesh43 <anweshthecool0@gmail.com>
2026-04-11 14:27:52 +0530

    mid-line-up-rot: adding MLURNode class

--- mid-line-up-rot.ts
+++ mid-line-up-rot.ts
@@ -145,3 +145,46 @@ class Animator {
         }
     }
 }
+
+class MLURNode {
+
+    prev: MLURNode | null = null
+    next: MLURNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new MLURNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawMLURNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): MLURNode {
+        var curr: MLURNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+}
