5562b6340dd8e7220814cf292804c65cde16d81a
Anwesh43 <anweshthecool0@gmail.com>
2026-04-12 09:54:49 +0530

    bar-shift-rot-up: adding BSRUNode class

--- bar-shift-rot-up.ts
+++ bar-shift-rot-up.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class BSRUNode {
+
+    prev: BSRUNode | null = null
+    next: BSRUNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BSRUNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D | null) {
+        DrawingUtil.drawBSRUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BSRUNode {
+        var curr: BSRUNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
