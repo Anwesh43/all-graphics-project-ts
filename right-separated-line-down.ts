a6157ab8993f76dfe0a5e88cb291fa474f0cccd9
Anwesh43 <anweshthecool0@gmail.com>
2026-04-18 12:51:40 +0530

    right-separated-line-down: adding RightSeparatedLineDown class

--- right-separated-line-down.ts
+++ right-separated-line-down.ts
@@ -167,6 +167,10 @@ class RSLDNode {
         this.state.update(cb)
     }
 
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
     getNext(dir: number, cb: () => void): RSLDNode {
         var curr: RSLDNode | null = this.prev
         if (dir === 1) {
@@ -177,5 +181,28 @@ class RSLDNode {
         }
         cb()
         return this
+    }
+}
+
+class RightSeparatedLineDown {
+
+    curr: RSLDNode = new RSLDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
     }
 }
