cb7df280e7d6b7d6715526058b4d35b9bd4b0ef5
Anwesh43 <anweshthecool0@gmail.com>
2026-04-24 00:51:53 +0530

    three-line-rot-drop: adding fix 2

--- three-line-rot-drop.ts
+++ three-line-rot-drop.ts
@@ -51,10 +51,12 @@ class DrawingUtil {
         const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
         const gap: number = (w / 2) / lines
         DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            let x: number = 0
             for (let j = 0; j < lines; j++) {
-                DrawingUtil.drawXY(context, gap * j * dsc(j * 2), -h * 0.5 * (1 - dsc(0)) + h * 0.5 * dsc(parts - 1), () => {
+                DrawingUtil.drawXY(context, x + gap * dsc(j * 2), -h * 0.5 * (1 - dsc(0)) + h * 0.5 * dsc(parts - 1), () => {
                     context.rotate(rot * dsc(2 * j + 1))
                     DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                    x += gap * dsc(j * 2)
                 })
             }
         })
