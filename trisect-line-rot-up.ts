fb1d4415989e55b68355cd44b7fbc3d475c1a7e9
Anwesh43 <anweshthecool0@gmail.com>
2026-05-01 13:51:00 +0530

    trisect-line-rot-up: adding TrisectLineRotUp class

--- trisect-line-rot-up.ts
+++ trisect-line-rot-up.ts
@@ -188,4 +188,27 @@ class TLRUNode {
         cb()
         return this
     }
-} 
+}
+
+class TrisectLineRotUp {
+
+    curr: TLRUNode = new TLRUNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
