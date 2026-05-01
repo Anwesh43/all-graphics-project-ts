327d45e86c5f9d8a87a42c775157e54e988fb40f
Anwesh43 <anweshthecool0@gmail.com>
2026-05-01 13:00:32 +0530

    trisect-line-rot-up: adding constants

---
+++ trisect-line-rot-up.ts
@@ -0,0 +1,18 @@
+const colors: Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
+const lines: number = 3
+const parts: number = lines + 2
+const scGap: number = 0.04 / parts
+const delay: number = 20
+const backColor: string = "#BDBDBD"
+const finalRot: number = 1.25 * Math.PI
+const rot: number = Math.PI / 2
+const trisectDeg: number = Math.PI / 4
+const w: number = window.innerWidth
+const h: number = window.innerHeight
+
