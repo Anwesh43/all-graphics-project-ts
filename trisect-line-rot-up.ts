4acd1949811d57a7bfa7246cb148c72784c2c66c
Anwesh43 <anweshthecool0@gmail.com>
2026-05-01 13:49:55 +0530

    trisect-line-rot-up: adding TLRUNode class

--- trisect-line-rot-up.ts
+++ trisect-line-rot-up.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class TLRUNode {
+
+    prev: TLRUNode | null = null
+    next: TLRUNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TLRUNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawTLRUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): TLRUNode {
+        var curr: TLRUNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+} 
