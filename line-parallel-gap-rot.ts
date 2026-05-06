a4974e9f8f44d14a4fb85aa467c8169090ff2863
Anwesh43 <anweshthecool0@gmail.com>
2026-05-06 18:10:33 +0530

    line-parallel-gap-rot: adding LineParallelGapRot class

--- line-parallel-gap-rot.ts
+++ line-parallel-gap-rot.ts
@@ -185,3 +185,26 @@ class LPGRNode {
         return this
     }
 }
+
+class LineParallelGapRot {
+
+    curr: LPGRNode = new LPGRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
