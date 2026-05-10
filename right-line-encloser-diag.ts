c8f6cece5f0f72b34d6cf7c37fa372ec54a19f8a
Anwesh43 <anweshthecool0@gmail.com>
2026-05-10 12:35:20 +0530

    right-line-encloser-diag: adding RightLineEncloserDiag clas

--- right-line-encloser-diag.ts
+++ right-line-encloser-diag.ts
@@ -183,5 +183,27 @@ class RLEDNode {
         cb()
         return this
     }
+}
 
+class RightLineEncloseDiag {
+
+    curr: RLEDNode = new RLEDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
 }
