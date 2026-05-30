f4600257cdeeb4f4888dc5f75556fe1e5630f764
Anwesh43 <anweshthecool0@gmail.com>
2026-05-30 13:52:13 +0530

    line-rot-encloser-right: adding LRERNode class

--- line-rot-encloser-right.ts
+++ line-rot-encloser-right.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class LRERNode {
+
+    prev: LRERNode | null = null
+    next: LRERNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRERNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLRERNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LRERNode {
+        var curr: LRERNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
