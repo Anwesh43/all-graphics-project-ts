d29d041bd83e5eca348fb7b7d0cba785b3f6d31a
Anwesh43 <anweshthecool0@gmail.com>
2026-06-05 17:34:19 +0530

    bent-line-move-upright: adding DrawingUtil class

--- bent-line-move-upright.ts
+++ bent-line-move-upright.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBentLineMoveUpRight(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w * 0.5 * (1 + dsc(3)), -h * 0.25 * dsc(2), () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, -h * 0.5 * (1 - dsc(0)), () => {
+                    context.rotate(bentDeg * dsc(1) * j)
+                    DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                })
+            }
+        })
+    }
+
+    static drawBLMURNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBentLineMoveUpRight(context, scale)
+    }
+}
