d7872edb839803e8d4b833a4b9943bc5e0bbfaa6
Anwesh43 <anweshthecool0@gmail.com>
2026-06-05 20:06:10 +0530

    bent-line-move-upright: adding BentLineMoveUpRight class

--- bent-line-move-upright.ts
+++ bent-line-move-upright.ts
@@ -181,3 +181,27 @@ class BLMURNode {
         return this
     }
 }
+
+class BentLineMoveUpRight {
+
+    curr: BLMURNode = new BLMURNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+
+}
