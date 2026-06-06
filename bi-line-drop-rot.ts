928267ae0482471ba6bdf5579cddaa6e530d2b17
Anwesh43 <anweshthecool0@gmail.com>
2026-06-06 13:52:33 +0530

    bi-line-drop-rot: adding BLDRNode class

--- bi-line-drop-rot.ts
+++ bi-line-drop-rot.ts
@@ -58,7 +58,7 @@ class DrawingUtil {
         })
     }
 
-    static drawaBLDRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+    static drawBLDRNode(context: CanvasRenderingContext2D, i: number, scale: number) {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor
         context.strokeStyle = colors[i]
@@ -140,5 +140,47 @@ class Animator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class BLDRNode {
+
+    prev: BLDRNode | null = null
+    next: BLDRNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLDRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBLDRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BLDRNode {
+        var curr: BLDRNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
