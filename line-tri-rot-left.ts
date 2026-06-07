c6afd451a733ca5f55acf6781f3f7c4291c4f9ae
Anwesh43 <anweshthecool0@gmail.com>
2026-06-07 12:55:20 +0530

    line-tri-rot-left: adding LTRLNode class

--- line-tri-rot-left.ts
+++ line-tri-rot-left.ts
@@ -156,3 +156,45 @@ class Animator {
         }
     }
 }
+
+class LTRLNode {
+
+    prev: LTRLNode | null = null
+    next: LTRLNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LTRLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLTRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LTRLNode {
+        var curr: LTRLNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
