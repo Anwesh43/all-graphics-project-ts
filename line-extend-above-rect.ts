a8188c2c437bdf5f7164d83e6aaf7930d61f9a75
Anwesh43 <anweshthecool0@gmail.com>
2026-06-17 20:38:33 +0530

    line-extend-above-rect: adding LEARNode class

--- line-extend-above-rect.ts
+++ line-extend-above-rect.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LEARNode {
+
+    prev: LEARNode | null = null
+    next: LEARNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LEARNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLEARNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LEARNode {
+        var curr: LEARNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
