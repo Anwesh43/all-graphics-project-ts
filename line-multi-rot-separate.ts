edc5ec7853c3cb5136374e507d9d3357af1b5428
Anwesh43 <anweshthecool0@gmail.com>
2026-06-19 15:43:19 +0530

    line-multi-rot-separate: adding fix 5

--- line-multi-rot-separate.ts
+++ line-multi-rot-separate.ts
@@ -55,7 +55,7 @@ class DrawingUtil {
                     for (let k = 0; k < 2; k++) {
                         DrawingUtil.drawLine(context, 0, 0, size * 0.4 * dsc(0) * (1 - 2 * k), size * 0.4 * dsc(1))
                     }
-                    DrawingUtil.drawXY(context, -size, size, () => {
+                    DrawingUtil.drawXY(context, -size, 0, () => {
                         DrawingUtil.drawLine(context, 0, 0, 2 * size * dsc(2), 0)
                     })
                 })
