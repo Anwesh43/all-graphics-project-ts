b944ecb372bda46e37805270953474952f450a3e
Anwesh43 <anweshthecool0@gmail.com>
2026-06-19 15:19:01 +0530

    line-multi-rot-separate: adding LMRSNode class

--- line-multi-rot-separate.ts
+++ line-multi-rot-separate.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class LMRSNode {
+
+    prev: LMRSNode | null = null
+    next: LMRSNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LMRSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLMRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LMRSNode {
+        var curr: LMRSNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
