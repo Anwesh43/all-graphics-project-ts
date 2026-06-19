b71e76d01a61c8bf11465e07bc8299d127af06c9
Anwesh43 <anweshthecool0@gmail.com>
2026-06-19 11:30:59 +0530

    line-multi-rot-separate: adding constants

--- line-multi-rot-separate.ts
+++ line-multi-rot-separate.ts
@@ -25,3 +25,48 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineMultiRotSeparate(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, 0, h * 0.5 * (1 - 2 * j) * dsc(4), () => {
+                    context.rotate(rot * dsc(3) * j)
+                    for (let k = 0; k < 2; k++) {
+                        DrawingUtil.drawLine(context, 0, 0, size * 0.4 * dsc(0) * (1 - 2 * k), size * 0.4 * dsc(j))
+                    }
+                    DrawingUtil.drawXY(context, -size, size, () => {
+                        DrawingUtil.drawLine(context, 0, 0, 2 * size * dsc(2), 0)
+                    })
+                })
+            }
+        })
+    }
+
+    static drawLMRSNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineMultiRotSeparate(context, scale)
+    }
+}
