ce0b375b075bd3110f8c46fae97d2f15f6fd7b3c
Anwesh43 <anweshthecool0@gmail.com>
2026-06-21 17:35:42 +0530

    divide-line-half-down: adding DivideLineHalfDown class

--- divide-line-half-down.ts
+++ divide-line-half-down.ts
@@ -188,3 +188,25 @@ class DLHDNode {
         return this
     }
 }
+
+class DivideLineHalfDown {
+
+    curr: DLHDNode = new DLHDNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
