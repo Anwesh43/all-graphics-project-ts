46919f8a4badf316b7ec00931fc03a6d43a4ddde
Anwesh43 <anweshthecool0@gmail.com>
2026-06-21 17:34:09 +0530

    divide-line-half-down: adding DLHDNode class

--- divide-line-half-down.ts
+++ divide-line-half-down.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class DLHDNode {
+
+    prev: DLHDNode | null = null
+    next: DLHDNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DLHDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawDLHDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): DLHDNode {
+        var curr: DLHDNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
