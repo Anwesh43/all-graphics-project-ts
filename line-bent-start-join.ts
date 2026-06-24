811810fe8d2f41efd728f908c0895bfc24e5ed0d
Anwesh43 <anweshthecool0@gmail.com>
2026-06-24 15:22:48 +0530

    line-bent-start-join: adding LBSJNode class

--- line-bent-start-join.ts
+++ line-bent-start-join.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class LBSJNode {
+
+    prev: LBSJNode | null = null
+    next: LBSJNode | null = null
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LBSJNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLBSJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LBSJNode {
+        var curr: LBSJNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
