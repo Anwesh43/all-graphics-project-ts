1391c725eaf5defc6e39b9a8ff0d60bc30dd7968
Anwesh43 <anweshthecool0@gmail.com>
2026-06-27 14:57:56 +0530

    up-mid-sq-line: adding UpMidSqLine class

--- up-mid-sq-line.ts
+++ up-mid-sq-line.ts
@@ -182,3 +182,26 @@ class UMSLNode {
         return this
     }
 }
+
+class UpMidSqLine {
+
+    curr: UMSLNode = new UMSLNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
