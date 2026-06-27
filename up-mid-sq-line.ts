6d3ca6801ebfe2252ec5d76e3868b9073dbc33d5
Anwesh43 <anweshthecool0@gmail.com>
2026-06-27 15:02:09 +0530

    up-mid-sq-line: adding fix

--- up-mid-sq-line.ts
+++ up-mid-sq-line.ts
@@ -48,9 +48,9 @@ class DrawingUtil {
     static drawUpMidSqLine(context: CanvasRenderingContext2D, scale: number) {
         const size: number = Math.min(w, h) / sizeFactor
         const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
-        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
-            context.rotate(rot * dsc(2))
-            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(0)), -h / 4, () => {
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(4), h / 2, () => {
+            context.rotate(rot * dsc(1) - 2 * rot * dsc(3))
+            DrawingUtil.drawXY(context, (w / 2) * (1 - dsc(0)), -h * .25 * (1 - dsc(2)), () => {
                 DrawingUtil.drawLine(context, 0, 0, size, 0)
             })
         })
