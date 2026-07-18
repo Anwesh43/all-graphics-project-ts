da067d23d22c7b74d93c23770a687f0f55f1d516
Anwesh43 <anweshthecool0@gmail.com>
2026-07-18 13:16:05 +0530

    line-mid-join-bent: adding LMJBNode class

--- line-mid-join-bent.ts
+++ line-mid-join-bent.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LMJBNode {
+
+    prev: LMJBNode | null = null
+    next: LMJBNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LMJBNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLMJBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LMJBNode {
+        var curr: LMJBNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
