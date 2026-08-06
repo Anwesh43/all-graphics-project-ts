7c3784c157fffd0426237b3b7fda5bb778aaff35
Anwesh43 <anweshthecool0@gmail.com>
2026-08-07 02:22:34 +0530

    bent-line-rot-down: adding BLRDNode class

--- bent-line-rot-down.ts
+++ bent-line-rot-down.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class BLRDNode {
+
+    prev: BLRDNode | null = null
+    next: BLRDNode | null = null
+    state: State = new State()
+
+    constructor(private i: number) {
+        this.addNegihbor()
+    }
+
+    addNegihbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BLRDNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawBLRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): BLRDNode {
+        var curr: BLRDNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
