1c0ae3e07790ec16f6f3216d510f02e87fedde8e
Anwesh43 <anweshthecool0@gmail.com>
2026-08-16 16:54:12 +0530

    line-touch-perp-line: adding DrawingUtil class

--- line-touch-perp-line.ts
+++ line-touch-perp-line.ts
@@ -1,17 +1,17 @@
-const colors: Array<String> = [
+const colors: Array<string> = [
     "#1A237E",
     "#EF5350",
     "#AA00FF",
     "#C51162",
     "#00C853"
 ]
-const parts: number = 5
+const parts: number = 6
 const scGap: number = 0.04 / parts
 const strokeFactor: number = 90
 const sizeFactor: number = 5.9
 const delay: number = 20
 const backColor: string = "#BDBDBD"
-const rot: number = Math.PI / 2
+const rot: number = Math.PI
 const w: number = window.innerWidth
 const h: number = window.innerHeight
 
@@ -23,5 +23,47 @@ class ScaleUtil {
 
     static divideScale(scale: number, i: number, n: number): number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.01 && Math.abs(y1 - y2) < 0.01) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineTouchPerpLine(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(5), h / 2, () => {
+            DrawingUtil.drawXY(context, 0, 0, () => {
+                context.rotate(rot * dsc(4))
+                DrawingUtil.drawLine(context, 0, 0, size * dsc(0), 0)
+            })
+            DrawingUtil.drawXY(context, size, -h * 0.5 * (1 - dsc(1)) + h * 0.5 * dsc(3), () => {
+                context.rotate(rot * dsc(2))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size)
+            })
+        })
+    }
+
+    static drawLTPLNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineTouchPerpLine(context, scale)
     }
 }
