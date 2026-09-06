2b39f50ffac20c65a5ccb50a65730eff0105e1f9
Anwesh43 <anweshthecool0@gmail.com>
2026-09-06 18:52:06 +0530

    right-from-down-lint: adding RFDLNode lass

--- right-from-down-lint.ts
+++ right-from-down-lint.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class RFDLNode {
+
+    prev: RFDLNode | null = null
+    next: RFDLNode | null = null
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RFDLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawRFDLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUdpating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): RFDLNode {
+        var curr: RFDLNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
