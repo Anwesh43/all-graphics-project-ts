2f857aa840560966873d0593fd931ae26a318b1c
Anwesh43 <anweshthecool0@gmail.com>
2026-09-12 18:48:55 +0530

    up-shrink-line-right: adding USLRNode class

--- up-shrink-line-right.ts
+++ up-shrink-line-right.ts
@@ -138,3 +138,45 @@ class Animator {
         }
     }
 }
+
+class USLRNode {
+
+    prev: USLRNode | null = null
+    next: USLRNode | null = null
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new USLRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawUSLRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): USLRNode {
+        var curr: USLRNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
