16aaa0bbf950145946bbec86f69f388fc2031bb7
Anwesh43 <anweshthecool0@gmail.com>
2026-09-12 18:50:54 +0530

    up-shrink-line-right: adding UpShirnkLineRight class

--- up-shrink-line-right.ts
+++ up-shrink-line-right.ts
@@ -180,3 +180,26 @@ class USLRNode {
         return this
     }
 }
+
+class UpShrinkLineRight {
+
+    curr: USLRNode = new USLRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
