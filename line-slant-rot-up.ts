30f6766cb10c47db297578357cc8ab5a75b85671
Anwesh43 <anweshthecool0@gmail.com>
2026-09-16 12:42:53 +0530

    line-slant-rot-up: adding Stage class

--- line-slant-rot-up.ts
+++ line-slant-rot-up.ts
@@ -70,3 +70,36 @@ class DrawingUtil {
         DrawingUtil.drawLineSlantRotUp(context, scale)
     }
 }
+
+class Stage {
+
+    canvas: HTMLCanvasElement = document.createElement('canvas')
+    context: CanvasRenderingContext2D | null = null
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    static init() {
+        const stage: Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
