ce1ae27926f8170de75bcb0ad84dbe130e7b5d9f
Anwesh43 <anweshthecool0@gmail.com>
2026-09-16 12:58:25 +0530

    line-slant-rot-up: adding LineSlantRotUp class

--- line-slant-rot-up.ts
+++ line-slant-rot-up.ts
@@ -189,3 +189,26 @@ class LSRUNode {
         return this
     }
 }
+
+class LineSlantRotUp {
+
+    curr: LSRUNode = new LSRUNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
