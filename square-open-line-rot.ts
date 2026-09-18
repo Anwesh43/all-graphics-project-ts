5d2c32b366548963f661b87902de952546d2cfbf
Anwesh43 <anweshthecool0@gmail.com>
2026-09-19 02:00:35 +0530

    square-open-line-rot: adding SquareOpenLineRot class

--- square-open-line-rot.ts
+++ square-open-line-rot.ts
@@ -185,3 +185,26 @@ class SOLRNode {
         return this
     }
 }
+
+class SquareOpenLineRot {
+
+    curr: SOLRNode = new SOLRNode(0)
+    dir: number = 1
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUdpating(cb)
+    }
+}
