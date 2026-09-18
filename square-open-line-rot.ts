b845274fd80e47bc8512a0384caed88ae7b1231e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-19 02:02:47 +0530

    square-open-line-rot: adding Renderer in Stage

--- square-open-line-rot.ts
+++ square-open-line-rot.ts
@@ -72,6 +72,7 @@ class Stage {
 
     canvas: HTMLCanvasElement = document.createElement('canvas')
     context: CanvasRenderingContext2D | null = null
+    renderer: Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -82,13 +83,17 @@ class Stage {
 
     render() {
         if (this.context) {
-
+            this.context.fillStyle = backColor
+            this.context.fillRect(0, 0, w, h)
+            this.renderer.render(this.context)
         }
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
