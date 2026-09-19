b10a74fc8dbfdfc520e49bf565d296baf210de60
Anwesh43 <anweshthecool0@gmail.com>
2026-09-20 02:10:55 +0530

    left-multi-rot-line: adding DrawingUtil class

--- left-multi-rot-line.ts
+++ left-multi-rot-line.ts
@@ -25,3 +25,39 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLeftMultiRotLine(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2 - (w / 2) * dsc(5), h / 2, () => {
+            DrawingUtil.drawLine(context, 0, 0, -size * 0.5 * dsc(0), 0)
+            DrawingUtil.drawXY(context, -size / 2, 0, () => {
+                context.rotate(rot * dsc(4))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size * 0.25 * dsc(1))
+                DrawingUtil.drawXY(context, 0, -size / 4, () => {
+                    context.rotate(rot * dsc(3))
+                    DrawingUtil.drawLine(context, 0, 0, size * 0.25 * dsc(2), 0)
+                })
+            })
+        })
+    }
+}
