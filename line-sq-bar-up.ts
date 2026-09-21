3370928cb91cbf28f0382ad7aadf500e6b3d895b
Anwesh43 <anweshthecool0@gmail.com>
2026-09-22 01:14:00 +0530

    line-sq-bar-up: adding Stage class

--- line-sq-bar-up.ts
+++ line-sq-bar-up.ts
@@ -14,6 +14,7 @@ const delay: number = 20
 const w: number = window.innerWidth
 const h: number = window.innerHeight
 const barSizeFactor: number = 9.2
+const backColor: string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -65,5 +66,38 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor
         context.strokeStyle = colors[i]
         DrawingUtil.drawLineSqBarUp(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas: HTMLCanvasElement = document.createElement('canvas')
+    context: CanvasRenderingContext2D | null = null
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        if (this.context) {
+            this.context.fillStyle = backColor
+            this.context.fillRect(0, 0, w, h)
+        }
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage: Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
