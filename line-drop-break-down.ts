b102426224c5a653b7a5cb50c63e02e4ee0c9fe7
Anwesh43 <anweshthecool0@gmail.com>
2026-09-24 00:47:11 +0530

    line-drop-break-down: ading DrawingUtil class

--- line-drop-break-down.ts
+++ line-drop-break-down.ts
@@ -26,3 +26,48 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
     }
 }
+
+class DrawingUtil {
+
+    static drawXY(context: CanvasRenderingContext2D, x: number, y: number, cb: () => void) {
+        context.save()
+        context.translate(x, y)
+        cb()
+        context.restore()
+    }
+
+    static drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+        if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+            return
+        }
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineDropBreakDown(context: CanvasRenderingContext2D, scale: number) {
+        const size: number = Math.min(w, h) / sizeFactor
+        const dsc: (a: number) => number = (i: number): number => ScaleUtil.divideScale(scale, i, parts)
+        DrawingUtil.drawXY(context, w / 2, h / 2, () => {
+            for (let j = 0; j < 2; j++) {
+                DrawingUtil.drawXY(context, (w / 4) * (1 - 2 * j) * dsc(1), -h * 0.5 * (1 - dsc(0)) + (h / 2) * dsc(4), () => {
+                    context.rotate(rot * dsc(3))
+                    for (let k = 0; k < 2; k++) {
+                        DrawingUtil.drawXY(context, 0, 0, () => {
+                            context.rotate(breakDeg * dsc(2))
+                            DrawingUtil.drawLine(context, 0, 0, 0, -size)
+                        })
+                    }
+                })
+            }
+        })
+    }
+
+    static drawLDBDNode(context: CanvasRenderingContext2D, i: number, scale: number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineDropBreakDown(context, scale)
+    }
+}
