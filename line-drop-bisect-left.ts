361c91dd4d52c59e40404497adaa534504c12e6b
Anwesh43 <anweshthecool0@gmail.com>
2026-09-26 01:59:02 +0530

    line-drop-bisect-left: adding LineDropBisectLeft class

--- line-drop-bisect-left.ts
+++ line-drop-bisect-left.ts
@@ -187,3 +187,26 @@ class LDBLNode {
         return this
     }
 }
+
+class LineDropBisectLeft {
+
+    dir: number = 1
+    curr: LDBLNode = new LDBLNode(0)
+
+    draw(context: CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb: () => void) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb: () => void) {
+        this.curr.startUpdating(cb)
+    }
+}
