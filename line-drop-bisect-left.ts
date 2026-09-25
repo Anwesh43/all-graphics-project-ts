7d87c9614cc889c70fdee9599f353428fa5d175e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-26 01:48:19 +0530

    line-drop-bisect-left: adding LDBLNode class

--- line-drop-bisect-left.ts
+++ line-drop-bisect-left.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LDBLNode {
+
+    prev: LDBLNode | null = null
+    next: LDBLNode | null = null
+    state: State = new State()
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDBLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i: number) {
+        this.addNeighbor()
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        DrawingUtil.drawLDBLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb: () => void) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb: () => void) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb: () => void): LDBLNode {
+        var curr: LDBLNode | null = this.prev
+        if (dir === 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
